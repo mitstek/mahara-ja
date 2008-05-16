5aa5d6ec6035f734b4b83bb24dfdf32444670d67
Mitsuhiro Yoshida <mits@mitstek.com>
2008-05-16 21:30:11 +0900

    Second commit of Japanese language files.

--- artefact/blog/blocktype/blog/lang/ja.utf8/blocktype.blog.php
+++ artefact/blog/blocktype/blog/lang/ja.utf8/blocktype.blog.php
@@ -20,7 +20,7 @@
 * @subpackage lang (Japanese)
 * @translator Mitsuhiro Yoshida (http://mitstek.com/)
 * @started    2008-01-19 11:25:00 GMT
-* @updated    2008-05-16 08:39:14 GMT
+* @updated    2008-05-16 12:28:03 GMT
 * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
 * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
 *
@@ -29,5 +29,7 @@
 defined('INTERNAL') || die();
 
 $string['title'] = 'ブログ';
+$string['description'] = 'ブログ全体を表示する';
+$string['defaulttitledescription'] = 'ここを空白にした場合、ブログタイトルが使用されます。';
 
 ?>
