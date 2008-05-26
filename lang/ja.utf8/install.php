9422e514df96f654b86d797356f7c4dd526f8715
Mitsuhiro Yoshida <mits@mitstek.com>
2008-05-27 04:08:10 +0900

    Translated a sting and a help file. Brushed up a transalted help file.

--- lang/ja.utf8/install.php
+++ lang/ja.utf8/install.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 GMT
- * @updated    2008-05-18 05:49:42 GMT
+ * @updated    2008-05-26 18:49:47 GMT
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -28,6 +28,25 @@
 
 defined('INTERNAL') || die();
 
+$string['aboutdefaultcontent'] = '<h2>Maharaとは</h2>
+
+<p>Maharaは、マシー大学、オークランド工科大学、ニュージーランドのオープン・ポリテクニック、ビクトリア大学ウェリントンを含む、ニュージーランド高等教育委員会のeラーニング共同開発ファンド (eCDF, Tertiary Education Commission\'s e-learning Collaborative Development Fund) の資金援助により、2006年に設立された共同事業です。</p>
+
+<p>Maharaはユーザを結び、コミュニティを作成することのできる、十分な機能を有したeポートフォリオ、ウェブログ、レジュメビルダおよびソーシャルネットワーキングシステムです。Maharaは、ユーザに個人学習、専門教育および開発環境を構築するツールを提供します。</p>
+
+<p>マオリ語 (Te Reo Māori) で「熟考」または「思索」を意味し、技術的解決策は教授方略の熟考なしに開発できないという信念と同様に、プロジェクト協力者の献身による、ユーザを中心とした生涯学習およびアプリケーションの開発に、その名称が反映されています。</p>
+
+<p>Maharaは (GNU General Public License 一般公衆利用許諾契約書の下)、無償のオープンソースソフトウェア として提供されます。これは下記条件を同意することにより、あなたにMaharaのコピー、使用および修正が許可されていることを意味します:</p>
+
+<ul>
+<li>他の人にソースコードを提供する。</li>
+<li>オリジナルのライセンスおよび著作権表示を削除しない。</li>
+<li>派生物に同じライセンスを適用する。</li>
+</ul>
+
+<p>Maharaに関する詳細は、私たちに<a href=\"contact.php\">お気軽にお問い合わせください</a>。</p>
+
+<p><a href=\"http://mahara.org\">http://mahara.org</a></p>';
 $string['homedefaultcontent'] = '<h2>Maharaにようこそ</h2>
 
 <p>Maharaはユーザを結び、コミュニティを作成することのできる、十分な機能を有したeポートフォリオ、ウェブログ、レジュメビルダおよびソーシャルネットワーキングシステムです。Maharahaは、あなたに個人学習および開発環境を構築するツールを提供します。</p>
