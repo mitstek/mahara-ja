18b1860eb5d42e746da8a27398a179a8dba9b447
Mitsuhiro Yoshida <mits@mitstek.com>
2008-10-13 23:03:34 +0900

    Brushed up a translated string.

--- notification/email/lang/ja.utf8/notification.email.php
+++ notification/email/lang/ja.utf8/notification.email.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2008-06-17 04:23:15 UTC
+ * @updated    2008-10-13 14:01:34 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -29,7 +29,7 @@
 defined('INTERNAL') || die();
 
 $string['emailsubject'] = '%s';
-$string['emailheader'] = 'あなたに %s から通知メールが送信されました。メッセージは以下のとおりです:';
+$string['emailheader'] = 'あなたに %s から通知メールが送信されました。メッセージは、以下のとおりです:';
 $string['emailfooter'] = 'これは %s から自動的に生成された通知メールです。あなたの通知プリファレンスを更新するには、%s にアクセスしてください。';
 $string['referurl'] = '詳細 %s';
 $string['name'] = 'メール';
