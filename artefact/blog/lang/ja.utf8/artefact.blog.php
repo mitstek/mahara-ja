62dc153c354393c48cfaaca4757cc1b0ab19e34f
Mitsuhiro Yoshida <mits@mitstek.com>
2009-04-08 14:10:08 +0900

    Brushed up a translated string for blog.

--- artefact/blog/lang/ja.utf8/artefact.blog.php
+++ artefact/blog/lang/ja.utf8/artefact.blog.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2009-02-09 14:29:20 UTC
+ * @updated    2009-04-08 05:07:05 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -37,7 +37,7 @@ $string['alignment'] = '配置';
 $string['attach'] = '添付';
 $string['attachedfilelistloaded'] = '添付ファイル一覧が読み込まれました。';
 $string['attachedfiles'] = '添付ファイル';
-$string['attachments'] = '添付書類';
+$string['attachments'] = '添付ファイル';
 $string['blogcopiedfromanotherview'] = '注意: このブロックは、別のビューからコピーされました。あなたは、このブロックを移動したり、削除することができますが、%s の内容を変更することはできません。';
 $string['blogdesc'] = '説明';
 $string['blogdescdesc'] = '例) 「Jillの経験および内省の記録」';
