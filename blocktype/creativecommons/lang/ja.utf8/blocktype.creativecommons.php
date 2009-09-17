203631b90095afb3a85d32660f357e7803bf9c4c
Mitsuhiro Yoshida <mits@mitstek.com>
2009-09-18 06:33:27 +0900

    Translated new strings and help files for Creative Commons blocktype.

---
+++ blocktype/creativecommons/lang/ja.utf8/blocktype.creativecommons.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006 onwards Catalyst IT Ltd (http://www.catalyst.net.nz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2009-09-17 21:32:28 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'クリエイティブ・コモンズ・ライセンス';
+$string['description'] = 'あなたのビューにクリエイティブ・コモンズ・ライセンスを添付します。';
+$string['blockcontent'] = 'コンテンツをブロックする';
+$string['alttext'] = 'クリエイティブ・コモンズ・ライセンス';
+$string['licensestatement'] = 'この作品は、<a rel="license" href="%s">Creative Commons %s 3.0 Unported License</a>により、利用許諾されています。';
+$string['sealalttext'] = 'このライセンスは、フリーな文化的作品 (Free Cultural Works) に関して許可されます。';
+$string['config:noncommercial'] = 'あなたの作品の商用利用を許可しますか?';
+$string['config:noderivatives'] = 'あなたの作品の修正を許可しますか?';
+$string['config:sharealike'] = 'Yes、他の人が同一許諾条件を保持する場合';
+$string['by'] = '表示';
+$string['by-sa'] = '表示-継承';
+$string['by-nd'] = '表示-改変禁止';
+$string['by-nc'] = '表示-非営利';
+$string['by-nc-sa'] = '表示-非営利-継承';
+$string['by-nc-nd'] = '表示-非営利-改変禁止';
+
+?>
