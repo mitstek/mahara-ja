1237f103cc9d83f4cc3edaf67ccdc3e4564e6cd0
Mitsuhiro Yoshida <mits@mitstek.com>
2009-10-30 23:00:52 +0900

    (#3422) Translated a new help file for LEAP2A Files. Translated new strings for the export progress bar.

--- lang/ja.utf8/export.php
+++ lang/ja.utf8/export.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2009-06-25 11:46:11 UTC
+ * @updated    2009-10-30 13:59:05 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -31,20 +31,30 @@ defined('INTERNAL') || die();
 $string['allmydata'] = '私のすべてのデータ';
 $string['chooseanexportformat'] = 'エクスポートフォーマットを選択する';
 $string['clicktopreview'] = 'プレビューするには、ここをクリック';
+$string['creatingzipfile'] = 'ZIPファイル作成中';
+$string['Done'] = '完了';
 $string['Export'] = 'エクスポート';
 $string['exportgeneratedsuccessfully'] = 'エクスポートが正常に生成されました。%sダウンロードするには、ここをクリック%s';
 $string['exportgeneratedsuccessfullyjs'] = 'エクスポートが正常に生成されました。%s続ける%s';
+$string['exportingartefactplugindata'] = 'アーティファクトプラグインデータのエクスポート中';
+$string['exportingartefacts'] = 'アーティファクトのエクスポート中';
+$string['exportingartefactsprogress'] = 'アーティファクトのエクスポート中: %s/%s';
+$string['exportingfooter'] = 'フッタのエクスポート中';
+$string['exportingviews'] = 'ビューのエクスポート中';
+$string['exportingviewsprogress'] = 'ビューのエクスポート中: %s/%s';
 $string['exportpagedescription'] = 'ここでは、あなたのポートフォリオをエクスポートすることができます。このツールは、サイト設定を除く、あなたのポートフォリオ情報すべて、およびビューをエクスポートします。';
 $string['exportyourportfolio'] = 'あなたのポートフォリオを生成する';
 $string['generateexport'] = 'エクスポートを生成する';
 $string['noexportpluginsenabled'] = '管理者によりエクスポートプラグインが有効にされていないため、あなたはこの機能を使用することはできません。';
 $string['justsomeviews'] = 'いくつかのマイビューのみ';
 $string['pleasewaitwhileyourexportisbeinggenerated'] = 'あなたのエクスポートを生成しています。お待ちください ...';
+$string['setupcomplete'] = 'セットアップ完了';
 $string['Starting'] = '開始';
 $string['unabletoexportportfoliousingoptions'] = '選択されたオプションでポートフォリオをエクスポートできません。';
 $string['unabletogenerateexport'] = 'エクスポートを生成できません。';
 $string['viewstoexport'] = 'エクスポートするビュー';
 $string['whatdoyouwanttoexport'] = '何をエクスポートしますか?';
+$string['writingfiles'] = 'ファイル書き出し中';
 $string['youarehere'] = 'あなたは、ここにいます';
 $string['youmustselectatleastoneviewtoexport'] = 'あなたは、エクスポートするビューを少なくとも1つ選択する必要があります。';
 $string['zipnotinstalled'] = 'あなたのシステムには、ZIPコマンドがありません。この機能を有効にするには、ZIPをインストールしてください。';
