b224fb6c99d716468f110e31b34726346140cca0
Mitsuhiro Yoshida <mits@mitstek.com>
2009-11-17 06:23:06 +0900

    Initial commit for 1.2_STABLE.

--- lang/ja.utf8/mahara.php
+++ lang/ja.utf8/mahara.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2009-11-09 04:30:28 UTC
+ * @updated    2009-11-16 21:17:58 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
--- lang/ja.utf8/performance.php
+++ lang/ja.utf8/performance.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2009-01-05 21:11:31 UTC
+ * @updated    2009-11-16 21:19:29 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
