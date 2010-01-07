29857de0ecc111061b24a2180cf856bc2a894e8c
Mitsuhiro Yoshida <mits@mitstek.com>
2010-01-08 01:41:45 +0900

    Brushed up a translated string for admin.

--- lang/ja.utf8/admin.php
+++ lang/ja.utf8/admin.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2009-11-18 21:14:03 UTC
+ * @updated    2010-01-07 16:41:00 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -434,7 +434,7 @@ $string['userstoaddorreject'] = '追加/拒否するユーザ';
 $string['addmembers'] = 'メンバーを追加する';
 $string['inviteuserstojoin'] = 'ユーザをインスティテューションに招待する';
 $string['Non-members'] = '非メンバー';
-$string['userstobeinvited'] = '招待されたユーザ';
+$string['userstobeinvited'] = '招待されるユーザ';
 $string['inviteusers'] = 'ユーザを招待する';
 $string['removeusersfrominstitution'] = 'インスティテューションからユーザを削除する';
 $string['currentmembers'] = '現在のメンバー';
