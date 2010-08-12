a0837767771aa73bf52b431d963d31588fc9a4ff
Mitsuhiro Yoshida <mits@mitstek.com>
2010-08-12 14:09:14 +0900

    Translated new strings for plan.

---
+++ artefact/plans/lang/ja.utf8/artefact.plans.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006 onwards Catalyst IT Ltd (http://www.catalyst.net.nz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2010-08-12 05:07:14 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['description'] = '説明';
+$string['deleteplanconfirm'] = '本当にこのプランを削除してもよろしいですか? このプランを削除することで、プランの中に含まれているすべてのタスクも削除されます。';
+$string['deleteplan'] = 'プランを削除する';
+$string['editplan'] = 'プランを編集する';
+$string['editingplan'] = 'プランの編集';
+$string['managetasks'] = 'タスクを管理する';
+$string['myplans'] = 'マイプラン';
+$string['newplan'] = '新しいプラン';
+$string['noplansaddone'] = 'まだプランはありません。%sプランを追加してください%s!';
+$string['noplans'] = '表示するプランはありません。';
+$string['plan'] = 'プラン';
+$string['plans'] = 'プラン';
+$string['plandeletedsuccessfully'] = 'プランが正常に削除されました。';
+$string['plannotdeletedsuccessfully'] = 'プランの削除中にエラーが発生しました。';
+$string['plansavedsuccessfully'] = 'プランが正常に保存されました。';
+$string['saveplan'] = 'プランを保存する';
+$string['title'] = 'タイトル';
+$string['deletetaskconfirm'] = '本当にこのタスクを削除してもよろしいですか?';
+$string['deletetask'] = 'タスクを削除する';
+$string['edittask'] = 'タスクを編集する';
+$string['editingtask'] = 'タスクの編集';
+$string['mytasks'] = 'マイタスク';
+$string['newtask'] = '新しいタスク';
+$string['notasks'] = '表示するタスクはありません。';
+$string['notasksaddone'] = 'まだタスクはありません。%sタスクを追加してください%s!';
+$string['savetask'] = 'タスクを保存する';
+$string['task'] = 'タスク';
+$string['tasks'] = 'タスク';
+$string['taskdeletedsuccessfully'] = 'タスクが正常に削除されました。';
+$string['tasksavedsuccessfully'] = 'タスクが正常に保存されました。';
+
+?>
