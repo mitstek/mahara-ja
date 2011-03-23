1e0ae1dc799d9456516a7d81878dbf23b2f890cd
Mitsuhiro Yoshida <mits@mitstek.com>
2011-03-23 13:30:57 +0900

    Updated translatd string for pieforms.

--- lang/ja.utf8/pieforms.php
+++ lang/ja.utf8/pieforms.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2011-03-23 04:23:52 UTC
+ * @updated    2011-03-23 04:29:55 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -28,29 +28,29 @@
 
 defined('INTERNAL') || die();
 
-$string['element.bytes.bytes'] = 'バイト';
-$string['element.bytes.kilobytes'] = 'キロバイト';
-$string['element.bytes.megabytes'] = 'メガバイト';
-$string['element.bytes.gigabytes'] = 'ギガバイト';
-$string['element.bytes.invalidvalue'] = '値は数字にしてください。';
-$string['element.calendar.invalidvalue'] = '無効な日付/時間が指定されました。';
-$string['element.date.or'] = 'または';
-$string['element.date.monthnames'] = '1月,2月,3月,4月,5月,6月,7月,8月,9月,10月,11月,12月';
-$string['element.date.notspecified'] = '未指定';
+$string['element.bytes.bytes'] = 'B';
+$string['element.bytes.kilobytes'] = 'KB';
+$string['element.bytes.megabytes'] = 'MB';
+$string['element.bytes.gigabytes'] = 'GB';
+$string['element.bytes.invalidvalue'] = '値は数値にしてください';
+$string['element.calendar.invalidvalue'] = '無効な日付/時間が指定されました';
+$string['element.date.or'] = 'or';
+$string['element.date.monthnames'] = '一月,二月,三月,四月,五月,六月,七月,八月,九月,十月,十一月,十二月';
+$string['element.date.notspecified'] = '指定なし';
 $string['element.expiry.days'] = '日';
 $string['element.expiry.weeks'] = '週';
 $string['element.expiry.months'] = '月';
 $string['element.expiry.years'] = '年';
 $string['element.expiry.noenddate'] = '終了日なし';
-$string['rule.before.before'] = 'フィールド「 %s 」の後に設定できません。';
-$string['rule.email.email'] = 'メールアドレスが無効です。';
-$string['rule.integer.integer'] = 'このフィールドは整数にしてください。';
-$string['rule.maxlength.maxlength'] = 'このフィールドは最大半角 %d 文字にしてください。';
-$string['rule.minlength.minlength'] = 'このフィールドは少なくとも半角 %d 文字にしてください。';
-$string['rule.minvalue.minvalue'] = 'この値には %d より小さな値を指定できません。';
-$string['rule.regex.regex'] = 'このフィールドは有効な形式ではありません。';
+$string['rule.before.before'] = 'フィールド「 %s 」の後に設定することはできません';
+$string['rule.email.email'] = 'メールアドレスが有効ではありません';
+$string['rule.integer.integer'] = 'フィールドは整数にしてください';
+$string['rule.maxlength.maxlength'] = 'このフィールドは最大半角 %d 文字にしてください';
+$string['rule.minlength.minlength'] = 'このフィールドは少なくとも半角 %d 文字にしてください';
+$string['rule.minvalue.minvalue'] = 'この値は %d 以下にすることはできません';
+$string['rule.regex.regex'] = 'このフィールドは正しい形式ではありません';
 $string['rule.required.required'] = 'このフィールドは必須入力フィールドです。';
-$string['rule.validateoptions.validateoptions'] = 'オプション「 %s 」が無効です。';
-$string['rule.maxvalue.maxvalue'] = 'この値には %d より大きな値を指定できません。';
+$string['rule.validateoptions.validateoptions'] = 'オプション「 %s 」が正しくありません';
+$string['rule.maxvalue.maxvalue'] = 'この値は %d 以下にしてください';
 
 ?>
