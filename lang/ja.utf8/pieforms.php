0ff0b7be3bc8886525437524708cc04057d6917a
Mitsuhiro Yoshida <mits@mitstek.com>
2011-03-23 13:10:46 +0900

    Translated new strings for pieforms.

---
+++ lang/ja.utf8/pieforms.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006 onwards Catalyst IT Ltd (http://www.catalyst.net.nz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2011-03-23 04:10:18 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['element.bytes.bytes'] = 'バイト';
+$string['element.bytes.kilobytes'] = 'キロバイト';
+$string['element.bytes.megabytes'] = 'メガバイト';
+$string['element.bytes.gigabytes'] = 'ギガバイト';
+$string['element.bytes.invalidvalue'] = '値は数字にしてください。';
+$string['element.calendar.invalidvalue'] = '無効な日付/時間が指定されました。';
+$string['element.date.or'] = 'または';
+$string['element.date.monthnames'] = '1月,2月,3月,4月,5月,6月,7月,8月,9月,10月,11月,12月';
+$string['element.date.notspecified'] = '未指定';
+$string['element.expiry.days'] = '日';
+$string['element.expiry.weeks'] = '週';
+$string['element.expiry.months'] = '月';
+$string['element.expiry.years'] = '年';
+$string['element.expiry.noenddate'] = '終了日なし';
+$string['rule.before.before'] = 'フィールド「 %s 」の後に設定できません。';
+$string['rule.email.email'] = 'メールアドレスが無効です。';
+$string['rule.integer.integer'] = 'このフィールドは整数にしてください。';
+$string['rule.maxlength.maxlength'] = 'このフィールドは最大半角 %d 文字にしてください。';
+$string['rule.minlength.minlength'] = 'このフィールドは少なくとも半角 %d 文字にしてください。';
+$string['rule.minvalue.minvalue'] = 'この値には %d より小さな値を指定できません。';
+$string['rule.regex.regex'] = 'このフィールドは有効な形式ではありません。';
+$string['rule.required.required'] = 'このフィールドは必須入力フィールドです。';
+$string['rule.validateoptions.validateoptions'] = 'オプション「 %s 」が無効です。';
+$string['rule.maxvalue.maxvalue'] = 'この値には %d より大きな値を指定できません。';
+
+?>
