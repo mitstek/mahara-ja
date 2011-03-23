987b10aada32fd8d20c70e76fc6bd4422edcd9cb
Richard Mansfield <richard.mansfield@catalyst.net.nz>
2011-03-23 15:46:21 +1300

    Move pieforms translation into language pack
    Signed-off-by: Richard Mansfield <richard.mansfield@catalyst.net.nz>

---
+++ lang/ja.utf8/pieforms.php
@@ -0,0 +1,39 @@
+<?php
+
+defined('INTERNAL') || die();
+
+$string['element.bytes.bytes'] = 'B';
+$string['element.bytes.kilobytes'] = 'KB';
+$string['element.bytes.megabytes'] = 'MB';
+$string['element.bytes.gigabytes'] = 'GB';
+$string['element.bytes.invalidvalue'] = '値は数値にしてください';
+
+$string['element.calendar.invalidvalue'] = '無効な日付/時間が指定されました';
+
+$string['element.date.or'] = 'or';
+$string['element.date.monthnames'] = '一月,二月,三月,四月,五月,六月,七月,八月,九月,十月,十一月,十二月';
+$string['element.date.notspecified'] = '指定なし';
+
+$string['element.expiry.days'] = '日';
+$string['element.expiry.weeks'] = '週';
+$string['element.expiry.months'] = '月';
+$string['element.expiry.years'] = '年';
+$string['element.expiry.noenddate'] = '終了日なし';
+
+$string['rule.before.before'] = 'フィールド「 %s 」の後に設定することはできません';
+
+$string['rule.email.email'] = 'メールアドレスが有効ではありません';
+
+$string['rule.integer.integer'] = 'フィールドは整数にしてください';
+
+$string['rule.maxlength.maxlength'] = 'このフィールドは、最大半角 %d 文字にしてください';
+
+$string['rule.minvalue.minvalue'] = 'この値は %d 以下にすることはできません';
+
+$string['rule.regex.regex'] = 'このフィールドは正しい形式ではありません';
+
+$string['rule.required.required'] = 'このフィールドは、必須入力フィールドです。';
+
+$string['rule.validateoptions.validateoptions'] = 'オプション「 %s 」が正しくありません';
+
+$string['rule.maxvalue.maxvalue'] = 'この値は %d 以下にしてください';
