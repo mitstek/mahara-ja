5c3f587950c870ae2683e7851e98cf851797d5ee
Mitsuhiro Yoshida <mits@mitstek.com>
2011-07-19 01:52:11 +0900

    Updated ja.po header.

--- artefact/blog/blocktype/blog/lang/ja.utf8/blocktype.blog.php
+++ artefact/blog/blocktype/blog/lang/ja.utf8/blocktype.blog.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2010-08-13 05:07:28 UTC
+ * @updated    2011-07-18 16:48:59 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
