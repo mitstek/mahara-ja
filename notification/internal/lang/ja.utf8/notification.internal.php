23a4f858de984e1bf969b7c026dbf4c46e03063c
Mitsuhiro Yoshida <mits@mitstek.com>
2011-09-22 07:45:23 +0900

    Updated translated help files for inbox.

--- notification/internal/lang/ja.utf8/notification.internal.php
+++ notification/internal/lang/ja.utf8/notification.internal.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2011-09-21 22:25:42 UTC
+ * @updated    2011-09-21 22:44:08 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
