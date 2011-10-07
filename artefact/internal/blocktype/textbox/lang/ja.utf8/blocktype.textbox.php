9174bb94211cd6b8fdd675d4da7830056cf9bd3c
Mitsuhiro Yoshida <mits@mitstek.com>
2011-10-07 14:19:24 +0900

    Brushed up a translated string for blocktype textbox.

--- artefact/internal/blocktype/textbox/lang/ja.utf8/blocktype.textbox.php
+++ artefact/internal/blocktype/textbox/lang/ja.utf8/blocktype.textbox.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2011-10-07 02:41:25 UTC
+ * @updated    2011-10-07 05:18:35 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -34,7 +34,7 @@ $string['blockcontent'] = 'ブロックコンテンツ';
 $string['usecontentfromanothertextbox'] = '別のテキストボックスのコンテンツを使用する';
 $string['textusedinotherblocks'] = 'あなたがこのブロックのテキストを編集した場合、テキストが表示されている %s 件の他のブロックも更新されます。';
 $string['managealltextboxcontent'] = 'すべてのテキストボックスコンテンツを管理する';
-$string['readonlymessage'] = 'あなたが選択したテキストは、このページで編集することはできません。';
+$string['readonlymessage'] = 'あなたが選択したテキストはこのページで編集することはできません。';
 $string['makeacopy'] = 'コピーを作成する';
 
 ?>
