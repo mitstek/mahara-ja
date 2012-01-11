f7176f0308a6fecf32ce69efe43ec418f5c19b69
Mitsuhiro Yoshida <mits@mitstek.com>
2012-01-12 00:48:36 +0900

    Translated new strings and a help file for BrowserID.

--- lang/ja.utf8/admin.php
+++ lang/ja.utf8/admin.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2012-01-10 17:28:03 UTC
+ * @updated    2012-01-10 22:07:59 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
