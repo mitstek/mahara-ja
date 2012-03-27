71b37b8e67b1d493209ee9fc3f4dfa747480d11b
Mitsuhiro Yoshida <mits@mitstek.com>
2012-03-28 03:58:39 +0900

    Brushed up a translated string.

--- lang/ja.utf8/mahara.php
+++ lang/ja.utf8/mahara.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2012-03-14 16:22:56 UTC
+ * @updated    2012-03-27 18:57:33 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -803,7 +803,7 @@ $string['back'] = '戻る';
 $string['backto'] = '%s に戻る';
 $string['alphabet'] = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z';
 $string['formatpostbbcode'] = 'BBコードを使用して、あなたの投稿を書式設定することができます。%s詳細情報%s';
-$string['Created'] = '作成日';
+$string['Created'] = '作成';
 $string['Updated'] = '更新日';
 $string['Total'] = '合計';
 $string['Visits'] = '訪問';
