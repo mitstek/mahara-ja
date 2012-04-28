0d973d35cd2806f4d85879be4a05226bf116d65c
Mitsuhiro Yoshida <mits@mitstek.com>
2012-04-28 14:41:14 +0900

    Translated a new string for Jounals block.

--- lang/ja.utf8/mahara.php
+++ lang/ja.utf8/mahara.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2012-04-26 14:16:05 UTC
+ * @updated    2012-04-28 05:39:21 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -918,5 +918,6 @@ $string['discusstopicslinked'] = '<a href="%s">トピック</a>を議論する';
 $string['howtodisable'] = '* あなたはこのボックスの可視性を<a href="%s">設定</a>ページにてコントロールすることができます。';
 $string['setblocktitle'] = 'ブロックタイトルを設定する';
 $string['filenotfound'] = 'ファイルが見つかりませんでした。';
+$string['betweenxandy'] = '範囲: %s - %s';
 
 ?>
