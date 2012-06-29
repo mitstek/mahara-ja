32c3632a2a8323a59c1694ed6c33deb8f32f4481
Mitsuhiro Yoshida <mits@mitstek.com>
2012-06-30 02:43:45 +0900

    Translated new strings for Statistics.

---
+++ lang/ja.utf8/statistics.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006 onwards Catalyst IT Ltd (http://www.catalyst.net.nz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2012-06-29 17:42:51 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['fieldname'] = '翻訳予定';
+$string['allowpublicprofiles'] = 'パブリックプロファイルを許可する';
+$string['allowpublicviews'] = 'パブリックページを許可する';
+$string['artefact_type_academicgoal'] = 'アーティファクト: アカデミックゴール';
+$string['artefact_type_academicskill'] = 'アーティファクト: アカデミックスキル';
+$string['artefact_type_activity'] = 'アーティファクト: PLD活動';
+$string['artefact_type_address'] = 'アーティファクト: 郵便番号';
+$string['artefact_type_aimscreenname'] = 'アーティファクト: AIMスクリーンネーム';
+$string['artefact_type_archive'] = 'アーティファクト: 圧縮ファイル';
+$string['artefact_type_audio'] = 'アーティファクト: オーディオファイル';
+$string['artefact_type_blog'] = 'アーティファクト: 日誌';
+$string['artefact_type_blogaddress'] = 'アーティファクト: ブログアドレス';
+$string['artefact_type_blogpost'] = 'アーティファクト: 日誌エントリ';
+$string['artefact_type_book'] = 'アーティファクト: 書籍および出版物';
+$string['artefact_type_businessnumber'] = 'アーティファクト: 勤務先電話';
+$string['artefact_type_careergoal'] = 'アーティファクト: キャリアゴール';
+$string['artefact_type_certification'] = 'アーティファクト: 証明、認定および受賞';
+$string['artefact_type_comment'] = 'アーティファクト: コメント';
+$string['artefact_type_country'] = 'アーティファクト: 国';
+$string['artefact_type_coverletter'] = 'アーティファクト: カバーレター';
+$string['artefact_type_cpd'] = 'アーティファクト: PLD';
+$string['artefact_type_file'] = 'アーティファクト: ファイル';
+$string['artefact_type_firstname'] = 'アーティファクト: 名';
+$string['artefact_type_folder'] = 'アーティファクト: フォルダ';
+$string['artefact_type_homenumber'] = 'アーティファクト: 自宅電話';
+$string['artefact_type_html'] = 'アーティファクト: HTMLファイル';
+$string['artefact_type_icqnumber'] = 'アーティファクト: ICQナンバー';
+$string['artefact_type_image'] = 'アーティファクト: イメージファイル';
+$string['artefact_type_mobilenumber'] = 'アーティファクト: 携帯電話';
+$string['artefact_type_msnnumber'] = 'アーティファクト: MSNチャット';
+$string['blocktype_folder'] = 'ブロック: フォルダ';
+$string['blocktype_googleapps'] = 'ブロック: Google Apps';
+$string['blocktype_groupinfo'] = 'ブロック: グループ情報';
+$string['blocktype_groupmembers'] = 'ブロック: グループメンバー';
+$string['blocktype_groupviews'] = 'ブロック: グループページ';
+$string['blocktype_image'] = 'ブロック: イメージ';
+$string['blocktype_navigation'] = 'ブロック: ナビゲーション';
+$string['blocktype_textbox'] = 'ブロック: テキストボックス';
+$string['blocktype_wall'] = 'ブロック: ウォール';
+$string['count_block_instance'] = 'ブロック数';
+$string['count_blocktype_wall_post'] = 'ウォール投稿数';
+$string['count_group'] = 'グループ数';
+$string['count_group_member'] = 'グループメンバー数';
+$string['count_host'] = 'Moodleホスト数';
+$string['count_institution'] = 'インスティテューション数';
+$string['count_interaction_forum_post'] = 'フォーラム投稿数';
+$string['count_usr'] = 'ユーザ数';
+$string['lang'] = '言語';
+$string['newstats'] = '新しい統計';
+$string['release'] = 'リリース';
+$string['sitename'] = 'サイト名';
+$string['theme'] = 'テーマ';
+$string['usersloggedin'] = '先週のログインユーザ数';
+$string['version'] = 'バージョン';
+$string['view_type_dashboard'] = 'ダッシュボード数';
+$string['view_type_grouphomepage'] = 'グループホームページ数';
+$string['view_type_portfolio'] = 'ポートフォリオページ数';
+$string['view_type_profile'] = 'プロファイルページ数';
+$string['wwwroot'] = 'WWWルート';
+
+?>
