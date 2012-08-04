197d39cf6191f90ad32e44f2e897125e2b0ba6c8
Mitsuhiro Yoshida <mits@mitstek.com>
2012-08-05 04:00:40 +0900

    Translated a new string for Mobile upload.

--- artefact/file/lang/ja.utf8/artefact.file.php
+++ artefact/file/lang/ja.utf8/artefact.file.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2012-08-01 16:08:32 UTC
+ * @updated    2012-08-02 04:04:32 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
