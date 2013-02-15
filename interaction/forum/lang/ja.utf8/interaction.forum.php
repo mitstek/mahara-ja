468f78caa375088eda5ef6d962a8560b49fa2621
Mitsuhiro Yoshida <mits@mitstek.com>
2013-02-15 14:45:08 +0900

    Translated new strings for admin.

--- interaction/forum/lang/ja.utf8/interaction.forum.php
+++ interaction/forum/lang/ja.utf8/interaction.forum.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2012-10-14 08:37:14 UTC
+ * @updated    2012-10-14 08:52:54 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
