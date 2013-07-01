9fd4203f05400e8f082b88be8fe3e4ebe8585dfc
Mitsuhiro Yoshida <mits@mitstek.com>
2013-07-01 12:34:55 +0900

    Updated translated strings for blog based on en.utf8 change.

--- lang/ja.utf8/admin.php
+++ lang/ja.utf8/admin.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-02-19 07:07:09 UTC
+ * @updated    2013-05-15 22:04:23 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
