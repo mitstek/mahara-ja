10971b7ac147484497cda91ce7ac6ba478c123ba
Mitsuhiro Yoshida <mits@mitstek.com>
2013-08-06 13:12:37 +0900

    Brushed up a translated string for share and network subtitle.

--- lang/ja.utf8/mahara.php
+++ lang/ja.utf8/mahara.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-07-01 21:57:16 UTC
+ * @updated    2013-08-06 04:11:20 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -936,7 +936,7 @@ $string['Organise'] = '整理';
 $string['organisesubtitle'] = 'あなたのポートフォリオのショーケースです。';
 $string['organisedescription'] = 'あなたのポートフォリオを<a href="%s">ページ</a>に整理することができます。異なる閲覧者に対して、異なるページを作成します - あなたがページに含む構成要素を選択します。';
 $string['sharenetwork'] = '共有およびネットワーク';
-$string['sharenetworksubtitle'] = 'フレンドに会って、グループに参加します。';
+$string['sharenetworksubtitle'] = 'フレンドを探して、グループに参加します。';
 $string['findfriendslinked'] = '<a href="%s">フレンド</a>を探す';
 $string['joingroups'] = '<a href="%s">グループ</a>に参加する';
 $string['controlyourprivacylinked'] = 'あなたの<a href="%s">プライバシー</a>をコントロールする';
