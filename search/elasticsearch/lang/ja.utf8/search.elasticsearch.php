088ced036b7d73ebd208e7f87bfb9668a2d6b67b
Mitsuhiro Yoshida <mits@mitstek.com>
2013-08-22 16:03:56 +0900

    Translated new strings for elasticsearch plugin.

---
+++ search/elasticsearch/lang/ja.utf8/search.elasticsearch.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006 onwards Catalyst IT Ltd (http://www.catalyst.net.nz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2013-08-22 07:03:16 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['admin'] = '管理者';
+$string['all'] = 'すべて';
+$string['analyzer'] = 'イラスティック検索アナライザ';
+$string['artefacttypelegend'] = 'アーティファクトタイプ';
+$string['artefacttypemaplegend'] = 'アーティファクトタイプ階層';
+$string['blogpost'] = '日誌タイプ';
+$string['collection'] = 'コレクション';
+$string['confignotset'] = '(未設定)';
+$string['forum'] = 'フォーラム';
+$string['forumpost'] = 'フォーラム投稿';
+$string['forumtopic'] = 'フォーラムトピック';
+$string['Group'] = 'グループ';
+$string['host'] = 'ホスト';
+$string['indexname'] = 'インデックス名';
+$string['license'] = 'ライセンス';
+$string['Media'] = 'メディア';
+$string['owner'] = 'オーナー';
+$string['page'] = 'ページ';
+$string['pages'] = 'ページ';
+$string['pagetitle'] = '検索';
+$string['password'] = '認証パスワード';
+$string['port'] = 'イラスティック検索ポート';
+$string['Portfolio'] = 'ポートフォリオ';
+$string['record'] = 'レコード';
+$string['records'] = 'レコード';
+$string['resetlegend'] = 'インデックスリセット';
+$string['resume'] = 'レジュメ';
+$string['tags'] = 'タグ';
+$string['tagsonly'] = 'タグのみ';
+$string['Text'] = 'テキスト';
+$string['username'] = '認証ユーザ名';
+$string['Users'] = 'ユーザ';
+$string['wallpost'] = 'ウォール投稿';
+
+?>
