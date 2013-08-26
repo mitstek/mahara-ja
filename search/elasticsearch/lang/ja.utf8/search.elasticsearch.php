03bd35f398bc2f3be0ee950be0588a2efa15f9aa
Mitsuhiro Yoshida <mits@mitstek.com>
2013-08-26 14:42:28 +0900

    Translated new strings and a help file for elastic search plugin.

--- search/elasticsearch/lang/ja.utf8/search.elasticsearch.php
+++ search/elasticsearch/lang/ja.utf8/search.elasticsearch.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-08-22 07:05:04 UTC
+ * @updated    2013-08-26 05:41:37 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -34,6 +34,7 @@ $string['analyzer'] = 'イラスティック検索アナライザ';
 $string['artefacttypelegend'] = 'アーティファクトタイプ';
 $string['artefacttypemaplegend'] = 'アーティファクトタイプ階層';
 $string['blogpost'] = '日誌タイプ';
+$string['bypassindexname'] = 'バイパスインデックス';
 $string['collection'] = 'コレクション';
 $string['confignotset'] = '(未設定)';
 $string['cronlimit'] = 'Cronレコード制限';
@@ -42,26 +43,36 @@ $string['forumpost'] = 'フォーラム投稿';
 $string['forumtopic'] = 'フォーラムトピック';
 $string['Group'] = 'グループ';
 $string['host'] = 'ホスト';
+$string['hostdescription'] = 'イラスティックサーバのホスト名です。デフォルトは「127.0.0.1」です。';
 $string['indexname'] = 'インデックス名';
+$string['indexnamedescription'] = 'イラスティック検索インデックス名です。デフォルトは「mahara」です。';
 $string['license'] = 'ライセンス';
 $string['Media'] = 'メディア';
+$string['noticeenabled'] = '現在、イラスティック検索プラグインは有効にされています。イラスティック検索プラグインを無効にするには、<a href="%s">サイトオプションの検索設定</a>にて、選択解除してください。';
+$string['noticenotenabled'] = '現在、イラスティック検索プラグインは無効にされています。イラスティック検索プラグインを有効にするには、<a href="%s">サイトオプションの検索設定</a>にて、選択してください。';
+$string['noticepostgresrequired'] = '現在のところ、イラスティック検索プラグインはPostgresqlデータベースのみで動作します。';
 $string['owner'] = 'オーナー';
 $string['page'] = 'ページ';
 $string['pages'] = 'ページ';
 $string['pagetitle'] = '検索';
 $string['password'] = '認証パスワード';
+$string['passworddescription'] = '(任意) HTTP基本認証経由でイラスティック検索するためのパスワードです。';
 $string['passwordlength'] = '(パスワード長: %s)';
 $string['port'] = 'イラスティック検索ポート';
+$string['portdescription'] = 'イラスティック検索が使用するポートです。デフォルトは「9200」です。';
 $string['Portfolio'] = 'ポートフォリオ';
 $string['record'] = 'レコード';
 $string['records'] = 'レコード';
+$string['relevance'] = '関連性';
 $string['resetallindexes'] = 'すべてのインデックスをリセットする';
 $string['resetlegend'] = 'インデックスリセット';
 $string['resume'] = 'レジュメ';
 $string['tags'] = 'タグ';
 $string['tagsonly'] = 'タグのみ';
 $string['Text'] = 'テキスト';
+$string['types'] = 'イラスティック検索タイプ';
 $string['username'] = '認証ユーザ名';
+$string['usernamedescription'] = '(任意) HTTP基本認証経由でイラスティック検索するためのユーザ名です。';
 $string['Users'] = 'ユーザ';
 $string['wallpost'] = 'ウォール投稿';
 
