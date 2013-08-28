c2b336a46413d886ae92c18041aacae631d7b9a1
Mitsuhiro Yoshida <mits@mitstek.com>
2013-08-28 09:01:02 +0900

    Translated new strings and a help file for elastic search.

--- search/elasticsearch/lang/ja.utf8/search.elasticsearch.php
+++ search/elasticsearch/lang/ja.utf8/search.elasticsearch.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-08-26 22:44:22 UTC
+ * @updated    2013-08-27 23:59:58 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -33,11 +33,18 @@ $string['all'] = 'すべて';
 $string['analyzer'] = 'イラスティック検索アナライザ';
 $string['artefacttypelegend'] = 'アーティファクトタイプ';
 $string['artefacttypemaplegend'] = 'アーティファクトタイプ階層';
+$string['atoz'] = 'A -> Z';
 $string['blogpost'] = '日誌タイプ';
 $string['bypassindexname'] = 'バイパスインデックス';
+$string['bypassindexnamedescription'] = '(任意) 設定された場合、Maharaはメインインデックス名の代わりに、このインデックス名にインデックスデータを読み込みます。';
 $string['collection'] = 'コレクション';
 $string['confignotset'] = '(未設定)';
+$string['createdby'] = '作成者: %s';
 $string['cronlimit'] = 'Cronレコード制限';
+$string['dateoldestfirst'] = '日付 (古い順)';
+$string['daterecentfirst'] = '日付 (新しい順)';
+$string['deleted'] = '削除済み';
+$string['deletedforumpost'] = '削除済みフォーラム投稿';
 $string['forum'] = 'フォーラム';
 $string['forumpost'] = 'フォーラム投稿';
 $string['forumtopic'] = 'フォーラムトピック';
@@ -51,6 +58,7 @@ $string['Media'] = 'メディア';
 $string['noticeenabled'] = '現在、イラスティック検索プラグインは有効にされています。イラスティック検索プラグインを無効にするには、<a href="%s">サイトオプションの検索設定</a>にて、選択解除してください。';
 $string['noticenotenabled'] = '現在、イラスティック検索プラグインは無効にされています。イラスティック検索プラグインを有効にするには、<a href="%s">サイトオプションの検索設定</a>にて、選択してください。';
 $string['noticepostgresrequired'] = '現在のところ、イラスティック検索プラグインはPostgresqlデータベースのみで動作します。';
+$string['noticepostgresrequiredtitle'] = '機能利用不可';
 $string['owner'] = 'オーナー';
 $string['page'] = 'ページ';
 $string['pages'] = 'ページ';
@@ -67,13 +75,17 @@ $string['relevance'] = '関連性';
 $string['resetallindexes'] = 'すべてのインデックスをリセットする';
 $string['resetlegend'] = 'インデックスリセット';
 $string['resume'] = 'レジュメ';
+$string['sortby'] = '並べ替え';
 $string['tags'] = 'タグ';
 $string['tagsonly'] = 'タグのみ';
 $string['Text'] = 'テキスト';
 $string['types'] = 'イラスティック検索タイプ';
+$string['usedonpage'] = 'ページ使用';
+$string['usedonpages'] = 'ページ使用';
 $string['username'] = '認証ユーザ名';
 $string['usernamedescription'] = '(任意) HTTP基本認証経由でイラスティック検索するためのユーザ名です。';
 $string['Users'] = 'ユーザ';
 $string['wallpost'] = 'ウォール投稿';
+$string['ztoa'] = 'Z -> A';
 
 ?>
