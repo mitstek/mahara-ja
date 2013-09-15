885f48ad79e270359b606add81a46c39b8985be7
Mitsuhiro Yoshida <mits@mitstek.com>
2013-09-16 08:08:52 +0900

    Translated new strings for auth LDAP. Translated new help files for Custom Layout.

--- auth/ldap/lang/ja.utf8/auth.ldap.php
+++ auth/ldap/lang/ja.utf8/auth.ldap.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-09-15 22:55:43 UTC
+ * @updated    2013-09-15 23:08:43 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
