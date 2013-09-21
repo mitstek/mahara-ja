6cb44b0a86add147ef29b17b1f9e489a68b6630c
Mitsuhiro Yoshida <mits@mitstek.com>
2013-09-21 12:53:18 +0900

    Translated new strings for skin.

---
+++ lang/ja.utf8/skin.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006 onwards Catalyst IT Ltd (http://www.catalyst.net.nz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2013-09-21 03:52:35 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['pluginname'] = 'スキン';
+$string['myskins'] = 'スキン';
+$string['siteskinmenu'] = 'スキン';
+$string['deletethisskin'] = 'このスキンを削除する';
+$string['skindeleted'] = 'スキンが削除されました。';
+$string['cantdeleteskin'] = 'あなたはこのスキンを削除できません。';
+$string['deletespecifiedskin'] = 'スキン「 %s 」を削除する';
+$string['importskins'] = 'スキンをインポートする';
+$string['import'] = 'インポート';
+$string['exportthisskin'] = 'このスキンをエクスポートする';
+$string['exportskins'] = 'スキンをエクスポートする';
+$string['createskin'] = 'スキンを作成する';
+$string['editskin'] = 'スキンを編集する';
+$string['skinsaved'] = 'スキンが正常に保存されました。';
+$string['skinimported'] = 'スキンが正常にインポートされました。';
+$string['skin'] = 'スキン';
+$string['skins'] = 'スキン';
+$string['allskins'] = 'すべてのスキン';
+$string['siteskins'] = 'サイトスキン';
+$string['userskins'] = 'マイスキン';
+$string['currentskin'] = '現在のスキン';
+$string['skindefault'] = 'デフォルトサイトスキン';
+$string['skintitle'] = 'スキンタイトル';
+$string['skindescription'] = 'スキン説明';
+$string['skinaccessibility'] = 'スキンアクセシビリティ';
+$string['backgroundscroll'] = 'スクロール';
+$string['textfontfamily'] = 'テキストフォント';
+$string['fontsize'] = 'フォントサイズ';
+$string['headerlogoimage'] = 'Maharaロゴイメージ';
+$string['skincustomcss'] = 'カスタムCSS';
+$string['sitefontsmenu'] = 'フォント';
+$string['sitefonts'] = 'フォント';
+$string['font'] = 'フォント';
+$string['fonts'] = 'フォント';
+$string['installfont'] = 'フォントをインストールする';
+$string['fontinstalled'] = 'フォントが正常にインストールされました。';
+$string['addfontvariant'] = 'フォントスタイルを追加する';
+$string['fontvariantadded'] = 'フォントスタイルが正常に追加されました。';
+$string['editfont'] = 'フォントを編集する';
+$string['fontedited'] = 'フォント属性を編集する';
+$string['deletefont'] = 'フォントを削除する';
+$string['deletespecifiedfont'] = 'フォント「 %s 」を削除する';
+$string['fontdeleted'] = 'フォントが削除されました。';
+$string['fontstyle'] = 'フォントスタイル';
+$string['fonttype'] = 'フォントタイプ';
+$string['fontfiles'] = 'フォントファイル';
+$string['fontfilesvg'] = 'SVGフォントファイル';
+$string['fontfilelicence'] = 'ライセンスファイル';
+$string['showfonts'] = '表示';
+$string['fonttypes.all'] = 'ローカルフォント';
+$string['fonttype.site'] = 'ローカルフォント';
+$string['samplesize'] = 'サイズ';
+$string['sampletext'] = 'テキスト';
+$string['samplefonttitle'] = 'フォント名';
+$string['fontlicence'] = 'フォントライセンス';
+$string['fontsort.alpha'] = 'アルファベット';
+
+?>
