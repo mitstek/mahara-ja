8257fb896bd8feffe8f359505ffee769bd16b647
Mitsuhiro Yoshida <mits@mitstek.com>
2013-09-27 08:01:35 +0900

    Translated a new string for artefact internal.

--- artefact/internal/lang/ja.utf8/artefact.internal.php
+++ artefact/internal/lang/ja.utf8/artefact.internal.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-06-28 01:39:56 UTC
+ * @updated    2013-09-26 23:01:00 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -115,6 +115,7 @@ $string['Notes'] = 'ノート';
 $string['mynotes'] = 'マイノート';
 $string['notesfor'] = '%s のノート';
 $string['containedin'] = '含まれる場所:';
+$string['currenttitle'] = 'タイトル';
 $string['notesdescription'] = 'あなたのページのテキストボックスブロック内で作成したHTML形式のノートです。';
 $string['editnote'] = 'ノートを編集する';
 $string['confirmdeletenote'] = 'このノートは %d ブロックおよび %d ページで使用されています。あなたが削除した場合、現在このテキストを含むすべてのブロックには空白が表示されてしまいます。';
