e85ef32fe2a537ae2d5d98a425185bb110504398
Mitsuhiro Yoshida <mits@mitstek.com>
2013-10-18 14:47:36 +0900

    Translated new strings for auth LDAP.

--- auth/ldap/lang/ja.utf8/auth.ldap.php
+++ auth/ldap/lang/ja.utf8/auth.ldap.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-09-28 11:58:55 UTC
+ * @updated    2013-10-18 05:46:21 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -16,9 +16,25 @@ defined('INTERNAL') || die();
 $string['title'] = 'LDAP';
 $string['description'] = 'LDAPサーバで認証する';
 $string['notusable'] = 'PHP LDAP拡張モジュールをインストールしてください。';
+$string['attributename'] = '値を基にグループを同期する場合に使用するLDAP属性名です (必須および大文字小文字を区別します)。';
+$string['cannotdeleteandsuspend'] = '同時に -d および -s を指定することはできません。';
+$string['cli_info_sync_groups'] = 'このコマンドラインPHPスクリプトはLDAPディレクトリとグループのインスティテューションリストの同期を試みます。
+存在しないグループは作成されて、「インスティテューション名 : LDAP属性値」のような名称になります。';
+$string['cli_info_sync_groups_attribute'] = 'このコマンドラインPHPスクリプトはLDAP属性の異なる値を基にLDAPディレクトリとグループのインスティテューションリストの同期を試みます。
+存在しないグループは作成されて、「インスティテューション名 : LDAP属性値」のような名称になります。';
+$string['cli_info_sync_users'] = 'このコマンドラインPHPスクリプトはLDAPディレクトリとMaharaアカウントのインスティテューションリストの同期を試みます。';
 $string['contexts'] = 'コンテクスト';
 $string['distinguishedname'] = '識別名';
+$string['dodelete'] = 'LDAPに存在しなくなったアカウントを削除する';
+$string['dosuspend'] = 'LDAPに存在しなくなったアカウントを利用停止する';
+$string['doupdate'] = 'LDAPデータ (LONG) により既存のアカウントを更新する';
+$string['dryrun'] = 'ダミー実行です。データベース処理は実施しません。';
+$string['excludelist'] = 'これらの正規表現に名称が合致するLDAPグループを除く';
+$string['extrafilterattribute'] = 'ユーザ検索を制限するための追加LDAPフィルタ';
+$string['grouptype'] = '作成するMaharaグループタイプ - デフォルト「standard」';
 $string['hosturl'] = 'ホストURI';
+$string['includelist'] = 'これらの正規表現に名称が合致するLDAPグループのみ処理する';
+$string['institutionname'] = '処理するインスティテューション名 (必須)';
 $string['ldapfieldforpreferredname'] = '表示名のLDAPフィールド';
 $string['ldapfieldforemail'] = 'メールアドレスのLDAPフィールド';
 $string['ldapfieldforfirstname'] = '名のLDAPフィールド';
@@ -26,9 +42,13 @@ $string['ldapfieldforsurname'] = '姓のLDAPフィールド';
 $string['ldapfieldforstudentid'] = '学生IDのLDAPフィールド';
 $string['ldapversion'] = 'LDAPバージョン';
 $string['nocreate'] = '新しいアカウントを作成しない';
+$string['nocreatemissinggroups'] = 'すでにインスティテューションに設定されている場合、LDAPグループを作成しません。';
+$string['nomatchingauths'] = 'このインスティテューションのLDAP認証プラグインは見つかりませんでした。';
 $string['starttls'] = 'TLS暗号化';
 $string['password'] = 'パスワード';
+$string['searchcontexts'] = 'これらのコンテクストにおける検索を制限します (認証プラグインで設定された値をオーバライドします)。';
 $string['searchsubcontexts'] = 'サブコンテクストを検索する';
+$string['searchsubcontextscliparam'] = 'サブコンテクスト内で (1) または (0) ではないものを検索します (認証プラグインで設定された値をオーバライドします)。';
 $string['syncgroupsautocreate'] = '未作成のグループを自動作成する';
 $string['syncgroupsbyclass'] = 'LDAPオブジェクトとして保存されているグループを同期する';
 $string['syncgroupsbyuserfield'] = 'ユーザ属性として保存されているグループを同期する';
