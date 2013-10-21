508143a03d8c1beca0ab4cfbe1337f288183f17c
Mitsuhiro Yoshida <mits@mitstek.com>
2013-10-21 15:43:26 +0900

    Translated new strings for import and a help file for LDAP.

--- lang/ja.utf8/import.php
+++ lang/ja.utf8/import.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-10-04 05:18:22 UTC
+ * @updated    2013-10-21 06:41:11 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,6 +14,9 @@
 defined('INTERNAL') || die();
 
 $string['Import'] = 'インポート';
+$string['importexceedquota'] = 'あなたがアップロードしたLeap2Aファイルの新しいファイル合計サイズがあなたのクオータを超えるようです。いくつかのファイルを削除して、あなたは再度試みる必要があります。';
+$string['importsuccessfully'] = 'あなたのポートフォリオは正常にインポートされました。';
+$string['importwitherrors'] = 'あなたのポートフォリオはエラーと共ににインポートされました。';
 $string['importartefactplugindata'] = 'アーティファクトプラグインデータのインポート';
 $string['importartefacts'] = 'アーティファクトのインポート';
 $string['importartefactsprogress'] = 'アーティファクトのインポート: %s/%s';
@@ -21,10 +24,20 @@ $string['importfooter'] = 'フッタのインポート';
 $string['importviews'] = 'ページのインポート';
 $string['importviewsprogress'] = 'ページのインポート: %s/%s';
 $string['importyourportfolio'] = 'あなたのポートフォリオをインポートする';
+$string['noimportpluginsenabled'] = 'サイト管理者はインポートプラグインを有効にしていません。そのため、あなたは使用することができません。';
+$string['noleapimportpluginsenabled'] = 'サイト管理者はLeap2Aインポートプラグインを有効にしていません。そのため、あなたは使用することができません。';
 $string['entry'] = 'エントリ';
 $string['entries'] = 'エントリ';
 $string['importentry'] = 'エントリをインポートする';
-$string['ignore'] = '無視';
-$string['replace'] = '置換';
+$string['duplicateditem'] = '重複アイテム';
+$string['existingitem'] = '既存のアイテム';
+$string['howtoimport'] = 'インポート方法';
+$string['ignore'] = '無視する';
+$string['replace'] = '置換する';
+$string['addnew'] = '新しく作成する';
+$string['append'] = '追加する';
+$string['importfolder'] = 'フォルダをインポートする %s';
+$string['importresult'] = 'インポート結果';
+$string['noimportentry'] = 'インポートエントリはありません。';
 
 ?>
