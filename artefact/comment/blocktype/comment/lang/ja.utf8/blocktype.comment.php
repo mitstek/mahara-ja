d49bfdcea1c954be07ede4c0e134bb915dab4e83
Mitsuhiro Yoshida <mits@mitstek.com>
2013-11-08 19:06:41 +0900

    Translated new strings for feedback block.

---
+++ artefact/comment/blocktype/comment/lang/ja.utf8/blocktype.comment.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2013-11-08 10:05:59 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'フィードバック';
+$string['description'] = 'フィードバックを表示するブロック';
+$string['ineditordescription'] = 'このページのフィードバックはページフッタではなく、ここに表示されます。';
+
+?>
