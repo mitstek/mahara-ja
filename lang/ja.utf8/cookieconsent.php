66c8358efbd27524dda9be640fe68bf431c401f2
Mitsuhiro Yoshida <mits@mitstek.com>
2013-12-16 13:31:45 +0900

    Translated new strings for Cookie Consent.

--- lang/ja.utf8/cookieconsent.php
+++ lang/ja.utf8/cookieconsent.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-12-16 04:22:51 UTC
+ * @updated    2013-12-16 04:31:32 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,6 +14,30 @@
 defined('INTERNAL') || die();
 
 $string['cookieconsent'] = 'クッキーコンセント';
+$string['readfulltext'] = '新しいディレクティブのフルテキストを読む';
+$string['directive2009136'] = 'ディレクティブ2009/136/EC';
+$string['readdirectiveBG'] = 'ディレクティブ2009/136/ECを読む (ブルガリア語)';
+$string['readdirectiveCS'] = 'ディレクティブ2009/136/ECを読む (チェコ語)';
+$string['readdirectiveDA'] = 'ディレクティブ2009/136/ECを読む (デンマーク語)';
+$string['readdirectiveDE'] = 'ディレクティブ2009/136/ECを読む (ドイツ語)';
+$string['readdirectiveEL'] = 'ディレクティブ2009/136/ECを読む (ギリシャ語)';
+$string['readdirectiveEN'] = 'ディレクティブ2009/136/ECを読む (英語)';
+$string['readdirectiveES'] = 'ディレクティブ2009/136/ECを読む (スペイン語)';
+$string['readdirectiveET'] = 'ディレクティブ2009/136/ECを読む (エストニア語)';
+$string['readdirectiveFI'] = 'ディレクティブ2009/136/ECを読む (フィンランド語)';
+$string['readdirectiveFR'] = 'ディレクティブ2009/136/ECを読む (フランス語)';
+$string['readdirectiveHU'] = 'ディレクティブ2009/136/ECを読む (ハンガリー語)';
+$string['readdirectiveIT'] = 'ディレクティブ2009/136/ECを読む (イタリア語)';
+$string['readdirectiveLT'] = 'ディレクティブ2009/136/ECを読む (リトアニア語)';
+$string['readdirectiveLV'] = 'ディレクティブ2009/136/ECを読む (ラトビア語)';
+$string['readdirectiveMT'] = 'ディレクティブ2009/136/ECを読む (マルタ語)';
+$string['readdirectiveNL'] = 'ディレクティブ2009/136/ECを読む (オランダ語)';
+$string['readdirectivePL'] = 'ディレクティブ2009/136/ECを読む (ポーランド語)';
+$string['readdirectivePT'] = 'ディレクティブ2009/136/ECを読む (ポルトガル語)';
+$string['readdirectiveRO'] = 'ディレクティブ2009/136/ECを読む (ルーマニア語)';
+$string['readdirectiveSK'] = 'ディレクティブ2009/136/ECを読む (スロバキア語)';
+$string['readdirectiveSL'] = 'ディレクティブ2009/136/ECを読む (スロベニア語)';
+$string['readdirectiveSV'] = 'ディレクティブ2009/136/ECを読む (スウェーデン語)';
 $string['cookieconsentenable'] = 'クッキーコンセントを有効にしますか?';
 $string['example'] = '例';
 $string['generaloptions'] = '一般オプション';
@@ -31,7 +55,11 @@ $string['socialDefaultTitle'] = 'ソーシャルメディア';
 $string['defaultTitle'] = 'デフォルトクッキータイトル';
 $string['defaultDescription'] = 'デフォルトクッキー説明';
 $string['closeWindow'] = 'ウィンドウを閉じる';
+$string['hideDetails'] = '詳細を隠す';
 $string['allowCookiesImplicit'] = '閉じる';
+$string['privacySettings'] = 'プライバシー設定';
+$string['privacySettingsDialogTitleA'] = 'プライバシー設定';
+$string['preferenceUseGlobal'] = 'グローバル設定を使用する';
 $string['allSitesSettingsDialogTitleA'] = 'プライバシー設定';
 $string['backToSiteSettings'] = 'ウェブサイト設定に戻る';
 
