6c4aac7f31856991a716b1870f9a255fb3c87958
Mitsuhiro Yoshida <mits@mitstek.com>
2013-12-16 13:24:35 +0900

    Translated new strings for Cookie Consent.

---
+++ lang/ja.utf8/cookieconsent.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2013-12-16 04:22:51 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['cookieconsent'] = 'クッキーコンセント';
+$string['cookieconsentenable'] = 'クッキーコンセントを有効にしますか?';
+$string['example'] = '例';
+$string['generaloptions'] = '一般オプション';
+$string['cookietypes'] = 'クッキータイプ';
+$string['cookietypessocial'] = 'ソーシャルメディア';
+$string['consentmode'] = 'コンセントモード';
+$string['stylingoptions'] = 'スタイリングオプション';
+$string['pluginstyle'] = 'スタイル';
+$string['hideprivacytab'] = 'プライバシー設定タブを隠す';
+$string['featureoptions'] = '機能オプション';
+$string['pagerefresh'] = 'ページリフレッシュ';
+$string['usessl'] = 'SSLを使用する';
+$string['addcode'] = 'コードを追加する';
+$string['socialDefaultTitle'] = 'ソーシャルメディア';
+$string['defaultTitle'] = 'デフォルトクッキータイトル';
+$string['defaultDescription'] = 'デフォルトクッキー説明';
+$string['closeWindow'] = 'ウィンドウを閉じる';
+$string['allowCookiesImplicit'] = '閉じる';
+$string['allSitesSettingsDialogTitleA'] = 'プライバシー設定';
+$string['backToSiteSettings'] = 'ウェブサイト設定に戻る';
+
+?>
