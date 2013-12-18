734a6c853acdc5b885b5f567ef6094ce2fd831c4
Mitsuhiro Yoshida <mits@mitstek.com>
2013-12-18 15:27:57 +0900

    Translated new strings for skin.

--- lang/ja.utf8/skin.php
+++ lang/ja.utf8/skin.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-12-18 06:17:04 UTC
+ * @updated    2013-12-18 06:27:52 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -22,6 +22,7 @@ $string['cantdeleteskin'] = 'あなたはこのスキンを削除できません
 $string['deletespecifiedskin'] = 'スキン「 %s 」を削除する';
 $string['deleteskinconfirm'] = '本当にこのスキンを削除してもよろしいですか? 元に戻すことはできません。';
 $string['importskins'] = 'スキンをインポートする';
+$string['importskinsnotice'] = 'インポートするスキン定義が含まれる有効なXMLファイルを選択してください。';
 $string['validxmlfile'] = '有効なXMLファイル';
 $string['notvalidxmlfile'] = 'アップロードされたファイルは有効なXMLファイルではありません。';
 $string['import'] = 'インポート';
@@ -31,15 +32,19 @@ $string['createskin'] = 'スキンを作成する';
 $string['editskin'] = 'スキンを編集する';
 $string['skinsaved'] = 'スキンが正常に保存されました。';
 $string['skinimported'] = 'スキンが正常にインポートされました。';
+$string['clickimagetoedit'] = 'イメージをクリックして編集する';
 $string['addtofavorites'] = 'お気に入りに追加する';
 $string['removefromfavorites'] = 'お気に入りから削除する';
 $string['skinaddedtofavorites'] = 'お気に入りにスキンが追加されました。';
 $string['skinremovedfromfavorites'] = 'お気に入りからスキンが削除されました。';
 $string['cantremoveskinfromfavorites'] = 'お気に入りからスキンを削除できません。';
+$string['viewmetadata'] = 'スキン情報を表示する';
 $string['metatitle'] = 'スキン情報';
 $string['title'] = 'タイトル';
 $string['displayname'] = 'オーナー';
 $string['description'] = '説明';
+$string['creationdate'] = '作成日時';
+$string['modifieddate'] = '修正日時';
 $string['noskins'] = 'スキンがありません。';
 $string['skin'] = 'スキン';
 $string['skins'] = 'スキン';
@@ -72,6 +77,11 @@ $string['textcolor'] = 'テキスト色';
 $string['headingcolor'] = 'ヘッディングテキスト色';
 $string['bodybgimage'] = 'スキン背景イメージ';
 $string['viewbgimage'] = 'ページ背景イメージ';
+$string['backgroundrepeat'] = '背景イメージの繰り返し';
+$string['backgroundrepeatboth'] = '両方向に繰り返す';
+$string['backgroundrepeatx'] = '水平に繰り返す';
+$string['backgroundrepeaty'] = '垂直に繰り返す';
+$string['backgroundrepeatno'] = '繰り返さない';
 $string['backgroundfixed'] = '固定';
 $string['backgroundscroll'] = 'スクロール';
 $string['viewwidth'] = 'ページ幅';
