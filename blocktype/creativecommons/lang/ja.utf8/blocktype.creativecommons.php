948a8aea5d860085bab10125aa92c5804ef02824
Mitsuhiro Yoshida <mits@mitstek.com>
2014-02-13 10:41:40 +0900

    Translated new strings for block Creative Commons license.

--- blocktype/creativecommons/lang/ja.utf8/blocktype.creativecommons.php
+++ blocktype/creativecommons/lang/ja.utf8/blocktype.creativecommons.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2011-05-03 16:38:37 UTC
+ * @updated    2014-02-13 01:41:03 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -17,13 +17,16 @@ $string['title'] = 'クリエイティブ・コモンズ・ライセンス';
 $string['description'] = 'あなたのページにクリエイティブ・コモンズ・ライセンスを添付します。';
 $string['blockcontent'] = 'コンテンツをブロックする';
 $string['alttext'] = 'クリエイティブ・コモンズ・ライセンス';
-$string['cclicensename'] = 'クリエイティブ・コモンズ %s 3.0 Unported';
+$string['cclicensename'] = 'クリエイティブ・コモンズ %s %s Unported';
 $string['cclicensestatement'] = '%s by %s は %s ライセンスの下に提供されています。';
 $string['otherpermissions'] = 'このライセンスの範囲を超えた使用は %s の許可が必要です。';
 $string['sealalttext'] = 'このライセンスはフリーな文化的作品 (Free Cultural Works) に関して許可されます。';
+$string['version30'] = '3.0';
+$string['version40'] = '4.0';
 $string['config:noncommercial'] = 'あなたの作品の商用利用を許可しますか?';
 $string['config:noderivatives'] = 'あなたの作品の修正を許可しますか?';
 $string['config:sharealike'] = 'Yes、他の人が同一許諾条件を保持する場合';
+$string['config:version'] = 'ライセンスバージョン';
 $string['by'] = '表示';
 $string['by-sa'] = '表示-継承';
 $string['by-nd'] = '表示-改変禁止';
