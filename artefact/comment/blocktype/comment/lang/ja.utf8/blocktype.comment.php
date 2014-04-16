0f0ce10a7332ec075ee2dd9127096e34c70d21be
Mitsuhiro Yoshida <mits@mitstek.com>
2014-04-17 06:39:47 +0900

    Translated new string for blocktype comment.

---
+++ artefact/comment/blocktype/comment/lang/ja.utf8/blocktype.comment.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2014-04-16 21:39:24 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'フィードバック';
+$string['description'] = 'フィードバックを表示するブロック';
+$string['ineditordescription1'] = 'このページのフィードバックはページ下部ではなく、ここに表示されます。';
+
+?>
