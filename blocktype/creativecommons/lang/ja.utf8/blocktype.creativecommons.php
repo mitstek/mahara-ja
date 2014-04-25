412a090fa2565f4a1d8a17f012c3e1ce8a40b28c
Mitsuhiro Yoshida <mits@mitstek.com>
2014-04-26 03:47:48 +0900

    Translated a new string for block creativecommons.

--- blocktype/creativecommons/lang/ja.utf8/blocktype.creativecommons.php
+++ blocktype/creativecommons/lang/ja.utf8/blocktype.creativecommons.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2011-05-03 16:38:37 UTC
+ * @updated    2014-04-25 18:47:26 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -21,9 +21,12 @@ $string['cclicensename'] = 'クリエイティブ・コモンズ %s 3.0 Unported
 $string['cclicensestatement'] = '%s by %s は %s ライセンスの下に提供されています。';
 $string['otherpermissions'] = 'このライセンスの範囲を超えた使用は %s の許可が必要です。';
 $string['sealalttext'] = 'このライセンスはフリーな文化的作品 (Free Cultural Works) に関して許可されます。';
+$string['version30'] = '3.0';
+$string['version40'] = '4.0';
 $string['config:noncommercial'] = 'あなたの作品の商用利用を許可しますか?';
 $string['config:noderivatives'] = 'あなたの作品の修正を許可しますか?';
 $string['config:sharealike'] = 'Yes、他の人が同一許諾条件を保持する場合';
+$string['config:version'] = 'ライセンスバージョン';
 $string['by'] = '表示';
 $string['by-sa'] = '表示-継承';
 $string['by-nd'] = '表示-改変禁止';
