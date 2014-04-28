ee15006d6551414ba1826ea29cd6b86f507f3d61
Mitsuhiro Yoshida <mits@mitstek.com>
2014-04-29 03:09:26 +0900

    Translated a new string for activity.

--- lang/ja.utf8/activity.php
+++ lang/ja.utf8/activity.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2014-04-28 18:07:48 UTC
+ * @updated    2014-04-28 18:09:13 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -15,6 +15,7 @@ defined('INTERNAL') || die();
 
 $string['typemaharamessage'] = 'システムメッセージ';
 $string['typeusermessage'] = '他のユーザからのメッセージ';
+$string['typefeedback'] = 'フィードバック';
 $string['typewatchlist'] = 'ウォッチリスト';
 $string['typeviewaccess'] = '新しいページアクセス';
 $string['typecontactus'] = 'お問い合わせ';
