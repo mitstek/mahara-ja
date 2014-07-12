e46c7f5d613e48f35854c6f29c8a95d7e19f1899
Mitsuhiro Yoshida <mits@mitstek.com>
2014-07-13 01:36:42 +0900

    Translated new strings for blocktype mygroups.

--- blocktype/mygroups/lang/ja.utf8/blocktype.mygroups.php
+++ blocktype/mygroups/lang/ja.utf8/blocktype.mygroups.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2009-01-05 21:07:44 UTC
+ * @updated    2014-07-12 16:35:38 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -16,5 +16,15 @@ defined('INTERNAL') || die();
 $string['title'] = 'マイグループ';
 $string['otherusertitle'] = '%s のグループ';
 $string['description'] = 'あなたが属しているグループの一覧を表示します。';
+$string['sortby'] = '並べ替え:';
+$string['earliest'] = '参加の古い順';
+$string['latest'] = '参加の新しい順';
+$string['alphabetical'] = 'A -> Z';
+$string['limitto'] = '最大表示グループ数';
+$string['limittodesc'] = 'ブロックに表示する最大グループ数です。あなたのグループすべてを表示するには、空白のままにしてください。';
+$string['limittodescsideblock'] = 'あなたの「マイグループ」リストに表示される最大グループ数です。あなたのグループすべてを表示するには、空白のままにしてください。';
+$string['numberofmygroupsshowing'] = 'マイグループ表示中 (%s/%s)';
+$string['numberofmygroupsshowingearliest'] = '参加の古いグループ表示中 (%s/%s)';
+$string['numberofmygroupsshowinglatest'] = '参加の新しいグループ表示中 (%s/%s)';
 
 ?>
