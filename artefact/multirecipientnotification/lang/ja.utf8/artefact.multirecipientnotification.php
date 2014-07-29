ace0cdde57535e04eee5dd3306e1f95eb3df88a2
Mitsuhiro Yoshida <mits@mitstek.com>
2014-07-29 15:16:35 +0900

    Translated new strings for notifications.

---
+++ artefact/multirecipientnotification/lang/ja.utf8/artefact.multirecipientnotification.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2014-07-29 06:15:03 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['sendmessageto'] = 'メッセージを送信する';
+$string['titlesubject'] = '件名';
+$string['titlerecipient'] = '受信者';
+$string['replysubjectprefix'] = 'Re:';
+$string['labelrecipients'] = 'To:';
+$string['labelsubject'] = '件名';
+$string['deletednotifications'] = '%s 件の通知を削除しました。送信箱からは内部通知を削除できないことに留意してください!';
+$string['notification'] = '通知';
+$string['cantsendemptysubject'] = 'あなたの件名は空白です。件名を入力してください。';
+$string['cantsendemptytext'] = 'あなたのメッセージは空白です。メッセージを入力してください。';
+$string['cantsendnorecipients'] = '少なくとも1名の受信者を選択してください。';
+$string['removeduserfromlist'] = 'あなたのメッセージを受信できないユーザは受信リストから削除されています。';
+$string['deleteduser'] = '削除済みユーザ';
+$string['returnurltext'] = '返信';
+$string['replyurltext'] = 'すべてに返信する';
+$string['linkindicator'] = '»';
+$string['labeloutbox'] = '送信箱';
+$string['labelinbox'] = '受信箱';
+$string['selectallread'] = 'すべての未読通知';
+$string['selectalldelete'] = '削除する通知すべて';
+$string['clickformore'] = '(詳細情報を表示するには、「Enter」を押してください)';
+
+?>
