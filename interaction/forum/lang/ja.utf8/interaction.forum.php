d6a500264fe0211b0a4e1b1197a7a0972e42cca9
Mitsuhiro Yoshida <mits@mitstek.com>
2014-08-22 00:43:16 +0900

    Updated a translated string for forum.

--- interaction/forum/lang/ja.utf8/interaction.forum.php
+++ interaction/forum/lang/ja.utf8/interaction.forum.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2012-10-14 08:46:16 UTC
+ * @updated    2014-08-21 15:42:08 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -170,7 +170,7 @@ $string['strftimerecentrelative'] = '%%v - %%H:%%M';
 $string['strftimerecentfullrelative'] = '%%v - %%H:%%M';
 $string['indentmode'] = 'フォーラムのインデントモード';
 $string['indentfullindent'] = '完全に広げる';
-$string['indentmaxindent'] = '最大インデントレベルまで広げる';
+$string['indentmaxindent'] = '最大まで広げる';
 $string['indentflatindent'] = 'インデントなし';
 $string['indentmodedescription'] = 'このフォーラム内のトピックが、どのようにインデントされるか指定してください。';
 $string['maxindent'] = '最大インデントレベル';
