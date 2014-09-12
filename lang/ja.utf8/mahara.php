32ccbe402410908709c6763dea1d225011a6b9fd
Mitsuhiro Yoshida <mits@mitstek.com>
2014-09-12 13:44:57 +0900

    Updated a translated string for core.

--- lang/ja.utf8/mahara.php
+++ lang/ja.utf8/mahara.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2014-03-16 13:06:21 UTC
+ * @updated    2014-09-12 04:43:42 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -840,6 +840,6 @@ $string['sharenetworksubtitle'] = 'フレンドを探して、グループに参
 $string['findfriendslinked'] = '<a href="%s">フレンド</a>を探す';
 $string['joingroups'] = '<a href="%s">グループ</a>に参加する';
 $string['sharenetworkdescription'] = 'あなたは誰がどのくらいの期間、それぞれのビューにアクセスできるか微調整することができます。';
-$string['howtodisable'] = '* あなたはこのボックスの可視性を<a href="%s">設定</a>ページにてコントロールすることができます。';
+$string['howtodisable'] = 'あなたは情報ボックスを非表示にしました。あなたはこのボックスの可視性を<a href="%s">設定</a>ページにてコントロールすることができます。';
 
 ?>
