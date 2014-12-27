33daadf8be46a620d5ff8ee5051b124657801bcc
Mitsuhiro Yoshida <mits@mitstek.com>
2014-12-28 04:01:18 +0900

    Translated new strings for web WebService.

---
+++ auth/webservice/lang/ja.utf8/auth.webservice.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2014-12-27 19:00:29 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['webservice'] = 'ウェブサービス';
+$string['title'] = 'ウェブサービス';
+$string['description'] = '記述';
+$string['webservicesconfig'] = '設定';
+$string['completeregistration'] = '登録を完了する';
+$string['protocol'] = 'プロトコル';
+$string['rest'] = 'REST';
+$string['soap'] = 'SOAP';
+$string['xmlrpc'] = 'XML-RPC';
+$string['certificates'] = 'ネットワーク証明書';
+$string['servicefunctiongroups'] = 'サービスグループを管理する';
+$string['servicegroups'] = 'サービスグループ';
+$string['component'] = 'コンポーネント';
+$string['functions'] = '関数';
+$string['enableservice'] = 'ウェブサービスを有効/無効にする';
+$string['usersonly'] = '現在のユーザのみ';
+$string['tokensonly'] = '現在のトークンのみ';
+$string['switchtousers'] = 'ユーザにスイッチする';
+$string['switchtotokens'] = 'トークンにスイッチする';
+$string['invalidservice'] = '無効なサービスが選択されました。';
+$string['invalidfunction'] = '無効な関数が選択されました。';
+$string['tokengenerationfailed'] = 'トークンの生成に失敗しました。';
+$string['codingerror'] = 'コーディングエラー';
+$string['missingfuncname'] = '関数名がありません。';
+$string['invalidparamdesc'] = '無効なパラメータ記述です。';
+$string['missingparamdesc'] = 'パラメータ記述がありません。';
+$string['apptokens'] = 'アプリケーション接続';
+$string['servicetokens'] = 'サービスアクセストークンを管理する';
+$string['tokens'] = 'サービスアクセストークン';
+$string['users'] = 'サービスユーザ';
+$string['username'] = 'ユーザ';
+$string['owner'] = 'オーナー';
+$string['servicename'] = 'サービス';
+$string['generate'] = 'トークンを生成する';
+$string['invalidtoken'] = '無効なトークン - トークンが見つかりませんでした。';
+$string['token'] = 'トークン';
+$string['tokenid'] = 'トークン「 %s 」';
+$string['invaliduserselected'] = '無効なユーザが選択されました。';
+$string['invaliduserselectedinstitution'] = 'ユーザ検索から無効なユーザのトークンインスティテューションが選択されました。';
+$string['noservices'] = 'サービスが設定されていません。';
+$string['titlewssigenc'] = 'WSSecurity';
+$string['instructions'] = 'インスティテューション';
+$string['webservicelogs'] = 'ウェブサービスログ';
+$string['errors'] = 'エラーのみ';
+$string['serviceuser'] = 'サービスオーナー';
+$string['serviceusername'] = 'サービスオーナー「 %s 」';
+$string['invalidserviceuser'] = '無効なサービスユーザが選択されました。';
+$string['nouser'] = 'ユーザを選択してください。';
+$string['functionname'] = '関数名';
+$string['classname'] = 'クラス名';
+$string['methodname'] = 'メソッド名';
+$string['webservices'] = 'ウェブサービス';
+$string['webservices_title'] = 'ウェブサービス設定';
+$string['headingusersearchtoken'] = 'ウェブサービストークンユーザ検索';
+$string['headingusersearchuser'] = 'ウェブサービスサービスユーザ検索';
+$string['function'] = '関数';
+$string['method'] = 'メソッド';
+$string['class'] = 'クラス';
+$string['arguments'] = '変数';
+$string['invalidparameter'] = '無効なパラメータ値が検出されました。実行を継続できません。';
+$string['wsdoc'] = 'ウェブサービスドキュメンテーション';
+$string['testclient'] = 'ウェブサービステストクライアント';
+$string['tokenauth'] = 'トークン';
+$string['userauth'] = 'ユーザ';
+$string['authtype'] = '認証タイプ';
+$string['sauthtype'] = '認証タイプ';
+$string['doc'] = 'ドキュメンテーション';
+$string['documentation'] = 'ウェブサービスドキュメンテーション';
+$string['enabledocumentation'] = '開発者ドキュメンテーションを有効にする';
+$string['enableprotocols'] = 'プロトコルを有効にする';
+$string['enablews'] = 'ウェブサービスを有効にする';
+$string['error'] = 'エラー: %s';
+$string['errorcodes'] = 'エラーメッセージ';
+$string['errorinvalidparam'] = 'パラメータ「 %s 」が無効です。';
+$string['errorinvalidparamsapi'] = '無効な外部APIパラメータです。';
+$string['errorinvalidparamsdesc'] = '無効な外部API説明です。';
+$string['errorinvalidresponseapi'] = '無効な外部APIレスポンスです。';
+$string['errorinvalidresponsedesc'] = '無効な外部APIレスポンス説明です。';
+$string['failedtolog'] = 'ログインに失敗しました。';
+$string['information'] = '情報';
+$string['iprestriction'] = 'IP制限';
+$string['key'] = 'キー';
+$string['missingpassword'] = 'パスワードがありません。';
+$string['missingusername'] = 'ユーザ名がありません。';
+$string['notoken'] = 'トークンリストが空です。';
+$string['restcode'] = 'REST';
+$string['restexception'] = 'REST';
+$string['fortokenusers'] = 'ユーザトークンアクセス';
+$string['usertokens'] = 'パーソナルユーザトークン';
+$string['serviceaccess'] = 'サービスアクセス';
+$string['securitykey'] = 'セキュリティキー (トークン)';
+$string['service'] = 'サービス';
+$string['wsdocumentation'] = 'ウェブサービスドキュメンテーション';
+$string['wspassword'] = 'ウェブサービスパスワード';
+$string['wsusername'] = 'ウェブサービスユーザ名';
+$string['accessdenied'] = 'アクセスが拒否されました。';
+$string['accessdeniedforinst'] = 'インスティテューション「 %s 」のアクセスが拒否されました。';
+$string['accessdeniedforinstuser'] = 'インスティテューション「 %s 」 (ユーザ 「 %s 」) のアクセスが拒否されました。';
+$string['accessdeniedforinstgroup'] = 'インスティテューション「 %s 」 (グループ「 %s 」) のアクセスが拒否されました。';
+$string['invalidauthtype'] = '無効な認証タイプ「 %s 」です。';
+$string['invaliduser'] = '無効なユーザ「 %s 」です。';
+$string['invaliduserid'] = '無効なユーザID「 %s 」です。';
+$string['invalidusername'] = '無効なユーザ名「 %s 」です。';
+$string['invalidremoteusername'] = '無効なリモートユーザ名「 %s 」です。';
+
+?>
