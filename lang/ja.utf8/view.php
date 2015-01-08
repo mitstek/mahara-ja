ee0b66d09c281cd682e19312eb570134e29637c6
Mitsuhiro Yoshida <mits@mitstek.com>
2015-01-09 07:02:11 +0900

    Updated a translated string for page based on en.utf8 change.

--- lang/ja.utf8/view.php
+++ lang/ja.utf8/view.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2014-12-27 17:34:21 UTC
+ * @updated    2015-01-08 22:00:59 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
