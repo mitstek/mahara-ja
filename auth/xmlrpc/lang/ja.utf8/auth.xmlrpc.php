67ef5a5a318ef888267cc2213667e8564ac22485
Mitsuhiro Yoshida <mits@mitstek.com>
2015-02-19 13:35:47 +0900

    Translated a new string for auth XML-RPC.

--- auth/xmlrpc/lang/ja.utf8/auth.xmlrpc.php
+++ auth/xmlrpc/lang/ja.utf8/auth.xmlrpc.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2012-09-10 18:41:24 UTC
+ * @updated    2015-02-19 04:34:56 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -18,6 +18,6 @@ $string['description'] = '外部アプリケーションからのSSOで認証す
 $string['notusable'] = 'XML-RPC、CurlおよびOpenSSL PHP拡張モジュールをインストールしてください。';
 $string['networkingdisabledonthissite'] = 'このサイトではネットワーキングが無効にされています。';
 $string['networkservers'] = 'ネットワークサーバ';
-$string['youhaveloggedinfrom'] = 'あなたは<a href="%s">%s</a>　からログインしています。';
+$string['youhaveloggedinfrom1'] = '<a href="%s">%s</a>に戻る';
 
 ?>
