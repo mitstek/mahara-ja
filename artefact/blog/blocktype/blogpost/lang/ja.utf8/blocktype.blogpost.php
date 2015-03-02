8944d170e4acf5e68b878163c78085bd0339ca57
Mitsuhiro Yoshida <mits@mitstek.com>
2015-03-03 07:54:20 +0900

    Translated a string for blocktype blog post.

--- artefact/blog/blocktype/blogpost/lang/ja.utf8/blocktype.blogpost.php
+++ artefact/blog/blocktype/blogpost/lang/ja.utf8/blocktype.blogpost.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-08-21 04:12:25 UTC
+ * @updated    2015-03-02 22:53:23 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -16,5 +16,6 @@ defined('INTERNAL') || die();
 $string['title'] = '日誌エントリ';
 $string['description'] = 'あなたの日誌の単一エントリです (「コンテンツ -> 日誌」をご覧ください)。';
 $string['defaulttitledescription'] = 'ここを空白にした場合、日誌エントリのタイトルが使用されます。';
+$string['choosepublishedblogpostsdescription'] = 'あなたの公開エントリの中から1つ選択してください。';
 
 ?>
