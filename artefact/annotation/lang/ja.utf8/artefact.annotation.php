2eb55ba38999f9b72cc47973005616f543500201
Mitsuhiro Yoshida <mits@mitstek.com>
2015-03-20 05:29:30 +0900

    Translated new strings for Annotation.

---
+++ artefact/annotation/lang/ja.utf8/artefact.annotation.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2015-03-19 20:29:05 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['pluginname'] = 'アノテーション';
+$string['Annotation'] = 'アノテーション';
+$string['Annotations'] = 'アノテーション';
+$string['annotation'] = 'アノテーション';
+$string['annotations'] = 'アノテーション';
+$string['Annotationfeedback'] = 'フィードバック';
+$string['typeannotationfeedback'] = 'アノテーションのフィードバック';
+$string['allowannotationfeedback'] = 'フィードバックを許可する';
+$string['editannotationfeedback'] = 'フィードバックを編集する';
+$string['private'] = 'プライベート';
+$string['public'] = 'パブリック';
+
+?>
