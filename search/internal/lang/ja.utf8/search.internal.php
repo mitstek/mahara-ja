803ca4784fd688d433db80d029df915338404afc
Mitsuhiro Yoshida <mits@mitstek.com>
2015-03-23 02:01:26 +0900

    Translated new string for search internal.

--- search/internal/lang/ja.utf8/search.internal.php
+++ search/internal/lang/ja.utf8/search.internal.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2011-05-26 17:32:39 UTC
+ * @updated    2015-03-22 17:01:03 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,6 +14,6 @@
 defined('INTERNAL') || die();
 
 $string['exactusersearch'] = 'ユーザ厳密検索';
-$string['exactusersearchdescription'] = 'チェックした場合、「ユーザを検索する」ボックスおよび「フレンドを探す」ページの結果には、検索キーワード全体に合致するプロファイルフィールドのユーザのみ表示されます。';
+$string['exactusersearchdescription1'] = 'この設定を有効にした場合、「ユーザを検索する」ボックスおよび「フレンドを探す」ページの結果には、検索キーワード全体に合致するプロファイルフィールドのユーザのみ表示されます。';
 
 ?>
