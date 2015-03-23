dee937b690543d3d56ddadb3d119966bace621dd
Mitsuhiro Yoshida <mits@mitstek.com>
2015-03-24 01:12:38 +0900

    Translated new strings for Annotation.

--- artefact/annotation/lang/ja.utf8/artefact.annotation.php
+++ artefact/annotation/lang/ja.utf8/artefact.annotation.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2015-03-22 16:47:01 UTC
+ * @updated    2015-03-23 16:12:16 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -22,7 +22,10 @@ $string['Annotationfeedback'] = 'フィードバック';
 $string['typeannotationfeedback'] = 'アノテーションのフィードバック';
 $string['allowannotationfeedback'] = 'フィードバックを許可する';
 $string['canteditnotauthor'] = 'あなたはこのフィードバックの投稿者ではありません。';
+$string['cantedittooold'] = 'あなたは %d 分以内のフィードバックのみ編集することができます。';
 $string['cantedithasreplies'] = 'あなたは最も最近のフィードバックのみ編集することができます。';
+$string['annotationfeedbackdeletedauthornotification'] = 'あなたの %s のフィードバックが削除されました: %s';
+$string['annotationfeedbackdeletednotificationsubject'] = '%s のフィードバックが削除されました。';
 $string['annotationfeedbackremoved'] = 'フィードバックが削除されました。';
 $string['annotationfeedbackupdated'] = 'フィードバックが更新されました。';
 $string['commentremovedbyauthor'] = '投稿者によって削除されたフィードバック';
@@ -34,9 +37,11 @@ $string['annotationfeedbacksubmittedprivateanon'] = 'プライベートフィー
 $string['newannotationfeedbacknotificationsubject'] = '%s の新しいフィードバック';
 $string['reallydeletethisannotationfeedback'] = '本当にこのフィードバックを削除してもよろしいですか?';
 $string['annotationfeedbackisprivate'] = 'このフィードバックはプライベートです。';
+$string['invalidcreateannotationfeedback'] = '自分のためのフィードバックは作成できません。';
 $string['nannotationfeedback'] = '%s フィードバック';
 $string['duplicatedannotation'] = '重複アノテーション';
 $string['existingannotation'] = '既存のフィードバック';
+$string['duplicatedannotationfeedback'] = '重複アノテーション';
 $string['existingannotationfeedback'] = '既存のフィードバック';
 $string['private'] = 'プライベート';
 $string['public'] = 'パブリック';
