80bf562d7098288de633c955844f3c70923799b3
Mitsuhiro Yoshida <mits@mitstek.com>
2015-04-21 01:46:14 +0900

    Translated new strings for admin and elasticsearch.

--- lang/ja.utf8/admin.php
+++ lang/ja.utf8/admin.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2014-12-28 21:15:48 UTC
+ * @updated    2015-04-20 16:44:33 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -711,7 +711,6 @@ $string['selectuserexport'] = 'エクスポートするため、コンテンツ�
 $string['selectuserexportdelete'] = 'エクスポートキューから削除するため、コンテンツ「 %s 」を選択する';
 $string['exportpending'] = '%s 現在の保留';
 $string['exportfailed'] = '失敗 %s';
-$string['Archive'] = 'アーカイブ';
 $string['exportqueuedeleted'] = '%s 行が正常に削除されました。';
 $string['exportqueuearchived'] = '%s 行が正常に更新されました。';
 $string['exportdataascsv'] = 'すべてのデータをCSVフォーマットでエクスポートする';
@@ -904,6 +903,7 @@ $string['makeuserinstitutionstaff'] = '初回ログイン時、このメール�
 $string['errorwhileunsuspending'] = '利用停止解除中にエラーが発生しました。';
 $string['institutionsuspended'] = 'インスティテューションが利用停止されました。';
 $string['institutionunsuspended'] = 'インスティテューションの利用停止が解除されました。';
+$string['institutionlogoutusers'] = 'ログアウト %s ユーザ';
 $string['suspendedinstitution'] = '利用停止';
 $string['suspendinstitution'] = 'インスティテューションを利用停止する';
 $string['suspendinstitutiondescription'] = 'ここであなたはインスティテューションを利用停止することができます。利用停止されたインスティテューションのユーザはインスティテューションが利用停止解除されるまで、ログインすることはできません。';
