8c1206d14ad8a9e88c4236b3e7520981d09be13c
Mitsuhiro Yoshida <mits@mitstek.com>
2015-06-29 00:51:17 +0900

    Brushed up translated strings for blocktype textbox.

--- artefact/internal/blocktype/textbox/lang/ja.utf8/blocktype.textbox.php
+++ artefact/internal/blocktype/textbox/lang/ja.utf8/blocktype.textbox.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2014-09-15 09:45:43 UTC
+ * @updated    2015-06-28 15:49:37 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -17,10 +17,10 @@ $string['title1'] = 'ノート';
 $string['description'] = 'あなたのページにテキストボックスを追加します。';
 $string['blockcontent'] = 'ブロックコンテンツ';
 $string['usecontentfromanothertextbox1'] = '別のノートのコンテンツを使用する';
-$string['textusedinotherblocks'] = 'あなたがこのブロックのテキストを編集した場合、テキストが表示されている %s 件の他のブロックも更新されます。';
+$string['textusedinotherblocks'] = 'あなたがこのブロックのテキストを編集した場合、テキストが表示されている他の %s 件のブロックも更新されます。';
 $string['managealltextboxcontent1'] = 'すべてのノートコンテンツを管理する';
 $string['readonlymessage'] = 'あなたが選択したテキストはこのページで編集することはできません。';
 $string['makeacopy'] = 'コピーを作成する';
-$string['textusedinothernotes'] = 'あなたがこのノートのテキストを編集した場合、テキストが表示される %s ブロックでも変更されます。';
+$string['textusedinothernotes'] = 'あなたがこのノートのテキストを編集した場合、テキストが表示されている %s 件のブロックでも更新されます。';
 
 ?>
