f332c73817ae1de1fe24d28bdd34322cd0dace08
Mitsuhiro Yoshida <mits@mitstek.com>
2015-06-29 00:51:40 +0900

    Brushed up a translated string for blocktype textbox.

--- artefact/internal/blocktype/textbox/lang/ja.utf8/blocktype.textbox.php
+++ artefact/internal/blocktype/textbox/lang/ja.utf8/blocktype.textbox.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2011-10-07 05:18:35 UTC
+ * @updated    2015-06-28 15:48:33 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -17,7 +17,7 @@ $string['title'] = 'テキストボックス';
 $string['description'] = 'あなたのページにテキストボックスを追加します。';
 $string['blockcontent'] = 'ブロックコンテンツ';
 $string['usecontentfromanothertextbox'] = '別のテキストボックスのコンテンツを使用する';
-$string['textusedinotherblocks'] = 'あなたがこのブロックのテキストを編集した場合、テキストが表示されている %s 件の他のブロックも更新されます。';
+$string['textusedinotherblocks'] = 'あなたがこのブロックのテキストを編集した場合、テキストが表示されている他の %s 件のブロックも更新されます。';
 $string['managealltextboxcontent'] = 'すべてのテキストボックスコンテンツを管理する';
 $string['readonlymessage'] = 'あなたが選択したテキストはこのページで編集することはできません。';
 $string['makeacopy'] = 'コピーを作成する';
