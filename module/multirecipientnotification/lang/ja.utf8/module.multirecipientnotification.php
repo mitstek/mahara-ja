cb766d8fbe2bee93b804b2133b8f82d01c10196a
Mitsuhiro Yoshida <mits@mitstek.com>
2015-07-25 01:46:01 +0900

    Translated new strings and help files for multi-recipient notifications.

---
+++ module/multirecipientnotification/lang/ja.utf8/module.multirecipientnotification.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2015-07-24 16:43:13 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['sendmessageto'] = 'メッセージを送信する';
+$string['composemessage'] = '作成';
+$string['composemessagedesc'] = '新しいメッセージを作成する';
+$string['titlesubject'] = '件名';
+$string['titlerecipient'] = '受信者';
+$string['replysubjectprefix'] = 'Re:';
+$string['labelrecipients'] = 'To:';
+$string['labelsubject'] = '件名:';
+$string['deletednotifications1'] = '%s 件の通知が削除されました。内部通知は「送信」エリアから削除できないことに留意してください。';
+$string['notification'] = '通知';
+$string['cantsendemptysubject'] = 'あなたの件名は空です。件名を入力してください。';
+$string['cantsendemptytext'] = 'あなたのメッセージは空です。メッセージを入力してください。';
+$string['cantsendnorecipients'] = '少なくとも1名の受信者を選択してください。';
+$string['removeduserfromlist'] = 'あなたからのメッセージを受信できないユーザは受信者リストから削除されました。';
+$string['deleteduser'] = '削除済みユーザ';
+$string['fromuser'] = 'From';
+$string['touser'] = 'To';
+$string['reply'] = '返信';
+$string['replyall'] = 'すべてに返信する';
+$string['linkindicator'] = '»';
+$string['labeloutbox1'] = '送信済み';
+$string['outboxdesc'] = '他のユーザに送信されたメッセージ';
+$string['labelinbox'] = '受信箱';
+$string['inboxdesc1'] = 'システムおよび他のユーザから受信したメッセージ';
+$string['replybuttonplaceholder'] = '...';
+$string['selectallread'] = 'すべての未読通知';
+$string['selectalldelete'] = '削除のためにすべての通知';
+$string['clickformore'] = '(さらに情報を表示するには「Enter」を押してください)';
+
+?>
