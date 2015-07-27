e012a020ef5a0383201d52c5e62b8dd0b131f3b6
Mitsuhiro Yoshida <mits@mitstek.com>
2015-07-27 11:19:15 +0900

    Brushed up translated strings for Persona authentication.

--- auth/browserid/lang/ja.utf8/auth.browserid.php
+++ auth/browserid/lang/ja.utf8/auth.browserid.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2012-09-07 03:33:06 UTC
+ * @updated    2015-07-27 02:15:52 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -17,13 +17,13 @@ $string['browserid'] = 'Persona';
 $string['title'] = 'Persona';
 $string['description'] = 'Personaで認証する';
 $string['notusable'] = 'PHP cURL拡張モジュールをインストールした後、Personaベリファイアへの接続をチェックしてください。';
-$string['badassertion'] = 'Personaアサーション (表明) が有効ではありません: %s';
+$string['badassertion'] = 'Personaアサーションが有効ではありません: %s';
 $string['badverification'] = 'Personaベリファイアの有効なJSONアウトプットをMaharaが受信しませんでした。';
 $string['login'] = 'Persona';
 $string['register'] = 'Persona登録';
-$string['missingassertion'] = 'Personaが英数字アサーション (表明) を戻しませんでした。';
-$string['emailalreadyclaimed'] = 'すでに他のユーザアカウントがメールアドレス「 %s 」を取得しています。';
-$string['emailclaimedasusername'] = 'すでに他のユーザアカウントがメールアドレス「 %s 」をユーザ名として取得しています。';
+$string['missingassertion'] = 'Personaが英数字アサーションを戻しませんでした。';
+$string['emailalreadyclaimed'] = 'すでに別のユーザアカウントがメールアドレス「 %s 」を取得しています。';
+$string['emailclaimedasusername'] = 'すでに別のユーザアカウントがメールアドレス「 %s 」をユーザ名として取得しています。';
 $string['browseridnotenabled'] = 'すべてのアクティブなインスティテューションにおいて、Persona認証プラグインは有効にされていません。';
 $string['emailnotfound'] = 'Personaが有効にされているインスティテューションすべてにおいて、メールアドレス「 %s 」のユーザは見つかりませんでした。';
 
