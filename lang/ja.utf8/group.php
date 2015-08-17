040057a74c048ce2ad5c0a6ef8fb29acc47d754c
Mitsuhiro Yoshida <mits@mitstek.com>
2015-08-17 15:11:31 +0900

    Brushed up a translated string for group.

--- lang/ja.utf8/group.php
+++ lang/ja.utf8/group.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2012-09-10 19:10:09 UTC
+ * @updated    2015-08-17 06:08:05 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -339,7 +339,7 @@ $string['removefromgroup'] = 'グループから削除する';
 $string['userremoved'] = 'ユーザが削除されました。';
 $string['About'] = 'About';
 $string['aboutgroup'] = '%s について';
-$string['Joined'] = '登録日';
+$string['Joined'] = '登録';
 $string['invitemembersdescription'] = 'あなたはユーザのプロファイルページ経由、または<a href="%s">同時に複数の招待状を送信する</a>ことで、ユーザにこのグループへの参加を招待することができます。';
 $string['membersdescription:controlled'] = 'これは「管理メンバーシップ」グループです。あなたはユーザをユーザプロファイルページより追加、または<a href="%s">一度に多くのユーザを追加することができます</a>。';
 $string['submit'] = '送信';
