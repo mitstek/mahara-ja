3e4d0424333dd32a39795972a1ea4673e67c2259
Mitsuhiro Yoshida <mits@mitstek.com>
2015-08-30 22:54:34 +0900

    Brushed up translated strings for blocktype External feed.

--- blocktype/externalfeed/lang/ja.utf8/blocktype.externalfeed.php
+++ blocktype/externalfeed/lang/ja.utf8/blocktype.externalfeed.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-05-02 22:38:25 UTC
+ * @updated    2015-08-30 13:48:55 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -40,10 +40,10 @@ $string['insecuresslmode'] = '安全ではないSSLモード';
 $string['insecuresslmodedesc'] = 'SSL証明書認証を無効にします。これはお勧めできる設定ではありませんが、無効または信頼できない証明書を使ってフィードが提供されている場合に必要であると思われます。';
 $string['itemstoshow'] = '表示するアイテム数';
 $string['itemstoshowdescription'] = '範囲: 1～20';
-$string['showfeeditemsinfull'] = 'フィードアイテムを完全に表示しますか?';
+$string['showfeeditemsinfull'] = 'フィードアイテムを完全に表示する';
 $string['showfeeditemsinfulldesc'] = 'ここではフィードアイテムの要約を表示するか、それぞれのフィードアイテムの詳細を表示するか選択してください。';
 $string['invalidurl'] = '指定されたURLが有効ではありません。あなたはhttpおよびhttpsのURLからのみフィードを表示することができます。';
-$string['invalidfeed'] = 'フィードが有効ではないようです。レポートされたエラーは次のとおりです: %s';
+$string['invalidfeed'] = 'フィードが有効ではないようです。報告されたエラーは次のとおりです: %s';
 $string['lastupdatedon'] = '最終更新日時: %s';
 $string['defaulttitledescription'] = 'ここを空白にした場合、フィードのタイトルが使用されます。';
 $string['reenterpassword'] = 'あなたはフィードのURLを変更しました。パスワードを再入力 (または削除) してください。';
