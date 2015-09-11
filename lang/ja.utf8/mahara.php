2256bbf4278cfe9c86a22ef34db222866b96b973
Mitsuhiro Yoshida <mits@mitstek.com>
2015-09-12 00:37:34 +0900

    Updated translated strings core.

--- lang/ja.utf8/mahara.php
+++ lang/ja.utf8/mahara.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2014-09-12 04:43:49 UTC
+ * @updated    2015-09-11 15:31:10 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -55,8 +55,8 @@ $string['no'] = 'No';
 $string['yes'] = 'Yes';
 $string['none'] = 'なし';
 $string['at'] = '@';
-$string['From'] = '開始日';
-$string['To'] = '終了日';
+$string['From'] = 'From';
+$string['To'] = 'To';
 $string['All'] = 'すべて';
 $string['enable'] = '有効にする';
 $string['disable'] = '無効にする';
