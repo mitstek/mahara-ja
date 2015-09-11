8fbc62538a4974e93c02a0191684e58891d8d2cf
Mitsuhiro Yoshida <mits@mitstek.com>
2015-09-12 00:35:25 +0900

    Updated translated strings core.

--- lang/ja.utf8/mahara.php
+++ lang/ja.utf8/mahara.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2015-05-14 05:09:28 UTC
+ * @updated    2015-09-11 15:33:34 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -80,8 +80,8 @@ $string['selected'] = '選択済み';
 $string['admin'] = '管理';
 $string['menu'] = 'メニュー';
 $string['at'] = '@';
-$string['From'] = '開始日';
-$string['To'] = '終了日';
+$string['From'] = 'From';
+$string['To'] = 'To';
 $string['ascending'] = '昇順';
 $string['descending'] = '降順';
 $string['sortorder'] = 'ファイルの並び順';
@@ -1019,7 +1019,6 @@ $string['usersdashboard'] = '%s のダッシュボード';
 $string['dashboarddescription'] = 'ダッシュボードページはあなたがログインして最初にホームページ上で閲覧することになるページです。このページはあなたのみアクセスすることができます。';
 $string['topicsimfollowing'] = '私がフォローしているトピック';
 $string['inboxblocktitle'] = '受信箱';
-$string['watchlistblocktitle'] = '私がフォローしているページ';
 $string['mymessages'] = 'マイメッセージ';
 $string['pleasedonotreplytothismessage'] = 'このメッセージに返信しないでください。';
 $string['deleteduser'] = '削除済みユーザ';
