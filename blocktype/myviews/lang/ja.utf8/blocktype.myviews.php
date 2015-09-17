fb76a4fcfa099b97813cf81829781c14dc648fec
Mitsuhiro Yoshida <mits@mitstek.com>
2015-09-18 06:20:27 +0900

    Translated new strings for blocktype My portfolios.

--- blocktype/myviews/lang/ja.utf8/blocktype.myviews.php
+++ blocktype/myviews/lang/ja.utf8/blocktype.myviews.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2015-09-17 19:29:46 UTC
+ * @updated    2015-09-17 21:19:26 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -13,8 +13,8 @@
 
 defined('INTERNAL') || die();
 
-$string['title'] = 'マイページ';
-$string['otherusertitle'] = '%s のページ';
-$string['description'] = 'あなたのプロファイルを閲覧している人に対して、あなたの閲覧可能なページすべてを表示します。';
+$string['title1'] = 'マイポートフォリオ';
+$string['otherusertitle1'] = '%s のポートフォリオ';
+$string['description1'] = 'あなたのプロファイルを閲覧している人に対して、あなたの閲覧可能なポートフォリオすべてを表示します。';
 
 ?>
