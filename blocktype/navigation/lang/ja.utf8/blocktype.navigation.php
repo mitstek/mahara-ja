dd222f94ec07d4c760e0af5b785ee7aabaaa415c
Mitsuhiro Yoshida <mits@mitstek.com>
2015-09-21 01:19:09 +0900

    Brushed up a translated string for blocktype Navigation.

--- blocktype/navigation/lang/ja.utf8/blocktype.navigation.php
+++ blocktype/navigation/lang/ja.utf8/blocktype.navigation.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-08-21 04:18:50 UTC
+ * @updated    2015-09-20 16:12:53 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -16,7 +16,7 @@ defined('INTERNAL') || die();
 $string['defaulttitledescription'] = 'ここを空白にした場合、コレクションのタイトルが使用されます。';
 $string['collection'] = 'コレクション';
 $string['title'] = 'ナビゲーション';
-$string['description'] = 'ページのコレクションをシンプルなナビゲーションとして表示します (「ポートフォリオ -> コレクション」、グループ内の場合はコレクションタブをご覧ください)。';
+$string['description'] = 'ページコレクションのナビゲーションです (「ポートフォリオ -> コレクション」、グループ内の場合はコレクションタブをご覧ください)。';
 $string['nocollections1'] = 'コレクションはありません。<a href="%s">コレクションを作成してください</a>。';
 
 ?>
