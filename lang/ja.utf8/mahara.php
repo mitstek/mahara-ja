992c00b878ff065fdcb537329c560433143bbf4d
Mitsuhiro Yoshida <mits@mitstek.com>
2015-09-28 00:49:09 +0900

    Brushed up a translated string for blocktype recent forum posts.

--- lang/ja.utf8/mahara.php
+++ lang/ja.utf8/mahara.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2015-09-11 15:33:34 UTC
+ * @updated    2015-09-23 21:40:18 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
