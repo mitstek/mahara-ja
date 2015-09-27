adb6f14c4dfd4da469d80456c15232ffece5b35c
Mitsuhiro Yoshida <mits@mitstek.com>
2015-09-28 00:56:09 +0900

    Brushed up a translated string for blocktype recent forum posts.

--- blocktype/recentforumposts/lang/ja.utf8/blocktype.recentforumposts.php
+++ blocktype/recentforumposts/lang/ja.utf8/blocktype.recentforumposts.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2015-09-27 15:46:40 UTC
+ * @updated    2015-09-27 15:52:58 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -13,8 +13,8 @@
 
 defined('INTERNAL') || die();
 
-$string['title'] = 'フォーラムの最近の投稿';
-$string['description'] = 'グループフォーラムの最近の投稿を表示します。';
+$string['title'] = '最近のフォーラム投稿';
+$string['description'] = 'グループの最近のフォーラム投稿を表示します。';
 $string['group'] = 'グループ';
 $string['nogroupstochoosefrom'] = '申し訳ございません、選択できるグループがありません。';
 $string['poststoshow'] = '表示する最大投稿数';
