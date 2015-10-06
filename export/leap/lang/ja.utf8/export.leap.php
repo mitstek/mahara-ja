b8e32a9584032353248c4d131e51cd38b1fe3a30
Mitsuhiro Yoshida <mits@mitstek.com>
2015-10-06 19:57:20 +0900

    Brushed up a translated string for Leap2A export.

--- export/leap/lang/ja.utf8/export.leap.php
+++ export/leap/lang/ja.utf8/export.leap.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-01-18 05:39:08 UTC
+ * @updated    2015-10-06 10:53:58 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -29,6 +29,6 @@
 defined('INTERNAL') || die();
 
 $string['title'] = 'Leap2A';
-$string['description'] = '<a href="http://wiki.leapspecs.org/2A/specification">Leap2Aスタンダードフォーマット</a>のエクスポートを作成します。あなたは後で、このデータを<a href="http://wiki.mahara.org/Developer_Area/Import//Export/Interoperability">他のLeap2A互換システム</a>にインポートすることができます。エクスポートは人間に読むことが難しいフォーマットで作成されます。';
+$string['description'] = 'ここでは<a href="http://wiki.leapspecs.org/2A/specification">Leap2A標準フォーマット</a>のエクスポートを作成します。エクスポートは人間にとって読むことが難しいフォーマットで作成されますが、このエクスポートを使用することにより、あなたのデータを後で<a href="https://wiki.mahara.org/wiki/Developer_Area/Import//Export/Interoperability">他のLeap2A互換システム</a>にインポートすることができます。';
 
 ?>
