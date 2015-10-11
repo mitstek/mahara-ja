001d4cd47c33cbeb3ce7f554fae0b76133a6c962
Mitsuhiro Yoshida <mits@mitstek.com>
2015-10-12 01:50:05 +0900

    Brushed up translated strings.

--- lang/ja.utf8/account.php
+++ lang/ja.utf8/account.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2012-06-10 14:23:01 UTC
+ * @updated    2015-10-11 16:22:50 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -28,14 +28,14 @@
 
 defined('INTERNAL') || die();
 
-$string['changepassworddesc'] = 'あなたのパスワードを変更したい場合、ここで詳細を入力してください。';
+$string['changepassworddesc'] = '新しいパスワード';
 $string['changepasswordotherinterface'] = 'あなたは異なるインターフェースで<a href="%s">パスワードを変更する</a>ことができます。';
 $string['oldpasswordincorrect'] = 'これはあなたの現在のパスワードではありません。';
 $string['changeusernameheading'] = 'ユーザ名を変更する';
 $string['changeusername'] = '新しいユーザ名';
-$string['changeusernamedesc'] = 'あなたが %s へのログインに使用するユーザ名です。ユーザ名の長さは半角3-30文字にしてください。ユーザ名には空白を除く、半角英数字および標準的な記号を使用することができます。';
+$string['changeusernamedesc'] = 'あなたが %s へのログインに使用するユーザ名です。ユーザ名の長さは半角3-30文字にしてください。ユーザ名には空白を除く半角英数字および標準的な記号を使用することができます。';
 $string['usernameexists'] = 'このユーザ名は取得されています。別のユーザ名を選択してください。';
-$string['accountoptionsdesc'] = 'あなたはここで一般アカウントオプションを設定することができます。';
+$string['accountoptionsdesc'] = '一般アカウントオプション';
 $string['friendsnobody'] = 'だれも私をフレンドとして追加できません。';
 $string['friendsauth'] = '新しいフレンドになるには私の承認が必要です。';
 $string['friendsauto'] = '新しいフレンドは自動的に承認されます。';
@@ -55,17 +55,17 @@ $string['showviewcolumns'] = 'ページの編集時、カラムの「追加」�
 $string['tagssideblockmaxtags'] = 'クラウド内の最大タグ数';
 $string['tagssideblockmaxtagsdescription'] = 'あなたのタグクラウドに表示されるタグの最大数です。';
 $string['enablemultipleblogs'] = '複数日誌を有効にする';
-$string['enablemultipleblogsdescription'] = 'デフォルトでは、あなたは1つの日誌のみMaharaに作成することができます。あなたが1つ以上の日誌を作成したい場合、このオプションをチェックしてください。';
+$string['enablemultipleblogsdescription'] = 'デフォルトではあなたは1つの日誌のみMaharaに作成することができます。あなたが1つ以上の日誌を作成したい場合、このオプションをチェックしてください。';
 $string['disablemultipleblogserror'] = '1つの日誌のみ作成していない限り、あなたは複数日誌を無効にすることはできません。';
 $string['hiderealname'] = '実名を隠す';
-$string['hiderealnamedescription'] = 'あなたがニックネームを設定した場合、そして、あなたの実名検索により、他のユーザから自分を探されたくない場合、このチェックボックスをチェックしてください。';
+$string['hiderealnamedescription'] = 'あなたがニックネームを設定した場合、そして、あなたの実名検索により他のユーザから自分を探されたくない場合、このチェックボックスをチェックしてください。';
 $string['showhomeinfo'] = 'ホームページにMaharaに関する情報を表示する';
 $string['mobileuploadtoken'] = 'モバイルアップロードトークン';
 $string['mobileuploadtokendescription'] = 'アップロードを有効にするため、ここおよびあなたの携帯電話にトークンを入力してください (注意: それぞれのアップロード終了、自動的に変更されます)。<br />問題が発生した場合 - ここおよびあなたの携帯電話の入力内容をリセットしてください。';
 $string['prefssaved'] = 'プリファレンスが保存されました。';
 $string['prefsnotsaved'] = 'あなたのプリファレンスの保存に失敗しました!';
 $string['maildisabled'] = 'Eメールが無効にされました。';
-$string['maildisabledbounce'] = '多くのメッセージがサーバに戻ってきたため、あなたのメールアドレス宛のメール送信は停止されました。アカウントプリファレンスの %s にて、Eメールを有効にする前に、あなたのEメールアカウントが正常に動作しているか確認してください。';
+$string['maildisabledbounce'] = '多くのメッセージがサーバに戻ってきたため、あなたのメールアドレス宛のメール送信は停止されました。Eメールを有効にする前に、あなたのEメールアカウントが正常に動作しているかアカウントプリファレンスの %s で確認してください。';
 $string['maildisableddescription'] = 'あなたのアカウント宛のメール送信は停止されました。アカウントプリファレンスページにて、<a href="%s">あなたのEメールを再度有効</a>にすることができます。';
 $string['deleteaccount'] = 'アカウントを削除する';
 $string['deleteaccountdescription'] = 'アカウントを削除した場合、今後、あなたのプロファイル情報およびページを他のユーザが閲覧できないようになります。あなたが投稿したフォーラム投稿のコンテンツは閲覧することができますが、投稿者名が表示されないようになります。';
