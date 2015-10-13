ecd208be1f4d3e10f1197c2237c751734dd09371
Mitsuhiro Yoshida <mits@mitstek.com>
2015-10-14 00:26:10 +0900

    Updated a translated string for Google Apps based on en change.

--- blocktype/googleapps/lang/ja.utf8/blocktype.googleapps.php
+++ blocktype/googleapps/lang/ja.utf8/blocktype.googleapps.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2014-08-18 00:20:14 UTC
+ * @updated    2015-10-13 15:25:56 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -16,7 +16,9 @@ defined('INTERNAL') || die();
 $string['title'] = 'Google Apps';
 $string['description'] = 'Googleカレンダーおよびドキュメントを埋め込みます。';
 $string['appscodeorurl'] = 'コードまたはURLを埋め込む';
-$string['appscodeorurldesc'] = 'Google Appsを外部から閲覧できるページの埋め込みコードまたはURLを貼り付けてください。';
+$string['appscodeorurldesc'] = 'Googleコンテンツ埋め込みのインストラクションに関して<a target="_blank" href="http://manual.mahara.org/en/15.10/blocks/external.html#google-apps">ユーザマニュアル</a>をご覧ください。
+<br />
+Google Appsを外部から閲覧できるページの埋め込みコードまたはURLを貼り付けてください。';
 $string['height'] = '高さ';
 $string['badurlerror'] = '埋め込みコードまたはURLを構文解析できません: %s';
 
