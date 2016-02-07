64cbdf6a41f7dab39b82b3ca2a4777564b20e089
Mitsuhiro Yoshida <mits@mitstek.com>
2016-02-07 22:16:44 +0900

    Brsuehd up translated string for import.

--- lang/ja.utf8/import.php
+++ lang/ja.utf8/import.php
@@ -14,13 +14,13 @@
 defined('INTERNAL') || die();
 
 $string['Import'] = 'インポート';
-$string['importexceedquota'] = 'あなたがアップロードしたLeap2Aファイルの新しいファイル合計サイズがあなたのクオータを超えるようです。いくつかのファイルを削除して、あなたは再度試みる必要があります。';
+$string['importexceedquota'] = 'あなたがアップロードしたLeap2Aファイルの新しいファイル合計サイズがあなたのクオータを超えます。いくつかのファイルを削除して、あなたは再度試みる必要があります。';
 $string['importsuccessfully'] = 'あなたのポートフォリオは正常にインポートされました。';
 $string['importfailed'] = '<p><strong>申し訳ございません - あなたのLeap2Aファイルのインポートに失敗しました。</strong></p>
 
 <p>あなたが有効なLeap2Aファイルをアップロードしていない、またはこのバージョンのMaharaではあなたのLeap2Aファイルのバージョンをサポートされていないことが考えられます。仮にLeap2Aが有効であったとしても、インポートを失敗させるバグがMaharaに含まれている可能性もあります。</p>
 
-<p>戻って、再度お試しください。問題が持続する場合、サポートを求めるため、<a href="https://mahara.org/forums/">Maharaフォーラム</a>に投稿しても良いでしょう。あなたのファイルのコピーを準備してください。</p>';
+<p>戻って、再度お試しください。問題が持続する場合、サポートを求めるため、<a href="https://mahara.org/forums/">Maharaフォーラム</a>に投稿してください。あなたのファイルのコピーを準備してください。</p>';
 $string['importwitherrors'] = 'あなたのポートフォリオはインポートされましたがエラーを含みます。';
 $string['importartefactplugindata'] = 'アーティファクトプラグインデータのインポート';
 $string['importartefacts'] = 'アーティファクトのインポート';
@@ -28,7 +28,7 @@ $string['importartefactsprogress'] = 'アーティファクトのインポート
 $string['importfooter'] = 'フッタのインポート';
 $string['importviews'] = 'ページのインポート';
 $string['importviewsprogress'] = 'ページのインポート: %s/%s';
-$string['importportfoliodescription'] = 'Leap2Aファイルがある場合、ここで別のサイトからあなたのポートフォリオにコンテンツをインポートすることができます。サイト設定、またはあなたがグループ内にアップロードまたは作成したコンテンツはインポートすることができません。';
+$string['importportfoliodescription'] = 'Leap2Aファイルがある場合、ここで別のサイトからあなたのポートフォリオにコンテンツをインポートすることができます。サイト設定またはあなたがグループ内にアップロードまたは作成したコンテンツはインポートすることができません。';
 $string['importyourportfolio'] = 'あなたのポートフォリオをインポートする';
 $string['howimportyourportfolio'] = 'あなたのポートフォリオアイテムのインポート方法を選択する';
 $string['howimportportfoliodescription'] = 'このステップではあなたの既存のコンテンツとインポートコンテンツをどのように統合するか決定します。';
@@ -37,7 +37,7 @@ $string['noleapimportpluginsenabled'] = 'サイト管理者はLeap2Aインポー
 $string['entry'] = 'エントリ';
 $string['entries'] = 'エントリ';
 $string['importentry'] = 'エントリをインポートする';
-$string['duplicateditem'] = '重複アイテム';
+$string['duplicateditem'] = '複製アイテム';
 $string['existingitem'] = '既存のアイテム';
 $string['howtoimport'] = 'インポート方法';
 $string['ignore'] = '無視する';
