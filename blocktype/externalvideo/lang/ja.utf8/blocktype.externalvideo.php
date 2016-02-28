469f397ea681d7849e16c56ec02b11f653f89474
Mitsuhiro Yoshida <mits@mitstek.com>
2016-02-29 01:24:33 +0900

    Brushed up translated strings for external video.

--- blocktype/externalvideo/lang/ja.utf8/blocktype.externalvideo.php
+++ blocktype/externalvideo/lang/ja.utf8/blocktype.externalvideo.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2012-04-26 14:24:48 UTC
+ * @updated    2016-02-28 16:17:29 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -13,12 +13,12 @@
 
 defined('INTERNAL') || die();
 
-$string['title'] = '外部ビデオ';
-$string['description'] = '外部ビデオを埋め込みます。';
+$string['title'] = '外部メディア';
+$string['description'] = '外部コンテンツを埋め込みます。';
 $string['urlorembedcode'] = 'URLまたは埋め込みコード';
 $string['videourldescription3'] = 'コンテンツが設置されているページの<strong>埋め込みコード</strong>または<strong>URL</strong>を貼り付けてください。';
-$string['validiframesites'] = '&lt;iframe&gt;タグを含む以下のサイトの<strong>埋め込みコード</strong>が許可されます:';
-$string['validurlsites'] = '以下のサイトの<strong>URL</strong>が許可されます:';
+$string['validiframesites'] = '&lt;iframe&gt;タグを含む次のサイトの<strong>埋め込みコード</strong>が許可されます:';
+$string['validurlsites'] = '次のサイトの<strong>URL</strong>が許可されます:';
 $string['width'] = '幅';
 $string['height'] = '高さ';
 $string['widthheightdescription'] = '「幅」および「高さ」フィールドはURLのみに使用されます。上記で「embed」または「iframe」タグを入力している場合、あなたはコード内で幅および高さを更新する必要があります。';
