621a5136db931de36a1c4d42c9cfe820986f9969
Mitsuhiro Yoshida <mits@mitstek.com>
2016-03-06 23:58:25 +0900

    Brushed up translated strings for core.

--- lang/ja.utf8/mahara.php
+++ lang/ja.utf8/mahara.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2016-02-28 15:46:42 UTC
+ * @updated    2016-03-06 14:58:18 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
