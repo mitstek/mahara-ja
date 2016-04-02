cb08e5b16016f2ffb55a91b963d3981eb8d7f4ec
Mitsuhiro Yoshida <mits@mitstek.com>
2016-04-03 06:37:12 +0900

    Translated new strings for blocktype Open Badges.

---
+++ blocktype/openbadgedisplayer/lang/ja.utf8/blocktype.openbadgedisplayer.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2016-04-02 21:36:43 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'Open Badges';
+$string['description'] = 'あなたのOpen Badgesを表示する';
+$string['issuerdetails'] = '発行者詳細';
+$string['badgedetails'] = 'バッジ詳細';
+$string['issuancedetails'] = '発行詳細';
+$string['name'] = '名称';
+$string['url'] = 'URL';
+$string['organization'] = '組織';
+$string['evidence'] = 'エビデンス';
+$string['issuedon'] = '発効日';
+$string['expires'] = '有効期限';
+$string['desc'] = '説明';
+$string['criteria'] = 'クライテリア';
+$string['nbadges'] = '%s バッジ';
+$string['nobackpack'] = 'Backpackが見つかりませんでした。<br />あなたの<a href="%s">プロファイル</a>に<a href="%s">Backpack</a>のメールアドレスを入力してください。';
+$string['nobadgegroups'] = 'パブリックバッジコレクション/バッジは見つかりませんでした。';
+$string['nobackpackidin'] = 'あなたのメールアドレスはサービス「 %s 」内に見つかりませんでした。';
+$string['nobadgegroupsin'] = '次のサービス内にパブリックバッジコレクション/バッジは見つかりませんでした: %s';
+$string['confighelp'] = 'このブロックで表示するバッジコレクションを選択してください。<br/>あなたのコレクションおよびバッジを管理するには次のサービスにアクセスしてください:<br />%s';
+$string['obppublicbadges'] = 'Open Badge Passport内すべてのパブリックバッジ';
+$string['title_backpack'] = 'Mozilla Backpack';
+$string['title_passport'] = 'Open Badge Passport';
+$string['fetchingbadges'] = 'エントリを取得しています。しばらくお待ちください。';
+
+?>
