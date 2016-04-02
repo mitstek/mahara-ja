811de9b395755a34587564cabc79e1e24dbe3923
Mitsuhiro Yoshida <mits@mitstek.com>
2016-04-03 05:25:58 +0900

    Translated a string forEmbedded media.

--- artefact/file/blocktype/internalmedia/lang/ja.utf8/blocktype.internalmedia.php
+++ artefact/file/blocktype/internalmedia/lang/ja.utf8/blocktype.internalmedia.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2015-04-16 21:51:25 UTC
+ * @updated    2016-04-02 20:25:45 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -19,5 +19,6 @@ $string['media'] = 'メディア';
 $string['flashanimation'] = 'Flashアニメーション';
 $string['typeremoved'] = 'このブロックでは管理者によって許可されないメディアタイプを示します。';
 $string['configdesc1'] = 'どのタイプのファイルをユーザがこのブロックに埋め込むことができるか設定します。あなたがすでにブロックで使用しているファイルタイプを無効にした場合、表示されないようになります。';
+$string['browsercannotplay'] = 'あなたのブラウザはこのメディアを再生できません。';
 
 ?>
