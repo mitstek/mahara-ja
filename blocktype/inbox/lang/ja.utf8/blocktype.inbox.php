522503a1b85d6496df6a0ba584a9c71b74db01f4
Mitsuhiro Yoshida <mits@mitstek.com>
2016-04-05 17:00:36 +0900

    Brushed up a translated string for blocktype inbox.

--- blocktype/inbox/lang/ja.utf8/blocktype.inbox.php
+++ blocktype/inbox/lang/ja.utf8/blocktype.inbox.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2012-09-07 03:40:29 UTC
+ * @updated    2016-04-05 08:00:08 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -19,7 +19,7 @@ $string['messagetypes'] = '表示するメッセージタイプ';
 $string['maxitems'] = '表示するアイテムの最大数';
 $string['maxitemsdescription'] = '設定範囲: 1～100';
 $string['More'] = 'さらに';
-$string['nomessages'] = 'メッセージなし';
+$string['nomessages'] = 'メッセージはありません。';
 $string['defaulttitledescription'] = 'タイトルフィールドを空白にした場合、デフォルトタイトルが生成されます。';
 
 ?>
