8e814ffd7930daee17954e83c80e3f7fad7d479b
Mitsuhiro Yoshida <mits@mitstek.com>
2016-04-26 01:35:18 +0900

    Brushed up a translated string for core.

--- lang/ja.utf8/mahara.php
+++ lang/ja.utf8/mahara.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2016-04-18 00:12:39 UTC
+ * @updated    2016-04-25 16:28:45 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -450,7 +450,7 @@ $string['forgotusernamepasswordemailmessagehtml'] = '<p>%s さん</p>
 
 <p>あなたの %s のアカウントに関するユーザ名/パスワードのリクエストを受け付けました。</p>
 
-<p>あなたのユーザ名は<strong>%s</strong> です。</p>
+<p>あなたのユーザ名は <strong>%s</strong> です。</p>
 
 <p>あなたのパスワードをリセットしたい場合、下記のリンクをクリックしてください:</p>
 
