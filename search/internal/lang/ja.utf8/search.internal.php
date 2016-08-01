8b607a44dcbbe30be53523e22d8a9e3b1cdff090
Mitsuhiro Yoshida <mits@mitstek.com>
2016-08-02 02:33:33 +0900

    Bug 1604601: Translated new strings for internal search.

--- search/internal/lang/ja.utf8/search.internal.php
+++ search/internal/lang/ja.utf8/search.internal.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2015-03-22 17:01:03 UTC
+ * @updated    2016-08-01 17:33:07 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,6 +14,6 @@
 defined('INTERNAL') || die();
 
 $string['exactusersearch'] = 'ユーザ厳密検索';
-$string['exactusersearchdescription1'] = 'この設定を有効にした場合、「ユーザを検索する」ボックスおよび「フレンドを探す」ページの結果には検索キーワード全体に合致するプロファイルフィールドのユーザのみ表示されます。';
+$string['exactusersearchdescription2'] = '「ユーザを検索する」ボックスおよび「フレンドを探す」ページの結果に検索キーワード全体に合致するプロファイルフィールドのユーザのみ表示されます。';
 
 ?>
