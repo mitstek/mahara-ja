7286f1227469357617b98d9da74d4e9b2c53eb93
Mitsuhiro Yoshida <mits@mitstek.com>
2016-08-09 01:44:28 +0900

    Brushed up a translated string for Journal block.

--- artefact/blog/blocktype/blog/lang/ja.utf8/blocktype.blog.php
+++ artefact/blog/blocktype/blog/lang/ja.utf8/blocktype.blog.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-08-21 04:13:01 UTC
+ * @updated    2016-08-08 16:43:23 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -16,6 +16,6 @@ defined('INTERNAL') || die();
 $string['title'] = '日誌';
 $string['description'] = '日誌全体です (「コンテンツ -> 日誌」をご覧ください)。';
 $string['defaulttitledescription'] = 'ここを空白にした場合、日誌のタイトルが使用されます。';
-$string['postsperpage'] = '1ページあたりの記事数';
+$string['postsperpage'] = '1ページあたりのエントリ数';
 
 ?>
