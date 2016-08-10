5e9d8ddc217ad41623c0ff5f6af72489f29ca845
Mitsuhiro Yoshida <mits@mitstek.com>
2016-08-11 01:46:36 +0900

    Brushed up translated strings for Recent journal entries block.

--- artefact/blog/blocktype/recentposts/lang/ja.utf8/blocktype.recentposts.php
+++ artefact/blog/blocktype/recentposts/lang/ja.utf8/blocktype.recentposts.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-08-21 04:14:57 UTC
+ * @updated    2016-08-10 16:45:11 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -13,8 +13,8 @@
 
 defined('INTERNAL') || die();
 
-$string['title'] = '最新の日誌エントリ';
-$string['description'] = '最新の日誌エントリを表示します (「コンテンツ -> 日誌」をご覧ください)。';
+$string['title'] = '最近の日誌エントリ';
+$string['description'] = '最近の日誌エントリを表示します (「コンテンツ -> 日誌」をご覧ください)。';
 $string['itemstoshow'] = '表示するアイテム数';
 $string['postedin'] = '-';
 $string['postedon'] = '投稿日時';
