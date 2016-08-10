94beab0df783fbec2b0fb0a53751415f50ba4ebb
Mitsuhiro Yoshida <mits@mitstek.com>
2016-08-11 01:12:35 +0900

    Brushed up a translated string for Journal entry block.

--- artefact/blog/blocktype/blogpost/lang/ja.utf8/blocktype.blogpost.php
+++ artefact/blog/blocktype/blogpost/lang/ja.utf8/blocktype.blogpost.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2015-01-12 20:38:21 UTC
+ * @updated    2016-08-10 16:12:25 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -16,6 +16,6 @@ defined('INTERNAL') || die();
 $string['title'] = '日誌エントリ';
 $string['description'] = 'あなたの日誌の単一エントリです (「コンテンツ -> 日誌」をご覧ください)。';
 $string['defaulttitledescription'] = 'ここを空白にした場合、日誌エントリのタイトルが使用されます。';
-$string['choosepublishedblogpostsdescription'] = 'あなたの公開エントリの中から1つ選択してください。';
+$string['choosepublishedblogpostsdescription'] = 'あなたの公開済み日誌エントリの中から1つ選択してください。';
 
 ?>
