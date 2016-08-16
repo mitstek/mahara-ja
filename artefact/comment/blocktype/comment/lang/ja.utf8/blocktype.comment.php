2f1c0bb9bb0c91c5502ff662b5725c24da2e29bc
Mitsuhiro Yoshida <mits@mitstek.com>
2016-08-16 20:55:46 +0900

    Brushed up a translated string for Comments block.

--- artefact/comment/blocktype/comment/lang/ja.utf8/blocktype.comment.php
+++ artefact/comment/blocktype/comment/lang/ja.utf8/blocktype.comment.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2015-09-24 16:23:25 UTC
+ * @updated    2016-08-16 11:55:19 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,7 +14,7 @@
 defined('INTERNAL') || die();
 
 $string['title'] = 'コメント';
-$string['description'] = 'コメントを表示するブロック';
+$string['description'] = 'コメントを表示するブロックです。';
 $string['ineditordescription1'] = 'このページのコメントはページ下部ではなく、ここに表示されます。';
 
 ?>
