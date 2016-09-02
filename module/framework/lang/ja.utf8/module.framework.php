97c9de86f98655190a73aba956d04845f9890336
Mitsuhiro Yoshida <mits@mitstek.com>
2016-09-03 02:15:54 +0900

    Bug 1409546: Translated new strings for SmartEvidence framework.

---
+++ module/framework/lang/ja.utf8/module.framework.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2016-09-02 17:15:20 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['Framework'] = 'スマートエビデンスフレームワーク';
+$string['frameworknav'] = 'スマートエビデンス';
+$string['frameworks'] = 'インストール済みスマートエビデンス';
+$string['frameworkdesc'] = 'あなたのポートフォリオと関連付けたいコンピテンシーフレームワークを選択してください。';
+$string['frameworksdesc'] = 'システムにインストールされているフレームワークの一覧です。有効時にコレクションと関連付けられていない限り、無効にされたフレームワークは表示されません。';
+$string['taskscompleted'] = 'タスク完了';
+$string['addpages'] = 'あなたがスマートエビデンスマップに表示させたい場合、さらにこのコレクションにページを追加することができます。';
+$string['addframework'] = 'フレームワークを追加する';
+$string['frameworkupdated'] = 'フレームワークが更新されました。';
+$string['matrixfile'] = 'マトリックスファイル';
+$string['matrixfiledesc'] = 'JSONにエンコードされたフレームワークを含む「.matrix」ファイルです。';
+$string['notvalidmatrixfile'] = 'これは有効な「.matrix」ファイルではありません。';
+$string['invalidjson'] = 'これは有効な「.matrix」ファイルではありません。JSONコンテンツを構文解析できません。';
+$string['jsonmissingvars'] = 'これは有効な「.matrix」ファイルではありません。「フレームワーク」または「フレームワーク名」がありません。';
+$string['manuallyremovematrices'] = 'インストールディレクトリ「 %s 」を削除することができません。手動で削除してください。';
+$string['changeframeworkproblems'] = 'あなたはフレームワークを変更できません。次のページにはこのフレームワークと関連付けられたエビデンスがあります:';
+$string['accessdeniednoframework'] = 'このコレクションはスマートエビデンスページを表示できません。これは次の理由が考えられます: スマートエビデンスプラグインがインストールまたは有効にされていない。あなたが属しているインスティテューションではスマートエビデンスが許可されていない。このコレクションではスマートエビデンスフレームワークが設定されていない。このコレクションにページが存在しない。';
+$string['noframeworkselected'] = 'なし';
+$string['matrixpointupdated'] = 'スマートエビデンスが更新されました。';
+$string['matrixpointinserted'] = 'スマートエビデンスが追加されました。';
+$string['standard'] = '標準';
+$string['standarddesc'] = 'このエビデンスに該当する標準を選択してください。あなたは標準を探すためにボックスに入力することができます。';
+$string['annotationclash'] = 'この標準に関してすでにページにアノテーションブロックがあります。';
+$string['needtoactivate'] = 'アノテーションプラグインを有効にする必要があります。あなたのサイト管理者にご相談ください。';
+$string['studentannotation'] = 'アノテーション:';
+$string['assessment'] = '評価';
+$string['begun'] = '評価準備完了';
+$string['incomplete'] = '標準に合致しない';
+$string['partialcomplete'] = '一部標準に合致する';
+$string['completed'] = '標準に合致する';
+$string['usedincollections'] = 'コレクションで使用済み';
+$string['selfassess'] = '自己評価';
+
+?>
