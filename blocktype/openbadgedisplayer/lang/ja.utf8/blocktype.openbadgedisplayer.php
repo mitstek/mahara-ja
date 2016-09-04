d7299a8dd14c76d22988bd5e7df883f6d7ecb00a
Mitsuhiro Yoshida <mits@mitstek.com>
2016-09-04 18:33:19 +0900

    Updated translated strings for Open Badges.

--- blocktype/openbadgedisplayer/lang/ja.utf8/blocktype.openbadgedisplayer.php
+++ blocktype/openbadgedisplayer/lang/ja.utf8/blocktype.openbadgedisplayer.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2016-09-03 14:06:37 UTC
+ * @updated    2016-09-04 09:31:53 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -13,8 +13,8 @@
 
 defined('INTERNAL') || die();
 
-$string['title'] = 'Open Badges';
-$string['description'] = 'あなたのOpen Badgesを表示する';
+$string['title'] = 'オープンバッジ';
+$string['description'] = 'あなたのオープンバッジを表示する';
 $string['issuerdetails'] = '発行者詳細';
 $string['badgedetails'] = 'バッジ詳細';
 $string['issuancedetails'] = '発行詳細';
@@ -27,14 +27,14 @@ $string['expires'] = '有効期限';
 $string['desc'] = '説明';
 $string['criteria'] = 'クライテリア';
 $string['nbadges'] = '%s バッジ';
-$string['nobackpack'] = 'Backpackが見つかりませんでした。<br />あなたの<a href="%s">プロファイル</a>に<a href="%s">Backpack</a>のメールアドレスを入力してください。';
+$string['nobackpack'] = 'バックパックが見つかりませんでした。<br />あなたの<a href="%s">プロファイル</a>に<a href="%s">バックパック</a>のメールアドレスを入力してください。';
 $string['nobadgegroups'] = 'パブリックバッジコレクション / バッジは見つかりませんでした。';
 $string['nobackpackidin'] = 'あなたのメールアドレスはサービス「 %s 」内に見つかりませんでした。';
 $string['nobadgegroupsin'] = '次のサービス内にパブリックバッジコレクション / バッジは見つかりませんでした: %s';
 $string['confighelp'] = 'このブロックで表示するバッジコレクションを選択してください。<br/>あなたのコレクションおよびバッジを管理するには次のサービスにアクセスしてください:<br />%s';
-$string['obppublicbadges'] = 'Open Badge Passport内すべてのパブリックバッジ';
-$string['title_backpack'] = 'Mozilla Backpack';
-$string['title_passport'] = 'Open Badge Passport';
+$string['obppublicbadges'] = 'オープンバッジパスポート内すべてのパブリックバッジ';
+$string['title_backpack'] = 'Mozillaバックパック';
+$string['title_passport'] = 'オープンバッジパスポート';
 $string['fetchingbadges'] = 'エントリを取得しています。しばらくお待ちください。';
 $string['missingbadgesources'] = 'ソース設定がありません。あなたのconfig.phpファイルに追加してください。例:
 <br />
