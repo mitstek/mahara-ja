c4de385ed5043690324a7d697f4ddce490caa631
Mitsuhiro Yoshida <mits@mitstek.com>
2016-09-26 13:24:10 +0900

    Translated new strings for Tagged journal entries.

--- artefact/blog/blocktype/taggedposts/lang/ja.utf8/blocktype.taggedposts.php
+++ artefact/blog/blocktype/taggedposts/lang/ja.utf8/blocktype.taggedposts.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2016-04-10 17:28:19 UTC
+ * @updated    2016-09-26 04:23:42 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -16,13 +16,15 @@ defined('INTERNAL') || die();
 $string['title'] = 'タグ付き日誌エントリ';
 $string['description'] = '特定のタグが付けられた日誌エントリを表示します (「コンテンツ -> 日誌」をご覧ください)。';
 $string['blockheadingtags'] = '「 %2$s 」がタグ付けされた日誌エントリ';
-$string['blockheadingtagsomit'] = '&nbsp;(タグ「 %2$s 」を除外)';
+$string['blockheadingtagsomitboth'] = '(「 %2$ 」を除外)';
+$string['blockheadingtagsomitonly'] = '「 %2$s 」がタグ付けされていない日誌エントリ';
 $string['defaulttitledescription'] = 'ここを空白にした場合、日誌のタイトルが使用されます。';
 $string['postsperpage'] = '1ページあたりのエントリ数';
 $string['taglist'] = '次のタグが付けられたエントリを表示する';
 $string['taglistdesc1'] = 'あなたが除外したいタグそれぞれの前にマイナス記号を入力してください。これらのタグは背景色赤色で表示されます。';
 $string['excludetag'] = '除外タグ:';
 $string['notags'] = '「 %s 」がタグ付けされた記事はありません。';
+$string['notagsomit'] = '「 %s 」がタグ付けされていない記事はありません。';
 $string['notagsboth'] = '「 %s 」 (「 %s 」を除外) がタグ付けされた記事はありません。';
 $string['notagsavailable'] = 'あなたはタグを作成していません。';
 $string['notagsavailableerror'] = 'タグが選択されていません - ここで選択する前にあなたの日誌エントリにタグを追加する必要があります。';
