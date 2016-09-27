c66baf2dc2a8f6a315c969d2346d00ac1f68e08b
Mitsuhiro Yoshida <mits@mitstek.com>
2016-09-28 01:05:12 +0900

    Bug 1533377: Translated new strings for Browserid and a new help file for Auto-migrate Persona users.

--- auth/browserid/lang/ja.utf8/auth.browserid.php
+++ auth/browserid/lang/ja.utf8/auth.browserid.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2015-07-27 02:15:07 UTC
+ * @updated    2016-09-27 15:45:45 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -17,14 +17,11 @@ $string['browserid'] = 'Persona';
 $string['title'] = 'Persona';
 $string['description'] = 'Personaで認証する';
 $string['notusable'] = 'PHP cURL拡張モジュールをインストールした後、Personaベリファイアへの接続をチェックしてください。';
-$string['badassertion'] = 'Personaアサーションが有効ではありません: %s';
-$string['badverification'] = 'Personaベリファイアの有効なJSONアウトプットをMaharaが受信しませんでした。';
-$string['login'] = 'Persona';
-$string['register'] = 'Persona登録';
-$string['missingassertion'] = 'Personaが英数字アサーションを戻しませんでした。';
-$string['emailalreadyclaimed'] = 'すでに別のユーザアカウントがメールアドレス「 %s 」を取得しています。';
-$string['emailclaimedasusername'] = 'すでに別のユーザアカウントがメールアドレス「 %s 」をユーザ名として取得しています。';
-$string['browseridnotenabled'] = 'すべてのアクティブなインスティテューションにおいて、Persona認証プラグインは有効にされていません。';
-$string['emailnotfound'] = 'Personaが有効にされているインスティテューションすべてにおいて、メールアドレス「 %s 」のユーザは見つかりませんでした。';
+$string['deprecatedmsg'] = '2016年11月30日以降、<a href="https://wiki.mozilla.org/Identity/Persona_Shutdown_Guidelines_for_Reliers">MozillaはPersona認証サービスを停止します/a>。このプラグインは既存のPersonaアカウントをPersonaから内部認証への移行を補助するための代理人となります。';
+$string['nobrowseridinstances'] = 'このサイトにはPersona認証インスタンスはありません。そのため必要な作業はありません。';
+$string['institutioncolumn'] = 'インスティテューション';
+$string['numuserscolumn'] = 'アクティブなPersonaユーザ数';
+$string['migratetitle'] = 'Personaユーザを自動移行する';
+$string['migratedesc'] = 'すべてのPersonaユーザを自動的に内部認証に移行した後、すべてのPersona認証インスタンスを削除します。';
 
 ?>
