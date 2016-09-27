d7932a26db574a3d4a9aafb0fba6b340260545f2
Mitsuhiro Yoshida <mits@mitstek.com>
2016-09-28 06:33:14 +0900

    Bug 1533377: Updated a transalted string for Browserid based on en change.

--- auth/browserid/lang/ja.utf8/auth.browserid.php
+++ auth/browserid/lang/ja.utf8/auth.browserid.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2016-09-27 18:43:09 UTC
+ * @updated    2016-09-27 21:31:14 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -16,7 +16,7 @@ defined('INTERNAL') || die();
 $string['browserid'] = 'Persona';
 $string['title'] = 'Persona';
 $string['description'] = 'Personaで認証する';
-$string['notusable'] = 'PHP cURL拡張モジュールをインストールした後、Personaベリファイアへの接続をチェックしてください。';
+$string['notusable'] = '廃止';
 $string['deprecatedmsg'] = '2016年11月30日以降、<a href="https://wiki.mozilla.org/Identity/Persona_Shutdown_Guidelines_for_Reliers">MozillaはPersona認証サービスを停止します</a>。このプラグインは既存のPersonaアカウントをPersonaから内部認証への移行を補助するための代理人となります。';
 $string['nobrowseridinstances'] = 'このサイトにはPersona認証インスタンスはありません。そのため必要な作業はありません。';
 $string['institutioncolumn'] = 'インスティテューション';
