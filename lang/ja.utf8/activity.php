939deca4e116e68382c74c2effe71a4f68b498c8
Mitsuhiro Yoshida <mits@mitstek.com>
2016-10-27 04:43:08 +0900

    Brushed up translated strings for activity.

--- lang/ja.utf8/activity.php
+++ lang/ja.utf8/activity.php
@@ -111,15 +111,15 @@ $string['objectionablecontentviewartefacttext'] = '「  %s (%s)  」の好まし
 %s
 報告者のプロファイルを閲覧するには次のリンクをクリックしてください:
 %s';
-$string['newgroupmembersubj'] = '%s がグループメンバーになりました!';
-$string['removedgroupmembersubj'] = 'もう %s はグループメンバーではありません。';
+$string['newgroupmembersubj'] = '%s がグループメンバーになりました。';
+$string['removedgroupmembersubj'] = '%s はグループメンバーではなくなりました。';
 $string['addtowatchlist'] = 'ウォッチリストに追加する';
 $string['removefromwatchlist'] = 'ウォッチリストから削除する';
 $string['missingparam'] = '次の活動タイプに関する必須パラメータ %s が空です: %s';
 $string['institutionrequestsubject'] = '%s が %s のメンバーシップをリクエストしました。';
-$string['institutionrequestmessage'] = 'あなたはインスティテューションメンバーページでユーザをインスティテューションに追加することができます:';
+$string['institutionrequestmessage'] = 'あなたは「インスティテューションメンバー」ページでユーザをインスティテューションに追加することができます:';
 $string['institutioninvitesubject'] = 'あなたはインスティテューション %s への参加を招待されました。';
-$string['institutioninvitemessage'] = 'あなたはインスティテューション設定ページでこのインスティテューションのメンバーシップを承認することができます:';
+$string['institutioninvitemessage'] = 'あなたは「インスティテューション設定」ページでこのインスティテューションのメンバーシップを承認することができます:';
 $string['deleteallnotifications'] = 'すべての通知を削除する';
 $string['reallydeleteallnotifications'] = '本当にこの活動タイプに関してあなたのすべての通知を削除してもよろしいですか?';
 $string['viewsubmittedsubject1'] = '%s への送信';
