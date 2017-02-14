c83a405d7ceb5aad51f4499525244eac372ad6d0
Mitsuhiro Yoshida <mits@mitstek.com>
2017-02-15 00:19:06 +0900

    Bug 1650995: Translated new strings for SAML authentication.

--- auth/saml/lang/ja.utf8/auth.saml.php
+++ auth/saml/lang/ja.utf8/auth.saml.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2017-02-09 15:37:08 UTC
+ * @updated    2017-02-14 15:17:52 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -34,17 +34,21 @@ $string['errornomemcache'] = 'auth/samlのためにmemcacheサーバが必要で
 $string['errornomemcache7php'] = 'auth/samlのためにmemcacheサーバが必要です。あなたのmemcacheサーバのパスを$cfg->memcacheservers設定変数に一覧記述するかPHPライブラリ「memcache」をローカルにインストールしてください:<br / >sudo apt-get install php-memcache<br />sudo phpenmod memcache<br />そしてウェブサーバを再起動してください。';
 $string['errorbadconfig'] = 'SimpleSAMLPHP設定ディレクトリ「 %s 」が正しくありません。';
 $string['errorbadmetadata'] = 'SAMLメタデータのフォーマットが正しくありません。XMLに1つの有効なアイデンティティプロバイダが含まれていることを確認してください。';
-$string['errorduplicateidp1'] = 'アイデンティティプロバイダ (%s) はすでに別のインスティテューション (%s) で使用されています。XMLに1つの有効かつユニークなアイデンティティプロバイダが含まれていることを確認してください。';
 $string['errorbadinstitutioncombo'] = 'このインスティテューション属性およびインスティテューション値の組み合わせの認証インスタンスはすでに存在します。';
+$string['errormissingmetadata'] = 'あなたは新しいアイデンティティプロバイダメタデータの追加を選択しましたが何も提供されていません。';
 $string['errormissinguserattributes1'] = 'あなたは認証されたようですが私たちは必要なユーザ属性を受信していません。あなたのアイデンティティプロバイダが姓名およびメールアドレスに対するSSOフィールドを %s にリリースしているか確認してください。または管理者にご連絡ください。';
 $string['errorregistrationenabledwithautocreate1'] = 'インスティテューションが登録を有効にされています。あなたがリモートユーザ名を使用していない限り、セキュリティ上の理由からユーザの自動作成を許可しません。';
 $string['errorremoteuser1'] = '「usersuniquebyusername」が無効にされた場合、「remoteuser」との合致は必須です。';
 $string['IdPSelection'] = 'アイデンティティプロバイダ選択';
 $string['noidpsfound'] = 'アイデンティティプロバイダなし';
 $string['idpentityid'] = 'アイデンティティプロバイダエンティティ';
+$string['idpentityadded'] = 'このSAMLインスタンスにアイデンティティプロバイダメタデータを追加しました。';
+$string['idpentityupdated'] = 'このSAMLインスタンスのアイデンティティプロバイダメタデータを更新しました。';
+$string['idpentityupdatedduplicates'] = 'このSAMLインスタンスおよび他の %s インスタンスのアイデンティティプロバイダメタデータを更新しました。';
 $string['idpprovider'] = 'プロバイダ';
 $string['institutionattribute'] = 'インスティテューション属性 (%s を含む)';
 $string['institutionidp'] = 'インスティテューションアイデンティティプロバイダSAMLメタデータ';
+$string['institutionidpentity'] = '利用可能なアイデンティティプロバイダ';
 $string['institutionvalue'] = '属性をチェックするためのインスティテューション値';
 $string['libchecks'] = '正しいライブラリのインストールを確認する: %s';
 $string['link'] = 'アカウントをリンクする';
@@ -55,6 +59,7 @@ $string['logintolinkdesc'] = '<p><b>現在、あなたはリモートユーザ�
 $string['logo'] = 'ロゴ';
 $string['institutionregex'] = 'インスティテューション省略名を部分的に文字列一致させる';
 $string['login'] = 'SSO';
+$string['newidpentity'] = '新しいアイデンティティプロバイダを追加する';
 $string['notusable'] = 'SimpleSAMLPHPサービスプロバイダライブラリをインストールしてください。';
 $string['obsoletesamlplugin'] = 'auth/samlプラグインを再設定する必要があります。<a href="%s">プラグイン設定</a>フォーム経由でプラグインを更新してください。';
 $string['obsoletesamlinstance'] = 'SAML認証インスタンス <a href="%s">%s</a> (「 %s 」インスティテューション) を更新する必要があります。';
