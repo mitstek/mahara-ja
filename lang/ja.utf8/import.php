1d44d81c4e1399fb5b6530885e5868471bdf3c61
Mitsuhiro Yoshida <mits@mitstek.com>
2017-02-19 15:33:17 +0900

    Brushed up translated strings for import.

--- lang/ja.utf8/import.php
+++ lang/ja.utf8/import.php
@@ -14,13 +14,13 @@
 defined('INTERNAL') || die();
 
 $string['Import'] = 'インポート';
-$string['importexceedquota'] = 'あなたがアップロードしたLeap2Aファイルの新しいファイル合計サイズがあなたのクオータを超えます。いくつかのファイルを削除して、あなたは再度試みる必要があります。';
+$string['importexceedquota'] = 'あなたがアップロードしたLeap2Aファイルの新しいファイル合計サイズがあなたのクオータを超えます。いくつかのファイルを削除して再度お試しください。';
 $string['importsuccessfully'] = 'あなたのポートフォリオは正常にインポートされました。';
 $string['importfailed'] = '<p><strong>申し訳ございません - あなたのLeap2Aファイルのインポートに失敗しました。</strong></p>
 
 <p>あなたが有効なLeap2Aファイルをアップロードしていない、またはこのバージョンのMaharaではあなたのLeap2Aファイルのバージョンをサポートされていないことが考えられます。仮にLeap2Aが有効であったとしても、インポートを失敗させるバグがMaharaに含まれている可能性もあります。</p>
 
-<p>戻って、再度お試しください。問題が持続する場合、サポートを求めるため、<a href="https://mahara.org/forums/">Maharaフォーラム</a>に投稿してください。あなたのファイルのコピーを準備してください。</p>';
+<p>戻って再度お試しください。問題が持続する場合、サポートを求めるため、<a href="https://mahara.org/forums/">Maharaフォーラム</a>に投稿してください。あなたのファイルのコピーを準備してください。</p>';
 $string['importwitherrors'] = 'あなたのポートフォリオはインポートされましたがエラーを含みます。';
 $string['importartefactplugindata'] = 'アーティファクトプラグインデータのインポート';
 $string['importartefacts'] = 'アーティファクトのインポート';
@@ -28,10 +28,10 @@ $string['importartefactsprogress'] = 'アーティファクトのインポート
 $string['importfooter'] = 'フッタのインポート';
 $string['importviews'] = 'ページのインポート';
 $string['importviewsprogress'] = 'ページのインポート: %s/%s';
-$string['importportfoliodescription'] = 'Leap2Aファイルがある場合、ここで別のサイトからあなたのポートフォリオにコンテンツをインポートすることができます。サイト設定またはあなたがグループ内にアップロードまたは作成したコンテンツはインポートすることができません。';
+$string['importportfoliodescription'] = 'Leap2Aファイルがある場合、ここで別のサイトからあなたのポートフォリオにコンテンツをインポートすることができます。サイト設定およびあなたがグループ内にアップロードまたは作成したコンテンツはインポートすることができません。';
 $string['importyourportfolio'] = 'あなたのポートフォリオをインポートする';
 $string['howimportyourportfolio'] = 'あなたのポートフォリオアイテムのインポート方法を選択する';
-$string['howimportportfoliodescription'] = 'このステップではあなたの既存のコンテンツとインポートコンテンツをどのように統合するか決定します。';
+$string['howimportportfoliodescription'] = 'このステップではあなたの既存のコンテンツおよびインポートコンテンツをどのように統合するか決定します。';
 $string['noimportpluginsenabled'] = 'サイト管理者はインポートプラグインを有効にしていません。そのため、あなたは使用することができません。';
 $string['noleapimportpluginsenabled'] = 'サイト管理者はLeap2Aインポートプラグインを有効にしていません。そのため、あなたは使用することができません。';
 $string['entry'] = 'エントリ';
