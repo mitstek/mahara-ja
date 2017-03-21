8d957779ce7bc5b245622bf26e0c88c918e40cf4
Mitsuhiro Yoshida <mits@mitstek.com>
2017-03-21 15:33:19 +0900

    Bug 1667181l: Translated new strings for LTI.

---
+++ module/lti/lang/ja.utf8/module.lti.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2017-03-21 06:33:05 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['autoconfiguredesc'] = 'LTIに必要な自動設定有効および設定です。';
+$string['autoconfiguretitle'] = 'LTI自動設定?';
+$string['autocreateusers'] = 'ユーザ自動作成?';
+$string['autocreationnotenabled'] = 'ユーザアカウントの自動作成は有効にされていません。';
+$string['configstep'] = '設定アイテム';
+$string['configstepstatus'] = 'ステータス';
+$string['ltiserviceexists'] = 'LTIサービスグループ登録済み';
+$string['noticeenabled'] = '現在、LTI APIは有効にされています。';
+$string['noticenotenabled'] = '現在、LTI APIは有効にされて<b>いません</b>。';
+$string['oauthprotocolenabled'] = 'OAuthプロトコル有効';
+$string['restprotocolenabled'] = 'RESTプロトコル有効';
+$string['usernameexists'] = 'ユーザ名はすでに存在しています 「 %s 」';
+$string['webserviceauthdisabled'] = 'このインスティテューションのウェブサービス認証は有効にされていません。';
+$string['webserviceproviderenabled'] = '内向きのウェブサービスリクエスト許可';
+$string['institutiondenied'] = '「 %s 」へのアクセスが拒否されました。あなたのインスティテューション管理者にご連絡ください。';
+
+?>
