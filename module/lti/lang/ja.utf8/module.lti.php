9d4d3bcfb850406139db841f7a0e51a1952442b0
Mitsuhiro Yoshida <mits@mitstek.com>
2017-03-22 10:02:03 +0900

    Brushed up a translated string LTI.

--- module/lti/lang/ja.utf8/module.lti.php
+++ module/lti/lang/ja.utf8/module.lti.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2017-03-21 06:33:05 UTC
+ * @updated    2017-03-22 01:00:27 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -24,7 +24,7 @@ $string['noticeenabled'] = '現在、LTI APIは有効にされています。';
 $string['noticenotenabled'] = '現在、LTI APIは有効にされて<b>いません</b>。';
 $string['oauthprotocolenabled'] = 'OAuthプロトコル有効';
 $string['restprotocolenabled'] = 'RESTプロトコル有効';
-$string['usernameexists'] = 'ユーザ名はすでに存在しています 「 %s 」';
+$string['usernameexists'] = 'ユーザ名はすでに存在します: %s';
 $string['webserviceauthdisabled'] = 'このインスティテューションのウェブサービス認証は有効にされていません。';
 $string['webserviceproviderenabled'] = '内向きのウェブサービスリクエスト許可';
 $string['institutiondenied'] = '「 %s 」へのアクセスが拒否されました。あなたのインスティテューション管理者にご連絡ください。';
