93371dad9032601479d0dda98ac1f13b5889bf5f
Mitsuhiro Yoshida <mits@mitstek.com>
2017-03-23 01:28:33 +0900

    Bug 1516981: Translated new strings for the Watchlist block.

--- blocktype/watchlist/lang/ja.utf8/blocktype.watchlist.php
+++ blocktype/watchlist/lang/ja.utf8/blocktype.watchlist.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2015-09-23 21:31:49 UTC
+ * @updated    2017-03-22 16:27:47 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -18,5 +18,24 @@ $string['defaulttitledescription'] = 'タイトルフィールドを空白にし
 $string['description'] = 'あなたのウォッチリストのページを表示します。';
 $string['nopages'] = 'あなたのウォッチリストにページはありません。';
 $string['itemstoshow'] = '表示するアイテム数';
+$string['typetoshow'] = '表示';
+$string['typetoshowdesc'] = 'あなたのウォッチリストまたはフレンドが作成したポートフォリオからダッシュボードにページをどのように表示するか選択してください。';
+$string['noactivities'] = '選択した期間に表示するものはありません。';
+$string['list.watchlist'] = '私のウォッチリストのページ';
+$string['list.follower'] = '私のフレンドの活動';
+$string['additionalfilters'] = '追加フィルタおよび設定';
+$string['filterby'] = '時間でフィルタする';
+$string['filterbydesc'] = 'あなたが活動を表示したい期間を選択してください。';
+$string['filterby.week'] = '先週';
+$string['filterby.month'] = '先月';
+$string['filterby.2months'] = '直近2ヶ月';
+$string['filterby.quarter'] = '直近3ヶ月';
+$string['filterby.half'] = '直近6ヶ月';
+$string['filterby.year'] = '昨年';
+$string['filterby.login'] = '前回のログインから';
+$string['orderby'] = '並べ替え順';
+$string['orderbydesc'] = 'ページの表示を新しい順またはページオーナー順から選択してください。';
+$string['orderby.activity'] = '新しい順';
+$string['orderby.owner'] = 'ページオーナー';
 
 ?>
