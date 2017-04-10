02c93a1f469aa2d0843f964b2d54bf1329dc7003
Mitsuhiro Yoshida <mits@mitstek.com>
2017-04-11 00:49:21 +0900

    Brushed up translated strings for core.

--- lang/ja.utf8/admin.php
+++ lang/ja.utf8/admin.php
@@ -171,7 +171,7 @@ $string['statsmaxviews'] = '平均ビュー数: %s (最もビューの多いユ�
 $string['statsnoviews'] = 'ビュー数: 0';
 $string['statsmaxgroups'] = '平均所属グループ数: %s (最も所属グループの多いユーザ: <a href="%s">%s</a> - 所属グループ数: %d)';
 $string['statsnogroups'] = '所属グループ数: 0';
-$string['statsmaxquotaused'] = '平均利用ディスククオータ: %s (最も利用クオータの多いユーザ: <a href="%s">%s</a> - 利用クオータ: %s)';
+$string['statsmaxquotaused'] = '平均利用ディスククオータ: %s (最も使用済みクオータの多いユーザ: <a href="%s">%s</a> - 使用済みクオータ: %s)';
 $string['groupcountsbytype'] = 'グループタイプごとのグループ数';
 $string['groupcountsbyjointype'] = 'アクセスタイプごとのグループ数';
 $string['blockcountsbytype'] = 'ポートフォリオビューで最も使用されたブロック';
