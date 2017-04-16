94d8fbdb81975730beb4498af3a20ee7ab787e69
Mitsuhiro Yoshida <mits@mitstek.com>
2017-04-17 01:07:59 +0900

    Brushed up translated strings for "as follows".

--- lang/ja.utf8/mahara.php
+++ lang/ja.utf8/mahara.php
@@ -91,7 +91,7 @@ $string['accountexpired'] = '申し訳ございません、あなたのアカウ
 $string['accountcreated'] = ': 新しいアカウント';
 $string['accountcreatedtext'] = '%s さん
 
-あなたの新しいアカウントが %s に作成されました。あなたの詳細情報は下記のとおりです:
+あなたの新しいアカウントが %s に作成されました。あなたの詳細情報は以下のとおりです:
 
 ユーザ名: %s
 パスワード: %s
@@ -101,7 +101,7 @@ $string['accountcreatedtext'] = '%s さん
 %s サイト管理者';
 $string['accountcreatedchangepasswordtext'] = '%s さん
 
-あなたの新しいアカウントが %s に作成されました。あなたの詳細情報は下記のとおりです:
+あなたの新しいアカウントが %s に作成されました。あなたの詳細情報は以下のとおりです:
 
 ユーザ名: %s
 パスワード: %s
@@ -113,7 +113,7 @@ $string['accountcreatedchangepasswordtext'] = '%s さん
 %s サイト管理者';
 $string['accountcreatedhtml'] = '<p>%s さん</p>
 
-<p>あなたの新しいアカウントが <a href="">%s</a> に作成されました。あなたの詳細情報は下記のとおりです:</p>
+<p>あなたの新しいアカウントが <a href="">%s</a> に作成されました。あなたの詳細情報は以下のとおりです:</p>
 
 <ul>
 <li><strong>ユーザ名:</strong> %s</li>
@@ -125,7 +125,7 @@ $string['accountcreatedhtml'] = '<p>%s さん</p>
 <p>%s サイト管理者</p>';
 $string['accountcreatedchangepasswordhtml'] = '<p>%s さん</p>
 
-<p>あなたの新しいアカウントが <a href="">%s</a> に作成されました。あなたの詳細情報は下記のとおりです:</p>
+<p>あなたの新しいアカウントが <a href="">%s</a> に作成されました。あなたの詳細情報は以下のとおりです:</p>
 
 <ul>
 <li><strong>ユーザ名:</strong> %s</li>
