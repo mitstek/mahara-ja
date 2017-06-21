f93bd7f6d741c8bfc4af2a30c9dac0aa1a79b6d7
Mitsuhiro Yoshida <mits@mitstek.com>
2017-06-21 14:11:17 +0900

    Brsuhed up translated strings for "can see", "can view", "be able to see" and "be able to view".

--- lang/ja.utf8/admin.php
+++ lang/ja.utf8/admin.php
@@ -210,7 +210,7 @@ $string['registeryourmaharasite'] = 'あなたのMaharaサイトを登録する'
 $string['registeryourmaharasitesummary'] = '<p>あなたは<a href="https://mahara.org/">mahara.org</a>にあなたのMaharaサイトを登録することができます。また、登録することで私たちは世界のMaharaインストール状況を作成することができます。サイトを登録することでこの通知が取り除かれます。</p>
 <p>あなたがサイトを登録する場合、送信される情報を<strong><a href="%sadmin/registersite.php">サイト登録ページ</a></strong>でプレビューすることができます。</p>';
 $string['registeryourmaharasitedetail'] = '<p>あなたは<a href="https://mahara.org/">mahara.org</a>にあなたのMaharaサイトを登録することができます。登録は無料です。また、登録することで私たちは世界のMaharaインストール状況を作成することができます。</p>
-<p>あなたはmahara.orgに送信された情報を閲覧することができます - あなたのユーザに関する個人情報は一切送信されません。</p>
+<p>あなたはmahara.orgに送信された情報を閲覧できます - あなたのユーザに関する個人情報は一切送信されません。</p>
 <p>あなたが「更新を週に1回送信しますか?」をこの設定を有効にした場合、Maharaは週に1回、あなたのインストール済みMaharaに関する更新情報をmahara.orgに自動送信します。</p>
 <p>サイトを登録した後、この通知は取り除かれます。あなたは更新内容を週に1回送信するかどうか、<a href="%sadmin/site/options.php">サイトオプション</a>ページで変更することができます。</p>';
 $string['siteregistered'] = 'あなたのサイトが正常に登録されました。あなたは<a href="%sadmin/site/options.php">サイトオプション</a>ページで</p>サイト更新内容の週ごとの送信を有効または無効にすることができます。';
