b4b4399a3545159f58d75b4a4115c026900abfc0
Mitsuhiro Yoshida <mits@mitstek.com>
2017-07-11 15:12:05 +0900

    Brushed up translated strings for Page.

--- lang/ja.utf8/view.php
+++ lang/ja.utf8/view.php
@@ -155,13 +155,13 @@ $string['submitconfirm'] = '評価のために「 %s 」を「 %s 」に送信�
 $string['collectionsubmitted'] = 'コレクションが送信されました。';
 $string['viewsalreadysubmitted'] = 'このコレクション内のいくつかのページはすでに送信されています:<br />%s<br />それらのページがリリースされるまであなたはコレクションを送信することができません。';
 $string['submitviewtogroup'] = '評価のため「 %s 」を「 %s 」に送信する';
-$string['cantsubmitviewtogroup'] = 'あなたは評価を目的として、このページをこのグループに送信することはできません。';
+$string['cantsubmitviewtogroup'] = 'あなたは評価を目的としてこのページをこのグループに送信することはできません。';
 $string['cantsubmitcollectiontogroup'] = 'あなたはこのコレクションを送信できません。';
 $string['cantsubmittogroup'] = 'あなたはこのグループに送信できません。';
 $string['cantdeleteview'] = 'あなたはこのページを削除できません。';
 $string['deletespecifiedview'] = 'ページ「 %s 」を削除する';
 $string['deleteviewconfirm1'] = '本当にこのページを削除してもよろしいですか? 元に戻すことはできません。';
-$string['deleteviewconfirmbackup'] = '<a href="%sexport/" target="_blank">エクスポート</a>により、このページのバックアップの作成をご検討ください。';
+$string['deleteviewconfirmbackup'] = '<a href="%sexport/" target="_blank">エクスポート</a>によるこのページのバックアップ作成をご検討ください。';
 $string['deleteviewconfirmnote1'] = '<strong>注意:</strong> このページにリンクしたすべてのファイルおよび日誌を利用することはできます。<br />しかし、このページに投稿されたすべてのフィードバックは削除されます。';
 $string['editaccesspagedescription3'] = 'デフォルトではあなたのページをあなたのみ閲覧できます。このページにアクセスルールを追加することであなたは他のユーザとページを共有することができます。アクセスルールを追加した後、ページ下部の「保存」ボタンをクリックしてください。';
 $string['editaccessdescription'] = 'チェックボックスで選択することにより、あなたは複数アイテムに同一設定することができます。あなたの選択完了後、ページ下部の「保存」ボタンをクリックしてください。';
