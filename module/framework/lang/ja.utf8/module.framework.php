5ed6ffe94f75c457a88b70705097e0f404664bc1
Mitsuhiro Yoshida <mits@mitstek.com>
2017-08-08 14:23:42 +0900

    Brushed up translated strings for SmartEvidence framework.

--- module/framework/lang/ja.utf8/module.framework.php
+++ module/framework/lang/ja.utf8/module.framework.php
@@ -15,26 +15,26 @@ defined('INTERNAL') || die();
 
 $string['Framework'] = 'スマートエビデンスフレームワーク';
 $string['frameworknav'] = 'スマートエビデンス';
-$string['frameworks'] = 'インストール済みスマートエビデンス';
+$string['frameworks'] = 'インストール済みフレームワーク';
 $string['frameworkdesc'] = 'あなたのポートフォリオと関連付けたいコンピテンシーフレームワークを選択してください。';
-$string['frameworksdesc'] = 'システムにインストールされているフレームワークの一覧です。有効時にコレクションと関連付けられていない限り、無効にされたフレームワークは一覧表示されません。';
+$string['frameworksdesc'] = 'システムにインストールされているフレームワークの一覧です。有効時にコレクションと関連付けられていない限り無効にされたフレームワークは一覧表示されません。';
 $string['taskscompleted'] = 'タスク完了';
 $string['addpages'] = 'あなたがスマートエビデンスマップに表示させたい場合、さらにこのコレクションにページを追加することができます。';
 $string['addframework'] = 'フレームワークを追加する';
 $string['frameworkupdated'] = 'フレームワークが更新されました。';
 $string['matrixfile'] = 'マトリックスファイル';
-$string['matrixfiledesc'] = 'JSONエンコード済みフレームワークを含む「.matrix」ファイルです。';
-$string['notvalidmatrixfile'] = 'これは有効な「.matrix」ファイルではありません。';
-$string['matrixfilenotfound'] = '有効な「.matrix」ファイルが選択されていません。';
-$string['invalidjson'] = 'これは有効な「.matrix」ファイルではありません。JSONコンテンツを構文解析できません。';
-$string['jsonmissingvars'] = 'これは有効な「.matrix」ファイルではありません。「フレームワーク」または「フレームワーク名」がありません。';
+$string['matrixfiledesc'] = 'JSONエンコード済みフレームワークを含む.matrixファイルです。';
+$string['notvalidmatrixfile'] = 'これは有効な.matrixファイルではありません。';
+$string['matrixfilenotfound'] = '有効な.matrixファイルが選択されていません。';
+$string['invalidjson'] = 'これは有効な.matrixファイルではありません。JSONコンテンツを構文解析できません。';
+$string['jsonmissingvars'] = 'これは有効な.matrixファイルではありません。「フレームワーク」または「フレームワーク名」がありません。';
 $string['manuallyremovematrices'] = 'インストールディレクトリ「 %s 」を削除することはできません。手動で削除してください。';
 $string['changeframeworkproblems'] = 'あなたはフレームワークを変更できません。次のページにはこのフレームワークと関連付けられたエビデンスがあります:';
-$string['accessdeniednoframework'] = 'このコレクションはスマートエビデンスページを表示できません。これは次の理由が考えられます:
+$string['accessdeniednoframework'] = 'このコレクションはスマートエビデンスページを表示できません。これには以下のいずれかの理由が考えられます:
 <ul>
 <li>スマートエビデンスプラグインがインストールまたは有効にされていない。</li>
 <li>あなたが属しているインスティテューションではスマートエビデンスが許可されていない。</li>
-<li>このコレクションではスマートエビデンスフレームワークが設定されていない。</li>
+<li>このコレクションにスマートエビデンスフレームワークが設定されていない。</li>
 <li>このコレクションにページが存在しない。</li>
 </ul>';
 $string['firstviewlink'] = 'コレクションの<a href="%s">最初のページ</a>に移動してください。';
@@ -42,7 +42,7 @@ $string['noframeworkselected'] = 'なし';
 $string['matrixpointupdated'] = 'スマートエビデンスが更新されました。';
 $string['matrixpointinserted'] = 'スマートエビデンスが追加されました。';
 $string['standard'] = '標準';
-$string['standarddesc'] = 'このエビデンスに該当する標準を選択してください。あなたは標準を探すためにボックスに入力することができます。';
+$string['standarddesc'] = 'このエビデンスに該当する標準を選択してください。あなたは標準を探すためボックスに入力することができます。';
 $string['annotationclash'] = 'この標準に関してページにすでにアノテーションブロックがあります。';
 $string['needtoactivate'] = 'アノテーションプラグインを有効にする必要があります。あなたのサイト管理者にご相談ください。';
 $string['studentannotation'] = 'アノテーション:';
