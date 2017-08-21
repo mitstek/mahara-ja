c9682dd35b027f8c1c4651733888a3820cb0a8eb
Mitsuhiro Yoshida <mits@mitstek.com>
2017-08-21 13:02:49 +0900

    Brushed up translated strings for notification.

--- notification/email/lang/ja.utf8/notification.email.php
+++ notification/email/lang/ja.utf8/notification.email.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2009-06-25 11:39:01 UTC
+ * @updated    2017-08-21 04:02:27 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,8 +14,8 @@
 defined('INTERNAL') || die();
 
 $string['emailsubject'] = '%s';
-$string['emailheader'] = 'あなたに %s から通知メールが送信されました。メッセージは以下のとおりです:';
-$string['emailfooter'] = 'これは %s から自動的に生成された通知メールです。あなたの通知プリファレンスを更新するには %s にアクセスしてください。';
+$string['emailheader'] = 'あなたに %s から通知が送信されました。メッセージは次のとおりです:';
+$string['emailfooter'] = 'これは %s から自動的に生成された通知です。あなたの通知プリファレンスを更新するには %s にアクセスしてください。';
 $string['referurl'] = '詳細 %s';
 $string['name'] = 'メール';
 
