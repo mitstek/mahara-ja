b32affbc9c89c1c6047c395a617d2aa2ecfb6e24
Mitsuhiro Yoshida <mits@mitstek.com>
2017-09-17 17:18:27 +0900

    Bug 1691317, 1716541: Translated a new string and a help file for Parent authority (LTI as child).

--- module/lti/lang/ja.utf8/module.lti.php
+++ module/lti/lang/ja.utf8/module.lti.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2017-08-09 08:40:54 UTC
+ * @updated    2017-09-17 08:17:49 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -30,5 +30,6 @@ $string['webserviceproviderenabled'] = '内向きのウェブサービスリク�
 $string['institutiondenied'] = '「 %s 」へのアクセスが拒否されました。あなたのインスティテューション管理者にご連絡ください。';
 $string['notreadylabel'] = '準備未了';
 $string['readylabel'] = '準備完了';
+$string['parentauthforlti'] = '親認証局';
 
 ?>
