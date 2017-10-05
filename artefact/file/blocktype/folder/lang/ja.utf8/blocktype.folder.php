92eb86686ede256655e911addc13b3a766d0efca
Mitsuhiro Yoshida <mits@mitstek.com>
2017-10-05 17:04:23 +0900

    Brushed up a translated string for Folder block.

--- artefact/file/blocktype/folder/lang/ja.utf8/blocktype.folder.php
+++ artefact/file/blocktype/folder/lang/ja.utf8/blocktype.folder.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-12-23 09:52:17 UTC
+ * @updated    2017-10-05 07:58:18 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -15,7 +15,7 @@ defined('INTERNAL') || die();
 
 $string['title'] = 'フォルダ';
 $string['description'] = 'あなたのファイルエリアの単独フォルダです (「コンテンツ -> ファイル」をご覧ください)。';
-$string['defaulttitledescription'] = 'ここを空白にした場合、フォルダのタイトルが使用されます。';
+$string['defaulttitledescription'] = 'ここを空白にした場合、フォルダ名が使用されます。';
 $string['foldersettings'] = 'フォルダ設定';
 $string['defaultsortorder'] = 'デフォルトのファイル並べ替え順';
 
