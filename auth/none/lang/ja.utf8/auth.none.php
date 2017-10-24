3a42070645f7cf163d42c5baaf626da60c7ce1df
Mitsuhiro Yoshida <mits@mitstek.com>
2017-10-24 20:45:40 +0900

    Bug 1546769: Updated a translated string for the auth selection for 'none' based on en change

--- auth/none/lang/ja.utf8/auth.none.php
+++ auth/none/lang/ja.utf8/auth.none.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2016-09-26 15:58:55 UTC
+ * @updated    2017-10-24 11:45:20 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,6 +14,6 @@
 defined('INTERNAL') || die();
 
 $string['title'] = 'なし';
-$string['description'] = '誰でもログインできます。テストにのみ使用してください!';
+$string['description'] = '誰でもログインできます。実稼働モードでは動作しません!';
 
 ?>
