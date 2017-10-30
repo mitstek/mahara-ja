46ce1ad586a9955fccbb8bd1cc8a9fd2b1a0198a
Mitsuhiro Yoshida <mits@mitstek.com>
2017-10-30 09:17:49 +0900

    Brushed up translated strings for artefact internal.

--- artefact/internal/lang/ja.utf8/artefact.internal.php
+++ artefact/internal/lang/ja.utf8/artefact.internal.php
@@ -77,10 +77,10 @@ $string['emailaddress'] = '代替メールアドレス';
 $string['saveprofile'] = 'プロファイルを保存する';
 $string['profilesaved'] = 'プロファイルが正常に保存されました。';
 $string['profilefailedsaved'] = 'プロファイルの保存に失敗しました。';
-$string['emailvalidation_subject'] = 'メールアドレスの確認';
+$string['emailvalidation_subject'] = 'Eメール確認';
 $string['emailvalidation_body1'] = '%s さん、こんにちは。
 
-Mahara内のあなたのユーザアカウントにメールアドレス「 %s 」が追加されました。下記リンクにアクセスしてこのメールアドレスを有効化してください。
+%s のあなたのユーザアカウントにメールアドレス %s が追加されました。下記リンクにアクセスしてこのメールアドレスを有効化してください。
 
 %s
 
