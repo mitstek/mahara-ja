0b21f74a4e8248c2edd5bfb1c704d28c370526ec
Mitsuhiro Yoshida <mits@mitstek.com>
2017-11-10 10:39:02 +0900

    Brushed up a translated string for Profile information block.

--- artefact/internal/blocktype/profileinfo/lang/ja.utf8/blocktype.profileinfo.php
+++ artefact/internal/blocktype/profileinfo/lang/ja.utf8/blocktype.profileinfo.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2016-09-08 13:07:22 UTC
+ * @updated    2017-11-10 01:38:56 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -18,7 +18,7 @@ $string['description'] = '表示するプロファイル情報を選択してく
 $string['aboutme'] = '私について';
 $string['fieldstoshow'] = '表示するフィールド';
 $string['introtext'] = '自己紹介テキスト';
-$string['useintroductioninstead'] = '「表示するフィールド」の「自己紹介」チェックボックスをチェックしてこのフィールドを空白にすることにより、あなたのプロファイルの「自己紹介」フィールドを代わりに使用することができます。';
+$string['useintroductioninstead'] = '「自己紹介」を有効にしてこのフィールドを空白にすることにより、あなたの「自己紹介」プロファイルフィールドを代わりに使用することができます。';
 $string['dontshowprofileicon'] = 'プロファイル画像を表示しません。';
 $string['dontshowemail'] = 'メールアドレスを表示しません。';
 $string['uploadaprofileicon'] = 'あなたにはプロファイル画像がありません。<a href="%sartefact/file/profileicons.php">アップロードしてください</a>。';
