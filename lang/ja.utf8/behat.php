378a6dc106fdaf84250755eed6c62ce98cfb9fa8
Mitsuhiro Yoshida <mits@mitstek.com>
2017-11-19 22:40:14 +0900

    Brushed up translated strings for adnmi, Behat and internal authentication.

--- lang/ja.utf8/behat.php
+++ lang/ja.utf8/behat.php
@@ -17,6 +17,6 @@ $string['errorbehatcommand'] = 'Behat CLIコマンドの実行中にエラーが
 $string['errorcomposer'] = 'コンポーザ依存関係がインストールされていません。';
 $string['errordataroot'] = '「$CFG->behat_dataroot」が設定されていない、または無効です。';
 $string['errorsetconfig'] = 'config.phpに「$CFG->behat_dataroot」「$CFG->behat_dbprefix」および「$CFG->behat_wwwroot」を設定する必要があります。';
-$string['erroruniqueconfig'] = '「$CFG->behat_dataroot」「$CFG->behat_dbprefix」および「$CFG->behat_wwwroot」の値は「$CFG->dataroot」「$CFG->dbprefix」「$CFG->wwwroot」「$CFG->phpunit_dataroot」および「$CFG->phpunit_prefix」の値と異なる必要があります。';
+$string['erroruniqueconfig'] = '「$CFG->behat_dataroot」「$CFG->behat_dbprefix」および「$CFG->behat_wwwroot」の値は「$CFG->dataroot」「$CFG->dbprefix」「$CFG->wwwroot」「$CFG->phpunit_dataroot」および「$CFG->phpunit_prefix」の値とは異なる必要があります。';
 
 ?>
