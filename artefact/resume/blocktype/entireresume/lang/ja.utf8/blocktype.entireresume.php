6fcd6aca13ff0349b06762fba0333455785af88c
Mitsuhiro Yoshida <mits@mitstek.com>
2017-11-20 20:24:49 +0900

    Bug 1529750: Translated new strings for résumé block.

--- artefact/resume/blocktype/entireresume/lang/ja.utf8/blocktype.entireresume.php
+++ artefact/resume/blocktype/entireresume/lang/ja.utf8/blocktype.entireresume.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2015-03-01 19:46:14 UTC
+ * @updated    2017-11-20 11:24:45 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -13,6 +13,8 @@
 
 defined('INTERNAL') || die();
 
+$string['addresstag'] = 'アドレス: %s';
+$string['address'] = 'アドレス';
 $string['title'] = '私のレジュメ全体';
 $string['description'] = 'あなたのレジュメ全体を表示する素早い方法です。';
 $string['History'] = '履歴';
