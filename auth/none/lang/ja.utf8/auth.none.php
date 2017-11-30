b7fbec77c3f3790d0a0da1661ec68dc63ce6c8f9
Mitsuhiro Yoshida <mits@mitstek.com>
2017-11-30 16:51:32 +0900

    Brushed up a translated string for none authentication.

--- auth/none/lang/ja.utf8/auth.none.php
+++ auth/none/lang/ja.utf8/auth.none.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2017-10-24 11:45:20 UTC
+ * @updated    2017-11-30 07:51:12 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,6 +14,6 @@
 defined('INTERNAL') || die();
 
 $string['title'] = 'なし';
-$string['description'] = '誰でもログインできます。実稼働モードでは動作しません!';
+$string['description'] = '誰でもログインできます。実運用モードでは動作しません!';
 
 ?>
