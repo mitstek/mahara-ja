bae166e141d7c95b2c930f7ae01339d3c859e038
Mitsuhiro Yoshida <mits@mitstek.com>
2017-12-08 00:04:44 +0900

    Bug 1508300: Translated a new string for PDF block config.

--- artefact/file/blocktype/pdf/lang/ja.utf8/blocktype.pdf.php
+++ artefact/file/blocktype/pdf/lang/ja.utf8/blocktype.pdf.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2013-08-01 05:30:05 UTC
+ * @updated    2017-12-07 15:03:57 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -15,5 +15,6 @@ defined('INTERNAL') || die();
 
 $string['title'] = 'PDF';
 $string['description'] = 'あなたのファイルエリアの単独PDFファイルです。';
+$string['pdfwarning'] = '注意: 大きなPDFファイルを開いた場合、ブラウザのリソースに高負荷がかかる場合があります。';
 
 ?>
