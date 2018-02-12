a28002954fd4a9625dec1974e797602ff9404aea
Mitsuhiro Yoshida <mits@mitstek.com>
2018-02-12 13:36:16 +0900

    Brushed up a translated string for the most recently updated pages and collections block.

--- blocktype/newviews/lang/ja.utf8/blocktype.newviews.php
+++ blocktype/newviews/lang/ja.utf8/blocktype.newviews.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2015-09-17 21:24:54 UTC
+ * @updated    2018-02-12 04:36:02 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,7 +14,7 @@
 defined('INTERNAL') || die();
 
 $string['title1'] = '私が閲覧できる最新の変更';
-$string['description2'] = 'このサイトにおいて、あなたがアクセスした中から最も最近に更新されたページおよびコレクションを一覧表示します。';
+$string['description2'] = 'あなたがこのサイトでアクセスした中から最も最近に更新されたページおよびコレクションを一覧表示します。';
 $string['viewstoshow1'] = '表示する結果の最大数';
 $string['viewstoshowdescription'] = '設定範囲: 1～100';
 $string['defaulttitledescription'] = 'タイトルフィールドを空白にした場合、デフォルトタイトルが生成されます。';
