70bf06248f9f5a9e9981ca2cde91230c9deff7ea
Mitsuhiro Yoshida <mits@mitstek.com>
2018-02-25 14:11:46 +0900

    Brushed up translated strings for Recent forum posts block.

--- blocktype/recentforumposts/lang/ja.utf8/blocktype.recentforumposts.php
+++ blocktype/recentforumposts/lang/ja.utf8/blocktype.recentforumposts.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2015-09-27 15:52:58 UTC
+ * @updated    2018-02-25 05:11:40 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -16,10 +16,10 @@ defined('INTERNAL') || die();
 $string['title'] = '最近のフォーラム投稿';
 $string['description'] = 'グループの最近のフォーラム投稿を表示します。';
 $string['group'] = 'グループ';
-$string['nogroupstochoosefrom'] = '申し訳ございません、選択できるグループがありません。';
+$string['nogroupstochoosefrom'] = '申し訳ございません、選択できるグループはありません。';
 $string['poststoshow'] = '表示する最大投稿数';
 $string['poststoshowdescription'] = '設定範囲: 1-100';
-$string['recentforumpostsforgroup'] = '%s フォーラムの最近の投稿';
+$string['recentforumpostsforgroup'] = '%s の最近のフォーラム投稿';
 $string['defaulttitledescription'] = 'タイトルフィールドを空白にした場合、デフォルトタイトルが生成されます。';
 
 ?>
