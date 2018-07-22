9f50f56e3184dbcff14b3cf3dbc4fe72341f3db7
Mitsuhiro Yoshida <mits@mitstek.com>
2018-07-22 19:29:49 +0900

    Brushed up translated strings for admin.

--- lang/ja.utf8/admin.php
+++ lang/ja.utf8/admin.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2018-07-03 10:39:16 UTC
+ * @updated    2018-07-22 10:20:51 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -890,11 +890,11 @@ $string['membershipexpirydescription'] = 'ユーザがインスティテュー�
 $string['studentid'] = 'IDナンバー';
 $string['institutionstudentiddescription'] = 'インスティテューションを識別するための任意の識別子です。ユーザはこのフィールドを編集することはできません。';
 $string['userstodisplay'] = '表示するユーザ:';
-$string['institutionusersrequesters'] = 'インスティテューションのメンバーシップをリクエストしたユーザ';
-$string['institutionusersnonmembers'] = 'まだインスティテューションのメンバーシップをリクエストしていないユーザ';
-$string['institutionuserslastinstitution'] = 'インスティテューションを離れたユーザ';
-$string['institutionusersmembers'] = 'すでにインスティテューションのメンバーになっているユーザ';
-$string['institutionusersinvited'] = '招待されたユーザ';
+$string['institutionusersrequesters'] = 'インスティテューションのメンバーシップをリクエストした人';
+$string['institutionusersnonmembers'] = 'まだインスティテューションのメンバーシップをリクエストしていない人';
+$string['institutionuserslastinstitution'] = 'インスティテューションを離れた人';
+$string['institutionusersmembers'] = 'すでにインスティテューションのメンバーになっている人';
+$string['institutionusersinvited'] = '招待された人';
 $string['addnewmembers'] = '新しいメンバーを追加する';
 $string['usersrequested'] = 'メンバーシップをリクエストしたユーザ';
 $string['userstobeadded'] = 'メンバーとして追加されるユーザ';
