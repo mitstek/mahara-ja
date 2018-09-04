a839e0caef8c92e414b9a447fb401134fdac6146
Mitsuhiro Yoshida <mits@mitstek.com>
2018-09-04 15:14:04 +0900

    Brushed up translated strings for authentication.

--- lang/ja.utf8/auth.php
+++ lang/ja.utf8/auth.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2018-09-02 04:22:40 UTC
+ * @updated    2018-09-04 06:12:54 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -38,13 +38,13 @@ $string['wessoout'] = 'あちらへSSOする';
 $string['weimportcontent'] = 'こちらにコンテンツをインポートさせる';
 $string['weimportcontentdescription'] = '(一部のアプリケーションのみ)';
 $string['theyssoin'] = 'こちらにSSOさせる';
-$string['authloginmsg2'] = 'あなたが親認証局を選択していない場合、ログインフォーム経由でログインを試みるユーザに表示されるメッセージを入力してください。';
+$string['authloginmsg2'] = 'あなたが親認証局を選択していない場合、ログインフォームでログインを試みるユーザに表示されるメッセージを入力してください。';
 $string['authloginmsgnoparent'] = 'ユーザがログインフォームでログインを試みた場合に表示するメッセージを入力してください。';
 $string['application'] = 'アプリケーション';
-$string['cantretrievekey'] = 'リモートサーバの公開鍵検索中にエラーが発生しました。<br />アプリケーションおよびWWWルートフィールドが正しいこと、リモートホストのネットワーキングが有効にされていることを確認してください。';
+$string['cantretrievekey'] = 'リモートサーバの公開鍵検索中にエラーが発生しました。<br />アプリケーションおよびWWWルートフィールドが正しいことを確認してください。また、リモートホストのネットワーキングが有効にされていることを確認してください。';
 $string['ssodirection'] = 'SSOの方向';
 $string['active'] = 'アクティブ';
-$string['errorcertificateinvalidwwwroot'] = 'これは %s の証明書ですが、あなたは %s に対する使用を試みています。';
+$string['errorcertificateinvalidwwwroot'] = 'これは %s の証明書ですが、あなたは %s での使用を試みています。';
 $string['errorcouldnotgeneratenewsslkey'] = '新しいSSLキーを生成することができませんでした。本当にこのマシンに OpenSSLおよびOpenSSL用PHPモジュールがインストールされていますか?';
 $string['errnoauthinstances'] = '私たちには %s のホストに関して認証プラグインインスタンスが設定されていないようです。';
 $string['errornotvalidsslcertificate'] = '有効なSSLサーバ証明書ではありません。';
@@ -54,7 +54,7 @@ $string['errnoxmlrpcuser1'] = '現時点ではあなたを認証することが�
 
 *あなたのSSOセッションが失効しました。他のアプリケーションに戻って %s にログインするためのリンクを再度クリックしてください。
 *あなたの %s に対するSSOが許可されていません。あなたがSSOできるかどうか管理者に確認してください。';
-$string['toomanytries'] = 'あなたは最大ログイン回数を超過しました。このアカウントは5分間ロックされます。';
+$string['toomanytries'] = 'あなたは最大ログイン試行回数を超過しました。このアカウントは5分間ロックされます。';
 $string['unabletosigninviasso'] = 'SSO経由でサインインできません。';
 $string['xmlrpccouldnotlogyouin'] = '申し訳ございません、あなたをログインさせることができませんでした。';
 $string['xmlrpccouldnotlogyouindetail1'] = '申し訳ございません、今回はあなたを %s にログインさせることができませんでした。ログインを再度お試しください。問題が続く場合、あなたの管理者にご連絡ください。';
