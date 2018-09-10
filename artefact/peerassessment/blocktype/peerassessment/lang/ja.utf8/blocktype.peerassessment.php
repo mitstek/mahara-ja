7fbc1c142269f106bcbbeb334ec3c5f8031cb25a
Mitsuhiro Yoshida <mits@mitstek.com>
2018-09-10 16:09:38 +0900

    Bug 1784778: Translated new strings for Peer assessment block.

---
+++ artefact/peerassessment/blocktype/peerassessment/lang/ja.utf8/blocktype.peerassessment.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2018-09-10 07:08:34 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = '相互評価';
+$string['description'] = '相互評価を表示するためのブロックです。';
+$string['blockcontent'] = 'インストラクション';
+$string['nopeerassessment'] = '評価フィードバックはありません。';
+$string['addpeerassessment'] = '相互評価を追加する';
+$string['instructions'] = 'インストラクション';
+$string['draft'] = '下書きを保存する';
+$string['publish'] = '公開';
+$string['savepublishhelp'] = '<p><strong>下書きを保存する:</strong> あなたのみ閲覧することができます。あなたの評価が下書きステータスの間は変更することができます。</p>
+<p><strong>公開する:</strong> あなたが相互評価を与えている人はあなたの評価を閲覧することができます。ポートフォリオに承認ブロックが含まれずポートフォリオのオーナーによって承認されていない限り、ポートフォリオにアクセスする人は誰でも評価を閲覧することができます。あなたは公開済み評価を下書きステータスに戻すことはできません。</p>';
+
+?>
