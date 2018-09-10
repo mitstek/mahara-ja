d51f5fa8b9685216009cba776d4247c1ecccd86e
Mitsuhiro Yoshida <mits@mitstek.com>
2018-09-10 17:17:50 +0900

    Bug 1784778: Translated new strings for Peer assessment block.

---
+++ artefact/peerassessment/lang/ja.utf8/artefact.peerassessment.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2018-09-10 08:17:36 UTC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['pluginname'] = '相互評価';
+$string['Assessment'] = '評価';
+$string['Assessments'] = '評価';
+$string['assessment'] = '評価';
+$string['assessments'] = '評価';
+$string['makeassessmentpublic'] = '評価を公開する';
+$string['attachfile'] = '添付ファイル';
+$string['messageempty'] = '相互評価は空です。';
+$string['assessmentsubmitted'] = '相互評価が保存されました。';
+$string['assessmentsubmitteddraft'] = '相互評価が下書きとして保存されました。';
+$string['reallydeletethisassessment'] = 'この相互評価を削除する';
+$string['thisassessmentisprivate'] = '下書きとして保存されました。';
+$string['assessmentremoved'] = '相互評価が削除されました。';
+$string['deletednotificationsubject'] = 'ページ「 %s 」の相互評価が削除されました。';
+$string['deletedauthornotification'] = 'あなたのページ「 %s 」の評価が削除されました。あなたは次を記入しました:
+%s';
+$string['newassessmentnotificationsubject'] = 'ページ「 %s 」の新しい相互評価';
+$string['feedbacknotificationhtml'] = '<div style="padding: 0.5em 0; border-bottom: 1px solid #999;"><strong>%s が相互評価を追加しました - %s</strong><br />%s</div>
+
+<div style="margin: 1em 0;">%s</div>
+
+<div style="font-size: smaller; border-top: 1px solid #999;">
+<p><a href="%s">この相互評価をオンラインで閲覧する</a></p>
+</div>';
+$string['feedbacknotificationtext'] = '%s が相互評価を追加しました - %s
+%s
+------------------------------------------------------------------------
+
+%s
+
+------------------------------------------------------------------------
+この相互評価を閲覧するには次のリンクに従ってください:
+%s';
+$string['typeassessmentfeedback'] = '相互評価';
+$string['nopeerassessmentrequired'] = '相互評価を必要としないため、あなたはこのページでコンテンツを閲覧することができません。';
+
+?>
