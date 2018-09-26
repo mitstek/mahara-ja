44ad2e92cf8250b6e5c703e5b01889067a6bd367
Mitsuhiro Yoshida <mits@mitstek.com>
2018-09-26 16:47:44 +0900

    Bug 1789297: Translated new strings and a help file for LTI assignment submission.

--- module/lti/lang/ja.utf8/module.lti.php
+++ module/lti/lang/ja.utf8/module.lti.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2018-05-30 13:48:57 UTC
+ * @updated    2018-09-26 07:46:22 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -13,23 +13,52 @@
 
 defined('INTERNAL') || die();
 
+$string['archive'] = '評定済みをアーカイブする';
+$string['archivedescription'] = '評点が与えられた後、ポートフォリオのスナップショットが取得されます。';
 $string['autoconfiguredesc'] = 'LTIに必要な設定を自動的に有効にします。';
 $string['autoconfiguretitle'] = 'LTI自動設定';
 $string['autocreateusers'] = 'ユーザ自動作成';
 $string['autocreationnotenabled'] = 'ユーザアカウントの自動作成は有効にされていません。';
+$string['portfolioname'] = 'プロファイルタイトル';
 $string['configstep'] = '設定アイテム';
 $string['configstepstatus'] = 'ステータス';
+$string['configuration'] = '評価設定';
+$string['configurationintro'] = 'まだ誰もポートフォリオを提出していない場合、あなたは提出設定を変更することができます。提出されたものがある場合、設定はロックされます。';
+$string['configurationsaved'] = '評価設定が保存されました。';
+$string['emailtutors'] = '学生の提出時にチューターにメールする';
+$string['grade'] = '評定';
+$string['gradedby'] = '評定者:';
+$string['gradereceived'] = 'あなたに %s / 100 の評点が %s によって与えられました (評定日時: %s )。';
+$string['gradesubmissions'] = '評定のための提出';
+$string['gradesubmitted'] = '評定が正常に提出されました。';
+$string['groupname'] = '提出 (「 %s 」-「 %s 」) の評価';
+$string['institutiondenied'] = '「 %s 」へのアクセスが拒否されました。あなたのインスティテューション管理者にご連絡ください。';
+$string['lock'] = '評定後にポートフォリオのロックを継続する';
+$string['lockdescription'] = '評定後、ユーザがポートフォリオを変更しました。';
 $string['ltiserviceexists'] = 'LTIサービスグループは登録されています。';
+$string['nocollections'] = 'あなたには評価のために提出するポートフォリオはありません。';
 $string['noticeenabled'] = 'LTI APIは有効にされています。';
 $string['noticenotenabled'] = 'LTI APIは有効にされて<b>いません</b>。';
+$string['notconfigured'] = '現在、活動は提出を許可していません。後で確認してください。';
+$string['notreadylabel'] = '準備未了';
 $string['oauthprotocolenabled'] = 'OAuthプロトコル有効';
+$string['parentauthforlti'] = '親認証局';
+$string['portfoliosubmitted'] = 'あなたは評価のためポートフォリオ「 %s 」を「 %s 」に提出しました。';
+$string['readylabel'] = '準備完了';
 $string['restprotocolenabled'] = 'RESTプロトコル有効';
 $string['usernameexists2'] = 'ユーザ名「 %s 」は有効ではありません。';
+$string['saveandrelease'] = '保存して提出を許可する';
+$string['submitportfolio'] = '評価のためポートフォリオを提出する';
+$string['submitto'] = 'このポートフォリオを評価のため %s に提出します。%s';
+$string['submitintro'] = 'あなたがこの活動を提出したいページまたはコレクションを選択してください。あなたにまだポートフォリオがない場合、<a href="%sview/index.php" target="_blank">作成してください</a>。あなたの作成後、LMSからの活動を再度読み込んでください。';
+$string['timegraded'] = '評定日時';
+$string['timesubmitted'] = '提出日時';
+$string['usernameexists1'] = 'ユーザ名「 %s 」はすでに存在します。';
+$string['viewsubmittedmessage1'] = '%s が「 %s 」を %s に提出しました。
+
+あなたのLMSで活動「 %s 」をクリックして提出を評定してください。';
+$string['viewsubmittedsubject1'] = '%s への評価提出';
 $string['webserviceauthdisabled'] = 'このインスティテューションのウェブサービス認証は有効にされていません。';
 $string['webserviceproviderenabled'] = '内向きのウェブサービスリクエスト許可';
-$string['institutiondenied'] = '「 %s 」へのアクセスが拒否されました。あなたのインスティテューション管理者にご連絡ください。';
-$string['notreadylabel'] = '準備未了';
-$string['readylabel'] = '準備完了';
-$string['parentauthforlti'] = '親認証局';
 
 ?>
