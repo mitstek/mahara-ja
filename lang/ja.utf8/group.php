2e88c38b1ae364ddd14072fa475890edec50df5a
Mitsuhiro Yoshida <mits@mitstek.com>
2018-11-19 00:55:39 +0900

    Brushed up translated strings for group.

--- lang/ja.utf8/group.php
+++ lang/ja.utf8/group.php
@@ -20,7 +20,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2017-02-14 06:17:06 UTC
+ * @updated    2018-11-18 15:52:51 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
  * @copyright  (C) 2006 onwards Catalyst IT Ltd http://catalyst.net.nz
  *
@@ -44,7 +44,7 @@ $string['useradded'] = 'ユーザが追加されました。';
 $string['editgroup'] = 'グループを編集する';
 $string['savegroup'] = 'グループを保存する';
 $string['groupsaved'] = 'グループが正常に保存されました。';
-$string['invalidgroup'] = 'グループがありません。';
+$string['invalidgroup'] = 'グループが存在しません。';
 $string['canteditdontown'] = 'あなたの所有ではないためこのグループを編集できません。';
 $string['groupdescription'] = 'グループ説明';
 $string['membershiptype'] = 'グループメンバーシップタイプ';
@@ -172,7 +172,7 @@ $string['groupsnotin'] = '私が参加していないグループ';
 $string['allgroups'] = 'すべてのグループ';
 $string['allgroupmembers'] = 'グループメンバーすべて';
 $string['trysearchingforgroups'] = '参加するには%sグループを探す%sをお試しください!';
-$string['nogroupsfound'] = 'グループがありません :(';
+$string['nogroupsfound'] = 'グループが見つかりませんでした。';
 $string['group'] = 'グループ';
 $string['Group'] = 'グループ';
 $string['groups'] = 'グループ';
