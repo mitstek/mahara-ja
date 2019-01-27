b1bd6ef291168803209ac0744846ab9b16a2046b
Mitsuhiro Yoshida <mits@mitstek.com>
2019-01-27 14:36:56 +0900

    Brushed up translated strings for core.

--- lang/ja.utf8/mahara.php
+++ lang/ja.utf8/mahara.php
@@ -218,7 +218,7 @@ $string['profilecompletenesspreview'] = 'プロファイル完了プレビュー
 $string['profilecompletenesstips'] = 'プロファイル完了ヒント';
 $string['progressbargenerictask'] = '%d 追加する: %s';
 $string['profilecompletionforwhichinstitution'] = '-';
-$string['noprogressitems'] = 'このインスティテューションに関してプロファイル完了アイテムはありません。';
+$string['noprogressitems'] = 'このインスティテューションにプロファイル完了アイテムはありません。';
 $string['onlineusers'] = 'オンラインユーザ';
 $string['lastminutes'] = '直近の %s 分間';
 $string['allonline'] = 'すべてのオンラインユーザを表示する';
@@ -279,10 +279,10 @@ $string['accountcreatedchangepasswordhtml'] = '<p>%s さん</p>
 <p>利用開始するには<a href="%s">%s</a>にアクセスしてください!</p>
 
 <p>%s サイト管理者</p>';
-$string['accountexpirywarning'] = 'アカウント終了通知';
+$string['accountexpirywarning'] = 'アカウント有効期限切れ通知';
 $string['accountexpirywarningtext'] = '%s さん
 
-あなたの %s のアカウントは %s で終了します。
+あなたの %s のアカウントは %s に有効期限が切れます。
 
 エクスポートツールを使用してあなたのポートフォリオのコンテンツを保存することをお勧めします。この機能の使用に関するインストラクションはユーザガイドに記載されています。
 
@@ -293,17 +293,17 @@ $string['accountexpirywarningtext'] = '%s さん
 %s サイト管理者';
 $string['accountexpirywarninghtml'] = '<p>%s さん</p>
 
-<p>あなたの %s のアカウントは %s で終了します。</p>
+<p>あなたの %s のアカウントは %s に有効期限が切れます。</p>
 
 <p>エクスポートツールを使用してあなたのポートフォリオのコンテンツを保存することをお勧めします。この機能の使用に関するインストラクションはユーザガイドに記載されています。</p>
 
 <p>あなたのアカウントアクセスの有効期限を延長したい場合、または上記内容に関するご質問は<a href="%s">お気軽にお問い合わせください</a>。</P>
 
 <p>%s サイト管理者</p>';
-$string['institutionmembershipexpirywarning'] = 'インスティテューションメンバーシップ終了通知';
+$string['institutionmembershipexpirywarning'] = 'インスティテューションメンバーシップ有効期限切れ通知';
 $string['institutionmembershipexpirywarningtext'] = '%s さん
 
-あなたの %s における %s のメンバーシップは %s で終了します。
+あなたの %s における %s のメンバーシップは %s に有効期限が切れます。
 
 あなたのメンバーシップの有効期限を延長したい場合、または上記内容に関するご質問はお気軽にお問い合わせください:
 
@@ -312,7 +312,7 @@ $string['institutionmembershipexpirywarningtext'] = '%s さん
 %s サイト管理者';
 $string['institutionmembershipexpirywarninghtml'] = '<p>%s さん</p>
 
-<p>あなたの %s における %s のメンバーシップは %s で終了します。</p>
+<p>あなたの %s における %s のメンバーシップは %s に有効期限が切れます。</p>
 
 <p>あなたのメンバーシップの有効期限を延長したい場合、または上記内容に関するご質問は<a href="%s">お気軽にお問い合わせください</a>。</P>
 
