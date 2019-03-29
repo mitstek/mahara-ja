dd3668347baff2106ed53a657f2f4fb736b67eda
Mitsuhiro Yoshida <mits@mitstek.com>
2019-03-29 21:18:34 +0900

    Bug 1820927: Translated new strings for page to fix language strings clash.

--- lang/ja.utf8/view.php
+++ lang/ja.utf8/view.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2018-12-10 05:19:33 UTC
+ * @updated    2019-03-29 12:17:26 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -93,8 +93,6 @@ $string['Views'] = 'ページ';
 $string['portfolio'] = 'ポートフォリオ';
 $string['portfolios'] = 'ポートフォリオ';
 $string['Viewscollections'] = 'ページおよびコレクション';
-$string['viewsubmittedtogroup'] = 'このページは<a href="%s">%s</a>に送信されました。';
-$string['viewsubmittedtogroupon'] = 'このページは<a href="%s">%s</a>に対して %s に送信されました。';
 $string['viewsubmittedtogroupgrade'] = 'このポートフォリオは課題「 <a href="%s">%s</a> 」 (%s) に対して %s に送信されました。　';
 $string['collectionsubmittedtogroup'] = 'このコレクションは<a href="%s">%s</a>に送信されました。';
 $string['collectionsubmittedtogroupon'] = 'このコレクションは<a href="%s">%s</a>に対して %s に送信されました。';
@@ -412,8 +410,9 @@ $string['latestviewed'] = '最終閲覧日時';
 $string['mostvisited'] = '閲覧回数';
 $string['mostcomments1'] = 'コメント数';
 $string['dashboard'] = 'ダッシュボード';
-$string['profile'] = 'プロファイル';
-$string['grouphomepage'] = 'グループホームページ';
+$string['Profile'] = 'プロファイル';
+$string['Portfolio'] = 'ポートフォリオ';
+$string['Grouphomepage'] = 'グループホームページ';
 $string['grouphomepagedescription'] = 'グループホームページにはこのグループのAboutタブに表示されるコンテンツを含みます。';
 $string['pageaccessrules'] = 'ページアクセスルール';
 $string['sharedwithme'] = '私への共有';
