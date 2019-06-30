33f2ffc6b3e5a18a775a97aefa019e7cea5e75ec
Mitsuhiro Yoshida <mits@mitstek.com>
2019-06-30 18:11:39 +0900

    Brushed up translated strings for notification.

--- notification/email/lang/ja.utf8/notification.email.php
+++ notification/email/lang/ja.utf8/notification.email.php
@@ -30,7 +30,7 @@ defined('INTERNAL') || die();
 
 $string['emailsubject'] = '%s';
 $string['emailheader'] = 'あなたに %s から通知が送信されました。メッセージは次のとおりです:';
-$string['emailfooter'] = 'これは %s から自動的に生成された通知です。あなたの通知プリファレンスを更新するには %s にアクセスしてください。';
+$string['emailfooter'] = 'これは %s からの自動生成通知です。あなたの通知プリファレンスを更新するには %s にアクセスしてください。';
 $string['referurl'] = '詳細 %s';
 $string['name'] = 'メール';
 
