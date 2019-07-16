7945ea250c9fdd319d9b80178efa1ee0b181e78f
Mitsuhiro Yoshida <mits@mitstek.com>
2019-07-16 11:49:54 +0900

    Brushed up a translated string for Exact user searches.

--- search/internal/lang/ja.utf8/search.internal.php
+++ search/internal/lang/ja.utf8/search.internal.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2017-08-29 08:26:15 UTC
+ * @updated    2019-07-16 02:49:43 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,6 +14,6 @@
 defined('INTERNAL') || die();
 
 $string['exactusersearch'] = 'ユーザ完全一致検索';
-$string['exactusersearchdescription3'] = '「ユーザを検索する」ボックスおよび「人を探す」ページの結果には検索キーワード全体に合致するプロファイルフィールドのユーザのみ返されます。';
+$string['exactusersearchdescription3'] = '「ユーザを検索する」ボックスおよび「人を探す」ページの結果には検索キーワードすべてがプロファイルフィールドに合致するユーザのみ返されます。';
 
 ?>
