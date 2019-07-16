eb7ace55018f80e1b32d4012fbb824660faab124
Mitsuhiro Yoshida <mits@mitstek.com>
2019-07-16 11:51:41 +0900

    Brushed up a translated string for Exact user searches.

--- search/internal/lang/ja.utf8/search.internal.php
+++ search/internal/lang/ja.utf8/search.internal.php
@@ -14,6 +14,6 @@
 defined('INTERNAL') || die();
 
 $string['exactusersearch'] = 'ユーザ完全一致検索';
-$string['exactusersearchdescription2'] = '「ユーザを検索する」ボックスおよび「フレンドを探す」ページの結果には検索キーワード全体に合致するプロファイルフィールドのユーザのみ返されます。';
+$string['exactusersearchdescription2'] = '「ユーザを検索する」ボックスおよび「フレンドを探す」ページの結果には検索キーワードすべてがプロファイルフィールドに合致するユーザのみ返されます。';
 
 ?>
