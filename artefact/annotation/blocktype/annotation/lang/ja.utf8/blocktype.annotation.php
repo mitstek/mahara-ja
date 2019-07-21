197ec6d9f269b4295fd07c4bff2967ac6e209a4c
Mitsuhiro Yoshida <mits@mitstek.com>
2019-07-21 16:51:02 +0900

    Brushed up translated strings for Annotation block.

--- artefact/annotation/blocktype/annotation/lang/ja.utf8/blocktype.annotation.php
+++ artefact/annotation/blocktype/annotation/lang/ja.utf8/blocktype.annotation.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2015-05-31 16:59:52 UTC
+ * @updated    2019-07-21 07:50:54 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,7 +14,7 @@
 defined('INTERNAL') || die();
 
 $string['title'] = 'アノテーション';
-$string['description'] = 'なぜあなたのエビデンスが標準に合致するのか、あなたのアノテーションを表示するためのブロックです。';
-$string['annotationreadonlymessage'] = 'フィードバックが投稿されたためこれ以上アノテーションを編集することはできません。';
+$string['description'] = 'なぜあなたのエビデンスが標準に合致するかに関するアノテーションを表示するためのブロックです。';
+$string['annotationreadonlymessage'] = 'フィードバックが投稿されたため、これ以上アノテーションを編集できません。';
 
 ?>
