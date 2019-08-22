926c625a576a5f5ccc14f528ae0ad2d3144e481c
Mitsuhiro Yoshida <mits@mitstek.com>
2019-08-23 00:24:32 +0900

    Brushed up translated strings for "single".

--- artefact/file/blocktype/image/lang/ja.utf8/blocktype.image.php
+++ artefact/file/blocktype/image/lang/ja.utf8/blocktype.image.php
@@ -29,7 +29,7 @@
 defined('INTERNAL') || die();
 
 $string['title'] = 'イメージ';
-$string['description'] = 'あなたのファイルエリアの単独イメージです。';
+$string['description'] = 'あなたのファイルエリアの単一イメージです。';
 $string['showdescription'] = '説明を表示する';
 $string['width'] = '幅';
 $string['widthdescription1'] = 'あなたのイメージの幅 (ピクセル) を指定してください。イメージはこの幅にサイズ変更されます。オリジナルサイズが大きすぎる場合、ブロック幅に縮小されます。';
