4f64013610a8e9a9b3fa84035c729a2b14e12f4d
Mitsuhiro Yoshida <mits@mitstek.com>
2019-08-23 00:21:24 +0900

    Brushed up translated strings for "single".

--- artefact/file/blocktype/pdf/lang/ja.utf8/blocktype.pdf.php
+++ artefact/file/blocktype/pdf/lang/ja.utf8/blocktype.pdf.php
@@ -14,7 +14,7 @@
 defined('INTERNAL') || die();
 
 $string['title'] = 'PDF';
-$string['description'] = 'あなたのファイルエリアの単独PDFファイルです。';
+$string['description'] = 'あなたのファイルエリアの単一PDFファイルです。';
 $string['pdfwarning'] = '注意: 大きなPDFファイルを開いた場合、ブラウザのリソースに高負荷がかかる場合があります。';
 
 ?>
