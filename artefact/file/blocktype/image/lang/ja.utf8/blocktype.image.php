88814a800239441808a88c5e4908d2cda015e227
Mitsuhiro Yoshida <mits@mitstek.com>
2019-08-27 21:12:04 +0900

    Brushed up a translated string for image block.

--- artefact/file/blocktype/image/lang/ja.utf8/blocktype.image.php
+++ artefact/file/blocktype/image/lang/ja.utf8/blocktype.image.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2019-08-22 15:18:42 UTC
+ * @updated    2019-08-27 12:11:25 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -17,6 +17,6 @@ $string['title'] = 'イメージ';
 $string['description'] = 'あなたのファイルエリアの単一イメージです。';
 $string['showdescription'] = '説明を表示する';
 $string['width'] = '幅';
-$string['widthdescription1'] = 'あなたのイメージの幅 (ピクセル) を指定してください。イメージはこの幅にサイズ変更されます。オリジナルサイズが大きすぎる場合、ブロック幅に縮小されます。';
+$string['widthdescription1'] = 'あなたのイメージの幅 (ピクセル) を指定してください。イメージはこの幅にサイズ変更されます。イメージのオリジナルサイズを使用する場合、空白のままにしてください。オリジナルサイズが大きすぎる場合、ブロック幅に縮小されます。';
 
 ?>
