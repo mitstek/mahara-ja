cd1c7ae471961e4c3eb7ad51704221fa9412d522
Mitsuhiro Yoshida <mits@mitstek.com>
2019-09-20 19:03:02 +0900

    Bug 1844723: Translated new strings for date am/pm suffix options.

--- lang/ja.utf8/langconfig.php
+++ lang/ja.utf8/langconfig.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2018-11-01 06:24:41 UTC
+ * @updated    2019-09-20 09:47:47 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -18,17 +18,24 @@ $string['pieform_calendar_timeformat'] = '%%H:%%M';
 $string['strftimedate'] = '%%Y年 %%m月 %%d日';
 $string['strftimedateshort'] = '%%Y/%%m/%%d';
 $string['strftimedatetime'] = '%%Y年 %%m月 %%d日 %%H:%%M';
+$string['strftimedatetimesuffix'] = '%%Y年 %%m月 %%d日 %%H:%%M (%%P)';
 $string['strftimedatetimeshort'] = '%%Y/%%m/%%d %%H:%%M';
-$string['strftimedaydate'] = '%%Y年 %%m月 %%d日(%A)';
-$string['strftimedaydatetime'] = '%%Y年 %%m月 %%d日(%%A) %%H:%%M';
+$string['strftimedaydate'] = '%%Y年 %%m月 %%d日 (%%A)';
+$string['strftimedaydatetime'] = '%%Y年 %%m月 %%d日 (%%A) %%H:%%M';
+$string['strftimedaydatetimesuffix'] = '%%Y年 %%m月 %%d日 (%%A) %%H:%%M (%%P)';
 $string['strftimedayshort'] = '%%Y年 %%m月 %%d日';
 $string['strftimedaytime'] = '(%%a) %%H:%%M';
+$string['strftimedaytimesuffix'] = '(%%a) %%H:%%M (%%P)';
 $string['strftimemonthyear'] = '%%Y年 %%m月';
 $string['strftimenotspecified'] = '未指定';
 $string['strftimerecent'] = '%%m月 %%d日 %%H:%%M';
+$string['strftimerecentsuffix'] = '%%m月 %%d日 %%H:%%M (%%P)';
 $string['strftimerecentyear'] = '%%Y年 %%m月 %%d日 %%H:%%M';
-$string['strftimerecentfull'] = '%%Y年 %%m月 %%d日(%%a) %%H:%%M';
+$string['strftimerecentyearsuffix'] = '%%Y年 %%m月 %%d日 %%H:%%M (%%P)';
+$string['strftimerecentfull'] = '%%Y年 %%m月 %%d日 (%%a) %%H:%%M';
+$string['strftimerecentfullsuffix'] = '%%Y年 %%m月 %%d日 (%%a) %%H:%%M  (%%P)';
 $string['strftimetime'] = '%%H:%%M';
+$string['strftimetimesuffix'] = '%%H:%%M  (%%P)';
 $string['strfdaymonthyearshort'] = '%%Y/%%m/%%d';
 $string['strfdateofbirth'] = '%%Y/%%m/%%d';
 $string['strftimew3cdatetime'] = '%%Y-%%m-%%dT%%H:%%M:%%S%%z';
