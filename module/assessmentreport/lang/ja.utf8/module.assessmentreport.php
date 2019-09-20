06ae7eb20cb8c0da875e144206929d5cfba884e3
Mitsuhiro Yoshida <mits@mitstek.com>
2019-09-20 09:12:34 +0900

    Bug 1843691: Updated a translated string for submission report notification based on en change.

--- module/assessmentreport/lang/ja.utf8/module.assessmentreport.php
+++ module/assessmentreport/lang/ja.utf8/module.assessmentreport.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2019-06-23 05:18:30 UTC
+ * @updated    2019-09-20 00:11:28 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,6 +14,7 @@
 defined('INTERNAL') || die();
 
 $string['message'] = 'これはあなたが「 %s 」 (%s) をグループ「 %s 」 (提出日: %s) に正常に提出したこと確認するためのものです。
+
 この提出に関して質問がある場合、グループ管理者にご連絡ください。';
 $string['subject'] = 'あなたの「 %s 」は正常に提出されました。';
 $string['page'] = 'ページ';
