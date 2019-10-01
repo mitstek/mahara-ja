60ec901f6fd284927ef7e018b5357c63fdb21bce
Mitsuhiro Yoshida <mits@mitstek.com>
2019-10-02 07:30:20 +0900

    Bug 1837999: Translated new strings and a help file for Plans.

--- artefact/plans/lang/ja.utf8/artefact.plans.php
+++ artefact/plans/lang/ja.utf8/artefact.plans.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (http://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2018-04-23 15:12:55 UTC
+ * @updated    2019-10-01 22:18:24 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,6 +14,7 @@
 defined('INTERNAL') || die();
 
 $string['pluginname'] = 'プラン';
+$string['groupplans'] = 'プラン';
 $string['canteditdontownplan'] = 'あなたの所有ではないためこのプランを編集することはできません。';
 $string['description'] = '説明';
 $string['deleteplanconfirm'] = '本当にこのプランを削除してもよろしいですか? このプランを削除することでプランの中に含まれるすべてのタスクも削除されます。';
@@ -24,30 +25,31 @@ $string['editingplan'] = 'プランの編集';
 $string['managetasks'] = 'タスクを管理する';
 $string['managetasksspecific'] = '「 %s 」のタスクを管理する';
 $string['newplan'] = '新しいプラン';
-$string['noplansaddone'] = 'まだプランはありません。%sプランを追加してください%s!';
+$string['noplansaddone'] = 'まだプランはありません。%sプランを追加してください%s。';
 $string['noplans'] = '表示するプランはありません。';
+$string['Plan'] = 'プラン';
+$string['Plans'] = 'プラン';
 $string['plan'] = 'プラン';
 $string['plans'] = 'プラン';
-$string['Plans'] = 'プラン';
-$string['Plan'] = 'プラン';
 $string['plandeletedsuccessfully'] = 'プランが正常に削除されました。';
 $string['plannotdeletedsuccessfully'] = 'このプランの削除中にエラーが発生しました。';
 $string['plannotsavedsuccessfully'] = 'このフォームの送信中にエラーが発生しました。マークされたフィールドを確認して再度お試しください。';
 $string['plansavedsuccessfully'] = 'プランが正常に保存されました。';
-$string['planstasks'] = 'プラン「 %s 」のタスク';
+$string['planstasks1'] = 'プラン %s 「 %s 」のタスク';
+$string['templateplan'] = 'テンプレート';
 $string['planstasksdescription'] = 'あなたのプランを作成するには以下にタスクを追加するか「 %s 」ボタンを使用してください。';
 $string['saveplan'] = 'プランを保存する';
 $string['title'] = 'タイトル';
 $string['titledesc'] = 'それぞれのプランブロックタイプ内タスクに表示するためにタイトルが使用されます。';
-$string['youhavenplan'] = 'あなたには %d プランがあります。';
 $string['addtask'] = 'タスクを追加する';
 $string['addtaskspecific'] = '「 %s 」にタスクを追加する';
 $string['alltasks'] = 'すべてのタスク';
 $string['canteditdontowntask'] = 'あなたの所有ではないためこのタスクを編集することはできません。';
 $string['completed'] = '完了';
 $string['incomplete'] = '未完了';
-$string['overdue'] = '期限超過';
+$string['overdue'] = '期限切れ';
 $string['completiondate'] = '完了予定日';
+$string['completiondatedescription'] = 'フォーマット %s を使用してください。テンプレートのデフォルト完了日はグループの編集可能終了日より取得されます。';
 $string['completeddesc'] = 'あなたのタスクを完了済みにします。';
 $string['deletetaskconfirm'] = '本当にこのタスクを削除してもよろしいですか?';
 $string['deletetask'] = 'タスクを削除する';
@@ -60,12 +62,23 @@ $string['newtask'] = '新しいタスク';
 $string['notasks'] = '表示するタスクはありません。';
 $string['notasksaddone'] = 'まだタスクはありません。%sタスクを追加してください%s!';
 $string['savetask'] = 'タスクを保存する';
-$string['task'] = 'タスク';
 $string['Task'] = 'タスク';
-$string['tasks'] = 'タスク';
 $string['Tasks'] = 'タスク';
+$string['task'] = 'タスク';
+$string['tasks'] = 'タスク';
+$string['Grouptasks'] = 'グループタスク';
 $string['taskdeletedsuccessfully'] = 'タスクが正常に削除されました。';
 $string['tasksavedsuccessfully'] = 'タスクが正常に保存されました。';
+$string['selectionstatechangedpagereload'] = '選択状態が変更されました。現在の状態を表示するため、このページをリロードしてください。';
+$string['completedstatechangedpagereload'] = '選択状態が変更されました。現在の状態を表示するため、このページをリロードしてください。';
+$string['noselectiontask'] = 'これは課題タスクではありません。';
+$string['ownerfieldsnotset'] = 'オーナーフィールドが設定されていません。';
+$string['unsupportedportfoliotype'] = 'これはサポートされないポートフォリオタイプです。';
+$string['viewandoutputcontainsameelement'] = 'タスクおよび課題ポートフォリオは同じページまたはコレクション内のページを含めません。';
+$string['grouptaskselected'] = 'プランおよび選択済みタスクはあなたのパーソナルポートフォリオエリアに移動されました。適切な場合、課題ポートフォリオも同様にコピーされています。';
+$string['grouptaskunselected'] = 'プランおよび選択済みタスクはあなたのパーソナルポートフォリオエリアから削除されました。適切な場合、未編集の課題ポートフォリオも同様に削除されています。';
+$string['unselecttaskconfirm'] = '本当にあなたのプランからこのタスクを削除してもよろしいですか?';
+$string['wrongroleforfunction'] = 'あなたのロールはこの機能の実行を目的としていません。';
 $string['ntasks'] = '%s タスク';
 $string['duplicatedplan'] = '複製プラン';
 $string['existingplans'] = '既存プラン';
@@ -73,5 +86,47 @@ $string['duplicatedtask'] = '複製タスク';
 $string['existingtasks'] = '既存タスク';
 $string['progress_plan'] = '%s プランを追加する';
 $string['progress_task'] = '%s タスクをプランに追加する';
+$string['showassignedview'] = 'タスクプレビュー';
+$string['showassignedoutcome'] = 'ポートフォリオプレビュー';
+$string['editassignedoutcome'] = 'ポートフォリオを開く';
+$string['submitassignedoutcome'] = '評価のために送信する';
+$string['outcomeiscurrentlysubmitted'] = '現在、このポートフォリオは評価のため送信されています。';
+$string['template'] = 'テンプレート';
+$string['templatedescription'] = 'このプランをグループプラン作成のためのテンプレートとして使用します。';
+$string['startdate'] = '開始日';
+$string['startdatedescription'] = 'フォーマット %s を使用してください。テンプレートのデフォルト開始日はグループの編集可能開始日より取得されます。';
+$string['reminder'] = 'リマインダ';
+$string['reminderdescription'] = '完了日に関するリマインダ通知を送信します。';
+$string['taskview'] = 'タスクページ';
+$string['taskviewdescription'] = 'このタスクに詳細タスク説明ページを結びつけます。';
+$string['outcome'] = '課題ポートフォリオ';
+$string['outcomedescription'] = 'このタスクを終了するためポートフォリオを結びつけてください。注意: このポートフォリオは1つのタスクにのみ使用できます。';
+$string['selectionplan'] = '課題タスク';
+$string['selectionplandescription'] = 'このテンプレートプランに基づいてグループ内でプランが作成された後、グループメンバーは課題を完了するためこのプランのタスクを選択できます。';
+$string['none'] = 'ノート';
+$string['startdatemustbebeforecompletiondate'] = '開始日は完了日の前に設定してください。';
+$string['completiondatemustbeinfuture'] = '完了日は未来の日付にする必要があります。';
+$string['completiondatemustbesetforreminder'] = 'あなたはリマインダを設定するため完了日を入力する必要があります。';
+$string['choosetemplate'] = 'テンプレートを選択する';
+$string['notemplate'] = 'テンプレートなし';
+$string['close'] = 'クローズ';
+$string['fromtemplate'] = 'テンプレートから';
+$string['taskviewsfortemplateplan'] = 'プラン「 %s 」のタスク詳細を含むページ';
+$string['templatedialogdescription'] = 'このグループプランのベースとしてあなたのパーソナルプランよりテンプレートプランを選択してください。すべての関連タスク、タスクページおよび課題ポートフォリオはこのグループに自動的にコピーされます。';
+$string['targetgroupplancollectiontitleprefix'] = 'プランタスク:';
+$string['remindersubject'] = 'プランのタスク完了に関するリマインダ';
+$string['URL'] = 'タスクにリンクする';
+$string['reminderinfo'] = '情報';
+$string['reminderinfonearlydue'] = '残り %s 日';
+$string['reminderinfodue'] = '期限切れまで %s 日';
+$string['emailfooter'] = 'これは %s から自動生成された通知です。';
+$string['youhaveremindertasksmessagetext'] = '%s さん、こんにちは。
+
+私たちはあなたに次のプランタスクの完了を希望しています。
+
+%s
+
+%s チーム';
+$string['visitplans'] = 'タスク完了リマインダ';
 
 ?>
