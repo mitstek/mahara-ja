ac1c80922894fbc20d63468d63b9c3b56e8906df
Mitsuhiro Yoshida <mits@mitstek.com>
2019-10-05 15:35:51 +0900

    Replaced http://mitstek.com with https://mitstek.com.

--- lang/ja.utf8/view.php
+++ lang/ja.utf8/view.php
@@ -3,7 +3,7 @@
  *
  * @package    mahara
  * @subpackage lang (Japanese)
- * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
  * @updated    2016-07-21 11:11:12 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
