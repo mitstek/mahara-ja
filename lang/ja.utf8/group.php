6fb1b6432819cec9f6c096feb6db71a441a4e247
Mitsuhiro Yoshida <mits@mitstek.com>
2019-10-05 15:34:59 +0900

    Replaced http://mitstek.com with https://mitstek.com.

--- lang/ja.utf8/group.php
+++ lang/ja.utf8/group.php
@@ -3,7 +3,7 @@
  *
  * @package    mahara
  * @subpackage lang (Japanese)
- * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
  * @updated    2019-04-03 03:18:06 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
--- lang/ja.utf8/mahara.php
+++ lang/ja.utf8/mahara.php
@@ -3,7 +3,7 @@
  *
  * @package    mahara
  * @subpackage lang (Japanese)
- * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
  * @updated    2018-09-10 05:18:18 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
