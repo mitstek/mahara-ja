4a98b8590eaa3b04dc76543e9632180f6cf69e8c
Mitsuhiro Yoshida <mits@mitstek.com>
2019-10-05 15:37:19 +0900

    Replaced http://mitstek.com with https://mitstek.com.

--- blocktype/newviews/lang/ja.utf8/blocktype.recentforumposts.php
+++ blocktype/newviews/lang/ja.utf8/blocktype.recentforumposts.php
@@ -18,7 +18,7 @@
  *
  * @package    mahara
  * @subpackage lang (Japanese)
- * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
  * @updated    2010-05-22 20:12:46 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
--- lang/ja.utf8/export.php
+++ lang/ja.utf8/export.php
@@ -18,7 +18,7 @@
  *
  * @package    mahara
  * @subpackage lang (Japanese)
- * @translator Mitsuhiro Yoshida (http://mitstek.com/)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
  * @updated    2012-03-02 16:00:36 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
