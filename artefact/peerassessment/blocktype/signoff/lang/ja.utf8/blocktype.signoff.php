7e91a1d8f96f44a157eeff0e6c96833cd7059944
Mitsuhiro Yoshida <mits@mitstek.com>
2019-11-03 22:40:36 +0900

    Brushed up translated strings for Sign-off block.

--- artefact/peerassessment/blocktype/signoff/lang/ja.utf8/blocktype.signoff.php
+++ artefact/peerassessment/blocktype/signoff/lang/ja.utf8/blocktype.signoff.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2019-10-29 01:44:49 UTC
+ * @updated    2019-11-03 13:40:30 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -19,12 +19,12 @@ $string['placeholder'] = 'このブロックのコンテンツはページのブ
 $string['signoff'] = '承認';
 $string['signoffdesc'] = 'すべての必要条件を満たした場合、評価準備が完了したことを示すため、ポートフォリオオーナーはページを承認できます。';
 $string['verify'] = '検証';
-$string['verifydesc'] = 'ポートフォリオ評価作業の一環としてモデレータがこのページを検証する必要があるかどうか決定してください。';
+$string['verifydesc'] = 'ポートフォリオ評価過程の一環としてモデレータがこのページを検証する必要があるかどうか決定してください。';
 $string['signedoff'] = '承認';
 $string['verified'] = '検証';
 $string['signoffpagetitle'] = '承認ページ';
 $string['signoffpagedesc'] = 'あなたがすべての必要条件を満たしたことを示したい場合、「Yes」を選択してこのページを承認してください。中止するには「No」を選択してください。';
-$string['signoffpageundodesc'] = '「Yes」を選択した場合、あなたは承認ステータスを削除することになります。検証が評価作業の一環である場合、検証も削除されます。中止するには「No」を選択してください。';
+$string['signoffpageundodesc'] = '「Yes」を選択した場合、あなたは承認ステータスを削除することになります。検証が評価過程の一環である場合、検証も削除されます。中止するには「No」を選択してください。';
 $string['signoffpageconfirm'] = 'この処理を実行してもよろしいですか?';
 $string['verifypagetitle'] = 'ページを検証する';
 $string['verifypagedesc'] = 'ポートフォリオのオーナーがこのページすべての必要条件を満たしていると検証した場合、「Yes」を選択してください。検証せずにページに戻るには「No」を選択してください。';
