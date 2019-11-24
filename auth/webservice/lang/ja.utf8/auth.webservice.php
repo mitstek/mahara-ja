0584a9bfc224e9206142c2a50b1755d2a52c8548
Mitsuhiro Yoshida <mits@mitstek.com>
2019-11-25 05:30:52 +0900

    Brushed up translated strings for Internal authentication.

--- auth/webservice/lang/ja.utf8/auth.webservice.php
+++ auth/webservice/lang/ja.utf8/auth.webservice.php
@@ -23,11 +23,11 @@ $string['webserviceconnectionsconfigdesc'] = '登録済みプラグインの接�
 $string['completeregistration'] = '登録を完了する';
 $string['emailalreadytaken'] = 'このメールアドレスはすでにここで登録されています。';
 $string['iagreetothetermsandconditions'] = '私は利用条件に同意します。';
-$string['passwordinvalidform1'] = 'あなたのパスワードは少なくとも半角英数字 %s 文字にしてください。パスワードでは大文字小文字を区別します。また、あなたのユーザ名と異なるものにしてください。あなたのパスワードには %s を含む必要があります。';
+$string['passwordinvalidform1'] = 'あなたのパスワードは少なくとも半角英数字 %s 文字の長さにしてください。パスワードでは大文字小文字を区別します。また、あなたのユーザ名と異なるものにしてください。あなたのパスワードには %s を含む必要があります。';
 $string['registeredemailsubject'] = 'あなたは %s に登録されました。';
 $string['registeredemailmessagetext'] = '%s さん
 
-%s へのご登録ありがとうございます。サインアップ処理を完了するには次のリンクにアクセスしてください:
+%s へのご登録ありがとうございます。サインアップ手続きを完了するには次のリンクにアクセスしてください:
 
 %sregister.php?key=%s
 
@@ -36,7 +36,7 @@ $string['registeredemailmessagetext'] = '%s さん
 --
 %s チーム';
 $string['registeredemailmessagehtml'] = '<p>%s さん</p>
-<p>%s へのご登録ありがとうございます。サインアップ処理を完了するには次のリンクにアクセスしてください:</p>
+<p>%s へのご登録ありがとうございます。サインアップ手続きを完了するには次のリンクにアクセスしてください:</p>
 <p><a href="%sregister.php?key=%s">%sregister.php?key=%s</a></p>
 <p>リンクは24時間で有効期限が切れます。</p>
 
