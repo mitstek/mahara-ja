7c63707721a7c50ffd9fb833f9bb72d08f1e0770
Mitsuhiro Yoshida <mits@mitstek.com>
2019-12-10 12:52:52 +0900

    Translated a new string for page.

--- lang/ja.utf8/view.php
+++ lang/ja.utf8/view.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2019-10-07 20:03:58 UTC
+ * @updated    2019-12-10 03:52:41 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -446,8 +446,8 @@ $string['blockssizeupdated'] = 'ブロックサイズが正常に変更されま
 $string['dimensionsnotset'] = 'ブロックサイズが設定されていません。';
 $string['dontaskagain'] = '承認および記憶する';
 $string['pleaseconfirmtranslate'] = 'ページレイアウトを変換する';
-$string['confirmtranslationmessage'] = 'Mahara 19.10の一部として私たちはページレイアウトの新しい作成方法を導入しました。このページを編集するにはあなたは古いレイアウトを新しいグリッドレイアウトに変換する必要があります。
-あなたがこのページを単独で変換したい場合、「承認」をクリックしてください。あなたが編集するページすべてを変換して再度このメッセージを表示したくない場合、「承認および記憶する」をクリックしてください。このオプションは<a href="%s">設定</a>で変更できます。ページに戻るには「キャンセル」をクリックしてください。';
+$string['confirmconversionmessage'] = 'Mahara 19.10の一部として私たちはページレイアウトの新しい作成方法を導入しました。このページを編集するにはあなたは古いレイアウトを新しいレイアウトに変換する必要があります。
+あなたがこのページのみを変換したい場合、「承認」をクリックしてください。すべてのページを変換して再度このメッセージを表示したくない場合、「承認および記憶する」をクリックしてください。このオプションはあなたの<a href="%s">プリファレンス</a>で変更できます。編集せずにページに戻るには「キャンセル」をクリックしてください。';
 $string['accessibleview'] = 'アクセシブルレイアウト';
 $string['accessibleviewdescription'] = '1カラムページレイアウトを作成してドラッグ＆ドロップの代わりにキーボードで編集します。';
 $string['itemgrabbed'] = 'アイテムグラブ: %s';
