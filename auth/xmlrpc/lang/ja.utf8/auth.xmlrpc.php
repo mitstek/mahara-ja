c2d50c9404d0026c42d0d7540a9c33aaccf57029
Mitsuhiro Yoshida <mits@mitstek.com>
2019-12-29 09:43:27 +0900

    Brushed up a translated string for XML-RPC authentication.

--- auth/xmlrpc/lang/ja.utf8/auth.xmlrpc.php
+++ auth/xmlrpc/lang/ja.utf8/auth.xmlrpc.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2018-03-08 07:40:10 UTC
+ * @updated    2019-12-29 00:43:15 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -15,7 +15,7 @@ defined('INTERNAL') || die();
 
 $string['title'] = 'XML-RPC';
 $string['xmlrpcconfig'] = 'XML-RPC設定';
-$string['description'] = '外部アプリケーションからSSOで認証する';
+$string['description'] = '外部アプリケーションからSSO認証する';
 $string['notusable'] = 'XML-RPC、CurlおよびOpenSSL PHP拡張モジュールをインストールしてください。';
 $string['networkingdisabledonthissite'] = 'このサイトではネットワーキングが無効にされています。';
 $string['networkservers'] = 'ネットワークサーバ';
