40e73f635df7f5ecd6fdf36b949d94f5f5ec6bd1
Mitsuhiro Yoshida <mits@mitstek.com>
2019-12-30 10:07:23 +0900

    Brushed up a translated string for Group members block.

--- blocktype/groupmembers/lang/ja.utf8/blocktype.groupmembers.php
+++ blocktype/groupmembers/lang/ja.utf8/blocktype.groupmembers.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2016-10-11 05:42:47 UTC
+ * @updated    2019-12-30 01:07:19 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -15,7 +15,7 @@ defined('INTERNAL') || die();
 
 $string['title'] = 'グループメンバー';
 $string['description'] = 'このグループのメンバー一覧を表示します。';
-$string['show_all'] = 'このグループのメンバーすべてを表示する ...';
+$string['show_all'] = 'このグループのメンバーすべてを表示する';
 $string['options_numtoshow_title'] = '表示メンバー';
 $string['options_numtoshow_desc'] = 'あなたが表示したいメンバー数です。';
 $string['options_order_title'] = '表示順';
