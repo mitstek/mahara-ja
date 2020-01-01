326f7bea24605aebe8fdcb1953a8bf09677cb6b1
Mitsuhiro Yoshida <mits@mitstek.com>
2020-01-01 20:41:05 +0900

    Brushed up a translated string for Navigation block.

--- blocktype/navigation/lang/ja.utf8/blocktype.navigation.php
+++ blocktype/navigation/lang/ja.utf8/blocktype.navigation.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2019-04-02 06:11:12 UTC
+ * @updated    2020-01-01 11:40:56 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -16,7 +16,7 @@ defined('INTERNAL') || die();
 $string['defaulttitledescription'] = 'ここを空白にした場合、コレクションのタイトルが使用されます。';
 $string['collection'] = 'コレクション';
 $string['title'] = 'ナビゲーション';
-$string['description'] = 'コレクションのページのナビゲーションです。';
+$string['description'] = 'ページのコレクションのナビゲーションです。';
 $string['nocollections1'] = 'コレクションはありません。<a href="%s">コレクションを作成してください</a>。';
 $string['copytoall'] = 'すべてのページに追加する';
 $string['copytoalldesc'] = 'このページが属しているコレクション内の現在のページでナビゲーションブロックが追加されていなページすべてにナビゲーションブロックを追加します。';
