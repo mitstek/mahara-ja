fa70e4ef69b6840c17b0262084f39ac1df16ee90
Mitsuhiro Yoshida <mits@mitstek.com>
2020-01-04 07:04:40 +0900

    Brushed up a translated string for Placeholder block.

--- blocktype/placeholder/lang/ja.utf8/blocktype.placeholder.php
+++ blocktype/placeholder/lang/ja.utf8/blocktype.placeholder.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2019-10-07 19:40:19 UTC
+ * @updated    2020-01-03 22:04:36 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -18,6 +18,6 @@ $string['description'] = 'あなたが希望するブロックタイプをプレ
 $string['placeholdertext'] = 'タイプを選択するためこのブロックを設定してください。';
 $string['defaulttitledescription'] = 'タイトルフィールドを空白にした場合、デフォルトタイトルが生成されます。';
 $string['contenttypes'] = 'コンテンツタイプ';
-$string['placeholderblocktypenotenabled'] = 'ブロックタイプ「プレースホルダ」をインストールおよび有効にする必要があります。すでにインストールされている場合、データベースのテーブル「blocktype_installed」で有効に設定してください。';
+$string['placeholderblocktypenotenabled'] = 'ブロックタイプ「プレースホルダ」をインストールおよび有効にする必要があります。すでにインストールされている場合、データベースのテーブル「blocktype_installed」で有効にしてください。';
 
 ?>
