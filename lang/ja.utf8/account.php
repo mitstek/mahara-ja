f834fe932d232832df8ef839f1436759795320be
Mitsuhiro Yoshida <mits@mitstek.com>
2020-01-30 20:56:56 +0900

    Brushed up a translated string for account.

--- lang/ja.utf8/account.php
+++ lang/ja.utf8/account.php
@@ -29,7 +29,7 @@
 defined('INTERNAL') || die();
 
 $string['changepassworddesc'] = 'あなたのパスワードを変更したい場合、ここで詳細を入力してください。';
-$string['changepasswordotherinterface'] = 'あなたは異なるインターフェースで<a href="%s">パスワードを変更する</a>ことができます。';
+$string['changepasswordotherinterface'] = 'あなたは異なるインターフェースで<a href="%s">パスワードを変更できます</a>。';
 $string['oldpasswordincorrect'] = 'これはあなたの現在のパスワードではありません。';
 $string['accountoptionsdesc'] = 'あなたはここで一般アカウントオプションを設定できます。';
 $string['friendsnobody'] = '誰も私をフレンドとして追加できません。';
