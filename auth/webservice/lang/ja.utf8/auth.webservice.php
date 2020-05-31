7e23dc2819cc995e6b7fd8404abde341a959735d
Mitsuhiro Yoshida <mits@mitstek.com>
2020-06-01 08:14:01 +0900

    Brushed up translated strings for Web services.

--- auth/webservice/lang/ja.utf8/auth.webservice.php
+++ auth/webservice/lang/ja.utf8/auth.webservice.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2020-05-14 02:46:03 UTC
+ * @updated    2020-05-31 23:13:55 UTC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -245,9 +245,9 @@ $string['application_uri'] = 'アプリケーションURI';
 $string['application_title'] = 'アプリケーションタイトル';
 $string['errorupdate'] = '更新に失敗しました。';
 $string['erroruser'] = '無効なユーザが指定されました。';
-$string['authorise'] = 'アプリケーションアクセス権限を与える';
+$string['authorise'] = 'アプリケーションアクセス権を与える';
 $string['oauth_access'] = 'このアプリケーションはあなたのユーザ詳細およびリソースにアクセスします。';
-$string['oauth_instructions'] = 'あなたがこのアプリケーションにアクセス権限を与えたい場合、「アプリケーションアクセス権限を与える」をクリックしてください。あなたがアクセス権限を与えたくない場合、「キャンセル」をクリックしてください。';
+$string['oauth_instructions'] = 'あなたがこのアプリケーションにアクセス権を与えたい場合、「アプリケーションアクセス権を与える」をクリックしてください。あなたがアクセス権を与えたくない場合、「キャンセル」をクリックしてください。';
 $string['setauthinstancefailed'] = 'インスティテューション「 %s 」の「ウェブサービス」認証設定に失敗しました。「管理 -> インスティテューション -> 設定」ページで再度追加してください。';
 $string['needtosetowner'] = 'このサービスのオーナを設定する必要があります。';
 $string['accesstofunctionnotallowed'] = '関数「 %s() 」へのアクセスは許可されていません。サービスが関数を含むことを有効にされているかどうか確認してください。サービス設定において: サービスが制限されている場合でもユーザがリストに記載されているかどうか確認してください。さらにサービス設定ではIP制限およびサービスにケイパビリティが必要かどうか確認してください。';
