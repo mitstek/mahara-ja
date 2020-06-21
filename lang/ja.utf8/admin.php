16bb3d46a7da381362bee3fe62234094e7556a3f
Mitsuhiro Yoshida <mits@mitstek.com>
2020-06-21 12:15:27 +0900

    Brushed up translated strings for admin.

--- lang/ja.utf8/admin.php
+++ lang/ja.utf8/admin.php
@@ -172,7 +172,7 @@ $string['allowediframesitesdescription'] = '人はHTML &lt;iframe&gt; エレメ�
 $string['allowediframesitesdescriptiondetail'] = '外部メディアブロック設定時、作成者にアイコンおよび表示名が表示されます。同一表示名のサイトすべてが設定フォーム内でグルーピングされますが、サイト内におけるiframeソーステキストのマッチングは許可されます。';
 $string['iframeurldescription'] = 'iframeソースURL (http:// なし) の先頭に合致するテキストです。半角英数字、「.」「/」「_」および「-」のみ許可されます。';
 $string['iframedisplaynamedescription'] = '作成者に表示されるサイト名です。';
-$string['iframeinvalidsite'] = 'このフィールドには有効なホストおよび任意のパスを入力してください。フィールドには半角英数字、「.」「/」「_」および「-」のみ含むことができます。';
+$string['iframeinvalidsite'] = 'このフィールドには有効なホストおよび任意のパスを入力してください。フィールドには半角英数字、「.」「/」「_」および「-」のみ含めます。';
 $string['iframeiconhost'] = 'アイコンホスト';
 $string['urlalreadyexists'] = 'このURLはすでに存在します。あなたは2度追加することはできません。';
 $string['iframeiconhostdescription'] = '必要であれば、あなたはファビコン (favicon) イメージ用の異なるホストを指定できます。同一名称のすべてのサイトでこのアイコンが使用されます。';
@@ -606,7 +606,7 @@ $string['forceuserstochangepassworddescription'] = 'この設定を有効にし�
 $string['uploadcsvinstitution'] = '新しいアカウントのインスティテューションおよび認証方法';
 $string['configureauthplugin'] = 'あなたが人を追加するには最初に認証プラグインを設定する必要があります。';
 $string['csvfiledescription'] = '追加する人を含むファイル';
-$string['csvmaxusersdescription'] = 'このファイルには %s 以上含むことができません。';
+$string['csvmaxusersdescription'] = 'このファイルには %s 以上含めません。';
 $string['groupcsvfiledescription'] = '追加するグループを含むファイル';
 $string['groupmemberscsvfiledescription'] = '更新するグループメンバーを含むファイル';
 $string['csverroremptyfile'] = 'CSVファイルが空です。';
@@ -647,10 +647,10 @@ $string['uploadcsvpagedescription7'] = '<p>ここであなたは<acronym title="
 
 <p>既存のアカウントを更新する場合、あなたは「password」を除外できます。</p>
 
-<p>あなたのCSVファイルには初期値を事前設定するための他のプロファイルフィールドを含むことができます。任意フィールドは以下のとおりです:</p>
+<p>あなたのCSVファイルには初期値を事前設定するための他のプロファイルフィールドを含めます。任意フィールドは以下のとおりです:</p>
 
 %s\'';
-$string['uploadcsverrortoomanyusers'] = 'あなたのCSVファイルに含まれる行が多すぎます。あなたは %s 以上含むことができません。';
+$string['uploadcsverrortoomanyusers'] = 'あなたのCSVファイルに含まれる行が多すぎます。あなたは %s 以上含めません。';
 $string['uploadcsverrordatamustbenumeric'] = '行 %s: categoryの値は数字にしてください。';
 $string['uploadgroupcsverrordisplaynamealreadyexists'] = 'あなたのファイルの %s 行目に誤りがあります: 表示名「 %s 」はすでに存在します。';
 $string['uploadgroupcsverrorinvalidshortname'] = 'あなたのファイルの %s 行目に誤りがあります: グループ省略名 (shortname) 「 %s 」が有効ではありません。';
@@ -672,14 +672,14 @@ $string['uploadgroupcsvpagedescription2'] = '<p>あなたはこの機能によ�
 
 <p>この先頭行には<tt>shortname</tt>、<tt>displayname</tt>および<tt>roles</tt>フィールドを含んでください。</p>
 
-<p>rolesフィールドには以下の設定値を含むことができます: %s</p>
+<p>rolesフィールドには以下の設定値を含めます: %s</p>
 
 %s
 
-<p>必要に応じてあなたのCSVファイルには他のフィールドを含むことができます。フィールドの完全なリストは以下のとおりです:</p>
+<p>必要に応じてあなたのCSVファイルには他のフィールドを含めます。フィールドの完全なリストは以下のとおりです:</p>
 
 %s';
-$string['uploadgroupcsveditrolesdescription'] = '<p>editrolesフィールドには次のいずれかを含むことができます: %s</p>
+$string['uploadgroupcsveditrolesdescription'] = '<p>editrolesフィールドには次のいずれかを含めます: %s</p>
 %s';
 $string['uploadgroupmemberscsverrorduplicateusername'] = 'あなたのファイルの %s 行目に誤りがあります: グループ省略名 (shortname)「 %s 」およびユーザ名 (username)「 %s 」はすでにこのファイルで指定されています。';
 $string['uploadgroupmemberscsverrorinvalidrole'] = 'あなたのファイルの %s 行目に誤りがあります: 指定されたグループに対してロール「 %s 」は有効ではありません。';
@@ -783,7 +783,7 @@ $string['usersreactivated'] = 'アカウントを復活しました。';
 $string['accountsettings'] = 'アカウント設定';
 $string['siteaccountsettings'] = 'サイトアカウント設定';
 $string['changeusername'] = 'ユーザ名を変更する';
-$string['changeusernamedescription'] = 'この人のユーザ名を変更します。ユーザ名の長さは半角3-236文字にしてください。ユーザ名にはスペースを除く文字、数字および一般的な記号を含むことができます。';
+$string['changeusernamedescription'] = 'この人のユーザ名を変更します。ユーザ名の長さは半角3-236文字にしてください。ユーザ名にはスペースを除く文字、数字および一般的な記号を含めます。';
 $string['resetpassword'] = 'パスワードをリセットする';
 $string['resetpassworddescription'] = 'ここにテキストを入力した場合、人の現在のパスワードと入れ替えられます。';
 $string['forcepasswordchange'] = '次回ログイン時、パスワード変更を強制する';
@@ -1264,7 +1264,7 @@ $string['cli_deleteinactiveusers_onlydryrun1'] = 'ここでは以下の設定に
 次の日付以前: %s
 「usr」テーブルから削除するアカウント数: %s
 未ログイン数: %s';
-$string['cli_deleteinactivegroups_info'] = 'このコマンドラインPHPスクリプトではあなたが古いグループを削除できます。これは現在メンバーがいないグループのみに動作します。「onlyadmins」フラグが設定された場合、オーナー/管理者が1名のみのグループを対象に含むことができます。';
+$string['cli_deleteinactivegroups_info'] = 'このコマンドラインPHPスクリプトではあなたが古いグループを削除できます。これは現在メンバーがいないグループのみに動作します。「onlyadmins」フラグが設定された場合、オーナー/管理者が1名のみのグループを対象に含めます。';
 $string['cli_deleteinactivegroups_beforedate'] = 'この日付より最終修正日が前のグループを削除します。日付には次のような日時ストリングを使用できます: 「2018-02-25」または「-6 months」';
 $string['cli_deleteinactivegroups_limit'] = 'それぞれの削除で処理するグループ数を制限します。';
 $string['cli_deleteinactivegroups_cleangroups'] = '同時に「group」テーブルからグループの削除を試みます。';
