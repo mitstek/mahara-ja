086cd682b3729f38c3c679580e020340f852eafe
Mitsuhiro Yoshida <mits@mitstek.com>
2020-06-23 23:26:08 +0900

    Replaced all http:// with https://.

--- blocktype/googleapps/lang/ja.utf8/blocktype.googleapps.php
+++ blocktype/googleapps/lang/ja.utf8/blocktype.googleapps.php
@@ -6,7 +6,7 @@
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
  * @updated    2018-02-05 04:59:52 UTC
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
  */
@@ -16,7 +16,7 @@ defined('INTERNAL') || die();
 $string['title'] = 'Google Apps';
 $string['description'] = 'Googleカレンダーまたはドキュメントを埋め込みます。';
 $string['appscodeorurl'] = 'コードまたはURLを埋め込む';
-$string['appscodeorurldesc1'] = 'Googleコンテンツ埋め込み方法のインストラクションに関して<a href="http://manual.mahara.org/en/%s/blocks/external.html#google-apps">ユーザマニュアル</a>をご覧ください。
+$string['appscodeorurldesc1'] = 'Googleコンテンツ埋め込み方法のインストラクションに関して<a href="https://manual.mahara.org/en/%s/blocks/external.html#google-apps">ユーザマニュアル</a>をご覧ください。
 <br />
 埋め込みコードまたはGoogle Appsが一般公開されているページのURLを貼り付けてください。';
 $string['height'] = '高さ';
