f75bbb2050ba5bf6611dbf96fe923d282e3e1f41
Mitsuhiro Yoshida <mits@mitstek.com>
2020-06-23 23:25:55 +0900

    Replaced all http:// with https://.

--- search/elasticsearch/lang/ja.utf8/search.elasticsearch.php
+++ search/elasticsearch/lang/ja.utf8/search.elasticsearch.php
@@ -6,7 +6,7 @@
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
  * @updated    2019-12-10 03:46:54 UTC
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
  */
