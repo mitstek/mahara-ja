9c2da63537793dbc1bcdb6d41d61dac2c3b5e7a9
Mitsuhiro Yoshida <mits@mitstek.com>
2020-06-23 23:27:31 +0900

    Replaced all http:// with https://.

--- artefact/resume/blocktype/resumefield/lang/ja.utf8/blocktype.resumefield.php
+++ artefact/resume/blocktype/resumefield/lang/ja.utf8/blocktype.resumefield.php
@@ -6,7 +6,7 @@
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
  * @updated    2012-09-10 18:35:51 UTC
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
  */
