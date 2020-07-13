cf0e60df0cec039260b29f158e17585fd2be0d8b
Mitsuhiro Yoshida <mits@mitstek.com>
2020-07-13 09:00:49 +0900

    Brushed up translated strings for admin.

--- lang/ja.utf8/admin.php
+++ lang/ja.utf8/admin.php
@@ -234,7 +234,7 @@ $string['sendweeklyupdates'] = '週ごとの更新状況を送信する';
 $string['sendweeklyupdatesdescription'] = 'あなたのサイトの統計情報を含む週ごとの更新状況の<a href="https://mahara.org/">mahara.org</a>への送信を許可します。';
 $string['newregistrationpolicyinfo'] = '<p>私たちは登録サイトによる週ごとのデータ更新の提供を必須とするよう登録データポリシーを更新しました。あなたの登録を承認または削除するには以下のリンクに従ってください。</p>';
 $string['registerwithmahara'] = 'mahara.orgに登録する';
-$string['registerwithmaharadescription'] = '一旦あなたのサイトがMaharaプロジェクトに登録された場合、登録解除はできません。';
+$string['registerwithmaharadescription'] = 'あなたのサイトがMaharaプロジェクトに登録された場合、登録解除できません。';
 $string['Register'] = '登録';
 $string['Registration'] = '登録';
 $string['registrationcancelled'] = 'あなたはいつでも<a href="%sadmin/registersite.php">サイト登録ページ</a>にアクセスして登録できます。';
@@ -243,7 +243,7 @@ $string['registrationsuccessfulthanksforregistering'] = '正常に登録され�
 $string['registermaharasite'] = 'サイト登録';
 $string['registeryoursitesummary'] = '<p>あなたのMaharaサイトを<a href="https://mahara.org/">mahara.org</a>に登録することにより私たちが世界中のMaharaインストール状況を把握するための助けとなります。</p>';
 $string['registeryourmaharasitedetail'] = '<p>あなたのサイトをMaharaプロジェクトに登録できます。登録は無料であり私たちが世界中のMaharaインストール状況を把握するための助けとなります。</p>
-<p>あなたが週ごとの更新状況の送信を決定した場合、Maharaプロジェクトに送信される予定の情報を確認できます。あなたのサイトのすべての人に関して個人を特定されるものは一切送信されません。</p>';
+<p>あなたが週ごとの更新状況の送信を決定した場合、Maharaプロジェクトに送信される予定の情報を確認できます。あなたのサイトのすべての人に関して個人を特定できるものは一切送信されません。</p>';
 $string['sendingweeklyupdates1'] = 'あなたは週ごとの更新状況を送信しています。';
 $string['notsendingweeklyupdates'] = 'あなたは週ごとの更新状況を送信していません。';
 $string['siteisregisteredsince'] = '%s 以来、あなたのサイトは<a href="https://mahara.org/">mahara.org</a>に登録されています。';
