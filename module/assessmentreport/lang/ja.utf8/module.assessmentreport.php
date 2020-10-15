b2526b51c8d80766cae0342b2431d9083b23c7b2
Mitsuhiro Yoshida <mits@mitstek.com>
2020-10-15 18:39:13 +0900

    Bug 1896413: Translated a new string for external host submission.

--- module/assessmentreport/lang/ja.utf8/module.assessmentreport.php
+++ module/assessmentreport/lang/ja.utf8/module.assessmentreport.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2019-09-20 00:11:28 UTC
+ * @updated    2020-10-15 09:38:34 UTC
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,6 +14,9 @@
 defined('INTERNAL') || die();
 
 $string['message'] = 'これはあなたが「 %s 」 (%s) をグループ「 %s 」 (提出日: %s) に正常に提出したこと確認するためのものです。
+
+この提出に関して質問がある場合、グループ管理者にご連絡ください。';
+$string['messageexternal'] = 'これはあなたが「 %s 」 (%s) を外部ホスト「 %s 」 (提出日: %s) に正常に提出したこと確認するためのものです。
 
 この提出に関して質問がある場合、グループ管理者にご連絡ください。';
 $string['subject'] = 'あなたの「 %s 」は正常に提出されました。';
