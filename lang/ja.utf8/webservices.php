e8c3b34bbb121473e9a3b7d392876ef046056ecc
Mitsuhiro Yoshida <mits@mitstek.com>
2020-11-27 14:03:55 +0900

    Bug 1899855: Translated new strings for webservices/functions.

---
+++ lang/ja.utf8/webservices.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2020-11-27 05:02:50 UTC
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['accesskey'] = 'アクセスキー';
+$string['aimscreenname'] = 'AIMスクリーン名';
+$string['allowcomments'] = 'コメントを許可する';
+$string['apilevel'] = 'APIラベル';
+$string['approvecomments'] = 'コメントを承認する';
+$string['archiveonrelease'] = 'この送信済みポートフォリオをリリース時にアーカイブしてもよろしいですか?';
+$string['authplugins'] = '手動、LDAP、IMAP等を含む認証プラグイン';
+
+?>
