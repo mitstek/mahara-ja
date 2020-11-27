48e3ced817002a0170d395f769312c10490b8f0c
Mitsuhiro Yoshida <mits@mitstek.com>
2020-11-28 00:40:00 +0900

    Bug 1899855: Translated new strings for webservices/functions.

--- lang/ja.utf8/webservices.php
+++ lang/ja.utf8/webservices.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2020-11-27 05:02:50 UTC
+ * @updated    2020-11-27 15:39:35 UTC
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -20,5 +20,104 @@ $string['apilevel'] = 'APIラベル';
 $string['approvecomments'] = 'コメントを承認する';
 $string['archiveonrelease'] = 'この送信済みポートフォリオをリリース時にアーカイブしてもよろしいですか?';
 $string['authplugins'] = '手動、LDAP、IMAP等を含む認証プラグイン';
+$string['authuserfirstname'] = '認証済みの人の名';
+$string['authuserfullname'] = '認証済みの人のフルネーム';
+$string['authuserid'] = '認証済みの人のID';
+$string['authuserlastname'] = '認証済みの人の姓';
+$string['authuserusername'] = '認証済みの人のユーザネーム';
+$string['availfunctions'] = '利用可能な関数';
+$string['blog'] = '日誌';
+$string['blogaddress'] = '日誌ウェブアドレス';
+$string['blogauthor'] = '日誌作成者';
+$string['blogcreatetime'] = '日誌作成日時';
+$string['blogdesc'] = '日誌説明';
+$string['blogid'] = '日誌ID';
+$string['blogmodtime'] = '日誌修正日時';
+$string['blogofparent'] = '親日誌ID';
+$string['blogowner'] = '日誌オーナー';
+$string['blogowneremail'] = '日誌オーナーメールアドレス';
+$string['blogownerid'] = '日誌オーナーID';
+$string['blogownerremusername'] = '日誌オーナーリモートユーザ名';
+$string['blogownerusername'] = '日誌オーナーユーザ名';
+$string['blogpost'] = '日誌エントリ';
+$string['blogpostauthor'] = '日誌エントリ作成者';
+$string['blogpostcount'] = '日誌エントリ数';
+$string['blogpostcreatetime'] = '日誌エントリ作成日時';
+$string['blogpostdesc'] = '日誌エントリ説明';
+$string['blogpostdraft'] = '日誌エントリ下書きモード';
+$string['blogpostid'] = '日誌エントリID';
+$string['blogpostmodtime'] = '日誌エントリ修正日時';
+$string['blogpostowner'] = '日誌エントリオーナー';
+$string['blogposts'] = '日誌エントリ';
+$string['blogpostsids'] = '日誌エントリID';
+$string['blogposttitle'] = '日誌エントリタイトル';
+$string['blogs'] = '日誌';
+$string['blogscount'] = '日誌数';
+$string['blogsids'] = '日誌ID';
+$string['blogtitle'] = '日誌タイトル';
+$string['businessnumber'] = '勤務先電話';
+$string['city'] = '人の本籍地';
+$string['collections'] = 'コレクション';
+$string['collectionscount'] = 'コレクション数';
+$string['context_id'] = 'LTIコンテンツID';
+$string['context_label'] = 'LTIコンテンツラベル';
+$string['context_title'] = 'LTIコンテンツタイトル';
+$string['context_type'] = 'LTIコンテンツタイプ';
+$string['country'] = '人の国コード (JP、NZ等)';
+$string['deleteuserid'] = '削除する人のID';
+$string['deleteusername'] = '削除する人のユーザ名';
+$string['displayname'] = '人の表示名';
+$string['displaytitle'] = 'ページ表示タイトル';
+$string['emailaddress'] = '人のメールアドレス';
+$string['emailvalid'] = '有効およびユニークなメールアドレス';
+$string['ext_lms'] = 'LTI外部学習管理システム';
+$string['ext_user_username'] = 'LTI外部ユーザ名';
+$string['externalfullurl'] = '送信元サイトのフルURLパスです。';
+$string['favourites'] = 'お気に入り';
+$string['favshortname'] = 'お気に入りの省略名';
+$string['favsownerid'] = 'お気に入りのオーナーID';
+$string['favsownerusername'] = 'お気に入りオーナーのユーザ名';
+$string['favuserid'] = 'お気に入りの人のID';
+$string['favusername'] = 'お気に入りのユーザ名';
+$string['faxnumber'] = 'FAX番号';
+$string['feedbacknotify'] = '許可されるコメント通知:';
+$string['feedbacknotifyexplicit'] = 'コメント通知';
+$string['firstname'] = '人の名';
+$string['forcegroupcategory'] = '存在しない場合、グループカテゴリを作成する';
+$string['forcepasswordchange'] = '初回ログイン時、パスワード変更を強制する';
+$string['fullurl'] = 'フルURL';
+$string['functiondocuri'] = '関数ドキュメンテーションURL';
+$string['functionname'] = '関数名';
+$string['groupcaneditroles'] = '許可されるロールを編集する:';
+$string['groupcategory'] = 'グループカテゴリ - 既存のグループカテゴリのタイトル';
+$string['groupdesc'] = 'グループ説明';
+$string['grouphidden'] = 'グループを隠す';
+$string['groupid'] = 'グループID';
+$string['groupmemberactions'] = 'グループメンバーシップアクション';
+$string['groupmembership'] = 'グループメンバーシップ';
+$string['groupname'] = 'グループ名';
+$string['groupparticipationreports'] = '参加レポート';
+$string['groupshortname'] = 'APIのみの管理グループの省略名';
+$string['grouptype'] = 'グループタイプ:';
+$string['grouptypectrl'] = '管理 - グループ管理者は同意なしにグループに人を追加できます。また、メンバーはグループを離れることはできません。';
+$string['grouptypeopen'] = 'オープン - 人はグループ管理者の承認なしにグループに参加できます。';
+$string['grouptypepublic'] = 'パブリックグループ';
+$string['grouptypereq'] = 'リクエスト - 人はグループ管理者にメンバーシップリクエストを送信できます。';
+$string['grouptypesubmitpage'] = 'ページを送信する - メンバーはグループにページを送信できます。';
+$string['hidemembership'] = 'メンバーシップを隠す';
+$string['homenumber'] = '自宅電話番号';
+$string['icqnumber'] = 'ICQナンバー';
+$string['idownersubmitportfolio'] = '評価のためにポートフォリオを送信する人のMahara ID';
+$string['industry'] = '業種';
+$string['institution'] = 'Maharaインスティテューション';
+$string['institutioncontextauthuser'] = '認証済みの人のインスティテューションコンテクスト';
+$string['institutionforctrlgroups'] = 'Maharaインスティテューション - API管理グループに必要です。';
+$string['institutionnameauthuser'] = '認証済みの人のインスティテューションフルネームコンテクスト';
+$string['internal'] = '内部';
+$string['introduction'] = 'アカウントオーナーの自己紹介';
+$string['iscollection'] = 'このMahara IDはコレクションを意味しますか?';
+$string['jabberusername'] = 'Jabber/XMPPユーザ名';
+$string['mahara'] = 'Mahara';
+$string['memberactionexplicit'] = 'メンバーアクション: 追加または削除';
 
 ?>
