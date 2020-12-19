33032f8e7ccc2fce535687e7977dab88b22b06af
Mitsuhiro Yoshida <mits@mitstek.com>
2020-12-20 05:23:04 +0900

    Bug 1803101: Translated a new string for peer assessment artefact import.

--- artefact/peerassessment/lang/ja.utf8/artefact.peerassessment.php
+++ artefact/peerassessment/lang/ja.utf8/artefact.peerassessment.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2020-03-03 23:07:26 UTC
+ * @updated    2020-12-19 20:22:23 UTC
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -50,9 +50,10 @@ $string['feedbacknotificationtext1'] = '%s が相互評価を追加しました 
 $string['typeassessmentfeedback'] = '相互評価';
 $string['nopeerassessmentrequired'] = '相互評価を必要としないためあなたはこのページでコンテンツを閲覧できません。';
 $string['signedoffalertpeermsg'] = 'このページは %s によって承認されました。これ以上、あなたの相互評価を追加、編集または削除できません。あなたがまだ相互評価を仕上げたい場合、%s にご連絡ください。';
-$string['placeassessment'] = '相互評価を入力する';
-$string['verifyassessment'] = '相互評価を確認する';
+$string['placeassessment'] = '評価を入力する';
+$string['verifyassessment'] = '評価を確認する';
 $string['progress_peerassessment'] = '%s 相互評価をページに追加する';
 $string['progress_verify'] = '%s 確認を他の人の相互評価ページに与える';
+$string['importedassessment'] = 'インポート済み相互評価';
 
 ?>
