ed7e1b86772fc48ab9e6786c9773b8ee95b570cb
Mitsuhiro Yoshida <mits@mitstek.com>
2021-02-24 06:49:05 +0900

    Brushed up translated strings for admin.

--- lang/ja.utf8/admin.php
+++ lang/ja.utf8/admin.php
@@ -587,8 +587,8 @@ $string['bulkleap2aimportdescription'] = '<p>あなたのサーバ内にある�
 <p>あなたが多くのユーザをインポートする場合、注意してください。インポート処理に長時間を要する場合があります。</p>';
 $string['importfile'] = 'バルクエクスポートファイル';
 $string['importfilemissinglisting'] = 'ファイル名「usernames.csv」のバルクエクスポートファイルがありません。これらのユーザをエクスポートするためあなたはMaharaバルクエクスポータを使用しましたか?';
-$string['importfilenotafile'] = '送信中にエラーが発生しました: ファイルを認識できませんでした。';
-$string['importfilenotreadable'] = '送信中にエラーが発生しました: ファイルを読み込めませんでした。';
+$string['importfilenotafile'] = 'フォーム送信中にエラーが発生しました: ファイルを認識できませんでした。';
+$string['importfilenotreadable'] = 'フォーム送信中にエラーが発生しました: ファイルを読み込めませんでした。';
 $string['bulkleap2aimportfiledescription'] = 'あなたのサーバのZIPファイルにはエクスポートされたユーザすべて (Leap2Aフォーマット) およびユーザ名のCSVリストが含まれます。';
 $string['unzipnotinstalled'] = 'あなたのシステムではunzipコマンドを実行できない、または「$cfg->pathtounzip」設定が正しくありません。ZIPエクスポートファイルのインポートを有効にするにはunzipをインストールするか、「$cfg->pathtounzip」設定を訂正してください。';
 $string['importednuserssuccessfully'] = '%d / %d のユーザを正常にインポートしました。';
