1e73df01067c2d1fe871c2add24e2e7f12e8451f
Mitsuhiro Yoshida <mits@mitstek.com>
2021-04-23 13:52:09 +0900

    Bug 1862370: Transalted new strings for submissions plugin.

---
+++ module/submissions/lang/ja.utf8/module.submissions.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2021-04-23 04:48:28 UTC
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['Group'] = 'グループ';
+$string['Role'] = 'ロール';
+$string['Name'] = '名称';
+$string['PreferredName'] = '表示名';
+$string['Portfolio'] = 'ポートフォリオ';
+$string['Task'] = 'タスク';
+$string['Evaluator'] = '評価者';
+$string['Feedback'] = 'フィードバック';
+$string['Result'] = '結果';
+$string['State'] = 'ステータス';
+$string['assessor'] = '評価者';
+$string['submitter'] = '作成者';
+$string['fix'] = '固定';
+$string['releasing'] = 'リリース中 ...';
+$string['completed'] = 'リリース済み';
+$string['tooltip_success'] = '合格';
+$string['search'] = '検索:';
+$string['searchplaceholder'] = '検索語を入力する';
+$string['zerorecords'] = 'レコードなし';
+$string['first'] = '最初のページ';
+$string['last'] = '最後のページ';
+$string['quickfilter'] = 'クイックフィルタ';
+$string['Off'] = 'すべて';
+$string['Select'] = '選択';
+$string['All'] = 'すべて';
+$string['Search'] = '検索';
+$string['Success'] = '合格';
+$string['Fail'] = '不合格';
+$string['fail'] = '不合格';
+$string['success'] = '合格';
+
+?>
