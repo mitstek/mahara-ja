765cf83175405ffd7e6a73f16be2dc4b523b0672
Mitsuhiro Yoshida <mits@mitstek.com>
2021-04-23 13:58:43 +0900

    Bug 1862370: Transalted new strings for submissions plugin.

--- module/submissions/lang/ja.utf8/module.submissions.php
+++ module/submissions/lang/ja.utf8/module.submissions.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2021-04-23 04:48:28 UTC
+ * @updated    2021-04-23 04:58:39 UTC
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -13,34 +13,55 @@
 
 defined('INTERNAL') || die();
 
+$string['pluginname'] = '提出';
+$string['Submissions'] = '提出';
+$string['submissionstitlegroup'] = '%s - 提出';
 $string['Group'] = 'グループ';
 $string['Role'] = 'ロール';
-$string['Name'] = '名称';
+$string['Name'] = '氏名';
 $string['PreferredName'] = '表示名';
 $string['Portfolio'] = 'ポートフォリオ';
+$string['Date'] = '日付';
 $string['Task'] = 'タスク';
 $string['Evaluator'] = '評価者';
 $string['Feedback'] = 'フィードバック';
+$string['Rating'] = 'レーティング';
 $string['Result'] = '結果';
 $string['State'] = 'ステータス';
 $string['assessor'] = '評価者';
 $string['submitter'] = '作成者';
+$string['release'] = 'リリース予定';
 $string['fix'] = '固定';
+$string['notevaluated'] = '未評価';
 $string['releasing'] = 'リリース中 ...';
 $string['completed'] = 'リリース済み';
+$string['revision'] = '改定';
 $string['tooltip_success'] = '合格';
+$string['tooltip_remove'] = '不合格';
+$string['tooltip_refresh'] = '改定';
+$string['tooltip_question'] = '未評定';
+$string['emptytable'] = 'テーブルに利用可能なデータはありません。';
+$string['loading'] = '読み込み中';
+$string['processing'] = '処理中';
 $string['search'] = '検索:';
 $string['searchplaceholder'] = '検索語を入力する';
 $string['zerorecords'] = 'レコードなし';
 $string['first'] = '最初のページ';
 $string['last'] = '最後のページ';
+$string['next'] = '次のページ';
+$string['previous'] = '前のページ';
 $string['quickfilter'] = 'クイックフィルタ';
 $string['Off'] = 'すべて';
 $string['Select'] = '選択';
 $string['All'] = 'すべて';
 $string['Search'] = '検索';
+$string['Uncommented'] = 'コメントなし';
+$string['Commented'] = 'コメントあり';
 $string['Success'] = '合格';
 $string['Fail'] = '不合格';
+$string['Open'] = 'オープン';
+$string['Completed'] = '完了';
+$string['colvislabel'] = 'カラムを設定する';
 $string['fail'] = '不合格';
 $string['success'] = '合格';
 
