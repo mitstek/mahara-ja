df08d1a4e472623614f260dedf2624a6fee9e554
Mitsuhiro Yoshida <mits@mitstek.com>
2021-04-24 00:22:55 +0900

    Bug 1862370: Transalted new strings and help files for submissions plugin.

--- module/submissions/lang/ja.utf8/module.submissions.php
+++ module/submissions/lang/ja.utf8/module.submissions.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2021-04-23 04:58:39 UTC
+ * @updated    2021-04-23 15:21:16 UTC
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -30,17 +30,26 @@ $string['Result'] = '結果';
 $string['State'] = 'ステータス';
 $string['assessor'] = '評価者';
 $string['submitter'] = '作成者';
-$string['release'] = 'リリース予定';
-$string['fix'] = '固定';
+$string['release'] = 'リリースする';
+$string['fix'] = '修正する';
 $string['notevaluated'] = '未評価';
+$string['submitted'] = '提出済み';
 $string['releasing'] = 'リリース中 ...';
 $string['completed'] = 'リリース済み';
-$string['revision'] = '改定';
+$string['revision'] = '見直し';
+$string['evaluated'] = '評価済み';
+$string['unassignedselectboxitem'] = '- 未割り当て -';
+$string['releasesubmission'] = '評価を完了して提出済みポートフォリオをリリースしますか?';
+$string['fixsubmission'] = '提出を修正しますか?';
 $string['tooltip_success'] = '合格';
 $string['tooltip_remove'] = '不合格';
-$string['tooltip_refresh'] = '改定';
+$string['tooltip_refresh'] = '見直し';
 $string['tooltip_question'] = '未評定';
 $string['emptytable'] = 'テーブルに利用可能なデータはありません。';
+$string['info'] = '_START_ / _END_ (合計 _TOTAL_) エントリ';
+$string['infoempty'] = '0 エントリ';
+$string['thousands'] = ',';
+$string['lengthmenu'] = '_MENU_ エントリを表示する';
 $string['loading'] = '読み込み中';
 $string['processing'] = '処理中';
 $string['search'] = '検索:';
@@ -50,19 +59,59 @@ $string['first'] = '最初のページ';
 $string['last'] = '最後のページ';
 $string['next'] = '次のページ';
 $string['previous'] = '前のページ';
+$string['sortascending'] = ': カラムを昇順に並べ替える場合に有効にする';
+$string['sortdescending'] = ': カラムを降順に並べ替える場合に有効にする';
+$string['displayportfolio'] = 'ポートフォリオを表示する';
 $string['quickfilter'] = 'クイックフィルタ';
+$string['quickfiltertooltip'] = 'フィルタをリセットする';
 $string['Off'] = 'すべて';
+$string['Reset'] = 'リセット';
 $string['Select'] = '選択';
 $string['All'] = 'すべて';
 $string['Search'] = '検索';
+$string['MissingTask'] = 'タスク不明';
+$string['Unassigned'] = '割り当てなし';
 $string['Uncommented'] = 'コメントなし';
 $string['Commented'] = 'コメントあり';
+$string['Revision'] = '見直し';
 $string['Success'] = '合格';
 $string['Fail'] = '不合格';
+$string['Pending'] = '不明';
 $string['Open'] = 'オープン';
 $string['Completed'] = '完了';
 $string['colvislabel'] = 'カラムを設定する';
+$string['chooseresult'] = '結果を選択する';
+$string['noresult'] = '見直し';
 $string['fail'] = '不合格';
 $string['success'] = '合格';
+$string['lastsubmissionnoevaluationresultcontactinstructor'] = 'あなたが最後に提出したこのポートフォリオまたはグループタスクに評価結果がありません。この問題を解決するため、あなたのインストラクタにご連絡ください。あなたの提出は自動的にリリースされました。';
+$string['portfolioortaskalreadyevaluated'] = 'ポートフォリオまたはグループタスクはすでに評価されています。あなたの提出は自動的にリリースされました。';
+$string['portfoliocurrentlybeingreleased'] = 'このタスクへのポートフォリオは現在リリースされています。完全にリリースされるまで再提出はお待ちください。';
+$string['portfolioalreadysubmitted'] = 'このタスクへのポートフォリオはすでに提出されています。リリースされるまで再提出はお待ちください。';
+$string['submissionnotcreated'] = '提出未作成:';
+$string['submissionnotcreatedorupdated'] = '提出未作成または未更新:';
+$string['notallowedtoassesssubmission'] = 'あなたはこの提出へのアクセスを許可されていません。';
+$string['dependingonstatusmayeditupdatedfields'] = 'あなたは新しいステータスに応じて更新済みフィールドを編集できます。';
+$string['submissionreadonlynotupdated'] = '読み取り専用提出ステータスのため提出は更新されませんでした。';
+$string['submissionnotfixedmissingevaluationresult'] = '評価結果がないため提出は修正されませんでした。';
+$string['submissionnotreleasedorfixedwrongsubmissionstatus'] = '提出ステータスが正しくないため提出はリリースまたは修正されませんでした。';
+$string['missingcontrollerhandler'] = 'コントローラコマンドのハンドラがありません。';
+$string['unsupportedsubmissionownertype'] = '未サポートの提出ポートフォリオオーナータイプです。';
+$string['portfoliofieldsmustbesetforsettingtaskfields'] = '提出タスクフィールドの設定のためポートフォリオエレメントフィールドを設定する必要があります。';
+$string['unsupportedportfoliotype'] = '未サポートの提出ポートフォリオエレメントタイプです。';
+$string['submissionstatuschangedexternally'] = '外部から提出ステータスが変更されました。';
+$string['eventgroupnotfound'] = 'イベントデータでグループ「 %s 」が見つかりませんでした。';
+$string['eventgroupidnotfound'] = 'イベントデータでID「 %s 」のグループが見つかりませんでした。';
+$string['actionreleased'] = 'リリース済み';
+$string['actionfixed'] = '修正済み';
+$string['submissionreleased'] = '提出「 %s 」(作成者 %s) - %s';
+$string['ProceedWithBackRestrictionsToExistingTab'] = '提出済みポートフォリオはすでに別のタブで開かれています。このポートフォリオを開始できますが、あなたが戻った時点で「提出」テーブル内でハイライトされません。別のタブのポートフォリオも同様です。続けても宜しいですか?';
+$string['ProceedWithoutFlashbackFunctionality'] = '提出済みポートフォリオはすでに別のタブで開かれています。このポートフォリオを開始できますが、あなたがブラウザの「戻る」ボタンで戻った時点で「提出」テーブル内でハイライトされません。別のタブのポートフォリオも同様です。続けても宜しいですか?';
+$string['shownameaslastnamefirstname'] = '氏名を「姓 名」のように表示する';
+$string['shownameaslastnamefirstnamedescription'] = '提出テーブルのすべての氏名表示を「姓 名」のフォーマットで表示します。';
+$string['showportfoliobuttons'] = 'ポートフォリオリンクをボタンとして表示する';
+$string['showportfoliobuttonsdescription'] = '「提出」テーブルの「アーカイブとしてダウンロードする機能」の差別化のため標準的なMaharaのポートフォリオリンクの表示をボタンに変更します。';
+$string['retentionperiod'] = '提出およびアーカイブ済みポートフォリオの保存期間';
+$string['retentionperioddescription'] = 'ポートフォリオおよび関連するアーカイブ済みポートフォリオが削除されるリリース翌年から始まる年単位の期間です。この値にゼロに設定された場合、ポートフォリオ作成者のアカウントが削除された時点ですべての投稿および関連するポートフォリオが削除されます。';
 
 ?>
