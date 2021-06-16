ccab40a407e443f43b9a1738ce6d75d1c5f545c0
Mitsuhiro Yoshida <mits@mitstek.com>
2021-06-16 15:22:39 +0900

    Brushed up a translated string for Exact people searches.

--- search/internal/lang/ja.utf8/search.internal.php
+++ search/internal/lang/ja.utf8/search.internal.php
@@ -14,6 +14,6 @@
 defined('INTERNAL') || die();
 
 $string['exactusersearch'] = '完全一致人検索';
-$string['exactusersearchdescription4'] = '「人を検索する」ボックスおよび「人」ページの結果には検索キーワードすべてがプロファイルフィールドに合致する人のみ返されます。';
+$string['exactusersearchdescription4'] = 'すべての検索キーワードがプロファイルフィールドに合致する人のみ「人を検索する」ボックスおよび「人」ページの結果として返されます。';
 
 ?>
