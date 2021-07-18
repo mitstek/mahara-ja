ac71218293e055f5308cfbfd1541068d0efdd76d
Mitsuhiro Yoshida <mits@mitstek.com>
2021-07-18 10:30:47 +0900

    Brushed up translated strings for admin.

--- lang/ja.utf8/admin.php
+++ lang/ja.utf8/admin.php
@@ -556,7 +556,7 @@ $string['bulkleap2aimportdescription'] = '<p>あなたのサーバ内にある�
 &nbsp;&nbsp;nigel,mahara-export-leap-user1-1266458159.zip
 </pre>
 <p>上記、mahara-export-leap-user8-1265165366.zipおよびmahara-export-leap-user1-1266458159.zipはusersと呼ばれるサブディレクトリ内にあるファイルです。</p>
-<p>通常、このZIPファイルはMaharaに実装されているバルクエクスポートを使用して生成されます。</p>
+<p>通常、このZIPファイルはMahara内蔵のバルクエクスポートを使用して生成されます。</p>
 <p>あなたが多くのユーザをインポートする場合、注意してください。インポート処理に長時間を要する場合があります。</p>';
 $string['importfile'] = 'バルクエクスポートファイル';
 $string['importfilemissinglisting'] = 'ファイル名「usernames.csv」のバルクエクスポートファイルがありません。これらのユーザをエクスポートするためあなたはMaharaバルクエクスポータを使用しましたか?';
