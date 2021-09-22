8e92b9547c85e874000df7797e21757d16ba1366
Mitsuhiro Yoshida <mits@mitstek.com>
2021-09-23 08:42:28 +0900

    Bug 1944097: Translated new strings for default blocks on the dashboard.

--- blocktype/newviews/lang/ja.utf8/blocktype.newviews.php
+++ blocktype/newviews/lang/ja.utf8/blocktype.newviews.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2018-02-12 04:36:02 UTC
+ * @updated    2021-09-22 23:41:48 UTC
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -13,7 +13,7 @@
 
 defined('INTERNAL') || die();
 
-$string['title1'] = '私が閲覧できる最新の変更';
+$string['title2'] = '私に共有されているポートフォリオ';
 $string['description2'] = 'あなたがこのサイトでアクセスした中から最も最近に更新されたページおよびコレクションを一覧表示します。';
 $string['viewstoshow1'] = '表示する結果の最大数';
 $string['viewstoshowdescription'] = '設定範囲: 1～100';
--- blocktype/watchlist/lang/ja.utf8/blocktype.watchlist.php
+++ blocktype/watchlist/lang/ja.utf8/blocktype.watchlist.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2020-12-16 23:40:20 UTC
+ * @updated    2021-09-22 23:41:49 UTC
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -13,7 +13,7 @@
 
 defined('INTERNAL') || die();
 
-$string['title'] = 'ウォッチページ';
+$string['title1'] = '私が観察中のページ';
 $string['defaulttitledescription'] = 'タイトルフィールドを空白にした場合、デフォルトタイトルが生成されます。';
 $string['description'] = 'あなたのウォッチリストのページを表示します。';
 $string['nopages'] = 'あなたのウォッチリストにページはありません。';
