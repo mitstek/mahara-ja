34542b99abb3fbfcab9e346cb9eb35c0d39a3120
Mitsuhiro Yoshida <mits@mitstek.com>
2021-10-01 07:55:40 +0900

    Bug 1903601: Translated new strings for more detailed LTI advantage service error messages.

---
+++ module/lti_advantage/lang/ja.utf8/module.lti_advantage.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2021-09-30 22:54:46 UTC
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['platformvendorissnotfound'] = 'プラットフォームが返されたデータ内に「iss」を送信していません。キーがありません。';
+$string['platformvendorkeyinvalid'] = 'リクエスト内にプラットフォームベンダキーが見つかりませんでした。返されたリクエストのキーは次のとおりです: %s';
+$string['platformvendorkeynotfound'] = 'プラットフォームからの「iss」(%s) には「lti_advantage_registration」データベーステーブル内で合致する「Issuer」がありません。';
