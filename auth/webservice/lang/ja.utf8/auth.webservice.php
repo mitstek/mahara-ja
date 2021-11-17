dca7a8c736c90823a3a0e50088cfbfca0b6fd3af
Mitsuhiro Yoshida <mits@mitstek.com>
2021-11-17 14:59:47 +0900

    Brushed up translated strings for external application.

--- auth/webservice/lang/ja.utf8/auth.webservice.php
+++ auth/webservice/lang/ja.utf8/auth.webservice.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2021-10-01 04:40:49 UTC
+ * @updated    2021-11-17 05:59:09 UTC
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -229,11 +229,11 @@ $string['iterationtitle'] = '%s イテレーション: %s';
 $string['unabletoruntestclient'] = 'ウェブサービステストクライアントはhttpsの下で実運用モードで動作させるか、あなたのconfig.php内に「$cfg->productionmode = false」を記述する必要があります。';
 $string['accesstokens'] = 'OAuthアクセストークン';
 $string['notokens'] = 'あなたにはアプリケーショントークンがありません。';
-$string['externalapps'] = '外部アプリ';
+$string['externalapps'] = '外部アプリケーション';
 $string['externalappsdesc'] = 'ウェブサービスアクセスの外部アプリケーションを登録します。';
 $string['oauth1'] = 'OAuth1.x';
-$string['externalappsregister'] = '外部アプリ登録';
-$string['userapplications1'] = '外部アプリ設定';
+$string['externalappsregister'] = '外部アプリケーション登録';
+$string['userapplications1'] = '外部アプリケーション設定';
 $string['accessto'] = 'アクセス対象';
 $string['application'] = 'アプリケーション';
 $string['callback'] = 'コールバックURI';
@@ -252,7 +252,7 @@ $string['oauth_access'] = 'このアプリケーションはあなたのユー�
 $string['oauth_instructions'] = 'あなたがこのアプリケーションにアクセス権を与えたい場合、「アプリケーションアクセス権を与える」をクリックしてください。あなたがアクセス権を与えたくない場合、「キャンセル」をクリックしてください。';
 $string['setauthinstancefailed'] = 'インスティテューション「 %s 」の「ウェブサービス」認証設定に失敗しました。「管理 -> インスティテューション -> 設定」ページで再度追加してください。';
 $string['needtosetowner'] = 'このサービスのオーナを設定する必要があります。';
-$string['confirmdeleteexternalapp'] = '本当にこの外部アプリを削除してもよろしいですか?';
+$string['confirmdeleteexternalapp'] = '本当にこの外部アプリケーションを削除してもよろしいですか?';
 $string['accesstofunctionnotallowed'] = '関数「 %s() 」へのアクセスは許可されていません。サービスが関数を含むことを有効にされているかどうか確認してください。サービス設定において: サービスが制限されている場合でもユーザがリストに記載されているかどうか確認してください。さらにサービス設定ではIP制限およびサービスにケイパビリティが必要かどうか確認してください。';
 $string['accessexception'] = 'アクセスコントロール例外';
 $string['accessnotallowed'] = 'ウェブサービスへのアクセスは許可されていません。';
