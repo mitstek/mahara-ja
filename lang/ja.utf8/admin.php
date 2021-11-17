c52adfb22c39cb60ea3a6dc2c1d0f925094ca741
Mitsuhiro Yoshida <mits@mitstek.com>
2021-11-17 14:48:21 +0900

    Brushed up translated strings for admin.

--- lang/ja.utf8/admin.php
+++ lang/ja.utf8/admin.php
@@ -785,7 +785,7 @@ $string['howdoyouwanttocreatethisuser'] = 'どのようにこのユーザを作�
 $string['leap2aimportfailed'] = '<p><strong>申し訳ございません、Leap2Aファイルのインポートに失敗しました。</strong></p><p>これはあなたが有効なLeap2Aファイルをアップロードに選択しなかったから、またはこのMaharaのバージョンではあなたのLeap2Aファイルがサポートされていないからだと考えられます。また、Leap2Aファイルが有効な場合、Maharaのバグがあなたのファイルのアップロードを失敗させていることも考えられます。</p><p><a href="add.php">戻ってファイルのアップロードを再度お試しください</a>。この問題が続く場合、エラー状況を<a href="https://mahara.org/forums/">Maharaフォーラム</a>に投稿してください。その場合、あなたのファイルのコピーを準備してください。</p>';
 $string['newuseremailnotsent'] = '新しいユーザへのウェルカムメールの送信に失敗しました。';
 $string['newusercreated'] = '新しいユーザアカウントが正常に作成されました。';
-$string['noleap2axmlfiledetected'] = 'leap2a.xmlファイルが検出されませんでした - 再度、あなたのエクスポートファイルを確認してください。';
+$string['noleap2axmlfiledetected'] = 'leap2a.xmlファイルが検出されませんでした - 再度あなたのエクスポートファイルを確認してください。';
 $string['Or'] = 'または';
 $string['userwillreceiveemailandhastochangepassword'] = '新しいアカウント詳細を記載したメールがユーザに送信されます。新しく作成されたユーザは初回ログイン時にパスワードの変更を求められます。';
 $string['uploadleap2afile'] = 'Leap2Aファイルをアップロードする';
