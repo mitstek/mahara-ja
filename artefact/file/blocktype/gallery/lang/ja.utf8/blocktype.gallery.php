c6e50edcbc8f5fb005373bff77cfe2255e178308
Mitsuhiro Yoshida <mits@mitstek.com>
2021-11-24 11:15:30 +0900

    Bug 1950551: Translated new strings for the modal photo popup.

--- artefact/file/blocktype/gallery/lang/ja.utf8/blocktype.gallery.php
+++ artefact/file/blocktype/gallery/lang/ja.utf8/blocktype.gallery.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2021-06-08 02:06:17 UTC
+ * @updated    2021-11-24 02:13:20 UTC
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -31,18 +31,7 @@ $string['showdescriptions'] = '説明を表示する';
 $string['showdescriptionsdescription'] = 'それぞれのイメージに説明を表示する場合、このオプションを有効にしてください。';
 $string['cannotdisplayslideshow'] = 'スライドショーを表示できません。';
 $string['gallerysettings'] = 'ギャラリー設定';
-$string['usefancybox'] = 'Fancybox 3を使用する';
-$string['usefancyboxdesc'] = 'あなたのギャラリーにFancybox 3を使用します。あなたのギャラリーのイメージをクリックした場合、現在のページにオーバーレイ表示されます。';
+$string['usefancybox1'] = 'Bootstrapモーダルを使用する';
+$string['usefancyboxdesc1'] = 'あなたのギャラリーでBootstrapモーダルを使用します。あなたのギャラリーのイメージをクリックした場合、オーバーレイで開きます。';
 $string['previewwidth'] = '最大写真幅';
-$string['previewwidthdesc'] = '写真がSlimbox 2で表示される場合、リサイズされる最大幅を設定してください。';
-$string['CLOSE'] = '閉じる';
-$string['NEXT'] = '次へ';
-$string['PREV'] = '前へ';
-$string['ERROR'] = 'リクエストされたコンテンツを読み込めません。<br />後で再度お試しください。';
-$string['PLAY_START'] = 'スライドショーを開始する';
-$string['PLAY_STOP'] = 'スライドショーを一時停止する';
-$string['FULL_SCREEN'] = 'フルスクリーン';
-$string['THUMBS'] = 'サムネイル';
-$string['DOWNLOAD'] = 'ダウンロード';
-$string['SHARE'] = '共有';
-$string['ZOOM'] = 'ズーム';
+$string['previewwidthdesc1'] = 'Bootstrapモーダルでの写真表示時の最大リサイズ幅を設定します。';
