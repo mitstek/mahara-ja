939f8c037d5bbf93eb0586727f37191f12e831d8
Mitsuhiro Yoshida <mits@mitstek.com>
2021-12-12 15:05:23 +0900

    Brushed up translated strings admin.

--- lang/ja.utf8/behat.php
+++ lang/ja.utf8/behat.php
@@ -15,6 +15,6 @@ defined('INTERNAL') || die();
 
 $string['errorbehatcommand'] = 'Behat CLIコマンドの実行中にエラーが発生しました。さらに問題を調査するにはCLIで「{$a} --help」を手動実行してください。';
 $string['errorcomposer'] = 'コンポーザ依存関係がインストールされていません。';
-$string['errordataroot'] = '「$CFG->behat_dataroot」が設定されていないか無効です。';
+$string['errordataroot'] = '「$CFG->behat_dataroot」が設定されていないか有効ではありません。';
 $string['errorsetconfig'] = 'config.phpに「$CFG->behat_dataroot」「$CFG->behat_dbprefix」および「$CFG->behat_wwwroot」を設定する必要があります。';
 $string['erroruniqueconfig'] = '「$CFG->behat_dataroot」「$CFG->behat_dbprefix」および「$CFG->behat_wwwroot」の値は「$CFG->dataroot」「$CFG->dbprefix」「$CFG->wwwroot」「$CFG->phpunit_dataroot」および「$CFG->phpunit_prefix」の値とは異なる必要があります。';
