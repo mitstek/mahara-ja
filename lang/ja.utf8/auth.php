7b38b1c51c75efbcc1622c520c632ede55542dc6
Mitsuhiro Yoshida <mits@mitstek.com>
2021-12-29 15:59:43 +0900

    Brushed up translated strings for authentication and forum.

--- lang/ja.utf8/auth.php
+++ lang/ja.utf8/auth.php
@@ -54,7 +54,7 @@ $string['theyssoin'] = 'こちらにSSOさせる';
 $string['authloginmsg'] = 'ユーザがMaharaログインフォーム経由でログインを試みる場合、表示されるメッセージを入力してください。';
 $string['application'] = 'アプリケーション';
 $string['cantretrievekey'] = 'リモートサーバの公開鍵検索中にエラーが発生しました。<br />アプリケーションおよびWWWルートフィールドが正しいことを確認してください。また、リモートホストのネットワーキングが有効にされていることを確認してください。';
-$string['errorcertificateinvalidwwwroot'] = 'これは %s の証明書ですが、あなたは %s に対する使用を試みています。';
+$string['errorcertificateinvalidwwwroot'] = 'これは %s の証明書ですが、あなたは %s への使用を試みています。';
 $string['errnoauthinstances'] = '私たちには %s のホストに関して認証プラグインインスタンスが設定されていないようです。';
 $string['errornotvalidsslcertificate'] = '有効なSSLサーバ証明書ではありません。';
 $string['errnoxmlrpcinstances'] = '私たちには%s のホストに関してXMLRPCプラグインインスタンスが設定されていないようです。';
@@ -62,7 +62,7 @@ $string['errnoxmlrpcwwwroot'] = '私たちには %s のホストに関してレ�
 $string['errnoxmlrpcuser'] = '現時点ではあなたを認証できませんでした。可能性のある理由として以下が考えられます:
 
 *あなたのSSOセッションが失効しました。他のアプリケーションに戻って、Maharaにログインするためのリンクを再度クリックしてください。
-*あなたのMaharaに対するSSOが許可されていません。あなたがSSOできるかどうか管理者に確認してください。';
+*あなたのMaharaへのSSOが許可されていません。あなたがSSOできるかどうか管理者に確認してください。';
 $string['unabletosigninviasso'] = 'SSO経由でサインインできません。';
 $string['xmlrpccouldnotlogyouin'] = '申し訳ございません、あなたをログインさせることができませんでした :(';
 $string['xmlrpccouldnotlogyouindetail'] = '申し訳ございません、今回はあなたをMaharaにログインさせることができませんでした。ログインを再度お試しください。問題が続く場合、あなたの管理者にご連絡ください。';
