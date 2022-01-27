756e783f8eb8a6210a76abba7433d01169eddb59
Mitsuhiro Yoshida <mits@mitstek.com>
2022-01-28 08:02:20 +0900

    Bug 1949528: Translated a new string for block placeholder text.

--- blocktype/placeholder/lang/ja.utf8/blocktype.placeholder.php
+++ blocktype/placeholder/lang/ja.utf8/blocktype.placeholder.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2020-01-03 22:04:36 UTC
+ * @updated    2022-01-27 23:01:27 UTC
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -15,7 +15,7 @@ defined('INTERNAL') || die();
 
 $string['title'] = 'プレースホルダ';
 $string['description'] = 'あなたが希望するブロックタイプをプレースホルダブロックで選択できます。';
-$string['placeholdertext'] = 'タイプを選択するためこのブロックを設定してください。';
+$string['placeholdertext1'] = 'タイプを選択するためこのブロックを編集してください。';
 $string['defaulttitledescription'] = 'タイトルフィールドを空白にした場合、デフォルトタイトルが生成されます。';
 $string['contenttypes'] = 'コンテンツタイプ';
 $string['placeholderblocktypenotenabled'] = 'ブロックタイプ「プレースホルダ」をインストールおよび有効にする必要があります。すでにインストールされている場合、データベースのテーブル「blocktype_installed」で有効にしてください。';
