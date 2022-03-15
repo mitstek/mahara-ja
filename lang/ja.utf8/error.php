601d9d1337c4854cc5781931e21e4e7a9a6f90a3
Mitsuhiro Yoshida <mits@mitstek.com>
2022-03-15 15:16:53 +0900

    Brushed up translated strings for error messages.

--- lang/ja.utf8/error.php
+++ lang/ja.utf8/error.php
@@ -61,7 +61,7 @@ $string['artefacttypemismatch'] = 'アーティファクトタイプが一致し
 $string['classmissing'] = 'クラス %s (タイプ %s - プラグイン %s) がありません。';
 $string['artefacttypeclassmissing'] = 'すべてのアーティファクトタイプにはクラスを実装する必要があります。%s がありません。';
 $string['artefactpluginmethodmissing'] = 'アーティファクトプラグイン %s で必要な %s が実装されていません。';
-$string['blocktypelibmissing'] = 'アーティファクトプラグイン %s 内にブロック %s のlib.phpがありません。';
+$string['blocktypelibmissing'] = 'ブロック %s (アーティファクトプラグイン %s ) のlib.phpがありません。';
 $string['unabletosetmultipleblogs'] = 'ユーザ「 %s 」のページ「 %s 」のコピーによる複数日誌の有効化に失敗しました。これは<a href="%s">アカウント</a>ページで手動で設定できます。';
 $string['pleaseloginforjournals'] = 'あなたの日誌および記事すべてを閲覧するにはログアウトした後に再度ログインする必要があります。';
 $string['blocktypemissingconfigform'] = 'ブロックタイプ %s にinstance_config_formを実装する必要があります。';
