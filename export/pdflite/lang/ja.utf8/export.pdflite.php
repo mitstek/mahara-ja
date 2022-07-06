abc246db9f948234fdee75fceaf3256f792b13a5
Mitsuhiro Yoshida <mits@mitstek.com>
2022-07-06 13:54:05 +0900

    Brushed up translated strings for core and admin.

--- export/pdflite/lang/ja.utf8/export.pdflite.php
+++ export/pdflite/lang/ja.utf8/export.pdflite.php
@@ -17,5 +17,5 @@ $string['title'] = 'ページおよびコレクションのPDF liteファイル'
 $string['description'] = 'これはあなたのポートフォリオのPDFおよび類似性チェッカに送信可能なポートフォリオ関連ファイルを含む軽量なZIPファイルを作成します。あなたは再度これをインポートできませんが、標準的なPDFビューアで読めます。';
 $string['needsinstalledpdfexport'] = '「PDF」エクスポートプラグインのインストールが必要です。';
 $string['needsactivepdfexport'] = '「PDF」エクスポートプラグインの有効化が必要です。';
-$string['exportpdfdisabled'] = 'PDF liteエクスポート依存関係がありません。PDF liteエクスポートは無効にされています。詳細情報は<a href="%s">「プラグイン管理」</a>をご覧ください。';
+$string['exportpdfdisabled'] = 'PDF liteエクスポート依存関係がありません。PDF liteエクスポートは無効にされています。詳細は<a href="%s">「プラグイン管理」</a>をご覧ください。';
 $string['isexperimental'] = 'これは実験的なエクスポートオプションです。PDFエクスポートオプションのインストールおよび有効化に依存します。';
