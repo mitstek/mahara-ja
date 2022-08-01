6bd0982910144249a8cfda8b18989ab135a926b5
Mitsuhiro Yoshida <mits@mitstek.com>
2022-08-01 10:36:10 +0900

    Brushed up the expiration related translated strings for core.

--- blocktype/groupmembers/lang/ja.utf8/blocktype.groupmembers.php
+++ blocktype/groupmembers/lang/ja.utf8/blocktype.groupmembers.php
@@ -28,13 +28,13 @@
 
 defined('INTERNAL') || die();
 
-$string['title'] = 'グループメンバー';
-$string['description'] = 'このグループのメンバー一覧を表示します。';
-$string['show_all'] = 'このグループのメンバーすべてを表示する';
-$string['options_numtoshow_title'] = '表示メンバー';
-$string['options_numtoshow_desc'] = 'あなたが表示したいメンバー数です。';
+$string['title'] = 'グループメンバ';
+$string['description'] = 'このグループのメンバ一覧を表示します。';
+$string['show_all'] = 'このグループのメンバすべてを表示する';
+$string['options_numtoshow_title'] = '表示メンバ';
+$string['options_numtoshow_desc'] = 'あなたが表示したいメンバ数です。';
 $string['options_order_title'] = '表示順';
-$string['options_order_desc'] = 'あなたはグループメンバーの表示順に関して最新グループメンバーまたはランダムを選択できます。';
+$string['options_order_desc'] = 'あなたはグループメンバの表示順に関して最新グループメンバまたはランダムを選択できます。';
 $string['Latest'] = '最新';
 $string['Random'] = 'ランダム';
 $string['defaulttitledescription'] = 'タイトルフィールドを空白にした場合、デフォルトタイトルが生成されます。';
