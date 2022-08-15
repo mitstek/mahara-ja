70f83415a2f4d0b448c481192ea0c2f970b89690
Mitsuhiro Yoshida <mits@mitstek.com>
2022-08-15 17:33:43 +0900

    Brushed up translated strings and help files for gallery.

--- artefact/file/blocktype/gallery/lang/ja.utf8/blocktype.gallery.php
+++ artefact/file/blocktype/gallery/lang/ja.utf8/blocktype.gallery.php
@@ -13,14 +13,14 @@
 
 defined('INTERNAL') || die();
 
-$string['title'] = 'イメージギャラリー';
-$string['description1'] = 'あなたのファイルエリアまたは外部ギャラリーのイメージコレクションです。';
+$string['title'] = 'イメージギャラリ';
+$string['description1'] = 'あなたのファイルエリアまたは外部ギャラリのイメージコレクションです。';
 $string['select'] = 'イメージ選択';
 $string['selectfolder'] = '後でアップロードされたイメージを含むイメージすべてをフォルダから表示する';
 $string['selectimages'] = '表示するイメージを個別に選択する';
-$string['selectexternal'] = '外部ギャラリーのイメージを表示する';
-$string['externalgalleryurl'] = 'ギャラリーURLまたはRSS';
-$string['externalgalleryurldesc'] = 'あなたは次の外部ギャラリーを埋め込めます:';
+$string['selectexternal'] = '外部ギャラリのイメージを表示する';
+$string['externalgalleryurl'] = 'ギャラリURLまたはRSS';
+$string['externalgalleryurldesc'] = 'あなたは次の外部ギャラリを埋め込めます:';
 $string['width'] = '幅';
 $string['widthdescription'] = 'あなたのイメージの幅 (ピクセル) を指定してください。イメージはこの幅にサイズ変更されます。';
 $string['style'] = 'スタイル';
@@ -30,11 +30,11 @@ $string['styleslideshow'] = 'スライドショー';
 $string['showdescriptions'] = '説明を表示する';
 $string['showdescriptionsdescription'] = 'それぞれのイメージに説明を表示する場合、このオプションを有効にしてください。';
 $string['cannotdisplayslideshow'] = 'スライドショーを表示できません。';
-$string['gallerysettings'] = 'ギャラリー設定';
+$string['gallerysettings'] = 'ギャラリ設定';
 $string['useslimbox2'] = 'Slimbox 2を使用する';
-$string['useslimbox2desc1'] = 'この設定を有効にした場合、あなたのギャラリーにSlimbox 2が使用されます。あなたのギャラリーのイメージをクリックした場合、現在のページにオーバーレイ表示されます。';
+$string['useslimbox2desc1'] = 'この設定を有効にした場合、あなたのギャラリにSlimbox 2が使用されます。あなたのギャラリのイメージをクリックした場合、現在のページにオーバーレイ表示されます。';
 $string['photoframe'] = 'フォトフレームを使用する';
-$string['photoframedesc1'] = 'この設定を有効にした場合、ギャラリー内のそれぞれの写真のサムネイルの周りにフレームが描画されます。';
+$string['photoframedesc1'] = 'この設定を有効にした場合、ギャラリ内のそれぞれの写真のサムネイルの周りにフレームが描画されます。';
 $string['previewwidth'] = '最大写真幅';
 $string['previewwidthdesc'] = '写真がSlimbox 2で表示される場合、リサイズされる最大幅を設定してください。';
 $string['flickrsettings'] = 'Flickr設定';
@@ -51,7 +51,7 @@ $string['by'] = 'by';
 $string['panoramiocopyright'] = 'Panoramioから提供された写真はそのオーナが著作権を有します。';
 $string['panoramiouserphotos'] = 'Panoramioユーザ写真';
 $string['picasaalbums'] = 'Picasaアルバム';
-$string['windowslivephotoalbums'] = 'Windows Liveフォトギャラリーアルバム';
+$string['windowslivephotoalbums'] = 'Windows Liveフォトギャラリアルバム';
 $string['externalnotsupported'] = 'あなたが提供した外部URLはサポートされていません。';
 
 ?>
