c982c7d1a2975773c6f649316a91a843d908640e
Mitsuhiro Yoshida <mits@mitstek.com>
2022-08-15 17:32:33 +0900

    Brushed up translated strings and help files for gallery.

--- artefact/file/blocktype/gallery/lang/ja.utf8/blocktype.gallery.php
+++ artefact/file/blocktype/gallery/lang/ja.utf8/blocktype.gallery.php
@@ -13,14 +13,14 @@
 
 defined('INTERNAL') || die();
 
-$string['title'] = 'イメージギャラリー';
-$string['description1'] = 'あなたのファイルエリアまたは外部ギャラリーのイメージコレクションです。';
+$string['title'] = 'イメージギャラリ';
+$string['description1'] = 'あなたのファイルエリアまたは外部ギャラリのイメージコレクションです。';
 $string['select'] = 'イメージ選択';
 $string['selectfolder'] = '後でアップロードされたイメージを含むイメージすべてをフォルダから表示する';
 $string['selectimages'] = '表示するイメージを個別に選択する';
-$string['selectexternal'] = '外部ギャラリーのイメージを表示する';
-$string['externalgalleryurl'] = 'ギャラリーURLまたはRSS';
-$string['externalgalleryurldesc'] = 'あなたは次の外部ギャラリーを埋め込めます:';
+$string['selectexternal'] = '外部ギャラリのイメージを表示する';
+$string['externalgalleryurl'] = 'ギャラリURLまたはRSS';
+$string['externalgalleryurldesc'] = 'あなたは次の外部ギャラリを埋め込めます:';
 $string['width'] = '幅';
 $string['widthdescription'] = 'あなたのイメージの幅 (ピクセル) を指定してください。イメージはこの幅にサイズ変更されます。';
 $string['style'] = 'スタイル';
@@ -30,9 +30,9 @@ $string['styleslideshow'] = 'スライドショー';
 $string['showdescriptions'] = '説明を表示する';
 $string['showdescriptionsdescription'] = 'それぞれのイメージに説明を表示する場合、このオプションを有効にしてください。';
 $string['cannotdisplayslideshow'] = 'スライドショーを表示できません。';
-$string['gallerysettings'] = 'ギャラリー設定';
+$string['gallerysettings'] = 'ギャラリ設定';
 $string['usefancybox'] = 'Fancybox 3を使用する';
-$string['usefancyboxdesc'] = 'あなたのギャラリーにFancybox 3を使用します。あなたのギャラリーのイメージをクリックした場合、現在のページにオーバーレイ表示されます。';
+$string['usefancyboxdesc'] = 'あなたのギャラリにFancybox 3を使用します。あなたのギャラリのイメージをクリックした場合、現在のページにオーバーレイ表示されます。';
 $string['previewwidth'] = '最大写真幅';
 $string['previewwidthdesc'] = '写真がSlimbox 2で表示される場合、リサイズされる最大幅を設定してください。';
 $string['CLOSE'] = '閉じる';
