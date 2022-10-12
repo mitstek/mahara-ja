f3fed92483ce87957636ceafb838ed903fad1682
Mitsuhiro Yoshida <mits@mitstek.com>
2022-10-12 13:00:41 +0900

    Brushed up a translated string for Image gallery.

--- artefact/file/blocktype/gallery/lang/ja.utf8/blocktype.gallery.php
+++ artefact/file/blocktype/gallery/lang/ja.utf8/blocktype.gallery.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2022-10-12 03:37:08 UTC
+ * @updated    2022-10-12 03:59:45 UTC
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -28,7 +28,7 @@ $string['stylethumbs'] = 'サムネイル';
 $string['stylesquares'] = 'サムネイル (正方形)';
 $string['styleslideshow'] = 'スライドショー';
 $string['showdescriptions1'] = 'キャプションを表示する';
-$string['showdescriptionsdescription'] = 'それぞれのイメージに説明を表示する場合、このオプションを有効にしてください。';
+$string['showdescriptionsdescription'] = 'それぞれのイメージにキャプションを表示する場合、このオプションを有効にしてください。';
 $string['cannotdisplayslideshow'] = 'スライドショーを表示できません。';
 $string['gallerysettings'] = 'ギャラリ設定';
 $string['usefancybox1'] = 'Bootstrapモーダルを使用する';
