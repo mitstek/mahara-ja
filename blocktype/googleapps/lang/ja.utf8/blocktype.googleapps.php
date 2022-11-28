55de6a6a1d6e19f39e275227f8903b40ebc5c9b8
Mitsuhiro Yoshida <mits@mitstek.com>
2022-11-28 09:43:42 +0900

    Brushed up translated strings and help files  for Google Apps.

--- blocktype/googleapps/lang/ja.utf8/blocktype.googleapps.php
+++ blocktype/googleapps/lang/ja.utf8/blocktype.googleapps.php
@@ -13,12 +13,12 @@
 
 defined('INTERNAL') || die();
 
-$string['title'] = 'Google Apps';
+$string['title'] = 'Googleアプリ';
 $string['description'] = 'Googleカレンダーまたはドキュメントを埋め込みます。';
 $string['appscodeorurl'] = 'コードまたはURLを埋め込む';
 $string['appscodeorurldesc1'] = 'Googleコンテンツ埋め込み方法のインストラクションに関して<a href="https://manual.mahara.org/en/%s/blocks/external.html#google-apps">ユーザマニュアル</a>をご覧ください。
 <br />
-埋め込みコードまたはGoogle Appsが一般公開されているページのURLを貼り付けてください。';
+埋め込みコードまたはGoogleアプリが一般公開されているページのURLを貼り付けてください。';
 $string['height'] = '高さ';
 $string['badurlerror'] = '埋め込みコードまたはURLを構文解析できません: %s';
 
