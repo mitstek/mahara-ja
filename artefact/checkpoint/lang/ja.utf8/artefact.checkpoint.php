d0f56a0b158f8947d37c4b7b102ef1bdf871283a
Mitsuhiro Yoshida <mits@mitstek.com>
2023-02-09 08:13:19 +0900

    Bug 1991603, 1991604: Translated new strings and a help file for Outcomes portfolio.

--- artefact/checkpoint/lang/ja.utf8/artefact.checkpoint.php
+++ artefact/checkpoint/lang/ja.utf8/artefact.checkpoint.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2023-02-03 04:16:54 UTC
+ * @updated    2023-02-08 23:12:46 UTC
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -26,6 +26,8 @@ $string['achievementlevel'] = '達成レベル';
 $string['level'] = 'レベル';
 $string['level_cap'] = 'レベル';
 $string['save'] = '保存';
+$string['achievementauthor'] = '検証担当';
+$string['achievementleveltime'] = '検証日時';
 $string['checkpointfeedback'] = 'チェックポイントフィードバック';
 $string['placeassessment'] = 'チェックポイントを投稿する';
 $string['progress_checkpointfeedback'] = '%s のチェックポイントフィードバックを追加する';
