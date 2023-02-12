aa4e7aa7e489f88243f7b1b5a95680a810d6fb5d
Mitsuhiro Yoshida <mits@mitstek.com>
2023-02-13 08:48:14 +0900

    Brushed up a translated string for page.

--- lang/ja.utf8/view.php
+++ lang/ja.utf8/view.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2023-02-05 21:51:52 UTC
+ * @updated    2023-02-12 23:48:07 UTC
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -191,7 +191,7 @@ $string['moreoptions'] = '高度なオプション';
 $string['moreinstitutions'] = 'さらにインスティテューションを表示する';
 $string['allviews'] = 'すべてのページ';
 $string['peopleinmyinstitution'] = '「 %s 」の人';
-$string['peopleinmyinstitutions'] = '私のインスティテューションの人';
+$string['peopleinmyinstitutions'] = 'マイインスティテューションの人';
 $string['otherpeople'] = '他の人';
 $string['addaccess'] = '「 %s 」へのアクセスを追加する';
 $string['addaccessinstitution'] = 'インスティテューション「 %s 」へのアクセスを追加する';
