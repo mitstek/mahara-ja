177a9b08ce9de96947a10807171270eea189cb10
Mitsuhiro Yoshida <mits@mitstek.com>
2023-06-19 07:26:31 +0900

    Brushed up a translated string for Assessment report.

--- module/assessmentreport/lang/ja.utf8/module.assessmentreport.php
+++ module/assessmentreport/lang/ja.utf8/module.assessmentreport.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2022-08-14 23:35:28 UTC
+ * @updated    2023-06-18 22:26:23 UTC
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,7 +14,7 @@
 defined('INTERNAL') || die();
 
 $string['pluginname'] = '評価レポート';
-$string['message1'] = 'これはあなたが「 %s 」 (%s) をグループ「 %s 」 (提出日: %s) に正常に提出したこと確認するためのものです。
+$string['message1'] = 'これはあなたが「 %s 」 (%s) を「 %s 」 (提出日: %s) に正常に提出したこと確認するためのものです。
 
 この提出に関して質問がある場合、あなたのインストラクタにご連絡ください。';
 $string['messageexternal1'] = 'これはあなたが「 %s 」 (%s) を外部ホスト「 %s 」 (提出日: %s) に正常に提出したこと確認するためのものです。
