489605ca381134019e20bd7f466e48afa6a13c82
Mitsuhiro Yoshida <mits@mitstek.com>
2023-08-01 16:46:08 +0900

    Translated CPD-related strings and a help file.

---
+++ artefact/cpds/blocktype/cpds/lang/ja.utf8/blocktype.cpds.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2023-08-01 07:34:36 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'CPD';
+$string['description'] = 'あなたの継続的専門能力開発 (CPD) の記録を表示します。';
+$string['defaulttitledescription'] = '空白の場合、CPDのタイトルが使用されます。';
+$string['newercpds'] = '新しいCPD';
+$string['nocpdsaddone'] = 'まだCPDはありません。%s追加してください%s!';
+$string['oldercpds'] = '古いCPD';
+$string['cpdstoshow'] = '表示するCPD';
+$string['taglist'] = 'タグ付けされた活動を表示する';
+$string['notagsavailable'] = 'あなたはCPD活動にタグを付けしていません。';
+$string['notagsavailableerror'] = 'タグが選択されていません。あなたがここで選択できるようにするにはCPDエントリにタグを追加する必要があります。';
+$string['startdate'] = 'From';
+$string['startdatedesc'] = '活動開始日がこの日付以降のエントリをフィルタリングします。';
+$string['enddate'] = 'To';
+$string['enddatedesc'] = '活動開始日がこの日付以前のエントリをフィルタリングします。';
+$string['dateserror'] = '開始日は終了日の前に設定してください。';
+$string['addactivity'] = '活動を追加する';
+$string['addnewactivity'] = '新しい活動を追加する';
+$string['sortorder'] = '並べ替え順';
+$string['sortorderdesc'] = '開始日を以下の順序で並べ替える';
---
+++ artefact/cpds/lang/ja.utf8/artefact.cpds.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2023-08-01 07:44:18 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['pluginname'] = 'CPD';
+$string['description'] = '説明';
+$string['manageactivities'] = '活動を管理する';
+$string['mycpds'] = '継続的専門能力開発';
+$string['newcpd'] = '新しいCPD活動';
+$string['nocpdsselectone'] = 'CPD活動が選択されていません。表示するCPD活動を作成および選択してください。';
+$string['cpd'] = 'CPD';
+$string['cpds'] = 'CPD';
+$string['cpdsactivitiestitle'] = 'CPD活動';
+$string['cpdsavedsuccessfully'] = 'CPD活動が正常に保存されました。';
+$string['cpdsactivities'] = 'CPD %s 活動';
+$string['cpdsactivitiesdesc'] = '以下または右側のボタンで活動を追加してあなたのCPDの構築を開始してください。';
+$string['duplicatedcpd'] = '重複CPD活動';
+$string['savecpd'] = 'CPDを保存する';
+$string['title'] = 'タイトル';
+$string['titledesc'] = 'CPDブロックに表示するタイトルです。';
+$string['allactivities'] = 'すべての活動';
+$string['hours'] = '時間';
+$string['totalhours'] = '合計時間';
+$string['startdate'] = '開始日';
+$string['enddate'] = '終了日';
+$string['hoursdesc1'] = '時間を表す10進数のみ入力してください。例)「2.5」は「2時間30分」を意味します。';
+$string['deleteactivityconfirm'] = '本当にこの活動を削除してもよろしいですか?';
+$string['deleteactivity'] = '活動を削除する';
+$string['deletethisactivity'] = '活動を削除する: 「 %s 」';
+$string['editactivity'] = '活動を編集する';
+$string['editingactivity'] = '活動を編集する';
+$string['myactivities'] = '活動';
+$string['newactivity'] = '新しい活動';
+$string['noactivities'] = '表示する活動はありません。';
+$string['noactivitiesaddone'] = 'まだCPDはありません。%s追加してください%s!';
+$string['saveactivity'] = '活動を保存する';
+$string['activitytitle'] = '活動';
+$string['activity'] = '活動';
+$string['activities'] = '活動';
+$string['activitydeletedsuccessfully'] = '活動が正常に削除されました。';
+$string['activitiesavedsuccessfully'] = '活動が正常に保存されました。';
+$string['location'] = '場所';
+$string['locationdesc'] = 'あなたはどこでCPD活動に参加しましたか? これはオンラインでも物理的な場所でもかまいません。';
+$string['cpdactivity'] = 'CPD活動';
+$string['progress_cpdactivity'] = '%s のCPD活動を追加する';
