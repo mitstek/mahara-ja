57795db9bfcfabe50144dfc0e1d949a768f70087
Mitsuhiro Yoshida <mits@mitstek.com>
2023-11-01 08:28:12 +0900

    Brushed up a translated string for Email.

--- notification/email/lang/ja.utf8/notification.email.php
+++ notification/email/lang/ja.utf8/notification.email.php
@@ -23,6 +23,6 @@ $string['referurl'] = '詳細 %s';
 $string['unsubscribe'] = '購読解除するには %s にアクセスしてください。';
 $string['unsubscribetitle'] = '購読解除';
 $string['unsubscribesuccess'] = 'あなたは正常に購読解除しました。';
-$string['unsubscribefailed1'] = 'あなたは購読解除に失敗しました。あなたがすでに購読解除しているか手動で購読解除する必要があります。ログインしてサイトの関連セクションにアクセスしてください。';
+$string['unsubscribefailed1'] = 'あなたは購読解除に失敗しました。あなたがすでに購読解除しているか、手動で購読解除する必要があります。ログインしてサイトの関連セクションにアクセスしてください。';
 $string['unsubscribe_watchlist'] = 'これをあなたのウォッチリストから削除するには %s にアクセスしてください。';
 $string['unsubscribe_watchlist_heading'] = 'あなたの「 %s 」のウォッチリスト通知を削除する';
