1bde9304b6d5058c621b3f6ea4f18458dac7fad6
Mitsuhiro Yoshida <mits@mitstek.com>
2023-11-22 06:47:39 +0900

    Brushed up translated strings for Elasticsearch.

--- search/elasticsearch/lang/ja.utf8/search.elasticsearch.php
+++ search/elasticsearch/lang/ja.utf8/search.elasticsearch.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2023-11-19 20:22:50 UTC
+ * @updated    2023-11-21 21:47:26 UTC
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -43,7 +43,7 @@ $string['elasticsearchtooold'] = 'あなたのElasticsearchのバージョン %s
 $string['filterresultsby'] = '結果をフィルタする';
 $string['forum'] = 'フォーラム';
 $string['forumpost'] = 'フォーラム投稿';
-$string['forumpostedbylabel'] = '投稿:';
+$string['forumpostedbylabel'] = '投稿者:';
 $string['forumpostedby'] = '%s - %s';
 $string['forumtopic'] = 'フォーラムトピック';
 $string['Group'] = 'グループ';
