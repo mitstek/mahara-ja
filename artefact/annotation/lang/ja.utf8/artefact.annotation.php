e6ad7d169359548aef11c216bd9c94048a720e29
Mitsuhiro Yoshida <mits@mitstek.com>
2024-01-01 22:22:42 +0900

    Brushed up a translated string for Annotation.

--- artefact/annotation/lang/ja.utf8/artefact.annotation.php
+++ artefact/annotation/lang/ja.utf8/artefact.annotation.php
@@ -84,7 +84,7 @@ $string['annotationfeedbackdeletedtext'] = 'アノテーション %s のフィ�
 %s をオンラインで確認するには次のリンクにアクセスしてください:
 %s';
 $string['artefactdefaultpermissions'] = 'デフォルトアノテーションパーミッション';
-$string['artefactdefaultpermissionsdescription'] = '選択されたアーティファクトタイプではフィードバックの作成が有効にされます。ユーザは個々のアーティファクトでこれらの設定をオーバーライドできます。';
+$string['artefactdefaultpermissionsdescription'] = '選択されたアーティファクトタイプでは作成時にフィードバックが有効にされます。ユーザは個々のアーティファクトでこれらの設定をオーバーライドできます。';
 $string['annotationinformationerror'] = '私たちにはアノテーションを表示するための正しい情報がありません。';
 $string['invalidannotationfeedbacklinkerror'] = 'フィードバックはアーティファクトまたはページにリンクされる必要があります。';
 $string['entriesimportedfromleapexport'] = '他の場所でインポートできなかったLeap2Aエクスポートからインポートされたエントリです。';
