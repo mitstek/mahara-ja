2b79704e9a0bc649470611e07cc952587957ece5
Mitsuhiro Yoshida <mits@mitstek.com>
2024-01-15 09:30:15 +0900

    Brushed up a translated string for Journal entry.

--- artefact/blog/blocktype/blogpost/lang/ja.utf8/blocktype.blogpost.php
+++ artefact/blog/blocktype/blogpost/lang/ja.utf8/blocktype.blogpost.php
@@ -16,4 +16,4 @@ defined('INTERNAL') || die();
 $string['title'] = '日誌エントリ';
 $string['description'] = 'あなたの日誌の単一エントリです (「コンテンツ -> 日誌」をご覧ください)。';
 $string['defaulttitledescription'] = 'ここを空白にした場合、日誌エントリのタイトルが使用されます。';
-$string['choosepublishedblogpostsdescription'] = 'あなたの公開済み日誌エントリの中から1つ選択してください。';
+$string['choosepublishedblogpostsdescription'] = 'あなたの公開済み日誌エントリから1つ選択してください。';
