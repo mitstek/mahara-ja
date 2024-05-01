dd05d70b26e1bba5884072515fafc46b4e682583
Mitsuhiro Yoshida <mits@mitstek.com>
2024-05-01 15:19:26 +0900

    Brushed up translated strings for account and arteface Files.

--- lang/ja.utf8/account.php
+++ lang/ja.utf8/account.php
@@ -138,7 +138,7 @@ $string['canceldeletionadminemailhtml'] = '<p>管理者さん、こんにちは�
 <pre>--
 %s チーム</pre>';
 $string['resizeonuploaduserdefault1'] = 'アップロード時にイメージをリサイズする';
-$string['resizeonuploaduserdefaultdescription2'] = 'デフォルトで「イメージを自動的にリサイズする」オプションが有効にされます。最大高さおよび幅を超えたイメージはアップロード時にリサイズされます。個別のイメージアップロード時、あなたはこのデフォルト設定を無効にできます。';
+$string['resizeonuploaduserdefaultdescription2'] = 'デフォルトで「イメージを自動的にリサイズする」オプションが有効にされます。最大高および幅を超えたイメージはアップロード時にリサイズされます。個別のイメージアップロード時、あなたはこのデフォルト設定を無効にできます。';
 $string['devicedetection'] = 'デバイス検出';
 $string['devicedetectiondescription'] = 'このサイトの閲覧時のモバイルデバイス検出を有効にします。';
 $string['noprivacystatementsaccepted'] = 'このアカウントは現在のプライバシ保護方針を受け入れていません。';
