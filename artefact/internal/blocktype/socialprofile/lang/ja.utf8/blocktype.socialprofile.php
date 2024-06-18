0a9f903b3c31613080dc25adfaa16ba6924fca9e
Mitsuhiro Yoshida <mits@mitstek.com>
2024-06-19 06:41:42 +0900

    Brushed up a translated string for Social media block.

--- artefact/internal/blocktype/socialprofile/lang/ja.utf8/blocktype.socialprofile.php
+++ artefact/internal/blocktype/socialprofile/lang/ja.utf8/blocktype.socialprofile.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2018-08-23 04:48:19 UTC
+ * @updated    2024-06-18 21:41:33 UTC
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -23,4 +23,4 @@ $string['optiontexticon'] = 'アイコンおよびテキストのボタン';
 $string['optiontextonly'] = 'テキストのみのボタン';
 $string['displaydefaultemail'] = 'デフォルトメールアドレスのリンクをボタンとして表示しますか?';
 $string['displaymsgservices'] = 'メッセージングサービスをボタンとして表示しますか?';
-$string['noitemsselectone'] = '選択されたソーシャルメディアはありません。';
+$string['noitemsselectone'] = 'ソーシャルメディアが選択されていません。';
