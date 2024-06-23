02b3787fea32d3a8e915c7820b513830c51eb755
Mitsuhiro Yoshida <mits@mitstek.com>
2024-06-24 05:25:34 +0900

    Brushed up a translated string for Note block.

--- artefact/internal/blocktype/textbox/lang/ja.utf8/blocktype.textbox.php
+++ artefact/internal/blocktype/textbox/lang/ja.utf8/blocktype.textbox.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2019-05-02 20:35:25 UTC
+ * @updated    2024-06-23 20:25:21 UTC
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -17,7 +17,7 @@ $string['title'] = 'ノート';
 $string['description'] = 'あなたのページにノートを追加します。';
 $string['blockcontent'] = 'ブロックコンテンツ';
 $string['usecontentfromanothertextbox1'] = '別のノートのコンテンツを使用する';
-$string['textusedinotherblocks'] = 'あなたがこのブロックのテキストを編集した場合、テキストが表示されている他の %s ブロックも更新されます。';
+$string['textusedinotherblocks'] = 'あなたがこのブロックのテキストを編集した場合、テキストが表示されている他の %s ブロックでも更新されます。';
 $string['managealltextboxcontent1'] = 'すべてのノートコンテンツを管理する';
 $string['readonlymessage'] = 'あなたが選択したテキストはこのページで編集できません。';
 $string['makeacopy'] = 'コピーを作成する';
