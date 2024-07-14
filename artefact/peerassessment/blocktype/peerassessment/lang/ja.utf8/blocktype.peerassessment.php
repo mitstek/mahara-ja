d5712c5cb5b82243812f40484c5c86708f52eaf7
Mitsuhiro Yoshida <mits@mitstek.com>
2024-07-15 04:41:34 +0900

    Brushed up a translated string for Peer assessment.

--- artefact/peerassessment/blocktype/peerassessment/lang/ja.utf8/blocktype.peerassessment.php
+++ artefact/peerassessment/blocktype/peerassessment/lang/ja.utf8/blocktype.peerassessment.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2021-01-29 04:05:21 UTC
+ * @updated    2024-07-14 19:41:23 UTC
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -22,4 +22,4 @@ $string['instructions'] = 'インストラクション';
 $string['savedraft'] = '下書きを保存する';
 $string['publish'] = '公開する';
 $string['savepublishhelp'] = '<p><strong>下書きを保存する:</strong> あなたのみ閲覧できます。あなたの評価が下書きステータスの間は変更できます。</p>
-<p><strong>公開する:</strong> あなたが相互評価を与えている人はあなたの評価を閲覧できます。ポートフォリオにも承認ブロックが含まれポートフォリオオーナによって承認されていない限り、ポートフォリオにアクセスできる人は誰でも評価を閲覧できます。あなたは公開済み評価を下書きステータスに戻せません。</p>';
+<p><strong>公開する:</strong> あなたが相互評価を与えている人はあなたの評価を閲覧できます。ポートフォリオに承認ブロックが含まれていてポートフォリオオーナに承認されていない限り、ポートフォリオにアクセスできるすべての人は同様に評価を閲覧できます。あなたは公開済み評価を下書きステータスに戻せません。</p>';
