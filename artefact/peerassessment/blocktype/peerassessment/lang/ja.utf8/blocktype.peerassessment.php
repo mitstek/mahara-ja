c05363126b0b78dbe51759607d54fbeae75771b8
Mitsuhiro Yoshida <mits@mitstek.com>
2025-01-10 09:25:02 +0900

    Issue #477: Translated new strings for the Peer and Self Assessment block.

--- artefact/peerassessment/blocktype/peerassessment/lang/ja.utf8/blocktype.peerassessment.php
+++ artefact/peerassessment/blocktype/peerassessment/lang/ja.utf8/blocktype.peerassessment.php
@@ -1,14 +1,30 @@
 <?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2025-01-10 00:24:40 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
 
 defined('INTERNAL') || die();
 
-$string['addpeerassessment'] = '相互評価を追加する';
-$string['blockcontent'] = 'インストラクション';
-$string['description'] = '相互評価を表示するためのブロックです。';
-$string['instructions'] = 'インストラクション';
-$string['nopeerassessment'] = '評価フィードバックはありません。';
-$string['publish'] = '公開する';
-$string['savedraft'] = '下書きを保存する';
-$string['savepublishhelp'] = '<p><strong>下書きを保存する:</strong> あなたのみ閲覧できます。あなたの評価が下書きステータスの間は変更できます。</p>
-<p><strong>公開する:</strong> あなたが相互評価を与えている人はあなたの評価を閲覧できます。ポートフォリオに承認ブロックが含まれていてポートフォリオオーナに承認されていない限り、ポートフォリオにアクセスできるすべての人は同様に評価を閲覧できます。あなたは公開済み評価を下書きステータスに戻せません。</p>';
 $string['title'] = '相互評価';
+$string['description'] = '相互評価を表示するためのブロックです。';
+$string['blockcontent'] = 'インストラクション';
+$string['nopeerassessment'] = '評価フィードバックはありません。';
+$string['addpeerassessment'] = '相互評価を追加する';
+$string['addselfassessment'] = '自己評価を追加する';
+$string['instructions'] = 'インストラクション';
+$string['savedraft'] = '下書きを保存する';
+$string['publish'] = '公開する';
+$string['savepublishhelp1'] = '<p><strong>下書きを保存する:</strong> あなたのみ閲覧できます。あなたの評価が下書きステータスの間は変更できます。</p>
+<p><strong>公開する:</strong> あなたの評価の閲覧を許可されているすべての人がその評価を閲覧できます。公開した評価を下書き状態に戻せませんが、公開後 %s 分間は更新できます。また、あなたは評価を削除できます。</p>';
+$string['type'] = '評価タイプ';
+$string['self'] = '自己評価';
+$string['peer'] = '相互評価';
+$string['typedescription'] = 'このブロックの評価タイプを選択してください。';
