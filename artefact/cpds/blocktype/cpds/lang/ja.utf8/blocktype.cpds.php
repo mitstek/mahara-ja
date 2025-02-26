efc5b556c34df2dac1f4cfccbf8a8f64921bed8b
Mitsuhiro Yoshida <mits@mitstek.com>
2025-02-26 13:06:40 +0900

    Issue #447: Translated new strings for the CPD functionality.

--- artefact/cpds/blocktype/cpds/lang/ja.utf8/blocktype.cpds.php
+++ artefact/cpds/blocktype/cpds/lang/ja.utf8/blocktype.cpds.php
@@ -1,24 +1,42 @@
 <?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2025-02-26 03:30:45 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
 
 defined('INTERNAL') || die();
 
-$string['addactivity'] = '活動を追加する';
-$string['addnewactivity'] = '新しい活動を追加する';
-$string['cpdstoshow'] = '表示するCPD';
-$string['dateserror'] = '開始日は終了日の前に設定してください。';
-$string['defaulttitledescription'] = '空白の場合、CPDのタイトルが使用されます。';
+$string['title'] = 'CPD';
 $string['description'] = 'あなたの継続的専門能力開発 (CPD) の記録を表示します。';
-$string['enddate'] = '活動終了日';
-$string['enddatedesc'] = '活動開始日がこの日付以前のエントリをフィルタリングします。';
-$string['newercpds'] = '新しいCPD';
+$string['defaulttitledescription'] = '空白の場合、CPDのタイトルが使用されます。';
 $string['nocpdsaddone'] = 'まだCPDはありません。%sCPDを追加してください%s!';
+$string['notselected'] = '選択なし';
+$string['taglist'] = 'タグ付けされた活動を表示する';
 $string['notagsavailable'] = 'あなたはCPD活動にタグ付けしていません。';
 $string['notagsavailableerror'] = 'タグが選択されていません。あなたがここで選択できるようにするにはCPDエントリにタグを追加する必要があります。';
-$string['oldercpds'] = '古いCPD';
-$string['sortorder'] = '並べ替え順';
-$string['sortorderdesc'] = '開始日を以下の順序で並べ替える';
 $string['startdate'] = '活動開始日';
 $string['startdatedesc'] = '活動開始日がこの日付以降のエントリをフィルタリングします。';
+$string['enddate'] = '活動終了日';
+$string['enddatedesc'] = '活動開始日がこの日付以前のエントリをフィルタリングします。';
 $string['startenddate'] = '活動日';
-$string['taglist'] = 'タグ付けされた活動を表示する';
-$string['title'] = 'CPD';
+$string['dateserror'] = '開始日は終了日の前に設定してください。';
+$string['addactivity'] = '活動を追加する';
+$string['addnewactivity'] = '新しい活動を追加する';
+$string['sortorder'] = '並べ替え順';
+$string['sortorderdesc'] = '開始日を以下の順序で並べ替える';
+$string['Experiencedesc'] = 'あなたの活動を表示したいCPD経験を選択してください。';
+$string['Categorydesc'] = 'あなたが完了した時間を表示したいカテゴリを選択してください。';
+$string['experiencedesctitle'] = '%s 経験要件:';
+$string['categorydesctitle'] = '%s カテゴリ要件:';
+$string['requirementsminimum'] = '最小時間';
+$string['requirementsmaximum'] = '最大時間';
+$string['requirementscountable'] = '適正時間';
+$string['requirementsreflections'] = '内省';
+$string['requirementsoutof'] = '/';
--- artefact/cpds/lang/ja.utf8/artefact.cpds.php
+++ artefact/cpds/lang/ja.utf8/artefact.cpds.php
@@ -1,47 +1,130 @@
 <?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2025-02-26 04:06:18 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
 
 defined('INTERNAL') || die();
 
-$string['activities'] = '活動';
-$string['activitiesavedsuccessfully'] = '活動が正常に保存されました。';
-$string['activity'] = '活動';
-$string['activitydeletedsuccessfully'] = '活動が正常に削除されました。';
-$string['activitytitle'] = '活動';
-$string['allactivities'] = 'すべての活動';
+$string['pluginname'] = 'CPD';
+$string['description'] = '説明';
+$string['manageactivities'] = '活動を管理する';
+$string['newcpd'] = '新しいCPD活動';
 $string['cpd'] = 'CPD';
-$string['cpdactivity'] = 'CPD活動';
 $string['cpds'] = 'CPD';
-$string['cpdsactivities'] = 'CPD %s 活動';
-$string['cpdsactivitiesdesc'] = '以下の活動を追加するか、右のボタンであなたのCPD構築を開始してください。';
 $string['cpdsactivitiestitle'] = 'CPD活動';
 $string['cpdsavedsuccessfully'] = 'CPD活動が正常に保存されました。';
-$string['deleteactivity'] = '活動を削除する';
-$string['deleteactivityconfirm1'] = '本当にこの活動を完全に削除してもよろしいですか? この活動を削除した場合、表示されているすべてのポートフォリオから削除されます。この操作は元に戻せません。';
-$string['deletethisactivity'] = '活動を削除する: 「 %s 」';
-$string['description'] = '説明';
+$string['cpdsactivities'] = 'CPD %s 活動';
+$string['cpdsactivitiesdesc'] = '以下の活動を追加するか、右のボタンであなたのCPD構築を開始してください。';
 $string['duplicatedcpd'] = '重複CPD活動';
-$string['editactivity'] = '活動を編集する';
-$string['editingactivity'] = '活動を編集する';
-$string['enddate'] = '終了日';
-$string['hours'] = '時間';
-$string['hoursdesc1'] = '時間を表す10進数のみ入力してください。例)「2.5」は「2時間30分」を意味します。';
-$string['location'] = '場所';
-$string['locationdesc'] = 'あなたはどこでCPD活動に参加しましたか? これはオンラインでも物理的な場所でもかまいません。';
-$string['manageactivities'] = '活動を管理する';
-$string['myactivities'] = '活動';
-$string['mycpds'] = '継続的専門能力開発';
-$string['newactivity'] = '新しい活動';
-$string['newcpd'] = '新しいCPD活動';
-$string['noactivities'] = '表示する活動はありません。';
-$string['noactivitiesaddone'] = 'まだ活動はありません。%s活動を追加してください%s。';
-$string['nocpdsselectone'] = 'CPD活動が選択されていません。表示するCPD活動を作成および選択してください。';
-$string['pluginname'] = 'CPD';
-$string['progress_cpdactivity'] = array(
-    0 => '%s のCPD活動を追加する',
-);
-$string['saveactivity'] = '活動を保存する';
 $string['savecpd'] = 'CPDを保存する';
-$string['startdate'] = '開始日';
 $string['title'] = 'タイトル';
 $string['titledesc'] = 'CPDブロックに表示するタイトルです。';
+$string['allactivities'] = 'すべての活動';
+$string['hours'] = '時間';
 $string['totalhours'] = '合計時間';
+$string['totalhourscompleted'] = '合計時間完了';
+$string['startdate'] = '開始日';
+$string['enddate'] = '終了日';
+$string['hoursdesc1'] = '時間を表す10進数のみ入力してください。例)「2.5」は「2時間30分」を意味します。';
+$string['deleteactivityconfirm1'] = '本当にこの活動を完全に削除してもよろしいですか? この活動を削除した場合、表示されているすべてのポートフォリオから削除されます。この操作は元に戻せません。';
+$string['deleteactivity'] = '活動を削除する';
+$string['deletethisactivity'] = '活動を削除する: 「 %s 」';
+$string['editactivity'] = '活動を編集する';
+$string['editingactivity'] = '活動を編集する';
+$string['myactivities'] = '活動';
+$string['newactivity'] = '新しい活動';
+$string['noactivities'] = '表示する活動はありません。';
+$string['noactivitiesaddone'] = 'まだ活動はありません。%s活動を追加してください%s。';
+$string['saveactivity'] = '活動を保存する';
+$string['activitytitle'] = '活動';
+$string['activity'] = '活動';
+$string['activities'] = '活動';
+$string['activitydeletedsuccessfully'] = '活動が正常に削除されました。';
+$string['activitiesavedsuccessfully'] = '活動が正常に保存されました。';
+$string['location'] = '場所';
+$string['locationdesc'] = 'あなたはどこでCPD活動に参加しましたか? これはオンラインでも物理的な場所でもかまいません。';
+$string['organisation'] = '組織';
+$string['organisationdesc'] = 'あなたが活動を完了した組織名を入力してください。';
+$string['contactname'] = '担当者';
+$string['contactnamedesc'] = 'この活動に関して連絡可能な人の氏名を入力してください。';
+$string['contacttitle'] = '担当者役職';
+$string['contacttitledesc'] = 'プロジェクトマネージャ、プロダクトマネージャ、スーパーバイザ等の担当者の役職を入力してください。';
+$string['contactemail'] = '担当者メール';
+$string['contactemaildesc'] = '担当者のメールアドレスを入力してください。';
+$string['contactphone'] = '担当者電話番号';
+$string['contactphonedesc'] = '担当者の電話番号を入力してください。';
+$string['reflection'] = '内省';
+$string['experiencehours'] = '詳細経験時間';
+$string['experiencesubmittederror'] = 'ポートフォリオで提出された経験は選択できません。';
+$string['categoryerror'] = '経験を選択する場合、カテゴリを選択する必要があります。';
+$string['submitted'] = '送信済み';
+$string['Experience'] = '経験';
+$string['Experiencedesc'] = 'あなたが活動を追加したいCPD経験を選択してください。';
+$string['Experienceoverviewdesc'] = 'あなたが視覚化したいCPD経験を選択してください。';
+$string['experience'] = '経験';
+$string['experiences'] = '経験';
+$string['cpdsexperiences'] = 'CPD %s 経験';
+$string['cpdsexperiencesdesc'] = '以下に経験を追加するか、右のボタンであなたのCPD経験を構築してください。';
+$string['noexperiencesaddone'] = 'まだ経験はありません。%経験を追加してください%s!';
+$string['newexperience'] = '新しい経験';
+$string['newcpdexperience'] = '新しいCPD経験';
+$string['cpdsexperiencestitle'] = 'CPD経験';
+$string['experiencetitle'] = '経験タイトル';
+$string['experiencetitledesc'] = '総合的なCPD経験のタイトルを入力してください。';
+$string['experiencedesc'] = '経験説明';
+$string['experiencedescdesc'] = 'このCPD経験について目的、オーディエンス、最終更新日等の簡単な説明を入力してください。';
+$string['experienceminhours'] = '最小時間数';
+$string['experienceminhoursdesc'] = '全体験で達成する必要のある最小時間数 (整数) を入力してください。';
+$string['experiencemaxhours'] = '最大時間数';
+$string['experiencemaxhoursdesc'] = '全体験で達成する必要のある最大時間数 (整数) を入力してください。';
+$string['saveexperience'] = '経験を保存する';
+$string['saveexperienceconfirmation'] = '本当にこの経験の設定を変更してもよろしいですか? あなたの変更はすでにこの経験を使用しているすべてのアカウント所有者に影響します。';
+$string['experiencetabletitle'] = 'CPD経験';
+$string['active'] = 'アクティブ';
+$string['inactive'] = '非アクティブ';
+$string['cpdexperiencesavedsuccessfully'] = 'CPD経験が正常に保存されました。';
+$string['editexperience'] = '経験を編集する';
+$string['deleteexperienceconfirm1'] = '本当にこの経験を永久に削除してもよろしいですか? この体験を削除した場合、この体験が表示されているすべてのポートフォリオから削除されます。この操作は元に戻せません。';
+$string['editexperienceconfirm1'] = '本当に本当にこの経験を編集してもよろしいですか? %s アカウント保持者はこの経験に対して活動を記録しています。この経験を変更した場合、これらの変更はアカウント保持者に影響します。あなたが大きな変更を加えたい場合、別の経験を設定した方が良いでしょう。';
+$string['deleteexperience'] = '経験を削除する';
+$string['deletethisexperience'] = '経験を削除する: \'%s\'';
+$string['experiencedeletedsuccessfully'] = '経験が正常に削除されました。';
+$string['experiencedoesnotexist'] = '経験が存在しません。';
+$string['editthisexperience'] = '経験を編集する: \'%s\'';
+$string['categorydoesnotexist'] = 'カテゴリが存在しません。';
+$string['categorytitle'] = 'カテゴリタイトル';
+$string['categorytitledesc'] = 'このカテゴリのタイトルを入力してください。';
+$string['Category'] = 'カテゴリ';
+$string['categories'] = 'カテゴリ';
+$string['Categorydesc'] = 'あなたが活動を追加したいカテゴリ/サブカテゴリを選択してください。あなたは時間を追加できるカテゴリのみ選択できます。';
+$string['categoryparent'] = 'サブカテゴリ';
+$string['categoryparentdesc'] = 'あなたが現在のカテゴリをサブカテゴリにしたい場合、カテゴリを選択してください。';
+$string['categoryminhours'] = '最小時間数';
+$string['categoryminhoursdesc'] = 'カテゴリで達成する必要のある最小時間数 (整数) を入力してください。';
+$string['categorymaxhours'] = '最大時間数';
+$string['categorymaxhoursdesc'] = 'カテゴリで達成する必要のある最大時間数 (整数) を入力してください。';
+$string['categorynumreflec'] = '内省数';
+$string['categorynumreflecdesc'] = 'このカテゴリ全体で必要な内省数を入力してください。';
+$string['categoryhashours'] = '時間付きカテゴリ';
+$string['categoryhashoursdesc'] = 'このカテゴリを選択して時間を設定できますか?';
+$string['addcategorylink'] = 'カテゴリを追加する';
+$string['cannotdeleteinexperience'] = '一度経験が使用されたカテゴリは削除できません。';
+$string['cannotdeleteonlycategory'] = '唯一のカテゴリは削除できません。';
+$string['cannotdeleteparentcategory'] = '親カテゴリは削除できません。';
+$string['unnamedcategory'] = '名称なしカテゴリ';
+$string['deletecategory'] = '「 %s 」カテゴリを削除する';
+$string['deletecategoryconfirm'] = '本当にこのカテゴリを削除してもよろしいですか?';
+$string['notasubcategory'] = 'サブカテゴリではない';
+$string['parentcannotbesubcategory'] = 'このカテゴリは親カテゴリであり、サブカテゴリにできません。';
+$string['onlycategory'] = '他のカテゴリは存在しない';
+$string['noneselected'] = '選択なし';
+$string['cpdactivity'] = 'CPD活動';
+$string['progress_cpdactivity'] = '%s CPD活動を追加する';
