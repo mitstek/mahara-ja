15d619eff6708d33f0bdefdfaeaa301a1c02a8b7
Mitsuhiro Yoshida <mits@mitstek.com>
2025-06-16 07:44:03 +0900

    Revised the commas in the translated strings.

--- lang/ja.utf8/view.php
+++ lang/ja.utf8/view.php
@@ -71,7 +71,7 @@ $string['View'] = 'ビュー';
 $string['Views'] = 'ビュー';
 $string['viewsubmittedtogroup'] = 'このビューは<a href="%s">%s</a>に送信されました。';
 $string['viewsubmittedtogroupon'] = 'このビューは<a href="%s">%s</a>に対して %s に送信されました。';
-$string['nobodycanseethisview2'] = 'あなたのみ、このビューを閲覧できます。';
+$string['nobodycanseethisview2'] = 'あなたのみこのビューを閲覧できます。';
 $string['noviews'] = 'ビューはありません。';
 $string['youhavenoviews'] = 'あなたのビューはありません。';
 $string['youhaveoneview'] = 'あなたには 1 件のビューがあります。';
