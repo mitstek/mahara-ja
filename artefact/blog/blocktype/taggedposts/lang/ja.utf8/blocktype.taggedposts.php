9e608c7dc2da1a64ad8a06785257a5df5f9b4226
Mitsuhiro Yoshida <mits@mitstek.com>
2025-07-23 11:47:07 +0900

    Brushed up a translated string for Group portfolios.

--- artefact/blog/blocktype/taggedposts/lang/ja.utf8/blocktype.taggedposts.php
+++ artefact/blog/blocktype/taggedposts/lang/ja.utf8/blocktype.taggedposts.php
@@ -1,33 +1,38 @@
 <?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2025-07-23 02:34:44 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
 
 defined('INTERNAL') || die();
 
-$string['blockheadingtags'] = array(
-    0 => '「 %2$s 」がタグ付けされた日誌エントリ',
-);
-$string['blockheadingtagsomitboth'] = array(
-    0 => ' (「 %2$ 」を除外)',
-);
-$string['blockheadingtagsomitonly'] = array(
-    0 => '「 %2$s 」がタグ付けされていない日誌エントリ',
-);
-$string['configerror'] = 'ブロック設定中にエラーが発生しました。';
-$string['defaulttitledescription'] = 'ここを空白にした場合、日誌のタイトルが使用されます。';
+$string['title'] = 'タグ付き日誌エントリ';
 $string['description'] = '特定のタグが付けられた日誌エントリを表示します (「コンテンツ -> 日誌」をご覧ください)。';
-$string['excludetag'] = '除外タグ: ';
-$string['itemstoshow'] = '表示するアイテム数';
+$string['blockheadingtags'] = '「 %2$s 」がタグ付けされた日誌エントリ';
+$string['blockheadingtagsomitboth'] = '(「 %2$ 」を除外)';
+$string['blockheadingtagsomitonly'] = '「 %2$s 」がタグ付けされていない日誌エントリ';
+$string['defaulttitledescription'] = 'ここを空白にした場合、日誌のタイトルが使用されます。';
+$string['postsperpage'] = '1ページあたりのエントリ数';
+$string['taglist'] = '次のタグが付けられたエントリを表示する';
+$string['taglistdesc1'] = 'あなたが除外したいそれぞれのタグの前にマイナス記号を入力してください。これらのタグは背景色赤色で表示されます。';
+$string['excludetag'] = '除外タグ:';
 $string['notags'] = '「 %s 」がタグ付けされた日誌エントリはありません。';
+$string['notagsomit'] = '「 %s 」がタグ付けされていない日誌エントリはありません。';
+$string['notagsboth'] = '「 %s 」がタグ付け「 %s 」が除外された日誌エントリはありません。';
 $string['notagsavailable'] = 'あなたはタグを作成していません。';
 $string['notagsavailableerror'] = 'タグが選択されていません。ここで選択できるようにするにはあなたの日誌エントリにタグを追加する必要があります。';
-$string['notagsboth'] = '「 %s 」がタグ付け「 %s 」が除外された日誌エントリはありません。';
-$string['notagsomit'] = '「 %s 」がタグ付けされていない日誌エントリはありません。';
 $string['postedin'] = '-';
 $string['postedon'] = '-';
-$string['postsperpage'] = '1ページあたりのエントリ数';
+$string['updatedon'] = '最終更新日時';
+$string['itemstoshow'] = '表示するアイテム数';
+$string['configerror'] = 'ブロック設定中にエラーが発生しました。';
 $string['showjournalitemsinfull'] = '日誌アイテムをフルに表示する';
 $string['showjournalitemsinfulldesc1'] = 'この設定を有効にした場合、日誌エントリが表示されます。そうでない場合、日誌エントリのタイトルのみ表示されます。';
 $string['tag'] = 'タグ';
-$string['taglist'] = '次のタグが付けられたエントリを表示する';
-$string['taglistdesc1'] = 'あなたが除外したいそれぞれのタグの前にマイナス記号を入力してください。これらのタグは背景色赤色で表示されます。';
-$string['title'] = 'タグ付き日誌エントリ';
-$string['updatedon'] = '最終更新日時';
