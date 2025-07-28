9b87cd3d69adb67d0a77c85bbafd7e7913de9d16
Mitsuhiro Yoshida <mits@mitstek.com>
2025-07-28 09:17:51 +0900

    Brushed up a translated string for Inbox.

--- blocktype/inbox/lang/ja.utf8/blocktype.inbox.php
+++ blocktype/inbox/lang/ja.utf8/blocktype.inbox.php
@@ -14,7 +14,7 @@
 defined('INTERNAL') || die();
 
 $string['title'] = '受信箱';
-$string['description'] = 'あなたの受信箱より選択された最近のメッセージを表示します。';
+$string['description'] = 'あなたの最近の受信箱メッセージから選択したものを表示します。';
 $string['messagetypes'] = '表示するメッセージタイプ';
 $string['maxitems'] = '表示するアイテムの最大数';
 $string['maxitemsdescription'] = '設定範囲: 1～100';
