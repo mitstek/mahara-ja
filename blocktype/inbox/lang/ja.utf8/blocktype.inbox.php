1a4a4bf30e2b541a23d14e873066e6b0bcf2caa2
Mitsuhiro Yoshida <mits@mitstek.com>
2025-07-28 09:12:41 +0900

    Brushed up a translated string for Inbox.

--- blocktype/inbox/lang/ja.utf8/blocktype.inbox.php
+++ blocktype/inbox/lang/ja.utf8/blocktype.inbox.php
@@ -1,12 +1,23 @@
 <?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2025-07-28 00:12:36 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
 
 defined('INTERNAL') || die();
 
-$string['More'] = 'さらに';
-$string['defaulttitledescription'] = 'タイトルフィールドを空白にした場合、デフォルトタイトルが生成されます。';
-$string['description'] = 'あなたの受信箱より選択された最近のメッセージを表示します。';
+$string['title'] = '受信箱';
+$string['description'] = 'あなたの最近の受信箱メッセージから選択したものを表示します。';
+$string['messagetypes'] = '表示するメッセージタイプ';
 $string['maxitems'] = '表示するアイテムの最大数';
 $string['maxitemsdescription'] = '設定範囲: 1～100';
-$string['messagetypes'] = '表示するメッセージタイプ';
+$string['More'] = 'さらに';
 $string['nomessages'] = 'メッセージはありません。';
-$string['title'] = '受信箱';
+$string['defaulttitledescription'] = 'タイトルフィールドを空白にした場合、デフォルトタイトルが生成されます。';
