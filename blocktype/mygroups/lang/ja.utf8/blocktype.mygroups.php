3c340292db5bacd8536bce2ae8f20bab2b342558
Mitsuhiro Yoshida <mits@mitstek.com>
2025-07-30 07:44:20 +0900

    Brushed up translated strings for My groups.

--- blocktype/mygroups/lang/ja.utf8/blocktype.mygroups.php
+++ blocktype/mygroups/lang/ja.utf8/blocktype.mygroups.php
@@ -1,17 +1,28 @@
 <?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2025-07-29 22:44:13 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
 
 defined('INTERNAL') || die();
 
-$string['alphabetical'] = 'A -> Z';
+$string['title'] = 'マイグループ';
+$string['otherusertitle'] = '%s のグループ';
 $string['description'] = 'あなたが属しているグループの一覧を表示します。';
+$string['sortgroups'] = 'グループを並べ替える';
 $string['earliest'] = '参加の古い順';
 $string['latest'] = '参加の新しい順';
-$string['limitto1'] = 'ブロックに表示される最大グループ数';
-$string['limittodesc'] = 'ブロックに表示される最大グループ数です。あなたのグループすべてを表示するには、空白のままにしてください。';
+$string['alphabetical'] = 'A -> Z';
+$string['limitto1'] = '表示する最大グループ数';
+$string['limittodesc'] = 'ブロックに表示する最大グループ数です。あなたのグループすべてを表示するには、空白のままにしてください。';
 $string['limittodescsideblock1'] = 'あなたのサイドバーの「マイグループ」リストに表示される最大グループ数です。あなたのグループすべてを表示するには、空白のままにしてください。';
-$string['numberofmygroupsshowing'] = 'マイグループ表示中 (%s/%s)';
+$string['numberofmygroupsshowing'] = 'グループ表示中 (%s/%s)';
 $string['numberofmygroupsshowingearliest'] = '参加の古いグループ表示中 (%s/%s)';
 $string['numberofmygroupsshowinglatest'] = '参加の新しいグループ表示中 (%s/%s)';
-$string['otherusertitle'] = '%s のグループ';
-$string['sortgroups'] = 'グループを並べ替える';
-$string['title'] = 'マイグループ';
