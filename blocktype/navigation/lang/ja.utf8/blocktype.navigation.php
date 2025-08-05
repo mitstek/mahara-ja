50c4880714d524907fb8a327c6e5c766e8ee47ef
Mitsuhiro Yoshida <mits@mitstek.com>
2025-08-06 05:03:44 +0900

    Brushed up a translated string for Navigation block.

--- blocktype/navigation/lang/ja.utf8/blocktype.navigation.php
+++ blocktype/navigation/lang/ja.utf8/blocktype.navigation.php
@@ -4,7 +4,7 @@ defined('INTERNAL') || die();
 
 $string['collection'] = 'コレクション';
 $string['copytoall'] = 'すべてのページに追加する';
-$string['copytoalldesc'] = 'このページが属しているコレクション内の現在のページでナビゲーションブロックが追加されていなページすべてにナビゲーションブロックを追加します。';
+$string['copytoalldesc'] = 'このページが属しているコレクション内のページでナビゲーションブロックが追加されていない現在のページすべてにナビゲーションブロックを追加します。';
 $string['defaulttitledescription'] = 'ここを空白にした場合、コレクションのタイトルが使用されます。';
 $string['description'] = 'ページのコレクションのナビゲーションです。';
 $string['nocollections1'] = 'コレクションはありません。<a href="%s">コレクションを作成してください</a>。';
