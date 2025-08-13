adb93b793bbda85900953e27d567d170be34a120
Mitsuhiro Yoshida <mits@mitstek.com>
2025-08-13 15:44:54 +0900

    Brushed up translated strings for  Placeholder block.

--- blocktype/placeholder/lang/ja.utf8/blocktype.placeholder.php
+++ blocktype/placeholder/lang/ja.utf8/blocktype.placeholder.php
@@ -4,8 +4,8 @@ defined('INTERNAL') || die();
 
 $string['contenttypes'] = 'コンテンツタイプ';
 $string['defaulttitledescription'] = 'タイトルフィールドを空白にした場合、デフォルトタイトルが生成されます。';
-$string['description'] = 'あなたが希望するブロックタイプをプレースホルダブロックで選択できます。';
+$string['description'] = 'あなたは希望するブロックタイプをプレースホルダブロックで選択できます。';
 $string['placeholderblocktypenotenabled'] = 'ブロックタイプ「プレースホルダ」をインストールおよび有効にする必要があります。すでにインストールされている場合、データベースのテーブル「blocktype_installed」で有効にしてください。';
-$string['placeholdertext1'] = 'タイプを選択するためこのブロックを編集してください。';
+$string['placeholdertext1'] = 'タイプを選択するにはこのブロックを編集してください。';
 $string['recentcontenttypes'] = '最近のコンテンツタイプ';
 $string['title'] = 'プレースホルダ';
