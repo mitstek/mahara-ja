1ac3b2520eb30bda0b325f84455e9b1bc7bafa13
Mitsuhiro Yoshida <mits@mitstek.com>
2025-08-13 15:35:07 +0900

    Brushed up translated strings for  Placeholder block.

--- blocktype/placeholder/lang/ja.utf8/blocktype.placeholder.php
+++ blocktype/placeholder/lang/ja.utf8/blocktype.placeholder.php
@@ -1,11 +1,22 @@
 <?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2025-08-13 06:34:58 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
 
 defined('INTERNAL') || die();
 
-$string['contenttypes'] = 'コンテンツタイプ';
-$string['defaulttitledescription'] = 'タイトルフィールドを空白にした場合、デフォルトタイトルが生成されます。';
-$string['description'] = 'あなたが希望するブロックタイプをプレースホルダブロックで選択できます。';
-$string['placeholderblocktypenotenabled'] = 'ブロックタイプ「プレースホルダ」をインストールおよび有効にする必要があります。すでにインストールされている場合、データベースのテーブル「blocktype_installed」で有効にしてください。';
-$string['placeholdertext1'] = 'タイプを選択するためこのブロックを編集してください。';
-$string['recentcontenttypes'] = '最近のコンテンツタイプ';
 $string['title'] = 'プレースホルダ';
+$string['description'] = 'あなたは希望するブロックタイプをプレースホルダブロックで選択できます。';
+$string['placeholdertext1'] = 'タイプを選択するにはこのブロックを編集してください。';
+$string['defaulttitledescription'] = 'タイトルフィールドを空白にした場合、デフォルトタイトルが生成されます。';
+$string['contenttypes'] = 'コンテンツタイプ';
+$string['recentcontenttypes'] = '最近のコンテンツタイプ';
+$string['placeholderblocktypenotenabled'] = 'ブロックタイプ「プレースホルダ」をインストールおよび有効にする必要があります。すでにインストールされている場合、データベースのテーブル「blocktype_installed」で有効にしてください。';
