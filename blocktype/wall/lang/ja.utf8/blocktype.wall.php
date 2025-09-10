58f1d2e423d648c3a65a9ec450a304ffe8a6b096
Mitsuhiro Yoshida <mits@mitstek.com>
2025-09-10 10:23:03 +0900

    Brushed up a translated string for Wall.

--- blocktype/wall/lang/ja.utf8/blocktype.wall.php
+++ blocktype/wall/lang/ja.utf8/blocktype.wall.php
@@ -1,32 +1,43 @@
 <?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2025-09-10 01:22:58 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
 
 defined('INTERNAL') || die();
 
-$string['Post'] = '投稿';
-$string['addpostsuccess'] = '投稿が正常に追加されました。';
+$string['title'] = 'ウォール';
+$string['otherusertitle'] = '%s のウォール';
+$string['description'] = '人があなたにコメントを残せるエリアを表示します。';
+$string['noposts'] = '表示できるウォール投稿はありません。';
+$string['makeyourpostprivate'] = 'あなたの投稿をプライベートにする';
+$string['wallpostprivate'] = 'このウォール投稿はプライベートです。';
+$string['viewwall'] = 'ウォールを表示する';
 $string['backtoprofile'] = 'プロファイルに戻る';
+$string['wall'] = 'ウォール';
+$string['wholewall'] = 'ウォール全体を表示する';
+$string['reply'] = '返信';
 $string['delete'] = '投稿を削除する';
 $string['deletepost'] = '投稿を削除する';
-$string['deletepostsuccess'] = '投稿が正常に削除されました。';
+$string['Post'] = '投稿';
 $string['deletepostsure'] = '本当にこの処理を実行してもよろしいですか? 元に戻せません。';
-$string['description'] = '人がコメントをあなたに残せるエリアを表示します。';
-$string['makeyourpostprivate'] = 'あなたの投稿をプライベートにする';
+$string['deletepostsuccess'] = '投稿が正常に削除されました。';
+$string['addpostsuccess'] = '投稿が正常に追加されました。';
 $string['maxcharacters'] = '1投稿あたり最大 %s 文字入力可能です。';
-$string['newwallpostnotificationmessage'] = '%s';
-$string['newwallpostnotificationsubject'] = 'あなたのウォールの新しい投稿';
-$string['noposts'] = '表示できるウォール投稿はありません。';
-$string['otherusertitle'] = '%s のウォール';
+$string['sorrymaxcharacters'] = '申し訳ございません、あなたは %s 文字以上投稿できません。';
+$string['posttextrequired'] = 'このフィールドは必須です。';
 $string['postsizelimit'] = '投稿サイズ制限';
 $string['postsizelimitdescription'] = 'ここであなたはウォールの投稿サイズを制限できます。既存の投稿は変更されません。';
-$string['postsizelimitinvalid'] = 'これは有効な数字ではありません。';
 $string['postsizelimitmaxcharacters'] = '最大文字数';
+$string['postsizelimitinvalid'] = 'これは有効な数字ではありません。';
 $string['postsizelimittoosmall'] = '制限はゼロより下にできません。';
-$string['posttextrequired'] = 'このフィールドは必須です。';
-$string['reply'] = '返信';
-$string['sorrymaxcharacters'] = '申し訳ございません、あなたは %s 文字以上投稿できません。';
-$string['title'] = 'ウォール';
+$string['newwallpostnotificationsubject'] = 'あなたのウォールの新しい投稿';
+$string['newwallpostnotificationmessage'] = '%s';
 $string['typewallpost'] = 'ウォール投稿';
-$string['viewwall'] = 'ウォールを表示する';
-$string['wall'] = 'ウォール';
-$string['wallpostprivate'] = 'このウォール投稿はプライベートです。';
-$string['wholewall'] = 'ウォール全体を表示する';
