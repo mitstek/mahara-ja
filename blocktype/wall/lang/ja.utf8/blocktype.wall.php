290b456a7c2031d216fd0e120061aeca7b3a8a06
Mitsuhiro Yoshida <mits@mitstek.com>
2025-09-10 10:30:22 +0900

    Brushed up a translated string for Wall.

--- blocktype/wall/lang/ja.utf8/blocktype.wall.php
+++ blocktype/wall/lang/ja.utf8/blocktype.wall.php
@@ -15,7 +15,7 @@ defined('INTERNAL') || die();
 
 $string['title'] = 'ウォール';
 $string['otherusertitle'] = '%s のウォール';
-$string['description'] = '人がコメントをあなたに残せるエリアを表示します。';
+$string['description'] = '人があなたにコメントを残せるエリアを表示します。';
 $string['noposts'] = '表示できるウォール投稿はありません。';
 $string['makeyourpostprivate'] = 'あなたの投稿をプライベートにする';
 $string['viewwall'] = 'ウォールを表示する';
