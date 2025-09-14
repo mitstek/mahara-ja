761d4644e3e6d44f597ea3ccef10bd3390b011a0
Mitsuhiro Yoshida <mits@mitstek.com>
2025-09-15 05:42:21 +0900

    Brushed up a translated string for Wall.

--- blocktype/wall/lang/ja.utf8/blocktype.wall.php
+++ blocktype/wall/lang/ja.utf8/blocktype.wall.php
@@ -51,6 +51,6 @@ $string['postsizelimit'] = '投稿サイズ制限';
 $string['postsizelimitdescription'] = 'ここであなたはウォールの投稿サイズを制限できます。既存の投稿は変更されません。';
 $string['postsizelimitmaxcharacters'] = '最大文字数';
 $string['postsizelimitinvalid'] = 'これは有効な数字ではありません。';
-$string['postsizelimittoosmall'] = '制限はゼロより下にできません。';
+$string['postsizelimittoosmall'] = '制限はゼロより小さくできません。';
 
 ?>
