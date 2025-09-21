c7f7a54fbcc6337e7c2f8a26f4b1209b1c8f2e82
Mitsuhiro Yoshida <mits@mitstek.com>
2025-09-22 06:24:29 +0900

    Brushed up a translated string for watchlist.

--- blocktype/watchlist/lang/ja.utf8/blocktype.watchlist.php
+++ blocktype/watchlist/lang/ja.utf8/blocktype.watchlist.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2025-09-17 00:34:09 UTC
+ * @updated    2025-09-21 21:24:21 UTC
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -32,7 +32,7 @@ $string['filterby.2months'] = '直近2ヶ月';
 $string['filterby.quarter'] = '直近3ヶ月';
 $string['filterby.half'] = '直近6ヶ月';
 $string['filterby.year'] = '昨年';
-$string['filterby.login'] = '前回のログインから';
+$string['filterby.login'] = '前回ログインから';
 $string['orderby'] = '並べ替え順';
 $string['orderbydesc'] = 'ページの表示を新しい順またはページオーナ順より選択してください。';
 $string['orderby.activity'] = '新しい順';
