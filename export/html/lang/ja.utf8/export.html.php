77123fa531c080a799d67a3a1a022d435d37537c
Mitsuhiro Yoshida <mits@mitstek.com>
2025-09-24 05:31:16 +0900

    Brushed up a translated string for exporting standalone HTML website.

--- export/html/lang/ja.utf8/export.html.php
+++ export/html/lang/ja.utf8/export.html.php
@@ -1,12 +1,23 @@
 <?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2025-09-23 20:31:09 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
 
 defined('INTERNAL') || die();
 
+$string['title'] = 'スタンドアロンHTMLウェブサイト';
+$string['description'] = 'あなたのポートフォリオデータを内蔵したウェブサイトを作成します。あなたはこのファイルを再度インポートできませんが、標準的なウェブブラウザで閲覧できます。';
+$string['usersportfolio'] = '%s - ポートフォリオ';
+$string['preparing'] = '%s 準備中';
+$string['exportingdatafor'] = '%s データエクスポート中';
 $string['buildingindexpage'] = 'インデックスページ構築中';
 $string['copyingextrafiles'] = '追加ファイルコピー中';
-$string['description'] = 'あなたのポートフォリオデータを内蔵したウェブサイトを作成します。あなたはこのファイルをインポートできませんが、標準的なウェブブラウザで閲覧できます。';
 $string['duplicatepagetitle'] = 'ページタイトル重複のためエクスポートに失敗しました。タイトルがユニークであることを確認してください。';
-$string['exportingdatafor'] = '%s データエクスポート中';
-$string['preparing'] = '%s 準備中';
-$string['title'] = 'スタンドアロンHTMLウェブサイト';
-$string['usersportfolio'] = '%s - ポートフォリオ';
