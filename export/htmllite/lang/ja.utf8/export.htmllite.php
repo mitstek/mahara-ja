6647252b56575786e0e9512c9209f67c7b811f57
Mitsuhiro Yoshida <mits@mitstek.com>
2025-09-29 09:03:23 +0900

    Brushed up a translated string for exporting portfolio HTML lite files.

--- export/htmllite/lang/ja.utf8/export.htmllite.php
+++ export/htmllite/lang/ja.utf8/export.htmllite.php
@@ -1,6 +1,17 @@
 <?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2025-09-29 00:03:07 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
 
 defined('INTERNAL') || die();
 
-$string['description'] = 'ここではあなたのポートフォリオのHTMLファイルを含む軽量ZIPファイルを作成します。あなたはこれを再度インポートできません。このエクスポートは盗作または類似性チェッカと互換性があるよう設計されています。';
 $string['title1'] = 'ポートフォリオのHTML liteファイル';
+$string['description'] = 'これによりあなたのポートフォリオのHTMLファイルおよび類似性チェッカに送信可能な関連ファイルを含む軽量な圧縮ファイルが作成されます。あなたはこれを再度インポートできません。';
