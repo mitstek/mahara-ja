34126b1a8e8ba5952b1be5aa38914237fc756ae6
Mitsuhiro Yoshida <mits@mitstek.com>
2025-10-01 05:56:37 +0900

    Brushed up a translated string for Leap2A.

--- export/leap/lang/ja.utf8/export.leap.php
+++ export/leap/lang/ja.utf8/export.leap.php
@@ -2,5 +2,5 @@
 
 defined('INTERNAL') || die();
 
-$string['description1'] = 'ここでは<a href="https://web.archive.org/web/20140912151415/http://wiki.leapspecs.org/2A/specification">Leap2A標準フォーマット</a>のエクスポートを作成します。エクスポートは人間にとって難読ですが、後でこのエクスポートを使用してあなたのデータを<a href="https://wiki.mahara.org/wiki/Developer_Area/Import/Export/Interoperability">他のLeap2A互換システム</a>にインポートできます。';
+$string['description1'] = 'ここでは<a href="https://web.archive.org/web/20140912151415/http://wiki.leapspecs.org/2A/specification">Leap2A標準フォーマット</a>のエクスポートを作成します。エクスポートは人間にとって難読ですが、後でこのエクスポートを使用してあなたのデータを<a href="https://wiki.mahara.org/wiki/Developer_Area/Import/Export/Interoperability">他のLeap2A準拠システム</a>にインポートできます。';
 $string['title'] = 'Leap2A';
