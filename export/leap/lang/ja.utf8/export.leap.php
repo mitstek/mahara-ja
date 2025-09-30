d44eda30bab5fee95d265818e3ba9a16f9d2a730
Mitsuhiro Yoshida <mits@mitstek.com>
2025-10-01 05:58:24 +0900

    Brushed up a translated string for Leap2A.

--- export/leap/lang/ja.utf8/export.leap.php
+++ export/leap/lang/ja.utf8/export.leap.php
@@ -16,11 +16,11 @@
 
 defined('INTERNAL') || die();
 
-$string['description1'] = 'ここでは<a href="https://web.archive.org/web/20140912151415/http://wiki.leapspecs.org/2A/specification">Leap2A標準フォーマット</a>のエクスポートを作成します。エクスポートは人間にとって難読ですが、後でこのエクスポートを使用してあなたのデータを<a href="https://wiki.mahara.org/wiki/Developer_Area/Import/Export/Interoperability">他のLeap2A互換システム</a>にインポートできます。';
+$string['description1'] = 'ここでは<a href="https://web.archive.org/web/20140912151415/http://wiki.leapspecs.org/2A/specification">Leap2A標準フォーマット</a>のエクスポートを作成します。エクスポートは人間にとって難読ですが、後でこのエクスポートを使用してあなたのデータを<a href="https://wiki.mahara.org/wiki/Developer_Area/Import/Export/Interoperability">他のLeap2A準拠システム</a>にインポートできます。';
 $string['title'] = 'Leap2A';
 <<<<<<< HEAD
 =======
-$string['description1'] = 'ここでは<a href="https://web.archive.org/web/20140912151415/https://wiki.leapspecs.org/2A/specification">Leap2A標準フォーマット</a>のエクスポートを作成します。エクスポートは人間にとって難読ですが、後でこのエクスポートを使用してあなたのデータを後で<a href="https://wiki.mahara.org/wiki/Developer_Area/Import/Export/Interoperability">他のLeap2A互換システム</a>にインポートできます。';
+$string['description1'] = 'ここでは<a href="https://web.archive.org/web/20140912151415/https://wiki.leapspecs.org/2A/specification">Leap2A標準フォーマット</a>のエクスポートを作成します。エクスポートは人間にとって難読ですが、後でこのエクスポートを使用してあなたのデータを後で<a href="https://wiki.mahara.org/wiki/Developer_Area/Import/Export/Interoperability">他のLeap2A準拠システム</a>にインポートできます。';
 
 ?>
 >>>>>>> 92099ba5d5a5390b497675e7809f65e21f9a5091
