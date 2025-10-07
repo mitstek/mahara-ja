7b0f68ba0972ab43ef59fd676ee460dcdd5d483d
Mitsuhiro Yoshida <mits@mitstek.com>
2025-10-08 07:03:05 +0900

    Brushed up translated strings for exporting PDF file.

--- export/pdf/lang/ja.utf8/export.pdf.php
+++ export/pdf/lang/ja.utf8/export.pdf.php
@@ -4,13 +4,13 @@ defined('INTERNAL') || die();
 
 $string['beginpdfviewexport'] = 'PDFエクスポート処理を開始する';
 $string['description'] = 'これによりあなたのポートフォリオのPDFファイルを含むZIPファイルが作成されます。あなたはこれを再度インポートできませんが、PDFは標準的なPDFビューアで閲覧できます。';
-$string['exportpdfdisabled'] = 'PDFエクスポート依存関係が不足しているためPDFエクスーポートは無効にされています。詳細は<a href="%s">「プラグイン管理」</a>をご覧ください。';
+$string['exportpdfdisabled'] = 'PDFエクスポート依存関係が不足してます。PDFエクスーポートは無効にされています。詳細は<a href="%s">「プラグイン管理」</a>をご覧ください。';
 $string['needschromeheadless'] = 'PDF出力にHeadless Chromeを利用する実験的エクスポートオプションです。このプラグインを使用するにはサーバに最新版のChromeまたはChromiumブラウザをインストールしてください。';
 $string['needschromeheadlessphp'] = '「chrome-php」が必要です。あなたはこれを「make pdfexport」でインストールできます。';
 $string['needspdfcombiner'] = 'PDFを結合できるようにするには「pdfunite」または「ghostscript」が必要です。あなたは「apt-get install poppler-utils」で「pdfunite」をインストールできます。';
 $string['needspdfconfig'] = 'config.php設定「usepdfexport」を「true」にする必要があります。';
 $string['pdfchromestarted'] = 'Chromeブラウザヘッドレス起動中 ... 完了';
-$string['pdfchromestartederror'] = 'Chromeブラウザを開始できません。詳細情報はログをご覧ください。';
+$string['pdfchromestartederror'] = 'Chromeブラウザを開始できません。詳細情報はエラーログをご覧ください';
 $string['pdffoundcombiner'] = 'PDFコンバイナを使用する: %s';
 $string['startuppdfchrome'] = 'Chromeブラウザヘッドレス起動中 ...';
 $string['title1'] = 'ポートフォリオのPDFファイル';
