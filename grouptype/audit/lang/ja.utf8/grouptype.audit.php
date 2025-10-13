c74be1ba182cf14bb84c03d476471695b370a75a
Mitsuhiro Yoshida <mits@mitstek.com>
2025-10-13 12:41:07 +0900

    Brushed up a translated string for Portfolio audit.

--- grouptype/audit/lang/ja.utf8/grouptype.audit.php
+++ grouptype/audit/lang/ja.utf8/grouptype.audit.php
@@ -1,10 +1,21 @@
 <?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2025-10-13 03:41:00 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
 
 defined('INTERNAL') || die();
 
-$string['admin'] = '管理者';
-$string['member'] = 'メンバ';
 $string['name'] = '監査';
-$string['portfoliorandomtitle'] = '監査対象の %s によるポートフォリオ';
+$string['admin'] = '管理者';
 $string['tutor'] = 'チュータ';
+$string['member'] = 'メンバ';
 $string['youaregrouptutor'] = 'あなたはこのグループのチュータです。';
+$string['portfoliorandomtitle'] = '%s による監査対象ポートフォリオ';
