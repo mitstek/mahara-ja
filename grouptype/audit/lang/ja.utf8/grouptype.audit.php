1eb7ef17881581c4d08b9226768f460b2db8ebaf
Mitsuhiro Yoshida <mits@mitstek.com>
2025-10-13 12:49:33 +0900

    Brushed up a translated string for Portfolio audit.

--- grouptype/audit/lang/ja.utf8/grouptype.audit.php
+++ grouptype/audit/lang/ja.utf8/grouptype.audit.php
@@ -5,6 +5,6 @@ defined('INTERNAL') || die();
 $string['admin'] = '管理者';
 $string['member'] = 'メンバ';
 $string['name'] = '監査';
-$string['portfoliorandomtitle'] = '監査対象の %s によるポートフォリオ';
+$string['portfoliorandomtitle'] = '%s による監査対象ポートフォリオ';
 $string['tutor'] = 'チュータ';
 $string['youaregrouptutor'] = 'あなたはこのグループのチュータです。';
