7fbf2f5c3ff37909d047e5dce49c962bdaaf68ec
Mitsuhiro Yoshida <mits@mitstek.com>
2025-10-29 05:54:43 +0900

    Refined the translation of ‘by’.

--- interaction/forum/lang/ja.utf8/interaction.forum.php
+++ interaction/forum/lang/ja.utf8/interaction.forum.php
@@ -145,12 +145,12 @@ $string['notifyadministrator'] = '管理者に通知する';
 $string['notifyauthor'] = '投稿者に通知する';
 $string['rejectpost'] = '投稿拒否';
 $string['reasonempty'] = '理由フィールドは空白にできません。';
-$string['objectionablepostdeletedsubject'] = 'フォーラムトピック「 %s 」の好ましくない投稿は %s によって削除されました。';
+$string['objectionablepostdeletedsubject'] = 'フォーラムトピック「 %s 」の好ましくない投稿は %s により削除されました。';
 $string['objectionablepostdeletedbody'] = '%s は前に好ましくないコンテンツであると報告された %s による投稿を調査して削除しました。
 
 好ましくない投稿コンテンツは次のとおりです:
 %s';
-$string['objectionabletopicdeletedsubject'] = '好ましくないフォーラムトピック「 %s 」は %s によって削除されました。';
+$string['objectionabletopicdeletedsubject'] = '好ましくないフォーラムトピック「 %s 」は %s により削除されました。';
 $string['objectionabletopicdeletedbody'] = '%s は前に好ましくないコンテンツであると報告された %s によるトピックを調査して削除しました。
 
 好ましくないトピックコンテンツは次のとおりです:
@@ -171,7 +171,7 @@ $string['Poster'] = '投稿者';
 $string['postobjectionable'] = 'この投稿はあなたにより好ましくないコンテンツを含んでいると報告されました。';
 $string['postnotobjectionable'] = 'この投稿は好ましくないコンテンツを含んでいると報告されました。事実とは異なる場合、あなたはボタンをクリックして通知を削除した後、他の管理者に知らせることができます。';
 $string['postnotobjectionablebody'] = '%s は前に好ましくないコンテンツであると報告された %s による投稿を調査してもはや好ましくないコンテンツは含まれていないとマークしました。';
-$string['postnotobjectionablesubject'] = 'フォーラムトピック「 %s 」の投稿は %s によって好ましくない投稿ではないとマークされました。';
+$string['postnotobjectionablesubject'] = 'フォーラムトピック「 %s 」の投稿は %s により好ましくない投稿ではないとマークされました。';
 $string['postnotobjectionablesuccess'] = '投稿が好ましくない投稿ではないとマークされました。';
 $string['postnotobjectionablesubmit'] = '好ましくない投稿ではない';
 $string['postreply'] = '返信';
@@ -328,7 +328,7 @@ $string['rejectedposthtml'] = '<div style="padding: 0.5em 0; border-bottom: 1px 
 <p>拒否関連: <a href="%s">%s</a></p>
 <p>拒否者: <a href="%s">%s</a></p>
 </div>';
-$string['rejectedposttext'] = 'フォーラム「 %s 」のフォーラム投稿が「 %s 」によって拒否されました。
+$string['rejectedposttext'] = 'フォーラム「 %s 」のフォーラム投稿が「 %s 」により拒否されました。
 %s
 ------------------------------------------------------------------------
 
