6a30738062d6215ffeb6a0657253240ba057c51e
Mitsuhiro Yoshida <mits@mitstek.com>
2025-12-08 00:02:50 +0900

    Brushed up a translated string for Account.

--- lang/ja.utf8/account.php
+++ lang/ja.utf8/account.php
@@ -1,77 +1,71 @@
 <?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2025-12-07 15:02:38 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
 
 defined('INTERNAL') || die();
 
-$string['accessiblepagecreation'] = 'アクセシブルなページ作成';
-$string['accessiblepagecreationdescription'] = 'このオプションによりあなたはスクリーンリーダまたはキーボードのみでページを作成および編集できるようになります。';
-$string['accountdeleted'] = 'あなたのアカウントが削除されました。';
-$string['accountoptionsdesc'] = '一般アカウントオプション';
-$string['canceldeletionadminemailhtml'] = '<p>管理者さん、こんにちは。</p>
-<p>%s がサイトからのアカウント削除リクエストをキャンセルしました。</p>
-<p>あなたはこの人が属しているインスティテューションの管理者として記載されています。</p>
-<p>キャンセルされたリクエストの詳細は次のとおりです:</p>
-<p>氏名: %s</p>
-<p>メールアドレス: %s</p>
-<pre>--
-%s チーム</pre>';
-$string['canceldeletionadminemailsubject'] = '%s のアカウント削除リクエストのキャンセル';
-$string['canceldeletionadminemailtext'] = '管理者さん、こんにちは。
-
-%s がサイトからのアカウント削除リクエストをキャンセルしました。
-
-あなたはこの人が属しているインスティテューションの管理者として記載されています。
-
-キャンセルされたリクエストの詳細は次のとおりです:
-
-氏名: %s
-メールアドレス: %s
-
---
-%s チーム';
-$string['canceldeletionrequest'] = '削除リクエストをキャンセルする';
-$string['canceldeletionrequestconfirmation'] = 'これはインスティテューション管理者へのアカウント %s の削除リクエストをキャンセルします。本当に続けてもよろしいですか?';
-$string['cancelrequest'] = 'リクエストをキャンセルする';
 $string['changepassworddesc'] = '新しいパスワード';
 $string['changepasswordotherinterface'] = 'あなたは異なるインタフェースで<a href="%s">パスワードを変更できます</a>。';
-$string['changeprofileurl'] = 'プロファイルURLを変更する';
-$string['changeusername'] = '新しいユーザ名';
-$string['changeusernamedesc'] = 'あなたが %s へのログインに使用するユーザ名です。ユーザ名の長さは半角3-30文字にしてください。ユーザ名には空白を除く半角英数字および標準的な記号を使用できます。';
+$string['oldpasswordincorrect'] = 'これはあなたの現在のパスワードではありません。';
 $string['changeusernameheading'] = 'ユーザ名を変更する';
-$string['deleteaccountdescription'] = 'あなたのアカウントを削除した場合、あなたのすべてのコンテンツは完全に削除されます。あなたは元に戻せません。あなたのプロファイル情報およびポートフォリオは他の人が閲覧できないようになります。あなたが投稿したフォーラム投稿のコンテンツは閲覧できますが投稿者名が表示されないようになります。';
-$string['deleteaccountuser'] = '%s のアカウントを削除する';
-$string['deleterequestcanceled'] = 'あなたのアカウントの削除リクエストはキャンセルされました。';
-$string['disableemail'] = 'メールを無効にする';
-$string['enablemultipleblogs1'] = '複数日誌';
-$string['enablemultipleblogsdescription1'] = 'デフォルトではあなたは1つの日誌のみ作成できます。2つ以上の日誌を作成したい場合、このオプションを有効にしてください。';
+$string['changeusername'] = '新しいユーザ名';
+$string['changeusernamedesc'] = 'あなたが %s へのログインに使用するユーザ名です。ユーザ名の長さは半角3-30文字にしてください。ユーザ名には空白を除く半角英数字および一般的な記号を使用できます。';
+$string['usernameexists1'] = 'あなたはこのユーザ名を使用できません。別のユーザ名を選択してください。';
+$string['accountoptionsdesc'] = '一般アカウントオプション';
+$string['changeprofileurl'] = 'プロファイルURLを変更する';
+$string['profileurl'] = 'プロファイルURL';
+$string['profileurldescription'] = 'あなたのプロファイルページの独自URLです。このフィールドの長さは半角3-30文字にしてください。';
+$string['urlalreadytaken'] = 'このプロファイルURLはすでに取得されています。別のURLを選択してください。';
+$string['friendsnobody'] = '誰も私をフレンドとして追加できません。';
 $string['friendsauth'] = '新しいフレンドになるには私の承認が必要です。';
 $string['friendsauto'] = '新しいフレンドは自動的に承認されます。';
 $string['friendsdescr'] = 'フレンドコントロール';
-$string['friendsnobody'] = '誰も私をフレンドとして追加できません。';
-$string['hiderealname'] = '実名を秘匿する';
-$string['hiderealnamedescription'] = 'あなたが表示名を設定した場合、そしてあなたの実名検索により他の人から自分を探されたくない場合、このチェックボックスをチェックしてください。';
-$string['language'] = '言語';
+$string['updatedfriendcontrolsetting'] = 'フレンドコントロールが更新されました。';
+$string['wysiwygdescr'] = 'HTMLエディタ';
 $string['licensedefault'] = 'デフォルトライセンス';
 $string['licensedefaultdescription'] = 'あなたのコンテンツのデフォルトライセンスです。';
 $string['licensedefaultinherit'] = 'インスティテューションデフォルトを使用する';
+$string['messagesdescr'] = '他の人からのメッセージ';
+$string['messagesnobody'] = '誰も私にメッセージを送信できません。';
+$string['messagesfriends'] = 'マイフレンドリストに登録した人は私にメッセージを送信できます。';
+$string['messagesallow'] = '誰でも私にメッセージを送信できます。';
+$string['language'] = '言語';
+$string['tagssideblockmaxtags'] = 'クラウド内の最大タグ数';
+$string['tagssideblockmaxtagsdescription'] = 'あなたのタグクラウドに表示されるタグの最大数です。';
+$string['enablemultipleblogs1'] = '複数日誌';
+$string['enablemultipleblogsdescription1'] = 'デフォルトではあなたは1つの日誌のみ作成できます。2つ以上の日誌を作成したい場合、このオプションを有効にしてください。';
+$string['hiderealname'] = '実名を秘匿する';
+$string['hiderealnamedescription'] = 'あなたが表示名を設定した場合、そしてあなたの実名検索により他の人から自分を探されたくない場合、このチェックボックスをチェックしてください。';
+$string['showhomeinfo2'] = 'ダッシュボード情報';
+$string['showhomeinfodescription1'] = 'ダッシュボード上に %s の使用方法に関する情報を表示します。';
+$string['showlayouttranslatewarning'] = 'ページレイアウト変更前に確認する';
+$string['showlayouttranslatewarningdescription'] = 'ページ編集時、ページレイアウトの新しいレイアウトへの変更前に警告および確認リクエストを表示します。';
+$string['accessiblepagecreation'] = 'アクセシブルなページ作成';
+$string['accessiblepagecreationdescription'] = 'このオプションによりあなたはスクリーンリーダまたはキーボードのみでページを作成および編集できるようになります。';
+$string['showprogressbar'] = 'プロファイル完了プログレスバー';
+$string['showprogressbardescription'] = 'プログレスバーおよびあなたの %s プロファイルを完了するためのヒントを表示します。';
+$string['prefssaved'] = 'プレファレンスが保存されました。';
+$string['prefsnotsaved'] = 'あなたのプレファレンスの保存に失敗しました。';
 $string['maildisabled'] = 'メールが無効にされました。';
+$string['disableemail'] = 'メールを無効にする';
 $string['maildisabledbounce'] = '多くのメッセージがサーバに戻ってきたためあなたのメールアドレス宛のメール送信は停止されました。メールを有効にする前にあなたのメールアカウントが正常に動作しているか %s のアカウントプレファレンスで確認してください。';
 $string['maildisableddescription'] = 'あなたのアカウント宛のメール送信が停止されました。アカウントプレファレンスページで<a href="%s">あなたのメールを再度有効</a>にできます。';
-$string['messagesallow'] = '誰でも私にメッセージを送信できます。';
-$string['messagesdescr'] = '他の人からのメッセージ';
-$string['messagesfriends'] = 'マイフレンドリストに登録した人は私にメッセージを送信できます。';
-$string['messagesnobody'] = '誰も私にメッセージを送信できません。';
-$string['noprivacystatementsaccepted'] = 'このアカウントは現在のプライバシ保護方針を受け入れていません。';
-$string['oldpasswordincorrect'] = 'これはあなたの現在のパスワードではありません。';
-$string['pendingdeletionadminemailhtml'] = '<p>管理者さん、こんにちは。</p>
-<p>%s がサイトからの自分のアカウント削除をリクエストしました。</p>
-<p>あなたはこの人が属しているインスティテューションの管理者として記載されています。あなたは削除リクエストの承認または拒否を決定できます。これを実行するには次のリンクにアクセスしてください: <a href=\'%s\'>%s</a></p>
-<p>アカウント削除リクエストの詳細は次のとおりです:</p>
-<p>氏名: %s</p>
-<p>メールアドレス: %s</p>
-<p>理由: %s</p>
-<pre>--
-%s チーム</pre>';
+$string['deleteaccountuser'] = '%s のアカウントを削除する';
+$string['deleteaccountdescription'] = 'あなたのアカウントを削除した場合、あなたのすべてのコンテンツは完全に削除されます。あなたは元に戻せません。あなたのプロファイル情報およびポートフォリオは他の人が閲覧できないようになります。あなたが投稿したフォーラム投稿のコンテンツは閲覧できますが投稿者名が表示されないようになります。';
+$string['sendnotificationdescription1'] = 'あなたのアカウント削除を承認するよう管理者に通知が送信されます。あなたがアカウントの削除をリクエストした場合、あなたの個人コンテンツすべてが完全に削除されます。これはあなたがアップロードしたファイル、執筆した日誌エントリ、作成したポートフォリオが削除されることを意味します。あなたはそれらを元に戻せません。あなたがグループでファイルをアップロードしている場合、日誌エントリおよびポートフォリオを作成している場合、そこでフォーラムに投稿している場合、それらはそのままになりますがあなたの氏名は表示されないようになります。';
+$string['pendingdeletionsince'] = '%s 以来のアカウント削除保留';
 $string['pendingdeletionadminemailsubject'] = '%s のアカウント削除リクエスト';
+$string['resenddeletionadminemailsubject'] = '%s のアカウント削除リクエストのリマインダ';
+$string['canceldeletionadminemailsubject'] = '%s のアカウント削除リクエストのキャンセル';
 $string['pendingdeletionadminemailtext'] = '管理者さん、こんにちは。
 
 %s がサイトからの自分のアカウント削除をリクエストしました。
@@ -86,22 +80,17 @@ $string['pendingdeletionadminemailtext'] = '管理者さん、こんにちは。
 
 --
 %s チーム';
-$string['pendingdeletionemailsent'] = 'インスティテューション管理者に通知を送信しました。';
-$string['pendingdeletionsince'] = '%s 以来のアカウント削除保留';
-$string['prefsnotsaved'] = 'あなたのプレファレンスの保存に失敗しました。';
-$string['prefssaved'] = 'プレファレンスが保存されました。';
-$string['profileurl'] = 'プロファイルURL';
-$string['profileurldescription'] = 'あなたのプロファイルページの独自URLです。このフィールドの長さは半角3-30文字にしてください。';
-$string['resenddeletionadminemailhtml'] = '<p>管理者さん、こんにちは。</p>
-<p>これは %s によるサイトからのアカウント削除リクエストに関するリマインダです。</p>
+$string['pendingdeletionadminemailhtml'] = '<p>管理者さん、こんにちは。</p>
+<p>%s がサイトからの自分のアカウント削除をリクエストしました。</p>
 <p>あなたはこの人が属しているインスティテューションの管理者として記載されています。あなたは削除リクエストの承認または拒否を決定できます。これを実行するには次のリンクにアクセスしてください: <a href=\'%s\'>%s</a></p>
 <p>アカウント削除リクエストの詳細は次のとおりです:</p>
 <p>氏名: %s</p>
 <p>メールアドレス: %s</p>
-<p>メッセージ: %s</p>
+<p>理由: %s</p>
 <pre>--
 %s チーム</pre>';
-$string['resenddeletionadminemailsubject'] = '%s のアカウント削除リクエストのリマインダ';
+$string['accountdeleted'] = 'あなたのアカウントが削除されました。';
+$string['resenddeletionnotification'] = '削除通知を再送する';
 $string['resenddeletionadminemailtext'] = '管理者さん、こんにちは。
 
 これは %s によるサイトからのアカウント削除リクエストに関するリマインダです。
@@ -116,19 +105,41 @@ $string['resenddeletionadminemailtext'] = '管理者さん、こんにちは。
 
 --
 %s チーム';
-$string['resenddeletionnotification'] = '削除通知を再送する';
+$string['resenddeletionadminemailhtml'] = '<p>管理者さん、こんにちは。</p>
+<p>これは %s によるサイトからのアカウント削除リクエストに関するリマインダです。</p>
+<p>あなたはこの人が属しているインスティテューションの管理者として記載されています。あなたは削除リクエストの承認または拒否を決定できます。これを実行するには次のリンクにアクセスしてください: <a href=\'%s\'>%s</a></p>
+<p>アカウント削除リクエストの詳細は次のとおりです:</p>
+<p>氏名: %s</p>
+<p>メールアドレス: %s</p>
+<p>メッセージ: %s</p>
+<pre>--
+%s チーム</pre>';
+$string['pendingdeletionemailsent'] = 'インスティテューション管理者に通知を送信しました。';
+$string['cancelrequest'] = 'リクエストをキャンセルする';
+$string['deleterequestcanceled'] = 'あなたのアカウントの削除リクエストはキャンセルされました。';
+$string['canceldeletionrequest'] = '削除リクエストをキャンセルする';
+$string['canceldeletionrequestconfirmation'] = 'これはインスティテューション管理者へのアカウント %s の削除リクエストをキャンセルします。本当に続けてもよろしいですか?';
+$string['canceldeletionadminemailtext'] = '管理者さん、こんにちは。
+
+%s がサイトからのアカウント削除リクエストをキャンセルしました。
+
+あなたはこの人が属しているインスティテューションの管理者として記載されています。
+
+キャンセルされたリクエストの詳細は次のとおりです:
+
+氏名: %s
+メールアドレス: %s
+
+--
+%s チーム';
+$string['canceldeletionadminemailhtml'] = '<p>管理者さん、こんにちは。</p>
+<p>%s がサイトからのアカウント削除リクエストをキャンセルしました。</p>
+<p>あなたはこの人が属しているインスティテューションの管理者として記載されています。</p>
+<p>キャンセルされたリクエストの詳細は次のとおりです:</p>
+<p>氏名: %s</p>
+<p>メールアドレス: %s</p>
+<pre>--
+%s チーム</pre>';
 $string['resizeonuploaduserdefault1'] = 'アップロード時にイメージをリサイズする';
 $string['resizeonuploaduserdefaultdescription2'] = 'デフォルトで「イメージを自動的にリサイズする」オプションが有効にされます。最大高および幅を超えたイメージはアップロード時にリサイズされます。個別のイメージアップロード時、あなたはこのデフォルト設定を無効にできます。';
-$string['sendnotificationdescription1'] = 'あなたのアカウント削除を承認するよう管理者に通知が送信されます。あなたがアカウントの削除をリクエストした場合、あなたの個人コンテンツすべてが完全に削除されます。これはあなたがアップロードしたファイル、執筆した日誌エントリ、作成したポートフォリオが削除されることを意味します。あなたはそれらを元に戻せません。あなたがグループでファイルをアップロードしている場合、日誌エントリおよびポートフォリオを作成している場合、そこでフォーラムに投稿している場合、それらはそのままになりますがあなたの氏名は表示されないようになります。';
-$string['showhomeinfo2'] = 'ダッシュボード情報';
-$string['showhomeinfodescription1'] = 'ダッシュボード上に %s の使用方法に関する情報を表示します。';
-$string['showlayouttranslatewarning'] = 'ページレイアウト変更前に確認する';
-$string['showlayouttranslatewarningdescription'] = 'ページ編集時、ページレイアウトの新しいレイアウトへの変更前に警告および確認リクエストを表示します。';
-$string['showprogressbar'] = 'プロファイル完了プログレスバー';
-$string['showprogressbardescription'] = 'プログレスバーおよびあなたの %s プロファイルを完了するためのヒントを表示します。';
-$string['tagssideblockmaxtags'] = 'クラウド内の最大タグ数';
-$string['tagssideblockmaxtagsdescription'] = 'あなたのタグクラウドに表示されるタグの最大数です。';
-$string['updatedfriendcontrolsetting'] = 'フレンドコントロールが更新されました。';
-$string['urlalreadytaken'] = 'このプロファイルURLはすでに取得されています。別のURLを選択してください。';
-$string['usernameexists1'] = 'あなたはこのユーザ名を使用できません。別のユーザ名を選択してください。';
-$string['wysiwygdescr'] = 'HTMLエディタ';
+$string['noprivacystatementsaccepted'] = 'このアカウントは現在のプライバシ保護方針を受け入れていません。';
