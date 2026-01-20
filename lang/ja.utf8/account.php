704358d9eb66301d3711a7e065bec43d30366a3b
Mitsuhiro Yoshida <mits@mitstek.com>
2026-01-21 00:09:27 +0900

    Brushed up a translated string for Account.

--- lang/ja.utf8/account.php
+++ lang/ja.utf8/account.php
@@ -142,4 +142,4 @@ $string['canceldeletionadminemailhtml'] = '<p>管理者さん、こんにちは�
 %s チーム</pre>';
 $string['resizeonuploaduserdefault1'] = 'アップロード時にイメージをリサイズする';
 $string['resizeonuploaduserdefaultdescription2'] = 'デフォルトで「イメージを自動的にリサイズする」オプションが有効にされます。最大高および幅を超えたイメージはアップロード時にリサイズされます。個別のイメージアップロード時、あなたはこのデフォルト設定を無効にできます。';
-$string['noprivacystatementsaccepted'] = 'このアカウントは現在のプライバシ保護方針を受け入れていません。';
+$string['noprivacystatementsaccepted'] = 'このアカウントは現在のプライバシ保護方針を一切受け入れていません。';
