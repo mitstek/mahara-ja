a9dc7c8db111fdffa8471187f4c68fdeefec589f
Mitsuhiro Yoshida <mits@mitstek.com>
2026-05-19 12:39:07 +0900

    Brushed up a translated string for admin.

--- artefact/file/blocktype/image/lang/ja.utf8/blocktype.image.php
+++ artefact/file/blocktype/image/lang/ja.utf8/blocktype.image.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2025-04-08 08:45:40 UTC
+ * @updated    2026-05-19 03:37:33 UTC
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -15,28 +15,21 @@ defined('INTERNAL') || die();
 
 $string['title'] = 'イメージ';
 $string['description'] = 'あなたのファイルエリアの単一イメージです。';
-$string['showdescription1'] = 'キャプションを表示する';
-$string['showdescriptiondescription'] = 'イメージの下にキャプションを表示します。';
-$string['width'] = '幅';
-$string['widthdescription1'] = 'あなたのイメージの幅 (ピクセル) を指定してください。イメージはこの幅にサイズ変更されます。イメージのオリジナルサイズを使用する場合、空白のままにしてください。オリジナルサイズが大きすぎる場合、ブロック幅に縮小されます。';
+$string['title_help'] = '<h1>セクションタイトル</h1><p>あなたはイメージセクションにタイトルを付ける必要はありません。これは装飾イメージを追加したい場合に有用です。</p>';
+$string['showdescription'] = 'キャプションを表示する';
+$string['instconf.showdescription_help'] = '<h1>キャプションを表示する</h1><p>イメージの下にキャプションを表示します。あなたはイメージプロパティを編集してどのイメージにもキャプションを追加できます</p>';
 $string['select'] = 'イメージ選択';
 $string['selectfolder1'] = '後でアップロードされたイメージを含むイメージすべてをフォルダから表示する';
 $string['selectfolderdesc'] = 'このオプションは1つのフォルダのみ選択されている場合に有効になります。';
 $string['selectimages2'] = 'イメージ';
-$string['selectexternal'] = '外部ギャラリーからのイメージを表示する';
-$string['externalgalleryurl'] = 'ギャラリーURLまたはRSS';
-$string['externalgalleryurldesc'] = 'あなたは以下の外部ギャラリーを埋め込めます:';
-$string['widthdescription'] = 'あなたのイメージの幅 (ピクセル) を指定してください。イメージはこの幅にサイズ変更されます。';
 $string['multi_image'] = '複数イメージ表示';
-$string['multi_image_info'] = '注意: 表示するイメージが1つだけの場合、これらのオプションは無視されます。';
+$string['style_help'] = '<h1>複数イメージ表示</h1><p>表示するイメージが1つしかない場合、これらのオプションは無視されます。</p>';
 $string['stylethumbs'] = 'サムネイル';
 $string['stylesquares'] = 'サムネイル (正方形)';
 $string['styleslideshow'] = 'スライドショー';
-$string['showdescriptions1'] = 'キャプションを表示する';
-$string['showdescriptionsdescription'] = 'それぞれのイメージにキャプションを付けるには説明を選択してください。';
 $string['cannotdisplayslideshow'] = 'スライドショーを表示できません。';
 $string['gallerysettings'] = 'ギャラリー設定';
-$string['usefancybox1'] = 'Bootstrapモーダルを使用する';
-$string['usefancyboxdesc1'] = 'あなたのギャラリーでBootstrapモーダルを使用します。あなたのギャラリーのイメージをクリックした場合、オーバーレイで開きます。';
-$string['previewwidth'] = '最大写真幅';
-$string['previewwidthdesc1'] = 'Bootstrapモーダルでの写真表示時の最大リサイズ幅を設定します。';
+$string['usefancybox1'] = 'Bootstrapモーダル';
+$string['pluginconfig.usefancybox_help'] = '<h1>Bootstrapモーダル</h1><p>この設定を有効にした場合、あなたのギャラリー内イメージ選択時にBootstrapモーダルオーバーレイでイメージが開きます。</p>';
+$string['previewwidth'] = '最大幅';
+$string['pluginconfig.previewwidth_help'] = '<h1>最大幅</h1><p>Bootstrapのモーダルで表示される際にイメージがリサイズされる最大幅です。</p>';
--- artefact/file/blocktype/pdf/lang/ja.utf8/blocktype.pdf.php
+++ artefact/file/blocktype/pdf/lang/ja.utf8/blocktype.pdf.php
@@ -1,7 +1,20 @@
 <?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2026-05-19 03:18:25 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
 
 defined('INTERNAL') || die();
 
+$string['title'] = 'PDF';
 $string['description'] = 'あなたのファイルエリアの単一PDFファイルです。';
 $string['pdfwarning'] = '注意: 大きなPDFファイルを開いた場合、ブラウザのリソースに高負荷がかかる場合があります。';
-$string['title'] = 'PDF';
+$string['iframe_title'] = 'PDFファイルを表示するiFrame: %s';
+$string['iframe_title_none'] = 'このiFrameに表示するPDFを選択してください。';
