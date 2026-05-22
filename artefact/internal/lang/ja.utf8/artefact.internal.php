c50db6b1d03aa038df73976427226db207d693b4
Mitsuhiro Yoshida <mits@mitstek.com>
2026-05-22 13:45:33 +0900

    Translated new strings.

--- artefact/internal/lang/ja.utf8/artefact.internal.php
+++ artefact/internal/lang/ja.utf8/artefact.internal.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2025-06-03 22:24:29 UTC
+ * @updated    2026-05-22 04:44:34 UTC
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -15,13 +15,9 @@ defined('INTERNAL') || die();
 
 $string['pluginname'] = 'プロファイル';
 $string['profile1'] = '個人情報';
-$string['mandatoryfields'] = '必須フィールド';
-$string['mandatoryfieldsdescription'] = '入力する必要のあるプロファイルフィールドです。';
 $string['searchablefields'] = '検索可能フィールド';
 $string['searchablefieldsdescription'] = '他の人が検索できるプロファイルフィールドです';
 $string['adminusersearchfields'] = '管理「人検索」';
-$string['adminusersearchfieldsdescription'] = '管理「人検索」にカラムとして表示されるプロファイルフィールドです。';
-$string['aboutdescription'] = 'あなたの実際の姓名をここに入力してください。システム内の人に異なる名前を表示したい場合、あなたの表示名としてその名前を入力してください。';
 $string['infoisprivate'] = 'あなたが他の人と共有しているページに置かない限り、この情報は非公開となります。';
 $string['viewmyprofile'] = '私のプロファイルを表示する';
 $string['aboutprofilelinkdescription'] = '<p>他の人に表示したい情報を編集するにはあなたの<a href="%s">プロファイル</a>ページに移動してください。</p>';
@@ -49,26 +45,6 @@ $string['homenumber'] = '自宅電話';
 $string['businessnumber'] = '勤務先電話';
 $string['mobilenumber'] = '携帯電話';
 $string['faxnumber'] = 'FAX番号';
-$string['aim.input'] = 'AIMスクリーン名';
-$string['aim'] = 'AIM';
-$string['icq.input'] = 'ICQナンバ';
-$string['icq'] = 'ICQ';
-$string['jabber.input'] = 'Jabberユーザ名';
-$string['jabber'] = 'Jabber';
-$string['skype.input'] = 'Skypeユーザ名';
-$string['skype'] = 'Skype';
-$string['yahoo.input'] = 'Yahoo!メッセンジャ';
-$string['yahoo'] = 'Yahoo!メッセンジャ';
-$string['facebook.input'] = 'Facebook URL';
-$string['facebook'] = 'Facebook';
-$string['twitter.input'] = 'Twitterユーザ名';
-$string['twitter'] = 'Twitter';
-$string['instagram.input'] = 'Instagramユーザ名';
-$string['instagram'] = 'Instagram';
-$string['tumblr.input'] = 'Tumblr URL';
-$string['tumblr'] = 'Tumblr';
-$string['pinterest.input'] = 'Pinterestユーザ名';
-$string['pinterest'] = 'Pinterest';
 $string['occupation'] = '職業';
 $string['industry'] = '職種';
 $string['userroles'] = 'アカウントロール';
@@ -122,6 +98,8 @@ $string['cancelbutton'] = 'キャンセル';
 $string['emailingfailed'] = 'プロファイルが保存されましたが、次のメールアドレス宛にメール送信できませんでした: %s';
 $string['loseyourchanges'] = 'あなたの変更内容を失ってもよろしいですか?';
 $string['Title'] = 'タイトル';
+$string['Text'] = 'テキスト';
+$string['text'] = 'テキスト';
 $string['Created'] = '作成日時';
 $string['Description'] = '説明';
 $string['Download'] = 'ダウンロード';
@@ -131,24 +109,11 @@ $string['Preview'] = 'プレビュー';
 $string['Size'] = 'サイズ';
 $string['Type'] = 'タイプ';
 $string['profileinformation1'] = '個人情報';
+$string['Profilepage'] = 'プロファイルページ';
 $string['profilepage'] = 'プロファイルページ';
 $string['profileimagefor'] = '%s のプロファイル画像';
 $string['viewprofilepage'] = 'プロファイルページを表示する';
 $string['viewallprofileinformation1'] = 'すべての個人情報を表示する';
-$string['Note'] = 'ノート';
-$string['noteTitle'] = 'ノートタイトル';
-$string['blockTitle'] = 'ブロックタイトル';
-$string['Notes'] = 'ノート';
-$string['mynotes'] = 'マイノート';
-$string['notesfor'] = '%s のノート';
-$string['containedin'] = '含まれる場所:';
-$string['currenttitle'] = 'タイトル';
-$string['notesdescription1'] = 'あなたのページのノートブロック内で作成したHTML形式のノートです。';
-$string['editnote'] = 'ノートを編集する';
-$string['confirmdeletenote'] = 'このノートは %d ブロックおよび %d ページで使用されています。あなたがノートを削除した場合、現在このテキストが含まれるすべてのブロックは空白になります。';
-$string['notedeleted'] = 'ノートが削除されました。';
-$string['noteupdated'] = 'ノートが更新されました。';
-$string['html'] = 'ノート';
 $string['duplicatedprofilefieldvalue'] = '複製値';
 $string['existingprofilefieldvalues'] = '既存値';
 $string['progressbaritem_messaging'] = 'メッセージング';
@@ -177,11 +142,7 @@ $string['progress_industry'] = 'あなたの職種を追加する';
 $string['progress_joingroup'] = '%s グループに参加する';
 $string['progress_makefriend'] = '%s フレンドを作る';
 $string['socialprofile'] = 'ソーシャルメディア';
-$string['socialprofiledesc'] = 'ソーシャルメディアのアカウント名です。';
-$string['socialprofiles'] = 'ソーシャルメディアアカウント';
-$string['servicedesc'] = 'ソーシャルメディアネットワーク名を入力してください。例) Facebook、LinkedIn等';
 $string['profileurl1'] = 'アカウントURL';
-$string['profileurldesc1'] = 'あなたのソーシャルメディアアカウントの公開ページのURLです。';
 $string['deleteprofile'] = 'ソーシャルメディアアカウントを削除する';
 $string['deletethisprofile'] = 'ソーシャルメディアアカウントを削除する:「 %s 」';
 $string['deleteprofileconfirm'] = '本当にこのソーシャルメディアアカウントを削除してもよろしいですか?';
@@ -192,5 +153,4 @@ $string['profiledeletedsuccessfully'] = 'ソーシャルメディアアカウン
 $string['profilesavedsuccessfully'] = 'ソーシャルメディアアカウントが正常に保存されました。';
 $string['socialprofilerequired'] = '少なくとも1つのソーシャルメディアアカウントが必要です。';
 $string['duplicateurl'] = 'すでに入力したものとユーザ名またはURLが重複しているため、あなたはこのソーシャルメディアアカウントを追加できません。';
-$string['notes'] = 'ノート';
 $string['supportadmin'] = 'サポート管理者';
