afa2bc45d8e3e9ef6abb6cc9d959aa834eced715
Mitsuhiro Yoshida <mits@mitstek.com>
2026-05-23 23:22:25 +0900

    Translated new strings.

--- blocktype/inbox/lang/ja.utf8/blocktype.inbox.php
+++ blocktype/inbox/lang/ja.utf8/blocktype.inbox.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2026-05-19 01:36:31 UTC
+ * @updated    2026-05-23 13:21:31 UTC
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -19,4 +19,7 @@ $string['messagetypes'] = '表示するメッセージタイプ';
 $string['maxitems'] = '表示するアイテムの最大数';
 $string['maxitemsdescription'] = '設定範囲: 1～100';
 $string['More'] = 'さらに';
+$string['go_to_inbox'] = '受信箱に移動する';
+$string['nomessagesemptystatetitle'] = 'あなたに新しいメッセージはありません。';
+$string['nomessagesemptystatemessage'] = 'あなたの受信ボックスに移動するか、誰かに新しいメッセージを作成してください。';
 $string['title_help'] = '<h1>セクションタイトル</h1><p>このフィールドを空白のままにした場合、タイトルが生成されます。</p>';
