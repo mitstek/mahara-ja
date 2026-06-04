c2cbe43b1cba13e3b4bec4fb46d41a466d3f9e3c
Mitsuhiro Yoshida <mits@mitstek.com>
2026-06-04 14:19:26 +0900

    Brushed up the translation of ‘Otherwise’.

--- artefact/blog/blocktype/taggedposts/lang/ja.utf8/blocktype.taggedposts.php
+++ artefact/blog/blocktype/taggedposts/lang/ja.utf8/blocktype.taggedposts.php
@@ -31,7 +31,7 @@ $string['postedon'] = '-';
 $string['itemstoshow'] = '表示するアイテム数';
 $string['configerror'] = 'ブロック設定中にエラーが発生しました。';
 $string['showjournalitemsinfull'] = '日誌アイテムをフルに表示する';
-$string['showjournalitemsinfulldesc1'] = 'この設定を有効にした場合、日誌エントリが表示されます。そうでない場合、日誌エントリのタイトルのみ表示されます。';
+$string['showjournalitemsinfulldesc1'] = 'この設定を有効にした場合、日誌エントリが表示されます。そうではない場合、日誌エントリのタイトルのみ表示されます。';
 $string['tag'] = 'タグ';
 
 ?>
