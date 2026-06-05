7c2aab4a74960be4368f33a1c7118be1cba4d757
Mitsuhiro Yoshida <mits@mitstek.com>
2026-06-05 11:52:36 +0900

    Brushed up the translation of ‘Completion date’.

--- blocktype/externalvideo/lang/ja.utf8/blocktype.externalvideo.php
+++ blocktype/externalvideo/lang/ja.utf8/blocktype.externalvideo.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2025-07-16 01:16:35 UTC
+ * @updated    2026-06-04 12:25:08 UTC
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -14,14 +14,16 @@
 defined('INTERNAL') || die();
 
 $string['title'] = '外部メディア';
+$string['title_help'] = '<h1>セクションタイトル</h1><p>あなたがヘッディングを表示したくない場合、このフィールドを空白のままにできます。</p>';
 $string['description'] = '外部コンテンツを埋め込みます。';
 $string['urlorembedcode'] = 'URLまたは埋め込みコード';
 $string['videourldescription3'] = 'コンテンツが設置されているページの<strong>埋め込みコード</strong>または<strong>URL</strong>を貼り付けてください。';
 $string['validiframesites'] = '&lt;iframe&gt;タグを含む次のサイトの<strong>埋め込みコード</strong>が許可されます:';
 $string['validurlsites'] = '次のサイトの<strong>URL</strong>が許可されます:';
 $string['width'] = '幅';
+$string['width_help'] = '<h1>幅</h1><p>あなたがURLを使用する場合、メディアアイテムの幅を入力してください。あなたが埋め込みコードを使用する場合、上のコード内で直接幅を変更してください。</p>';
 $string['height'] = '高さ';
-$string['widthheightdescription'] = '「幅」および「高さ」フィールドはURLにのみ使用されます。上でembedまたはiframeコードを入力した場合、あなたはコード内で幅および高さを更新する必要があります。';
+$string['height_help'] = '<h1>高さ</h1><p>あなたがURLを使用する場合、メディアアイテムの高さを入力してください。あなたが埋め込みコードを使用する場合、上のコード内で直接高さを変更してください。</p>';
 $string['invalidurl'] = '無効なURL';
 $string['invalidurlorembed'] = '無効なURLまたは埋め込みコード';
 $string['googlevideo'] = 'Googleビデオ';
