f6e6b4844dd8c99411bae96026b9eab060e7e21b
Mitsuhiro Yoshida <mits@mitstek.com>
2026-06-16 18:19:13 +0900

    Translated new strings.

--- blocktype/creativecommons/lang/ja.utf8/blocktype.creativecommons.php
+++ blocktype/creativecommons/lang/ja.utf8/blocktype.creativecommons.php
@@ -5,7 +5,7 @@
  * @subpackage lang (Japanese)
  * @translator Mitsuhiro Yoshida (https://mitstek.com/)
  * @started    2008-01-19 11:25:00 UTC
- * @updated    2026-05-19 03:44:11 UTC
+ * @updated    2026-06-16 09:18:44 UTC
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
  * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
@@ -34,3 +34,21 @@ $string['by-nd'] = '表示-改変禁止';
 $string['by-nc'] = '表示-非営利';
 $string['by-nc-sa'] = '表示-非営利-継承';
 $string['by-nc-nd'] = '表示-非営利-改変禁止';
+$string['instconf.noncommercial_help'] = '<h1>利用方法</h1>
+<dl>
+<dt>商用</dt>
+<dd>あなたは商用目的を含む他者が作品を複製、配布、表示および作業実施を許可します。</dd>
+<dt>非営利</dt>
+<dd>あなたは非営利目的でのみ他者が作品を複製、配布、表示および作業実施を許可します。</dd>
+</dl>';
+$string['noderivatives_help'] = '<h1>あなたの作品の変更を許可する</h1>
+<dl>
+<dt>Yes</dt>
+<dd>あなたは他者が作品を複製、配布、表示、再生およびそれに基づく派生作品の作成を許可します。</dd>
+<dt>同一ライセンスでの再配布</dt>
+<dd>あなたは同じライセンスまたはライセンサの作品を管理するライセンスと互換性のあるライセンスの下でのみ他者の派生作品配布を許可します。</dd>
+<dt>派生物なし</dt>
+<dd>あなたは作品の未変更コピーのみ、他者のコピー、配布および送信を許可します。作品に基づく派生作品は許可しません。</dd>
+</dl>';
+$string['version_help'] = '<h1>ライセンスバージョン</h1>
+<p>あなたは2007年に発効したバージョン3、そして2013年にリリースされてバージョン3より多くの改善が施されたバージョン4のどちらかを選択できます。詳細情報は次をご覧ください: <a href="https://creativecommons.org/version4">「4.0の新しい内容」</a></p>';
