52c6829940463cd676a550320464b76d600d72ac
Mitsuhiro Yoshida <mits@mitstek.com>
2026-07-27 06:00:25 +0900

    Brushed up a translated string for admin.

--- lang/ja.utf8/cookieconsent.php
+++ lang/ja.utf8/cookieconsent.php
@@ -1,19 +1,23 @@
 <?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang (Japanese)
+ * @translator Mitsuhiro Yoshida (https://mitstek.com/)
+ * @started    2008-01-19 11:25:00 UTC
+ * @updated    2026-07-23 07:47:35 UTC
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
 
 defined('INTERNAL') || die();
 
 $string['cookieconsent'] = 'クッキーコンセント';
-$string['cookieconsentdisabled'] = 'クッキーコンセントが無効にされました。';
-$string['cookieconsentdismiss'] = '了解!';
-$string['cookieconsentenable'] = 'クッキーコンセントを有効にする';
-$string['cookieconsentenabled'] = 'クッキーコンセントが有効にされました。';
-$string['cookieconsentintro1'] = '「クッキー命令」または「クッキー法」として知られる欧州議会および理事会の2009/136/EC命令は欧州におけるクッキー同意に関する必要条件を定義した法律文書です。これは基本的に前の命令「2002/58/EC」の修正案です。そしてウェブのデータおよびプライバシ、その他の電子コミュニケーションに対して広範に関係します。';
-$string['cookieconsentintro2'] = '新しい命令は2011年5月25日に発効されました。本文は26ページに渡りますが、クッキーに関する最も重要な段落は20ページにあります。これは前の命令「2002/58/EC」の記事5(3)を修正したものです。';
+$string['cookieconsentintro11'] = '「クッキー命令」として知られる命令2009/136/ECはEU全体におけるクッキーの同意に関する要件を定めています。これは命令2002/58/ECを改正するものであり、電子通信におけるデータ保護およびプライバシを対象としています。';
+$string['cookieconsentintro21'] = '新しい命令は2011年5月25日に施行されました。クッキーに関する主な規定は改正された命令の第5条第3項に記載されています。';
 $string['cookieconsentintro3'] = '「加盟国は情報の保存またはすでに保存されている情報へのアクセスに関して命令95/46/ECに従って明確および包括的な情報を提供されている購読者またはユーザの端末装置において、特に処理の目的に関する同意を得る必要があります。これは技術的保存または電子証券取引ネットワークに関する通信伝送を実現するための唯一のアクセス手段、購読者またはユーザからサービスの提供を求められている厳密に必要な情報社会サービス提供者を阻むものであってはなりません」';
-$string['cookieconsentintro4'] = '要するにコンピュータ、モバイルフォンまたは他のデバイスから情報を保存または検索する前にユーザの同意が必要であることを意味します。この目的はエンドユーザのプライバシを増すこと、そして国民が知ることなしに組織が情報を取得することを防ぐことにあります。';
-$string['cookieconsentintro52'] = '以下で%sクッキーコンセント%sを有効にしてください。ユーザには画面下部にあなたのプライバシページにリンクしたクッキーコンセントバナーが表示されます。あなたのクッキー使用に関してそこでユーザに情報提供してください。';
-$string['cookieconsentlearnmore'] = '詳細';
-$string['cookieconsentmessage'] = 'あなたが私たちのウェブサイトで最良の経験を得るためこのウェブサイトではクッキーを使用しています。';
+$string['readfulltext1'] = '命令全文を読む';
 $string['directive2009136'] = '命令2009/136/EC';
 $string['readdirectiveBG'] = '命令2009/136/ECを読む (ブルガリア語)';
 $string['readdirectiveCS'] = '命令2009/136/ECを読む (チェコ語)';
@@ -37,4 +41,11 @@ $string['readdirectiveRO'] = '命令2009/136/ECを読む (ルーマニア語)';
 $string['readdirectiveSK'] = '命令2009/136/ECを読む (スロバキア語)';
 $string['readdirectiveSL'] = '命令2009/136/ECを読む (スロベニア語)';
 $string['readdirectiveSV'] = '命令2009/136/ECを読む (スウェーデン語)';
-$string['readfulltext1'] = '命令全文を読む';
+$string['cookieconsentintro41'] = '要するにデバイスに情報を保存したり、デバイスから情報を取得したりする前に人はインフォームドコンセントを与える必要があります。これはプライバシを保護した上で組織が人に知らせずに情報収集することを防ぐことを目的としています。';
+$string['cookieconsentintro53'] = '下の%クッキーコンセント%sを有効にした場合、画面下部にあなたのプライバシページへのリンクを含むバナーが表示されます。あなたのプライバシページにクッキーの使用に関する情報が含まれていることを確認してください。';
+$string['cookieconsentenable'] = 'クッキーコンセントを有効にする';
+$string['cookieconsentenabled'] = 'クッキーコンセントが有効にされました。';
+$string['cookieconsentdisabled'] = 'クッキーコンセントが無効にされました。';
+$string['cookieconsentmessage'] = 'あなたが私たちのウェブサイトで最良の経験を得るためこのウェブサイトではクッキーを使用しています。';
+$string['cookieconsentdismiss'] = '了解!';
+$string['cookieconsentlearnmore'] = '詳細';
