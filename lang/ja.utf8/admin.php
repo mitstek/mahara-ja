d621b5ed78489c1964c196b447506204bef107da
Mitsuhiro Yoshida <mits@mitstek.com>
2026-08-23 00:22:48 +0900

    Brushed up a translated string for admin.

--- lang/ja.utf8/admin.php
+++ lang/ja.utf8/admin.php
@@ -1283,7 +1283,7 @@ $string['masqueradenotifiedreasondescription'] = 'この人としてログイン
 $string['masqueradetime'] = 'マスカレーディング開始日時';
 $string['masquerader'] = 'マスカレーディング管理者';
 $string['masqueradee'] = '人';
-$string['nomasquerades'] = 'マスカレーディングセッションのロギングが有効にされているため別の誰かとして代理ログインした管理者はいません。';
+$string['nomasquerades'] = 'マスカレーディングセッションのログが有効にされて以降、別の誰かとして代理ログインした管理者はいません。';
 $string['loginaslog'] = 'マスカレーディングセッション';
 $string['masqueradingnotloggedwarning'] = '<b>注意</b>: マスカレーディングセッションのロギングは現在無効にされています。このテーブルのデータを閲覧するには管理者が「 <a href="%sadmin/site/options.php">サイト設定</a> 」の「ロギング設定」を有効にする必要があります。';
 $string['masqueradenotificationsubject'] = '管理者があなたとしてログインしました。';
