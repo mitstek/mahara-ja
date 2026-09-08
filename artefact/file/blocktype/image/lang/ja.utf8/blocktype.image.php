e18d0eee5a151b4aed8ba4a18c5b0ec155c36fa1
Mitsuhiro Yoshida <mits@mitstek.com>
2026-09-09 01:50:36 +0900

    Revised the translations of strings related to consent.

--- artefact/file/blocktype/image/lang/ja.utf8/blocktype.image.php
+++ artefact/file/blocktype/image/lang/ja.utf8/blocktype.image.php
@@ -30,7 +30,7 @@ defined('INTERNAL') || die();
 
 $string['title'] = 'イメージ';
 $string['description'] = 'あなたのファイルエリアの単一イメージです。';
-$string['showdescription'] = '説明を表示しますか?';
+$string['showdescription'] = '説明を表示してもよろしいですか?';
 $string['width'] = '幅';
 $string['widthdescription'] = 'あなたのイメージの幅 (ピクセル) を指定してください。イメージはこの幅にサイズ変更されます。イメージのオリジナルサイズを使用する場合、空白のままにしてください。';
 
