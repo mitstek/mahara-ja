5946815fff975cdc83d3e7350b93f05af79c84e2
Mitsuhiro Yoshida <mits@mitstek.com>
2026-09-23 08:08:02 +0900

    Brushed up translated strings for admin.

--- lang/ja.utf8/admin.php
+++ lang/ja.utf8/admin.php
@@ -1402,7 +1402,7 @@ $string['cli_deleteinactivegroups_info'] = 'このコマンドラインPHPスク
 $string['cli_deleteinactivegroups_beforedate'] = 'この日付より最終修正日が前のグループを削除します。日付には次のような日時ストリングを使用できます: 「2018-02-25」または「-6 months」';
 $string['cli_deleteinactivegroups_limit'] = 'それぞれの削除で処理するグループ数の制限です。';
 $string['cli_deleteinactivegroups_cleangroups'] = '「group」テーブルからもグループの削除を試みます。';
-$string['cli_deleteinactivegroups_onlyadmins'] = 'メンバがオーナ/管理者のみのグループも含みます。';
+$string['cli_deleteinactivegroups_onlyadmins'] = 'オーナ/管理者のみをメンバとするグループも含みます。';
 $string['cli_deleteinactivegroups_onlydryrun'] = 'ここでは以下の設定によるドライランのみ実行します。
 インスティテューション: %s
 次の日付以前: %s
